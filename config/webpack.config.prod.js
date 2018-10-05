761dd149919034f5f4399f93e7f346294d0c913e
Robert Buels <rbuels@gmail.com>
2018-10-05 08:05:20 -0700

    manually apply https://github.com/facebook/create-react-app/pull/3934

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -143,7 +143,32 @@ module.exports = {
               name: 'static/media/[name].[hash:8].[ext]',
             },
           },
-          // Process JS with Babel.
+
+          // Process WebWorker JS with Babel.
+          // The preset includes JSX, Flow, and some ESnext features.
+          {
+            test: /\.worker\.(js|jsx|mjs)$/,
+            include: paths.appSrc,
+            use: [
+              require.resolve('worker-loader'),
+              // This loader parallelizes code compilation, it is optional but
+              // improves compile time on larger projects
+              require.resolve('thread-loader'),
+              {
+                loader: require.resolve('babel-loader'),
+                options: {
+                  // @remove-on-eject-begin
+                  babelrc: false,
+                  presets: [require.resolve('babel-preset-react-app')],
+                  // @remove-on-eject-end
+                  compact: true,
+                  highlightCode: true,
+                },
+              },
+            ],
+          },
+
+          // Process application JS with Babel.
           {
             test: /\.(js|jsx|mjs)$/,
             include: paths.appSrc,
