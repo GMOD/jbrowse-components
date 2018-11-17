f10532ea0a3018344b8366be3a8573456251d9a1
Garrett Stevens <stevens.garrett.j@gmail.com>
2018-11-17 11:06:23 -0700

    Change track model from array to map, add ability to show and hide tracks.

--- packages/jbrowse-web/src/index.js
+++ packages/jbrowse-web/src/index.js
@@ -18,13 +18,13 @@ model.views[0].displayRegions([
   { assembly: 'volvox', refName: 'ctgB', start: 0, end: 100000000 },
 ])
 
-model.views[0].showTrack('foo', 'Foo Track', 'AlignmentsTrack')
-model.views[0].showTrack('bar', 'Bar Track', 'AlignmentsTrack')
-model.views[0].showTrack('baz', 'Baz Track', 'AlignmentsTrack')
+model.views[0].addTrack('foo', 'Foo Track', 'AlignmentsTrack')
+model.views[0].addTrack('bar', 'Bar Track', 'AlignmentsTrack')
+model.views[0].addTrack('baz', 'Baz Track', 'AlignmentsTrack')
 model.addView('LinearGenomeView')
-model.views[1].showTrack('bee', 'Bee Track', 'AlignmentsTrack')
-model.views[1].showTrack('bonk', 'Bonk Track', 'AlignmentsTrack')
-model.views[1].tracks[0].configuration.backgroundColor.set('red')
+model.views[1].addTrack('bee', 'Bee Track', 'AlignmentsTrack')
+model.views[1].addTrack('bonk', 'Bonk Track', 'AlignmentsTrack')
+model.views[1].tracks.get('bee').configuration.backgroundColor.set('red')
 
 model.addDrawerWidget('HierarchicalTrackSelectorDrawerWidget')
 
