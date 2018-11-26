de2779c1ebf9151c9beb2a2dd95cc51dbbe765d2
Robert Buels <rbuels@gmail.com>
2018-11-25 16:16:59 -0800

    write a little test for the server-side renderer

---
+++ packages/jbrowse-web/src/render.test.js
@@ -0,0 +1,31 @@
+import { ExpansionPanelActions } from '@material-ui/core'
+import JBrowse from './JBrowse'
+import { renderRegion } from './render'
+
+const jbrowse = new JBrowse().configure()
+
+test('can render a single region with Pileup + BamAdapter', async () => {
+  const testprops = {
+    region: { assembly: 'volvox', refName: 'ctgA', start: 0, end: 800 },
+    adapterType: 'BamAdapter',
+    adapterConfig: {
+      _configId: '7Hc9NkuD4x',
+      type: 'BamAdapter',
+      bamLocation: {
+        path: require.resolve('../public/test_data/volvox-sorted.bam'),
+      },
+      index: {
+        _configId: 'sGW8va26pr',
+        location: {
+          path: require.resolve('../public/test_data/volvox-sorted.bam.bai'),
+        },
+      },
+    },
+    rendererType: 'PileupRenderer',
+    renderProps: {},
+  }
+
+  const result = await renderRegion(jbrowse, testprops)
+  expect(result.features.length).toBe(93)
+  expect(result.html).toMatchSnapshot()
+})
