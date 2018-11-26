24907027d0e02eaa4d4f0d16a83628da5f2fa13b
Robert Buels <rbuels@gmail.com>
2018-11-25 20:38:25 -0800

    html rendering perf tests

--- packages/jbrowse-web/src/plugins/AlignmentsTrack/components/PileupRendering.js
+++ packages/jbrowse-web/src/plugins/AlignmentsTrack/components/PileupRendering.js
@@ -1,10 +1,46 @@
-import React from 'react'
-import { observer } from 'mobx-react'
+import React, { Component } from 'react'
+import { observer, PropTypes } from 'mobx-react'
+import ReactPropTypes from 'prop-types'
 
 import './PileupRendering.scss'
 
-const PileupRendering = observer(({ data }) => (
-  <div className="PileupRendering">{data.length} features</div>
-))
+function Feature({ feature, layout, bpPerPx, region, flipped }) {
+  // const leftBase = region.start
+  // const startbp = fRect.l * bpPerPx + leftBase
+  // const endbp = (fRect.l + fRect.w) * bpPerPx + leftBase
+  // const top = layout.addRect(feature.id(), startbp, endbp, fRect.h, feature)
 
+  return <div className="feature" data-feature-id={feature.id()} style={{}} />
+}
+
+Feature.propTypes = {
+  feature: PropTypes.objectOrObservableObject.isRequired,
+  layout: PropTypes.objectOrObservableObject.isRequired,
+  region: PropTypes.objectOrObservableObject.isRequired,
+  bpPerPx: ReactPropTypes.number.isRequired,
+  flipped: PropTypes.boolean,
+}
+
+Feature.defaultProps = { flipped: false }
+
+@observer
+class PileupRendering extends Component {
+  static propTypes = {
+    data: PropTypes.arrayOrObservableArrayOf(PropTypes.objectOrObservableObject)
+      .isRequired,
+    layout: PropTypes.objectOrObservableObject.isRequired,
+    region: PropTypes.objectOrObservableObject.isRequired,
+  }
+
+  render() {
+    const { data: features } = this.props
+    return (
+      <div className="PileupRendering">
+        {features.map(feature => (
+          <Feature key={feature.id()} feature={feature} {...this.props} />
+        ))}
+      </div>
+    )
+  }
+}
 export default PileupRendering
