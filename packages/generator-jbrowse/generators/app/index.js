a622fe334e731c5be647c2798a4e6930bab3f019
Garrett Stevens <stevens.garrett.j@gmail.com>
2018-12-03 16:26:07 -0500

    Move eslint config to separate file, update lint standards

--- packages/generator-jbrowse/generators/app/index.js
+++ packages/generator-jbrowse/generators/app/index.js
@@ -1,40 +1,39 @@
-"use strict";
-const Generator = require("yeoman-generator");
-const chalk = require("chalk");
-const yosay = require("yosay");
+const Generator = require('yeoman-generator')
+const chalk = require('chalk')
+const yosay = require('yosay')
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(
       yosay(
-        `Welcome to the finest ${chalk.red("generator-jbrowse")} generator!`
-      )
-    );
+        `Welcome to the finest ${chalk.red('generator-jbrowse')} generator!`,
+      ),
+    )
 
     const prompts = [
       {
-        type: "confirm",
-        name: "someAnswer",
-        message: "Would you like to enable this option?",
-        default: true
-      }
-    ];
+        type: 'confirm',
+        name: 'someAnswer',
+        message: 'Would you like to enable this option?',
+        default: true,
+      },
+    ]
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
+      this.props = props
+    })
   }
 
   writing() {
     this.fs.copy(
-      this.templatePath("dummyfile.txt"),
-      this.destinationPath("dummyfile.txt")
-    );
+      this.templatePath('dummyfile.txt'),
+      this.destinationPath('dummyfile.txt'),
+    )
   }
 
   install() {
-    this.installDependencies();
+    this.installDependencies()
   }
-};
+}
--- packages/generator-jbrowse/generators/app/index.test.js
+++ packages/generator-jbrowse/generators/app/index.test.js
@@ -1,16 +1,13 @@
-"use strict";
-const path = require("path");
-const assert = require("yeoman-assert");
-const helpers = require("yeoman-test");
+const path = require('path')
+const assert = require('yeoman-assert')
+const helpers = require('yeoman-test')
 
-describe("generator-jbrowse:app", () => {
-  beforeAll(() => {
-    return helpers
-      .run(path.join(__dirname, "."))
-      .withPrompts({ someAnswer: true });
-  });
+describe('generator-jbrowse:app', () => {
+  beforeAll(() =>
+    helpers.run(path.join(__dirname, '.')).withPrompts({ someAnswer: true }),
+  )
 
-  it("creates files", () => {
-    assert.file(["dummyfile.txt"]);
-  });
-});
+  it('creates files', () => {
+    assert.file(['dummyfile.txt'])
+  })
+})
