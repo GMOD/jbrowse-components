a503dfb878779d5ee26b11c5418912761b9fad90
Robert Buels <rbuels@gmail.com>
2018-12-11 12:28:52 -0800

    remove imagemaps, too slow

--- packages/jbrowse-web/src/plugins/AlignmentsTrack/components/PileupRendering.js
+++ packages/jbrowse-web/src/plugins/AlignmentsTrack/components/PileupRendering.js
@@ -4,32 +4,10 @@ import ReactPropTypes from 'prop-types'
 
 import './PileupRendering.scss'
 import PrerenderedCanvas from './PrerenderedCanvas'
-import PrecomputedLayout from '../../../util/PrecomputedLayout'
 
 const layoutPropType = ReactPropTypes.shape({
   getRectangles: ReactPropTypes.func.isRequired,
 })
-
-function ImageMapFromPrecomputedLayout({ layout }) {
-  return (
-    <map>
-      {Object.entries(layout.getRectangles()).map(([featureId, coords]) => {
-        if (!coords.join) debugger
-        return (
-          <area
-            key={featureId}
-            alt={`feature ${featureId}`}
-            shape="rect"
-            coords={coords.join(',')}
-          />
-        )
-      })}
-    </map>
-  )
-}
-ImageMapFromPrecomputedLayout.propTypes = {
-  layout: layoutPropType.isRequired,
-}
 
 @observer
 class PileupRendering extends Component {
@@ -41,7 +19,6 @@ class PileupRendering extends Component {
     const { layout } = this.props
     return (
       <div className="PileupRendering">
-        <ImageMapFromPrecomputedLayout layout={layout} />
         <PrerenderedCanvas {...this.props} />
       </div>
     )
