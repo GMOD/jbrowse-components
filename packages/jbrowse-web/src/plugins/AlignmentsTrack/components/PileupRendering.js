687918cbe2d36dc9ac0fd0c3ce5b3a320b866292
Robert Buels <rbuels@gmail.com>
2018-12-11 20:25:52 -0800

    implement feature event system for canvas pileup renderer, so we can know everything about the mouse interacting with features (but not subfeatures)

--- packages/jbrowse-web/src/plugins/AlignmentsTrack/components/PileupRendering.js
+++ packages/jbrowse-web/src/plugins/AlignmentsTrack/components/PileupRendering.js
@@ -8,6 +8,12 @@ import PrerenderedCanvas from './PrerenderedCanvas'
 import { PropTypes as CommonPropTypes } from '../../../mst-types'
 import { bpToPx } from '../../../util'
 
+function distance(x1, y1, x2, y2) {
+  const dx = x1 - x2
+  const dy = y1 - y2
+  return Math.sqrt(dx * dx + dy * dy)
+}
+
 const layoutPropType = ReactPropTypes.shape({
   getRectangles: ReactPropTypes.func.isRequired,
 })
@@ -20,7 +26,32 @@ class PileupRendering extends Component {
     width: ReactPropTypes.number.isRequired,
     region: CommonPropTypes.Region.isRequired,
     bpPerPx: ReactPropTypes.number.isRequired,
-    horizontallyFlipped: ReactPropTypes.bool.isRequired,
+    horizontallyFlipped: ReactPropTypes.bool,
+
+    onFeatureMouseDown: ReactPropTypes.func,
+    onFeatureMouseEnter: ReactPropTypes.func,
+    onFeatureMouseOut: ReactPropTypes.func,
+    onFeatureMouseOver: ReactPropTypes.func,
+    onFeatureMouseUp: ReactPropTypes.func,
+    onFeatureMouseLeave: ReactPropTypes.func,
+    onFeatureMouseMove: ReactPropTypes.func,
+
+    // synthesized from mouseup and mousedown
+    onFeatureClick: ReactPropTypes.func,
+  }
+
+  static defaultProps = {
+    horizontallyFlipped: false,
+
+    onFeatureMouseDown: () => {},
+    onFeatureMouseEnter: () => {},
+    onFeatureMouseOut: () => {},
+    onFeatureMouseOver: () => {},
+    onFeatureMouseUp: () => {},
+    onFeatureMouseLeave: () => {},
+    onFeatureMouseMove: () => {},
+
+    onFeatureClick: () => {},
   }
 
   constructor(props) {
@@ -28,12 +59,73 @@ class PileupRendering extends Component {
     this.highlightOverlayCanvas = React.createRef()
   }
 
+  onMouseDown = event => {
+    this.callFeatureMouseHandler('onFeatureMouseDown', event)
+    if (this.featureUnderMouse) {
+      this.lastFeatureMouseDown = {
+        featureId: this.featureUnderMouse,
+        x: event.clientX,
+        y: event.clientY,
+      }
+    } else {
+      this.lastFeatureMouseDown = undefined
+    }
+  }
+
+  onMouseEnter = event => {
+    this.callFeatureMouseHandler('onFeatureMouseEnter', event)
+  }
+
+  onMouseOut = event => {
+    this.callFeatureMouseHandler('onFeatureMouseOut', event)
+    this.callFeatureMouseHandler('onFeatureMouseLeave', event)
+    this.featureUnderMouse = undefined
+  }
+
+  onMouseOver = event => {
+    this.callFeatureMouseHandler('onFeatureMouseOver', event)
+  }
+
+  onMouseUp = event => {
+    this.callFeatureMouseHandler('onFeatureMouseUp', event)
+
+    // synthesize a featureClick event if we are on a feature
+    // and it's close to the last mouse down
+    if (this.featureUnderMouse && this.lastFeatureMouseDown) {
+      const { featureId, x, y } = this.lastFeatureMouseDown
+      const { clientX, clientY } = event
+      if (
+        this.featureUnderMouse === featureId &&
+        distance(x, y, clientX, clientY) <= 2
+      ) {
+        this.callFeatureMouseHandler('onFeatureClick', event)
+        this.lastFeatureMouseDown = undefined
+      }
+    }
+  }
+
   onMouseLeave = event => {
-    const canvas = this.highlightOverlayCanvas.current
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    this.callFeatureMouseHandler('onFeatureMouseOut', event)
+    this.callFeatureMouseHandler('onFeatureMouseLeave', event)
+    this.featureUnderMouse = undefined
   }
 
   onMouseMove = event => {
+    const featureIdCurrentlyUnderMouse = this.findFeatureIdUnderMouse(event)
+    if (this.featureUnderMouse === featureIdCurrentlyUnderMouse) {
+      this.callFeatureMouseHandler('onFeatureMouseMove', event)
+    } else {
+      if (this.featureUnderMouse) {
+        this.callFeatureMouseHandler('onFeatureMouseOut', event)
+        this.callFeatureMouseHandler('onFeatureMouseLeave', event)
+      }
+      this.featureUnderMouse = featureIdCurrentlyUnderMouse
+      this.callFeatureMouseHandler('onFeatureMouseOver', event)
+      this.callFeatureMouseHandler('onFeatureMouseEnter', event)
+    }
+  }
+
+  findFeatureIdUnderMouse(event) {
     const { offsetX, offsetY } = event.nativeEvent
     if (!(offsetX >= 0))
       throw new Error(
@@ -41,10 +133,6 @@ class PileupRendering extends Component {
       )
 
     const { layout, bpPerPx, region, horizontallyFlipped } = this.props
-    const canvas = this.highlightOverlayCanvas.current
-    const ctx = canvas.getContext('2d')
-    ctx.fillStyle = 'rgba(0,0,0,0.2)'
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
     for (const [
       id,
       [leftBp, topPx, rightBp, bottomPx],
@@ -60,9 +148,16 @@ class PileupRendering extends Component {
         offsetY >= topPx &&
         offsetY <= bottomPx
       ) {
-        ctx.fillRect(leftPx, topPx, rightPx - leftPx, bottomPx - topPx)
-        return
+        return id
       }
+    }
+
+    return undefined
+  }
+
+  callFeatureMouseHandler(handlerName, event, always = false) {
+    if (always || this.featureUnderMouse) {
+      this.props[handlerName](this.featureUnderMouse, event)
     }
   }
 
@@ -77,8 +172,15 @@ class PileupRendering extends Component {
           style={{ position: 'absolute', left: 0, top: 0 }}
           className="highlightOverlayCanvas"
           ref={this.highlightOverlayCanvas}
-          onMouseMove={this.onMouseMove}
+          onMouseDown={this.onMouseDown}
+          onMouseEnter={this.onMouseEnter}
+          onMouseOut={this.onMouseOut}
+          onMouseOver={this.onMouseOver}
+          onMouseUp={this.onMouseUp}
           onMouseLeave={this.onMouseLeave}
+          onMouseMove={this.onMouseMove}
+          onFocus={() => {}}
+          onBlur={() => {}}
         />
       </div>
     )
