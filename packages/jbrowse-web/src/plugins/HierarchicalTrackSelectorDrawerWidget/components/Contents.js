211ac8890b36c3e980a28d19199dd2c4b1381cbf
Garrett Stevens <stevens.garrett.j@gmail.com>
2019-01-08 15:40:14 -0500

    Use requestIdleCallback to load tracks incrementally

--- packages/jbrowse-web/src/plugins/HierarchicalTrackSelectorDrawerWidget/components/Contents.js
+++ packages/jbrowse-web/src/plugins/HierarchicalTrackSelectorDrawerWidget/components/Contents.js
@@ -2,15 +2,18 @@ import Checkbox from '@material-ui/core/Checkbox'
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import Fade from '@material-ui/core/Fade'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import FormGroup from '@material-ui/core/FormGroup'
 import Icon from '@material-ui/core/Icon'
-import Tooltip from '@material-ui/core/Tooltip'
+import CircularProgress from '@material-ui/core/CircularProgress'
 import { withStyles } from '@material-ui/core/styles'
+import Tooltip from '@material-ui/core/Tooltip'
 import Typography from '@material-ui/core/Typography'
 import { observer, PropTypes as MobxPropTypes } from 'mobx-react'
 import propTypes from 'prop-types'
 import React from 'react'
+import { requestIdleCallback } from 'request-idle-callback'
 import { readConfObject } from '../../../configuration'
 
 const styles = {
@@ -75,45 +78,64 @@ class Contents extends React.Component {
   }
 
   state = {
-    numTracksRendered: 0,
+    categories: [],
+    trackConfigurations: [],
   }
 
   componentDidMount() {
-    console.log('howdy')
+    this.loadMoreTracks()
+  }
+
+  loadMoreTracks() {
+    this.setState((state, props) => {
+      const { categories, trackConfigurations } = state
+      const { category } = props
+      const numLoaded = categories.length + trackConfigurations.length
+      Array.from(category)
+        .slice(numLoaded, numLoaded + 10)
+        .forEach(([name, contents]) => {
+          if (contents._configId) {
+            trackConfigurations.push(contents)
+          } else {
+            categories.push([name, contents])
+          }
+        })
+      if (categories.length + trackConfigurations.length !== category.size)
+        requestIdleCallback(() => {
+          this.loadMoreTracks()
+        })
+      return { categories, trackConfigurations }
+    })
   }
 
   render() {
+    const { categories, trackConfigurations } = this.state
     const { category, model, filterPredicate, path } = this.props
-    const categories = []
-    const trackConfigurations = []
-    category.forEach((contents, name) => {
-      if (contents._configId) {
-        trackConfigurations.push(contents)
-      } else {
-        categories.push([name, contents])
-      }
-    })
     return (
-      <div>
+      <>
         <FormGroup>
           {trackConfigurations.filter(filterPredicate).map(trackConf => (
-            <Tooltip
-              key={trackConf._configId}
-              title={readConfObject(trackConf, 'description')}
-              placement="left"
-              enterDelay={500}
-            >
-              <FormControlLabel
-                control={<Checkbox />}
-                label={readConfObject(trackConf, 'name')}
-                checked={model.view.tracks.some(
-                  t => t.configuration === trackConf,
-                )}
-                onChange={() => model.view.toggleTrack(trackConf)}
-              />
-            </Tooltip>
+            <Fade in key={trackConf._configId}>
+              <Tooltip
+                title={readConfObject(trackConf, 'description')}
+                placement="left"
+                enterDelay={500}
+              >
+                <FormControlLabel
+                  control={<Checkbox />}
+                  label={readConfObject(trackConf, 'name')}
+                  checked={model.view.tracks.some(
+                    t => t.configuration === trackConf,
+                  )}
+                  onChange={() => model.view.toggleTrack(trackConf)}
+                />
+              </Tooltip>
+            </Fade>
           ))}
         </FormGroup>
+        {categories.length + trackConfigurations.length !== category.size ? (
+          <CircularProgress />
+        ) : null}
         {categories.map(([name, contents]) => (
           <Category
             key={name}
@@ -124,7 +146,7 @@ class Contents extends React.Component {
             model={model}
           />
         ))}
-      </div>
+      </>
     )
   }
 }
