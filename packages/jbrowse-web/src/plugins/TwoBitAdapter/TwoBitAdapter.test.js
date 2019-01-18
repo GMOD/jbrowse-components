736438c7b366512c0aa69933d601ca9f961839f7
Colin <colin.diesh@gmail.com>
2019-01-18 09:43:26 -0500

    Update tests

--- packages/jbrowse-web/src/plugins/TwoBitAdapter/TwoBitAdapter.test.js
+++ packages/jbrowse-web/src/plugins/TwoBitAdapter/TwoBitAdapter.test.js
@@ -8,24 +8,13 @@ test('adapter can fetch features from volvox.2bit', async () => {
     twoBitLocation: { path: require.resolve('./test_data/volvox.2bit') },
   })
 
-  const features = adapter.getSequence({
+  const features = adapter.getFeaturesInRegion({
     assembly: 'volvox',
     refName: 'ctgA',
     start: 0,
     end: 20000,
   })
-  console.log(features)
 
   const featuresArray = await features.pipe(toArray()).toPromise()
-  expect(featuresArray[0].get('seq_id')).toBe('ctgA')
-  const featuresJsonArray = featuresArray.map(f => f.toJSON())
-  expect(featuresJsonArray.length).toEqual(3809)
-  expect(featuresJsonArray.slice(1000, 1010)).toMatchSnapshot()
-
-  expect(await adapter.refIdToName(0)).toBe('ctgA')
-  expect(await adapter.refIdToName(1)).toBe(undefined)
-
-  expect(
-    await adapter.hasDataForRefSeq({ assembly: 'volvox', refName: 'ctgA' }),
-  ).toBe(true)
+  expect(featuresArray).toMatchSnapshot()
 })
