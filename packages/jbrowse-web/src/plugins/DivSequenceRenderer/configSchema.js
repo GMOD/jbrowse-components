142b040657391fddf0a00a360e332928e3657bae
Robert Buels <rbuels@gmail.com>
2019-01-22 15:49:54 -0800

    make sequence renderer height default to 16

--- packages/jbrowse-web/src/plugins/DivSequenceRenderer/configSchema.js
+++ packages/jbrowse-web/src/plugins/DivSequenceRenderer/configSchema.js
@@ -4,6 +4,6 @@ export default ConfigurationSchema('DivSequenceRenderer', {
   height: {
     type: 'number',
     description: 'height in pixels of each line of sequence',
-    defaultValue: 10,
+    defaultValue: 16,
   },
 })
