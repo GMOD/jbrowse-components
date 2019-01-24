7fd1446dd1055f4bd3da3ba583a18298e024fa4b
Colin <colin.diesh@gmail.com>
2019-01-24 15:12:29 -0500

    Add wiggle rendering class

--- packages/jbrowse-web/src/plugins/WiggleRenderer/wiggleRenderer.js
+++ packages/jbrowse-web/src/plugins/WiggleRenderer/wiggleRenderer.js
@@ -1,9 +1,99 @@
+import React from 'react'
+
+import {
+  createCanvas,
+  createImageBitmap,
+} from '../../util/offscreenCanvasPonyfill'
+
 import WiggleRendering from './components/WiggleRendering'
+
+import { readConfObject } from '../../configuration'
+import { bpToPx, iterMap } from '../../util'
 
 import ConfigSchema from './configSchema'
 import ServerSideRenderer from '../../renderers/serverSideRenderer'
 
-class WiggleRenderer extends ServerSideRenderer {}
+class WiggleRenderer extends ServerSideRenderer {
+  layoutFeature(
+    feature,
+    subLayout,
+    config,
+    bpPerPx,
+    region,
+    horizontallyFlipped = false,
+  ) {
+    // const leftBase = region.start
+    const startPx = bpToPx(
+      feature.get('start'),
+      region,
+      bpPerPx,
+      horizontallyFlipped,
+    )
+    const endPx = bpToPx(
+      feature.get('end'),
+      region,
+      bpPerPx,
+      horizontallyFlipped,
+    )
+    const heightPx = readConfObject(config, 'height', [feature])
+    // if (Number.isNaN(startPx)) debugger
+    // if (Number.isNaN(endPx)) debugger
+    if (feature.get('seq_id') !== region.refName) {
+      throw new Error(
+        `feature ${feature.id()} is not on the current region's reference sequence ${
+          region.refName
+        }`,
+      )
+    }
+    const topPx = subLayout.addRect(
+      feature.id(),
+      feature.get('start'),
+      feature.get('end'),
+      heightPx, // height
+      feature,
+    )
+
+    return {
+      feature,
+      startPx,
+      endPx,
+      topPx,
+      heightPx,
+    }
+  }
+
+  async makeImageData({
+    features,
+    config,
+    region,
+    bpPerPx,
+    horizontallyFlipped,
+  }) {
+    const width = (region.end - region.start) / bpPerPx
+    const height = 200
+    if (!(width > 0) || !(height > 0)) return { height: 0, width: 0 }
+
+    const canvas = createCanvas(Math.ceil(width), height)
+    const ctx = canvas.getContext('2d')
+    features.forEach(feature => {
+      ctx.fillStyle = readConfObject(config, 'color', [feature])
+      ctx.fillRect(feature.start, 0, feature.end - feature.start, feature.score)
+    })
+
+    const imageData = await createImageBitmap(canvas)
+    return { imageData, height, width }
+  }
+
+  async render(renderProps) {
+    const { height, width, imageData } = await this.makeImageData(renderProps)
+    const element = React.createElement(
+      this.ReactComponent,
+      { ...renderProps, height, width, imageData },
+      null,
+    )
+    return { element, imageData, height, width }
+  }
+}
 
 export default (/* pluginManager */) =>
   new WiggleRenderer({
