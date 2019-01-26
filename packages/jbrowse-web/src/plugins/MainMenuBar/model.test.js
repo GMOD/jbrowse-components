3979fd3470dc3e37974f68834c38a416fa5918c7
Robert Buels <rbuels@gmail.com>
2019-01-26 14:09:57 -0800

    more testing

--- packages/jbrowse-web/src/plugins/MainMenuBar/model.test.js
+++ packages/jbrowse-web/src/plugins/MainMenuBar/model.test.js
@@ -1,5 +1,5 @@
 import { types } from 'mobx-state-tree'
-import { MenuItemModel } from './model'
+import { MenuItemModel, MainMenuBarModel } from './model'
 import { ConfigurationSchema } from '../../configuration'
 
 jest.mock('file-saver')
@@ -21,4 +21,38 @@ test('can download configuration', () => {
 
   const jsonString = model.item.func()
   expect(jsonString).toContain('"configId"')
+})
+
+test('can push menus', () => {
+  const menubar = MainMenuBarModel.create({ type: 'MainMenuBar' })
+  menubar.pushMenu({
+    name: 'Admin',
+    menuItems: [
+      {
+        name: 'Download configuration',
+        icon: 'get_app',
+        callback: 'downloadConfiguration',
+      },
+    ],
+  })
+
+  expect(menubar.menus.length).toBe(2)
+  expect(menubar.menus.map(m => m.name)).toEqual(['Help', 'Admin'])
+})
+
+test('can unshift menus', () => {
+  const menubar = MainMenuBarModel.create({ type: 'MainMenuBar' })
+  menubar.unshiftMenu({
+    name: 'Admin',
+    menuItems: [
+      {
+        name: 'Download configuration',
+        icon: 'get_app',
+        callback: 'downloadConfiguration',
+      },
+    ],
+  })
+
+  expect(menubar.menus.length).toBe(2)
+  expect(menubar.menus.map(m => m.name)).toEqual(['Admin', 'Help'])
 })
