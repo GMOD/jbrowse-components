4fcdca7a41a02416963f9eb0736a86c5aa9f56ff
Robert Buels <rbuels@gmail.com>
2019-02-04 17:33:12 -0800

    improve filter component attr/value discovery

--- packages/jbrowse-web/src/plugins/FilteringTrack/components/FilterControls.js
+++ packages/jbrowse-web/src/plugins/FilteringTrack/components/FilterControls.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactPropTypes from 'prop-types'
 import { withStyles, FormControlLabel, Checkbox } from '@material-ui/core'
 import { observer, PropTypes as MxPropTypes } from 'mobx-react'
+import { reaction } from 'mobx'
+import * as deepmerge from 'deepmerge'
+
 import { getConf } from '../../../configuration'
 
 const styles = theme => ({
@@ -25,41 +28,82 @@ function visibleValues(model, attrName) {
   return Array.from(values.values()).sort()
 }
 
-function FilterControls({ classes, style, model }) {
-  return (
-    <div style={style} className={classes.root}>
-      {getConf(model, 'filterAttributes').map(attrName => (
-        <React.Fragment key={attrName}>
-          <div>{attrName}</div>
-          {visibleValues(model, attrName).map(value => (
-            <FormControlLabel
-              key={`${attrName}-${value}`}
-              control={
-                <Checkbox
-                  checked={
-                    !(
-                      model.filterOut.has(attrName) &&
-                      model.filterOut.get(attrName).get(String(value))
-                    )
-                  }
-                  onChange={evt =>
-                    model.toggleFilter(attrName, value, evt.target.checked)
-                  }
-                />
-              }
-              label={value}
-            />
+class FilterControls extends Component {
+  static propTypes = {
+    classes: ReactPropTypes.shape({ root: ReactPropTypes.string }).isRequired,
+    style: ReactPropTypes.shape({ height: ReactPropTypes.any }),
+    model: MxPropTypes.objectOrObservableObject.isRequired,
+  }
+
+  static defaultProps = { style: {} }
+
+  constructor(props) {
+    super(props)
+    this.state = { seenAttributes: {} }
+    const { model } = props
+    this.autorunDisposer = reaction(
+      () => {
+        const newSeenAttributes = {}
+        getConf(model, 'filterAttributes').forEach(attrName => {
+          if (!newSeenAttributes[attrName]) newSeenAttributes[attrName] = {}
+          visibleValues(model, attrName).forEach(value => {
+            newSeenAttributes[attrName][value] = true
+          })
+        })
+        return newSeenAttributes
+      },
+      newSeenAttributes => {
+        this.setState(state => ({
+          seenAttributes: deepmerge(newSeenAttributes, state.seenAttributes),
+        }))
+      },
+    )
+  }
+
+  componentWillUnmount() {
+    this.autorunDisposer()
+  }
+
+  render() {
+    const { classes, style, model } = this.props
+    const { seenAttributes } = this.state
+    return (
+      <div style={style} className={classes.root}>
+        {Object.keys(seenAttributes)
+          .sort()
+          .map(attrName => (
+            <React.Fragment key={attrName}>
+              <div>{attrName}</div>
+              {Object.keys(seenAttributes[attrName])
+                .sort()
+                .map(value => (
+                  <FormControlLabel
+                    key={`${attrName}-${value}`}
+                    control={
+                      <Checkbox
+                        checked={
+                          !(
+                            model.filterOut.has(attrName) &&
+                            model.filterOut.get(attrName).get(String(value))
+                          )
+                        }
+                        onChange={evt =>
+                          model.toggleFilter(
+                            attrName,
+                            value,
+                            evt.target.checked,
+                          )
+                        }
+                      />
+                    }
+                    label={value}
+                  />
+                ))}
+            </React.Fragment>
           ))}
-        </React.Fragment>
-      ))}
-    </div>
-  )
+      </div>
+    )
+  }
 }
-FilterControls.propTypes = {
-  classes: ReactPropTypes.shape({ root: ReactPropTypes.string }).isRequired,
-  style: ReactPropTypes.shape({ height: ReactPropTypes.any }),
-  model: MxPropTypes.objectOrObservableObject.isRequired,
-}
-FilterControls.defaultProps = { style: {} }
 
 export default withStyles(styles)(observer(FilterControls))
