5631e1fa829df6109d548bc6b92590159c992264
Robert Buels <rbuels@gmail.com>
2019-03-25 11:28:04 -0700

    add more FromConfigAdapter tests

--- packages/jbrowse-web/src/plugins/FromConfigAdapter/FromConfigAdapter.test.js
+++ packages/jbrowse-web/src/plugins/FromConfigAdapter/FromConfigAdapter.test.js
@@ -19,10 +19,38 @@ test('adapter can fetch features', async () => {
   expect(featuresArray[0].toJSON()).toEqual(features[0])
 })
 
-test('adapter can fetch regions', async () => {
+test('adapter can fetch regions 1', async () => {
   const features = [
     { uniqueId: 'one', refName: 'ctgA', start: 250, end: 400 },
     { uniqueId: 'two', refName: 'ctgA', start: 150, end: 300 },
+    { uniqueId: 'three', refName: 'ctgB', start: 50, end: 60 },
+  ]
+  const adapter = new Adapter({ features })
+  const result = await adapter.getRegions()
+  expect(result).toEqual([
+    { refName: 'ctgA', start: 150, end: 400 },
+    { refName: 'ctgB', start: 50, end: 60 },
+  ])
+})
+
+test('adapter can fetch regions 2', async () => {
+  const features = [
+    { uniqueId: 'three', refName: 'ctgB', start: 50, end: 60 },
+    { uniqueId: 'two', refName: 'ctgA', start: 150, end: 300 },
+    { uniqueId: 'one', refName: 'ctgA', start: 250, end: 400 },
+  ]
+  const adapter = new Adapter({ features })
+  const result = await adapter.getRegions()
+  expect(result).toEqual([
+    { refName: 'ctgA', start: 150, end: 400 },
+    { refName: 'ctgB', start: 50, end: 60 },
+  ])
+})
+
+test('adapter can fetch regions 3', async () => {
+  const features = [
+    { uniqueId: 'two', refName: 'ctgA', start: 150, end: 300 },
+    { uniqueId: 'one', refName: 'ctgA', start: 250, end: 400 },
     { uniqueId: 'three', refName: 'ctgB', start: 50, end: 60 },
   ]
   const adapter = new Adapter({ features })
