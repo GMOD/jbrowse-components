6b999bf5b961dbcf2786bc423ef91269fb55c2ac
Colin <colin.diesh@gmail.com>
2019-04-08 14:03:20 -0400

    Add some tests about scales

--- packages/jbrowse-web/src/plugins/WiggleRenderer/util.test.js
+++ packages/jbrowse-web/src/plugins/WiggleRenderer/util.test.js
@@ -1,13 +1,37 @@
 import { getScale } from './util'
 
 test('linear scale', () => {
-  const scale = getScale('linear', [0, 100], [0, 100])
-  expect(scale.ticks(1)).toEqual([0, 100])
-  expect(scale.domain()).toEqual([0, 100])
+  const dom = [0, 100]
+  const range = [0, 100]
+  const scale = getScale('linear', dom, range)
+  expect(scale.domain()).toEqual(dom)
 })
 
 test('log scale', () => {
-  const scale = getScale('log', [1, 100], [0, 100])
-  expect(scale.ticks(1)).toEqual([1, 100])
-  expect(scale.domain()).toEqual([1, 100])
+  const dom = [1, 100]
+  const range = [0, 100]
+  const scale = getScale('log', dom, range)
+  expect(scale.domain()).toEqual(dom)
+})
+
+test('test inverted', () => {
+  const dom = [1, 100]
+  const range = [0, 100]
+  const scale = getScale('log', dom, range, { inverted: true })
+  expect(scale.domain()).toEqual(dom)
+  expect(scale.range()).toEqual(range.reverse())
+})
+
+test('test minScore', () => {
+  const dom = [0, 100]
+  const range = [0, 100]
+  const s = getScale('linear', dom, range, { minScore: 50 })
+  expect(s.domain()).toEqual([50, 100])
+})
+
+test('test min and max score', () => {
+  const dom = [1, 100]
+  const range = [0, 100]
+  const s = getScale('linear', dom, range, { minScore: 50, maxScore: 70 })
+  expect(s.domain()).toEqual([50, 70])
 })
