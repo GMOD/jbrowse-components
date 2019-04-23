08b31cce1e5f439ae1adfb191d1c7b286271ff46
Robert Buels <rbuels@gmail.com>
2019-04-23 11:43:02 -0700

    manually merge @garrettjstevens eslint work from a60e152e8413a6122654616e77e929d9137958d4

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,73 @@
- module.exports = {
-  "parser": "@typescript-eslint/parser",
-  "plugins": [
-    "prettier"
+module.exports = {
+  parser: 'babel-eslint',
+  extends: [
+    'airbnb-base',
+    'react-app',
+    'plugin:monorepo/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
+    'prettier/@typescript-eslint',
   ],
-  "extends": [
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "airbnb-base",
-    "plugin:monorepo/recommended"
-  ],
-  "rules": {
-    "class-methods-use-this": "off",
-    "no-restricted-syntax": "off",
-    "no-param-reassign": "warn",
-    "semi": ["error", "never"],
-    "@typescript-eslint/no-var-requires": "off",
-    "monorepo/no-internal-import": "off"
+  plugins: ['prettier'],
+  env: {
+    browser: true,
   },
-  "env": { "browser": true },
-  "overrides": [
+  rules: {
+    'class-methods-use-this': 'off',
+    'monorepo/no-internal-import': 'off',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-debugger': 'warn',
+    'no-param-reassign': 'off',
+    'no-restricted-syntax': 'off',
+    'no-underscore-dangle': 'warn',
+    'no-unused-vars': 'warn',
+    'no-use-before-define': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'all',
+      },
+    ],
+    'react/destructuring-assignment': 'warn',
+    'react/jsx-filename-extension': 'off',
+    'react/no-unused-state': 'warn',
+    'react/prefer-stateless-function': 'warn',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+  },
+  overrides: [
     {
-      "files": [
-        "**/*.test.js"
-      ],
-      "env": {
-        "jest": true,
-        "node": true,
-        "browser": false
-      }
-    }
-  ]
+        files: ['packages/generator-jbrowse/**/*'],
+        env: {node: true},
+        rules: {'@typescript-eslint/no-var-requires': 'off'}
+      },
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: { ecmaFeatures: { jsx: true } },
+      plugins: ['@typescript-eslint', 'prettier'],
+    },
+    {
+      files: ['**/*.worker.js'],
+      globals: { self: true },
+      rules: { 'no-restricted-globals': 'off' },
+    },
+    {
+      files: ['**/*.test.js'],
+      env: { jest: true },
+      globals: {
+        document: true,
+        it: true,
+        describe: true,
+        test: true,
+      },
+      rules: {
+        'import/no-extraneous-dependencies': 'off',
+        // 'no-restricted-globals': 'off',
+      },
+    },
+  ],
 }
