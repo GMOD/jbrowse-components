2cb6bdae55265e2e66728cf6996c06e2c7b1b0fe
Robert Buels <rbuels@gmail.com>
2019-05-02 12:23:58 -0700

    add a `regionCannotBeRendered` to the sequence track that prevents fetching/rendering bases if bpPerPx is above 2, fixes #156

--- packages/jbrowse-web/src/plugins/SequenceTrack/model.js
+++ packages/jbrowse-web/src/plugins/SequenceTrack/model.js
@@ -1,7 +1,7 @@
 import { types } from 'mobx-state-tree'
 
 import { ConfigurationReference } from '@gmod/jbrowse-core/configuration'
-import { getParentRenderProps } from '@gmod/jbrowse-core/util/tracks'
+import { getParentRenderProps, getContainingView } from '@gmod/jbrowse-core/util/tracks'
 
 import BlockBasedTrackComponent from '../LinearGenomeView/components/BlockBasedTrack'
 import BlockBasedTrack from '../LinearGenomeView/models/blockBasedTrack'
@@ -24,6 +24,19 @@ export default (pluginManager, configSchema, trackType) =>
             config: self.configuration.rendering,
           }
         },
+
+        /**
+         * @param {Region} region
+         * @returns falsy if the region is fine to try rendering. Otherwise,
+         *  return a string of text saying why the region can't be rendered.
+         */
+        regionCannotBeRendered(region) {
+          const view = getContainingView(self)
+          if (view.bpPerPx > 2) {
+            return 'Zoom in to see sequence'
+          }
+          return undefined
+        }
       }))
       .volatile(() => ({
         reactComponent: BlockBasedTrackComponent,
