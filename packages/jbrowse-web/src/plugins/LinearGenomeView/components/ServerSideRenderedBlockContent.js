996ef6abbb75e93690a69d4650aabe6a2f43835b
Robert Buels <rbuels@gmail.com>
2019-05-01 18:37:41 -0700

    in passing, improve loading marker. fixes #271

--- packages/jbrowse-web/src/plugins/LinearGenomeView/components/ServerSideRenderedBlockContent.js
+++ packages/jbrowse-web/src/plugins/LinearGenomeView/components/ServerSideRenderedBlockContent.js
@@ -1,15 +1,17 @@
 import { withStyles } from '@material-ui/core'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { observer, PropTypes } from 'mobx-react'
-import { setLivelinessChecking } from 'mobx-state-tree'
 import ServerSideRenderedContent from './ServerSideRenderedContent'
-
-setLivelinessChecking('error')
 
 const styles = {
   loading: {
-    color: 'gray',
     paddingLeft: '0.6em',
+    position: 'absolute',
+    backgroundColor: "#f1f1f1",
+    backgroundImage: "repeating-linear-gradient(45deg, transparent, transparent 5px, rgba(255,255,255,.5) 5px, rgba(255,255,255,.5) 10px)",
+    height: '100%',
+    width: '100%',
+    textAlign: 'center',
   },
   error: {
     display: 'block',
@@ -20,9 +22,17 @@ const styles = {
   },
 }
 
-const LoadingMessage = withStyles(styles)(classes => (
-  <div className={classes.loading}>Loading ...</div>
-))
+const LoadingMessage = withStyles(styles)(({classes}) => {
+
+  // only show the loading message after 300ms to prevent excessive flickering
+  const [shown, setShown] = useState(false)
+  useEffect(() => {
+   const timeout = setTimeout(() => setShown(true), 300)
+   return () => clearTimeout(timeout)
+  })
+
+  return shown ? <div className={classes.loading}>Loading ...</div> : null
+})
 
 const ErrorMessage = withStyles(styles)(({ error, classes }) => (
   <div className={classes.error}>{error.message}</div>
