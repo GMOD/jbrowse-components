3e13dc69feeff003457bfe09940494d0532e43bc
Colin <colin.diesh@gmail.com>
2019-05-06 20:29:22 -0400

    Fix some return type warnings

--- packages/jbrowse-web/src/plugins/TwoBitAdapter/TwoBitAdapter.ts
+++ packages/jbrowse-web/src/plugins/TwoBitAdapter/TwoBitAdapter.ts
@@ -4,34 +4,35 @@ import { openLocation } from '@gmod/jbrowse-core/util/io'
 import BaseAdapter, { Region } from '@gmod/jbrowse-core/BaseAdapter'
 import SimpleFeature, { Feature } from '@gmod/jbrowse-core/util/simpleFeature'
 import { ObservableCreate } from '@gmod/jbrowse-core/util/rxjs'
-import { Observer } from 'rxjs'
+import { Observable, Observer } from 'rxjs'
 
 export default class TwoBitAdapter extends BaseAdapter {
   private twobit: any
 
   public static capabilities = ['getFeatures', 'getRefNames', 'getRegions']
 
-  public constructor(config: Record<string, any>) {
+  public constructor(config: { twoBitLocation: string }) {
     super(config)
-    const { twoBitLocation } = config
     const twoBitOpts = {
-      filehandle: openLocation(twoBitLocation),
+      filehandle: openLocation(config.twoBitLocation),
     }
 
     this.twobit = new TwoBitFile(twoBitOpts)
   }
 
-  public async getRefNames() {
+  public async getRefNames(): Promise<string[]> {
     return this.twobit.getSequenceNames()
   }
 
-  public async getRegions() {
+  public async getRegions(): Promise<Region[]> {
     const refSizes = await this.twobit.getSequenceSizes()
-    return Object.keys(refSizes).map(refName => ({
-      refName,
-      start: 0,
-      end: refSizes[refName],
-    }))
+    return Object.keys(refSizes).map(
+      (refName: string): Region => ({
+        refName,
+        start: 0,
+        end: refSizes[refName],
+      }),
+    )
   }
 
   /**
@@ -39,17 +40,21 @@ export default class TwoBitAdapter extends BaseAdapter {
    * @param {Region} param
    * @returns {Observable[Feature]} Observable of Feature objects in the region
    */
-  public getFeatures({ refName, start, end }: Region) {
-    return ObservableCreate<Feature>(async (observer: Observer<Feature>) => {
-      const seq = await this.twobit.getSequence(refName, start, end)
-      observer.next(
-        new SimpleFeature({
-          id: `${refName} ${start}-${end}`,
-          data: { refName, start, end, seq },
-        }),
-      )
-      observer.complete()
-    })
+  // @ts-ignore confusion between base proj and jbrowse-web rxjs creates type error
+  public getFeatures({ refName, start, end }: Region): Observable<Feature> {
+    // @ts-ignore same as above
+    return ObservableCreate<Feature>(
+      async (observer: Observer<Feature>): Promise<void> => {
+        const seq = await this.twobit.getSequence(refName, start, end)
+        observer.next(
+          new SimpleFeature({
+            id: `${refName} ${start}-${end}`,
+            data: { refName, start, end, seq },
+          }),
+        )
+        observer.complete()
+      },
+    )
   }
 
   /**
@@ -57,5 +62,5 @@ export default class TwoBitAdapter extends BaseAdapter {
    * will not be needed for the forseeable future and can be purged
    * from caches, etc
    */
-  public freeResources(/* { region } */) {}
+  public freeResources(/* { region } */): void {}
 }
