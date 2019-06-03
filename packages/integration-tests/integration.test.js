68f765cccddc2a618ecada637071901f8b5fc95e
Colin Diesh <colin.diesh@gmail.com>
2019-06-03 18:33:39 -0400

    Add a BUILT_TESTS flag for the fs.exists tests (#334)

--- packages/integration-tests/integration.test.js
+++ packages/integration-tests/integration.test.js
@@ -2,15 +2,17 @@
 /* eslint-disable global-require */
 const fs = require('fs')
 
-test('JBrowse Web built', () => {
-  expect(fs.existsSync('packages/jbrowse-web/build/index.html')).toBeTruthy()
-})
+if (process.env.BUILT_TESTS) {
+  test('JBrowse Web built', () => {
+    expect(fs.existsSync('packages/jbrowse-web/build/index.html')).toBeTruthy()
+  })
 
-test('Protein Widget built', () => {
-  expect(
-    fs.existsSync('packages/protein-widget/umd/jbrowse-protein-viewer.js'),
-  ).toBeTruthy()
-})
+  test('Protein Widget built', () => {
+    expect(
+      fs.existsSync('packages/protein-widget/umd/jbrowse-protein-viewer.js'),
+    ).toBeTruthy()
+  })
+}
 
 test('can import JBrowse Generator', () => {
   const generatorJbrowse = require('../generator-jbrowse')
