f651daef48e51aa11bd764b2a4c7fa287e18348e
Colin <colin.diesh@gmail.com>
2019-06-16 12:15:45 -0400

    Mock shortid

--- packages/jbrowse-web/src/plugins/AlignmentsTrack/index.test.js
+++ packages/jbrowse-web/src/plugins/AlignmentsTrack/index.test.js
@@ -2,6 +2,7 @@ import { getSnapshot } from 'mobx-state-tree'
 import MyPlugin from './index'
 import { createTestEnv } from '../../JBrowse'
 
+jest.mock('shortid', () => ({ generate: () => 'testid' }))
 test('plugin in a stock JBrowse', async () => {
   const { pluginManager } = await createTestEnv()
   expect(() => pluginManager.addPlugin(new MyPlugin())).toThrow(
