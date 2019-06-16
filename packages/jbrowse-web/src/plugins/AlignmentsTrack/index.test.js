06704f8ce9abda7ddc79127354a4d8aafc2d716a
Colin <colin.diesh@gmail.com>
2019-06-16 11:46:19 -0400

    Add basic installed correctly tests

---
+++ packages/jbrowse-web/src/plugins/AlignmentsTrack/index.test.js
@@ -0,0 +1,17 @@
+import { getSnapshot } from 'mobx-state-tree'
+import MyPlugin from './index'
+import { createTestEnv } from '../../JBrowse'
+
+test('plugin in a stock JBrowse', async () => {
+  const { pluginManager } = await createTestEnv()
+  expect(() => pluginManager.addPlugin(new MyPlugin())).toThrow(
+    /JBrowse already configured, cannot add plugins/,
+  )
+
+  const AlignmentsTrack = pluginManager.getTrackType('AlignmentsTrack')
+  // console.log(AlignmentsTrack)
+  const config = AlignmentsTrack.configSchema.create({
+    type: 'AlignmentsTrack',
+  })
+  expect(getSnapshot(config)).toMatchSnapshot()
+})
---
+++ packages/jbrowse-web/src/plugins/VariantTrack/index.test.js
@@ -0,0 +1,14 @@
+import { getSnapshot } from 'mobx-state-tree'
+import MyPlugin from './index'
+import { createTestEnv } from '../../JBrowse'
+
+test('plugin in a stock JBrowse', async () => {
+  const { pluginManager } = await createTestEnv()
+  expect(() => pluginManager.addPlugin(new MyPlugin())).toThrow(
+    /JBrowse already configured, cannot add plugins/,
+  )
+
+  const VariantTrack = pluginManager.getTrackType('VariantTrack')
+  const config = VariantTrack.configSchema.create({ type: 'VariantTrack' })
+  expect(getSnapshot(config)).toMatchSnapshot()
+})
