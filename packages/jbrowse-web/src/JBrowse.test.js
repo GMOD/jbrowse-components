68a17532857950b2303b151ac0917f8f93bd0f87
Colin <colin.diesh@gmail.com>
2019-06-19 18:54:21 -0400

    Update jest environment since jest-environment-dom by default uses an old version

--- packages/jbrowse-web/src/JBrowse.test.js
+++ packages/jbrowse-web/src/JBrowse.test.js
@@ -50,6 +50,22 @@ afterEach(cleanup)
 
 fetchMock.mock('*', readBuffer)
 
+// this is just a little hack to silence a warning that we'll get until react
+// fixes this: https://github.com/facebook/react/pull/14853
+const originalError = console.error
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Warning.*not wrapped in act/.test(args[0])) {
+      return
+    }
+    originalError.call(console, ...args)
+  }
+})
+
+afterAll(() => {
+  console.error = originalError
+})
+
 describe('valid file tests', () => {
   it('access about menu', async () => {
     const { getByText } = render(<JBrowse configs={[config]} />)
