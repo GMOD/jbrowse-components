9beff072e6f2e0181443cce6f179be01c603a976
Colin <colin.diesh@gmail.com>
2019-06-25 11:58:23 -0400

    Mock root/view to put track model under test

--- packages/alignments/src/index.test.js
+++ packages/alignments/src/index.test.js
@@ -1,12 +1,42 @@
 import { createTestEnv } from '@gmod/jbrowse-web/src/JBrowse'
-import { getSnapshot } from 'mobx-state-tree'
-import MyPlugin from './index'
+import { getSnapshot, types } from 'mobx-state-tree'
+import { render, waitForElement } from 'react-testing-library'
+import React from 'react'
+import AlignmentsPlugin from './index'
+import AlignmentsDrawerWidget from './AlignmentsFeatureDetail'
 
-test('plugin in a stock JBrowse', async () => {
+const createMockTrackStateModel = track =>
+  types
+    .model({
+      selectedFeature: types.frozen(),
+      bpPerPx: 0.05,
+      staticBlocks: types.frozen(),
+      track: track.stateModel,
+    })
+    .actions(self => {
+      return {
+        setSelection(thing) {
+          self.selection = thing
+        },
+        clearSelection() {
+          self.selection = undefined
+        },
+      }
+    })
+
+const createMockTrack = track =>
+  createMockTrackStateModel(track).create({
+    staticBlocks: [],
+    track: {
+      configuration: track.configSchema.create({
+        type: 'AlignmentsTrack',
+      }),
+      type: 'AlignmentsTrack',
+    },
+  })
+
+test('create bam adapter config', async () => {
   const { pluginManager } = await createTestEnv()
-  expect(() => pluginManager.addPlugin(new MyPlugin())).toThrow(
-    /JBrowse already configured, cannot add plugins/,
-  )
 
   const BamAdapter = pluginManager.getAdapterType('BamAdapter')
   const config = BamAdapter.configSchema.create({ type: 'BamAdapter' })
@@ -14,6 +44,9 @@ test('plugin in a stock JBrowse', async () => {
     configId: expect.any(String),
     index: { configId: expect.any(String) },
   })
+})
+test('create track config', async () => {
+  const { pluginManager } = await createTestEnv()
 
   const AlignmentsTrack = pluginManager.getTrackType('AlignmentsTrack')
   const config2 = AlignmentsTrack.configSchema.create({
@@ -40,4 +73,42 @@ test('plugin in a stock JBrowse', async () => {
       configId: expect.any(String),
     },
   })
+})
+
+test('test selection in alignments track model with mock root', async () => {
+  const { pluginManager } = await createTestEnv()
+
+  const rootModel = createMockTrack(
+    pluginManager.getTrackType('AlignmentsTrack'),
+  )
+
+  rootModel.track.selectFeature({
+    id() {
+      return 1234
+    },
+  })
+  expect(rootModel.selection.id()).toBe(1234)
+
+  rootModel.track.clearFeatureSelection()
+  expect(rootModel.selection).not.toBeTruthy()
+})
+
+test('open up a drawer widget', async () => {
+  const { pluginManager } = await createTestEnv()
+
+  const rootModel = createMockTrack(
+    pluginManager.getTrackType('AlignmentsTrack'),
+  )
+
+  rootModel.track.selectFeature({
+    id() {
+      return 1234
+    },
+  })
+  console.log('AlignmentsDrawerWidget', AlignmentsDrawerWidget)
+
+  const { container, getByTestId } = render(
+    <AlignmentsDrawerWidget model={rootModel.track} />,
+  )
+  console.log(container)
 })
