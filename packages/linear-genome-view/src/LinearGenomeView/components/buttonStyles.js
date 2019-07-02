41f2fa9af0099c7d3360bd7cb5ebc26b084ec6dd
Colin <colin.diesh@gmail.com>
2019-07-01 23:04:52 -0400

    Lint error fix

--- packages/linear-genome-view/src/LinearGenomeView/components/buttonStyles.js
+++ packages/linear-genome-view/src/LinearGenomeView/components/buttonStyles.js
@@ -1,9 +1,9 @@
-export default theme =>  ({
+export default theme => ({
   iconButton: {
     padding: theme.spacing(0.5),
   },
   toggleButton: {
     height: theme.spacing(4),
     border: 'none',
-  }
+  },
 })
