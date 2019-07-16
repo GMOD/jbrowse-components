043663bc6924a14e81e27580505e4eda2cd22f90
Colin <colin.diesh@gmail.com>
2019-07-16 01:00:11 -0400

    Use react hooks for code editor

--- packages/config/src/ConfigurationEditorDrawerWidget/components/CallbackEditor.js
+++ packages/config/src/ConfigurationEditorDrawerWidget/components/CallbackEditor.js
@@ -4,10 +4,9 @@ import {
   InputLabel,
   withStyles,
 } from '@material-ui/core'
-import debounce from 'debounce'
 import { observer, PropTypes } from 'mobx-react'
 import ReactPropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import Editor from 'react-simple-code-editor'
 import { highlight, languages } from 'prismjs/components/prism-core'
 import 'prismjs/components/prism-clike'
@@ -25,44 +24,53 @@ const styles = theme => ({
   },
 })
 
-class CallbackEditor extends Component {
-  static propTypes = {
-    slot: PropTypes.objectOrObservableObject.isRequired,
-    classes: ReactPropTypes.objectOf(ReactPropTypes.string).isRequired,
-  }
+function useDebounce(value, delay, slot) {
+  const [debouncedValue, setDebouncedValue] = useState(value)
 
-  constructor(props) {
-    super(props)
-    const { slot } = props
-    this.state = { code: slot.value }
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [value, delay, slot])
 
-    this.updateSlot = debounce(code => slot.set(code), 400)
-  }
-
-  render() {
-    const { slot, classes } = this.props
-    const { code } = this.state
-    return (
-      <FormControl>
-        <InputLabel shrink htmlFor="callback-editor">
-          {slot.name}
-        </InputLabel>
-        <Editor
-          className={classes.callbackEditor}
-          value={code}
-          onValueChange={newCode => {
-            this.setState({ code: newCode })
-            this.updateSlot(newCode)
-          }}
-          highlight={newCode =>
-            highlight(newCode, languages.javascript, 'javascript')
-          }
-          padding={10}
-        />
-        <FormHelperText>{slot.description}</FormHelperText>
-      </FormControl>
-    )
-  }
+  return debouncedValue
 }
 
+export function CallbackEditor(props) {
+  const { slot, classes } = props
+
+  const [code, setCode] = useState(slot.value, 400)
+  const debouncedCode = useDebounce(code, 400)
+
+  useEffect(() => {
+    slot.set(debouncedCode)
+  }, [debouncedCode, slot])
+
+  return (
+    <FormControl>
+      <InputLabel shrink htmlFor="callback-editor">
+        {slot.name}
+      </InputLabel>
+      <Editor
+        className={classes.callbackEditor}
+        value={code}
+        onValueChange={newCode => {
+          setCode(newCode)
+        }}
+        highlight={newCode =>
+          highlight(newCode, languages.javascript, 'javascript')
+        }
+        padding={10}
+      />
+      <FormHelperText>{slot.description}</FormHelperText>
+    </FormControl>
+  )
+}
+CallbackEditor.propTypes = {
+  slot: PropTypes.objectOrObservableObject.isRequired,
+  classes: ReactPropTypes.objectOf(ReactPropTypes.string).isRequired,
+}
 export default withStyles(styles)(observer(CallbackEditor))
