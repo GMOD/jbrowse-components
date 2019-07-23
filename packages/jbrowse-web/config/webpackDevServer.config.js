4c998f5567f27be72bb863637c985b4646d8d61b
Colin <colin.diesh@gmail.com>
2019-07-23 10:50:00 -0400

    Unset hot module reloading (was applied only for css) to the webpack dev server

--- packages/jbrowse-web/config/webpackDevServer.config.js
+++ packages/jbrowse-web/config/webpackDevServer.config.js
@@ -58,7 +58,7 @@ module.exports = function(proxy, allowedHost) {
     // updated. The WebpackDevServer client is included as an entry point
     // in the Webpack development configuration. Note that only changes
     // to CSS are currently hot reloaded. JS changes will refresh the browser.
-    hot: true,
+    hot: false,
     // It is important to tell WebpackDevServer to use the same "root" path
     // as we specified in the config. In development, we always serve from /.
     publicPath: config.output.publicPath,
