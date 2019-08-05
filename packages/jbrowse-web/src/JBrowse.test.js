715ec64310331e946e0bbbd63f8739cef7975494
Garrett Stevens <stevens.garrett.j@gmail.com>
2019-08-05 15:38:06 -0400

    Mock/polyfill TextEncoder and localStorage in test

--- packages/jbrowse-web/src/JBrowse.test.js
+++ packages/jbrowse-web/src/JBrowse.test.js
@@ -8,6 +8,7 @@ import React from 'react'
 import fetchMock from 'fetch-mock'
 import { LocalFile } from 'generic-filehandle'
 import rangeParser from 'range-parser'
+import { TextEncoder, TextDecoder } from 'text-encoding-polyfill'
 import JBrowse from './JBrowse'
 import config from '../test_data/config_integration_test.json'
 import rootModel from './rootModel'
@@ -18,6 +19,13 @@ function timeout(ms) {
 }
 window.requestIdleCallback = cb => cb()
 window.cancelIdleCallback = () => {}
+window.TextEncoder = TextEncoder
+window.TextDecoder = TextDecoder
+
+Storage.prototype.getItem = jest.fn(() => null)
+Storage.prototype.setItem = jest.fn()
+Storage.prototype.removeItem = jest.fn()
+Storage.prototype.clear = jest.fn()
 
 const getFile = url => new LocalFile(require.resolve(`../${url}`))
 // fakes server responses from local file object with fetchMock
