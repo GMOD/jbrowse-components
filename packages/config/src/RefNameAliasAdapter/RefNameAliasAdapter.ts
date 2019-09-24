c1a54a1b3cb38c6c935d773ec2e0ea8e33af8810
Colin <colin.diesh@gmail.com>
2019-09-23 23:54:29 -0400

    Make BaseRefNameAliasAdapter

--- packages/config/src/RefNameAliasAdapter/RefNameAliasAdapter.ts
+++ packages/config/src/RefNameAliasAdapter/RefNameAliasAdapter.ts
@@ -1,16 +1,11 @@
-import BaseAdapter from '@gmod/jbrowse-core/BaseAdapter'
+import { BaseRefNameAliasAdapter, Alias } from '@gmod/jbrowse-core/BaseAdapter'
 import { IFileLocation } from '@gmod/jbrowse-core/mst-types'
 import { openLocation } from '@gmod/jbrowse-core/util/io'
 import { Feature } from '@gmod/jbrowse-core/util/simpleFeature'
 import { Observable } from 'rxjs'
 import { GenericFilehandle } from 'generic-filehandle'
 
-interface Alias {
-  refName: string
-  aliases: string[]
-}
-
-export default class RefNameAliasAdapter extends BaseAdapter {
+export default class RefNameAliasAdapter extends BaseRefNameAliasAdapter {
   public static capabilities = ['getRefNameAliases']
 
   private location: GenericFilehandle
@@ -37,20 +32,4 @@ export default class RefNameAliasAdapter extends BaseAdapter {
   getRefNameAliases(): Promise<Alias[]> {
     return this.promise
   }
-
-  async getRefNames(): Promise<string[]> {
-    const res = await this.promise
-    return res.map((a: Alias) => a.refName)
-  }
-
-  getFeatures(): Observable<Feature> {
-    return new Observable<Feature>()
-  }
-
-  /**
-   * called to provide a hint that data tied to a certain region
-   * will not be needed for the forseeable future and can be purged
-   * from caches, etc
-   */
-  freeResources(/* { region } */): void {}
 }
