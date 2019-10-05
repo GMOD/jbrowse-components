6a0eb831b2bea6bbafb8a77ee479114f086d5d87
Colin <colin.diesh@gmail.com>
2019-10-05 12:27:53 -0400

    Remove some duplicated code

--- packages/alignments/src/AlignmentsTrack/model/alignmentsTrack.js
+++ packages/alignments/src/AlignmentsTrack/model/alignmentsTrack.js
@@ -17,61 +17,54 @@ const rendererTypes = new Map([
 ])
 
 export default (pluginManager, configSchema) =>
-  types.compose(
-    'AlignmentsTrack',
-    blockBasedTrackModel,
-    types
-      .model({
+  types
+    .compose(
+      'AlignmentsTrack',
+      blockBasedTrackModel,
+      types.model({
         type: types.literal('AlignmentsTrack'),
         configuration: ConfigurationReference(configSchema),
         // the renderer that the user has selected in the UI, empty string
         // if they have not made any selection
         selectedRendering: types.optional(types.string, ''),
-      })
-      .volatile(() => ({
-        ReactComponent: BlockBasedTrack,
-        rendererTypeChoices: Array.from(rendererTypes.keys()),
-      }))
-      .views(self => ({
-        /**
-         * the renderer type name is based on the "view"
-         * selected in the UI: pileup, coverage, etc
-         */
-        get rendererTypeName() {
-          const defaultRendering = getConf(self, 'defaultRendering')
-          const viewName = self.selectedRendering || defaultRendering
-          const rendererType = rendererTypes.get(viewName)
-          if (!rendererType)
-            throw new Error(`unknown alignments view name ${viewName}`)
-          return rendererType
-        },
+      }),
+    )
+    .volatile(() => ({
+      ReactComponent: BlockBasedTrack,
+      rendererTypeChoices: Array.from(rendererTypes.keys()),
+    }))
+    .views(self => ({
+      /**
+       * the renderer type name is based on the "view"
+       * selected in the UI: pileup, coverage, etc
+       */
+      get rendererTypeName() {
+        const defaultRendering = getConf(self, 'defaultRendering')
+        const viewName = self.selectedRendering || defaultRendering
+        const rendererType = rendererTypes.get(viewName)
+        if (!rendererType)
+          throw new Error(`unknown alignments view name ${viewName}`)
+        return rendererType
+      },
 
-        get ControlsComponent() {
-          return TrackControls
-        },
+      get ControlsComponent() {
+        return TrackControls
+      },
 
-        /**
-         * the react props that are passed to the Renderer when data
-         * is rendered in this track
-         */
-        get renderProps() {
-          // view -> [tracks] -> [blocks]
-          const config = self.rendererType.configSchema.create(
-            getConf(self, ['renderers', self.rendererTypeName]) || {},
-          )
-          return {
-            ...getParentRenderProps(self),
-            trackModel: self,
-            config,
-            onFeatureClick(event, featureId) {
-              // try to find the feature in our layout
-              const feature = self.features.get(featureId)
-              self.selectFeature(feature)
-            },
-            onClick() {
-              self.clearFeatureSelection()
-            },
-          }
-        },
-      })),
-  )
+      /**
+       * the react props that are passed to the Renderer when data
+       * is rendered in this track
+       */
+      get renderProps() {
+        // view -> [tracks] -> [blocks]
+        const config = self.rendererType.configSchema.create(
+          getConf(self, ['renderers', self.rendererTypeName]) || {},
+        )
+        return {
+          ...self.composedRenderProps,
+          ...getParentRenderProps(self),
+          trackModel: self,
+          config,
+        }
+      },
+    }))
--- packages/variants/src/VariantTrack/model.js
+++ packages/variants/src/VariantTrack/model.js
@@ -2,9 +2,7 @@ import {
   ConfigurationReference,
   getConf,
 } from '@gmod/jbrowse-core/configuration'
-import CompositeMap from '@gmod/jbrowse-core/util/compositeMap'
 import { getParentRenderProps } from '@gmod/jbrowse-core/util/tracks'
-import { getSession } from '@gmod/jbrowse-core/util'
 import {
   BlockBasedTrack,
   blockBasedTrackModel,
@@ -19,112 +17,59 @@ const rendererTypes = new Map([
 ])
 
 export default configSchema =>
-  types.compose(
-    'VariantTrack',
-    blockBasedTrackModel,
-    types
-      .model({
+  types
+    .compose(
+      'VariantTrack',
+      blockBasedTrackModel,
+      types.model({
         type: types.literal('VariantTrack'),
         configuration: ConfigurationReference(configSchema),
         // the renderer that the user has selected in the UI, empty string
         // if they have not made any selection
         selectedRendering: types.optional(types.string, ''),
         height: types.optional(types.integer, 100),
-      })
-      .volatile(() => ({
-        ReactComponent: BlockBasedTrack,
-        rendererTypeChoices: Array.from(rendererTypes.keys()),
-      }))
-      .actions(self => ({
-        selectFeature(feature) {
-          const session = getSession(self)
-          const featureWidget = session.addDrawerWidget(
-            'VariantFeatureDrawerWidget',
-            'variantFeature',
-            { featureData: feature.data },
-          )
-          session.showDrawerWidget(featureWidget)
-          session.setSelection(feature)
-        },
-        clearFeatureSelection() {
-          const session = getSession(self)
-          session.clearSelection()
-        },
-        setRenderer(newRenderer) {
-          self.selectedRendering = newRenderer
-        },
-      }))
-      .views(self => ({
-        /**
-         * the renderer type name is based on the "view"
-         * selected in the UI: pileup, coverage, etc
-         */
-        get rendererTypeName() {
-          const defaultRendering = getConf(self, 'defaultRendering')
-          const viewName = self.selectedRendering || defaultRendering
-          const rendererType = rendererTypes.get(viewName)
-          if (!rendererType)
-            throw new Error(`unknown alignments view name ${viewName}`)
-          return rendererType
-        },
+      }),
+    )
+    .volatile(() => ({
+      ReactComponent: BlockBasedTrack,
+      rendererTypeChoices: Array.from(rendererTypes.keys()),
+    }))
+    .actions(self => ({
+      setRenderer(newRenderer) {
+        self.selectedRendering = newRenderer
+      },
+    }))
+    .views(self => ({
+      /**
+       * the renderer type name is based on the "view"
+       * selected in the UI: pileup, coverage, etc
+       */
+      get rendererTypeName() {
+        const defaultRendering = getConf(self, 'defaultRendering')
+        const viewName = self.selectedRendering || defaultRendering
+        const rendererType = rendererTypes.get(viewName)
+        if (!rendererType)
+          throw new Error(`unknown alignments view name ${viewName}`)
+        return rendererType
+      },
 
-        /**
-         * returns a string feature ID if the globally-selected object
-         * is probably a feature
-         */
-        get selectedFeatureId() {
-          const session = getSession(self)
-          if (!session) return undefined
-          const { selection } = session
-          // does it quack like a feature?
-          if (
-            selection &&
-            typeof selection.get === 'function' &&
-            typeof selection.id === 'function'
-          ) {
-            // probably is a feature
-            return selection.id()
-          }
-          return undefined
-        },
-
-        /**
-         * the react props that are passed to the Renderer when data
-         * is rendered in this track
-         */
-        get renderProps() {
-          // view -> [tracks] -> [blocks]
-          const config = self.rendererType.configSchema.create(
-            getConf(self, ['renderers', self.rendererTypeName]) || {},
-          )
-          return {
-            ...getParentRenderProps(self),
-            config,
-            trackModel: self,
-            onFeatureClick(event, featureId) {
-              // try to find the feature in our layout
-              const feature = self.features.get(featureId)
-              self.selectFeature(feature)
-            },
-            onClick() {
-              self.clearFeatureSelection()
-            },
-          }
-        },
-        get ControlsComponent() {
-          return TrackControls
-        },
-        /**
-         * a CompositeMap of featureId -> feature obj that
-         * just looks in all the block data for that feature
-         */
-        get features() {
-          const featureMaps = []
-          for (const block of self.blockState.values()) {
-            if (block.data && block.data.features)
-              featureMaps.push(block.data.features)
-          }
-          return new CompositeMap(featureMaps)
-        },
-      })),
-  )
+      /**
+       * the react props that are passed to the Renderer when data
+       * is rendered in this track
+       */
+      get renderProps() {
+        // view -> [tracks] -> [blocks]
+        const config = self.rendererType.configSchema.create(
+          getConf(self, ['renderers', self.rendererTypeName]) || {},
+        )
+        return {
+          ...self.composedRenderProps,
+          ...getParentRenderProps(self),
+          config,
+          trackModel: self,
+        }
+      },
+      get ControlsComponent() {
+        return TrackControls
+      },
+    }))
