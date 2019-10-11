30979bec65043b2e6d45713d257b046d1b9693c7
Garrett Stevens <stevens.garrett.j@gmail.com>
2019-10-10 20:24:19 -0400

    Terminate window workers on page reload

--- packages/jbrowse-sv-inspector-desktop/src/index.js
+++ packages/jbrowse-sv-inspector-desktop/src/index.js
@@ -2,6 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import JBrowse from './JBrowse'
 
+const { BrowserWindow, getCurrentWindow } = window.electron.remote
+
+window.onbeforeunload = () => {
+  const thisWindowId = getCurrentWindow().id
+  BrowserWindow.getAllWindows().forEach(win => {
+    if (win.id !== thisWindowId) win.close()
+  })
+}
+
 const app = <JBrowse config={{ uri: 'test_data/config.json' }} />
 
 ReactDOM.render(app, document.getElementById('root'))
