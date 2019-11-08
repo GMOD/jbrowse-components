94b929d3ceee9a20549917862bb8c2c6cf5ad934
Garrett Stevens <stevens.garrett.j@gmail.com>
2019-11-07 19:27:27 -0700

    Reduce bundle size by importing MUI subpackages

--- packages/breakpoint-split-view/src/components/BreakpointSplitView.tsx
+++ packages/breakpoint-split-view/src/components/BreakpointSplitView.tsx
@@ -1,4 +1,4 @@
-import { makeStyles } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 import { Instance } from 'mobx-state-tree'
 import { BreakpointViewStateModel } from '../models/BreakpointSplitView'
 
--- packages/jbrowse-desktop/src/ui/NewSessionCards.js
+++ packages/jbrowse-desktop/src/ui/NewSessionCards.js
@@ -1,5 +1,5 @@
 import { readConfObject } from '@gmod/jbrowse-core/configuration'
-import { CardContent } from '@material-ui/core'
+import CardContent from '@material-ui/core/CardContent'
 import Button from '@material-ui/core/Button'
 import Card from '@material-ui/core/Card'
 import FormControl from '@material-ui/core/FormControl'
