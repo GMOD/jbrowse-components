187b4e9abe0fa326177d00ce37893a758892a4bd
Colin <colin.diesh@gmail.com>
2019-11-09 13:17:40 -0500

    Remove unneeded disableMobxWarning rule, use mock console instead

--- config/jest/disableMobxWarning.js
+++
@@ -1,8 +0,0 @@
-// filter mobx-state-tree onAction warning
-const originalWarn = console.warn
-console.warn = (...args) => {
-  if (/onAction/.test(args[0])) {
-    return
-  }
-  originalWarn.call(console, ...args)
-}
