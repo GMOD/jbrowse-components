4fd5e43d1d98f7bf69398e3c5b6a8190bc4ad065
Colin <colin.diesh@gmail.com>
2019-12-16 13:26:49 -0500

    Create MCScan adapter

--- packages/synteny-view/src/index.js
+++ packages/synteny-view/src/index.js
@@ -1,7 +1,21 @@
+import AdapterType from '@gmod/jbrowse-core/pluggableElementTypes/AdapterType'
+import {
+  AdapterClass as MCScanAnchorsAdapterClass,
+  configSchema as mcScanAnchorsAdapterConfigSchema,
+} from './MCScanAnchorsAdapter'
+
 export default class SyntenyViewPlugin {
   install(pluginManager) {
     pluginManager.addViewType(() =>
       pluginManager.jbrequire(require('./SyntenyView')),
+    )
+    pluginManager.addAdapterType(
+      () =>
+        new AdapterType({
+          name: 'MCScanAnchorsAdapter',
+          configSchema: mcScanAnchorsAdapterConfigSchema,
+          AdapterClass: MCScanAnchorsAdapterClass,
+        }),
     )
   }
 
