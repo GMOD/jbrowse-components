741c71db6e28a6f4a0d8f4411a51c9d0f19e83c3
Colin <colin.diesh@gmail.com>
2020-01-28 22:24:33 -0500

    Update SNPCoverage

--- packages/alignments/src/SNPCoverageRenderer/index.js
+++ packages/alignments/src/SNPCoverageRenderer/index.js
@@ -2,10 +2,10 @@ import { ConfigurationSchema } from '@gmod/jbrowse-core/configuration'
 import ConfigSchema from '../configSchema'
 
 export { default as ReactComponent } from '@gmod/jbrowse-plugin-wiggle/src/WiggleRendering'
-export { default } from './SNPXYRenderer'
+export { default } from './SNPCoverageRenderer'
 
 export const configSchema = ConfigurationSchema(
-  'SNPXYPlotRenderer',
+  'SNPCoveragePlotRenderer',
   {},
   { baseConfiguration: ConfigSchema, explicitlyTyped: true },
 )
