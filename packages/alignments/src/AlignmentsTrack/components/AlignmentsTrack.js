cfa5dbf2488f638111b56d390dddcd4c4a845c58
peterkxie <peterkxie@yahoo.com>
2020-03-05 15:25:36 -0800

    improvements to context menu, both menu and scalebar now show (div needed styling) and menu changes happen after closed on screen

--- packages/alignments/src/AlignmentsTrack/components/AlignmentsTrack.js
+++ packages/alignments/src/AlignmentsTrack/components/AlignmentsTrack.js
@@ -13,20 +13,21 @@ const initialState = {
 }
 
 function AlignmentsTrackComponent(props) {
-  const [state, setState] = React.useState(initialState)
-  const [trackState, setTrackState] = React.useState({
-    showCoverage: true,
-    showPileup: true,
-  })
-
   const { model } = props
-  const { PileupTrack, SNPCoverageTrack } = model
+  const { PileupTrack, SNPCoverageTrack, height } = model
 
   let showScalebar = false
   if (SNPCoverageTrack) {
     const { ready, stats, needsScalebar } = SNPCoverageTrack
     if (ready && stats && needsScalebar) showScalebar = true
   }
+
+  // Set up context menu
+  const [state, setState] = React.useState(initialState)
+  const [trackState, setTrackState] = React.useState({
+    showCoverage: true,
+    showPileup: true,
+  })
 
   const handleClick = e => {
     e.preventDefault()
@@ -42,12 +43,13 @@ function AlignmentsTrackComponent(props) {
 
   const handleTrackToggle = e => {
     e.preventDefault()
-    setTrackState(prevState => ({
-      ...trackState,
-      [e.target.getAttribute('name')]: !prevState[
-        e.target.getAttribute('name')
-      ],
-    }))
+    const trackSelected = e.target.getAttribute('name')
+    setTimeout(() => {
+      setTrackState(prevState => ({
+        ...trackState,
+        [trackSelected]: !prevState[trackSelected],
+      }))
+    }, 300) // short delay so text changes/disable after menu close
     handleClose()
   }
 
@@ -55,42 +57,40 @@ function AlignmentsTrackComponent(props) {
     // <BlockBasedTrack {...props} {...PileupTrack} {...SNPCoverageTrack}>
     //   {showScalebar ? <YScaleBar model={SNPCoverageTrack} /> : null}
     // </BlockBasedTrack>
-
-    <div onContextMenu={handleClick}>
+    <div onContextMenu={handleClick} style={{ position: 'relative', height }}>
       <BlockBasedTrack {...props} {...PileupTrack} {...SNPCoverageTrack}>
         {showScalebar ? <YScaleBar model={SNPCoverageTrack} /> : null}
-
-        <Menu
-          keepMounted
-          open={state.mouseY !== null}
-          onClose={handleClose}
-          anchorReference="anchorPosition"
-          anchorPosition={
-            state.mouseY !== null && state.mouseX !== null
-              ? { top: state.mouseY, left: state.mouseX }
-              : undefined
-          }
-        >
-          <MenuItem
-            name="showCoverage"
-            onClick={handleTrackToggle}
-            disabled={!trackState.showPileup}
-          >
-            {trackState.showCoverage
-              ? 'Hide Coverage Track'
-              : 'Show Coverage Track'}
-          </MenuItem>
-          <MenuItem
-            name="showPileup"
-            onClick={handleTrackToggle}
-            disabled={!trackState.showCoverage}
-          >
-            {trackState.showPileup ? 'Hide Pileup Track' : 'Show Pileup Track'}
-          </MenuItem>
-          <MenuItem onClick={handleClose}>Sort</MenuItem>
-          <MenuItem onClick={handleClose}>Copy</MenuItem>
-        </Menu>
       </BlockBasedTrack>
+      <Menu
+        keepMounted
+        open={state.mouseY !== null}
+        onClose={handleClose}
+        anchorReference="anchorPosition"
+        anchorPosition={
+          state.mouseY !== null && state.mouseX !== null
+            ? { top: state.mouseY, left: state.mouseX }
+            : undefined
+        }
+      >
+        <MenuItem
+          name="showCoverage"
+          onClick={handleTrackToggle}
+          disabled={!trackState.showPileup}
+        >
+          {trackState.showCoverage
+            ? 'Hide Coverage Track'
+            : 'Show Coverage Track'}
+        </MenuItem>
+        <MenuItem
+          name="showPileup"
+          onClick={handleTrackToggle}
+          disabled={!trackState.showCoverage}
+        >
+          {trackState.showPileup ? 'Hide Pileup Track' : 'Show Pileup Track'}
+        </MenuItem>
+        <MenuItem onClick={handleClose}>Sort</MenuItem>
+        <MenuItem onClick={handleClose}>Copy</MenuItem>
+      </Menu>
     </div>
   )
 }
