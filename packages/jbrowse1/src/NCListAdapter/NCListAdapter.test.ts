5284995224dfebd8ff8b14c089a462445107baf8
Robert Buels <rbuels@gmail.com>
2020-03-10 19:02:00 -0700

    wip

--- packages/jbrowse1/src/NCListAdapter/NCListAdapter.test.ts
+++ packages/jbrowse1/src/NCListAdapter/NCListAdapter.test.ts
@@ -21,9 +21,9 @@ test('adapter can fetch features from ensembl_genes test set', async () => {
     .replace(/\\/g, '\\\\')
   await fsPromises.stat(rootTemplate.replace('{refseq}', '21')) // will throw if doesnt exist
   const args = {
+    refNames: [],
     rootUrlTemplate: decodeURI(new URL(`file://${rootTemplate}`).href),
   }
-  const hash = objectHash(args)
   const adapter = new Adapter(configSchema.create(args))
 
   const features = await adapter.getFeatures({
@@ -35,7 +35,7 @@ test('adapter can fetch features from ensembl_genes test set', async () => {
 
   const featuresArray = await features.pipe(toArray()).toPromise()
   expect(featuresArray[0].get('refName')).toBe('21')
-  expect(featuresArray[0].id()).toBe(`${hash}-21,0,0,19,22,0`)
+  expect(featuresArray[0].id()).toMatch(/-21,0,0,19,22,0$/)
   const featuresJsonArray = featuresArray.map(f => f.toJSON())
   expect(featuresJsonArray.length).toEqual(94)
   for (const feature of featuresJsonArray) {
