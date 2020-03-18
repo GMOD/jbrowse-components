ee912e0ef4e52c48cfefd9fd3d574fb718833548
Garrett Stevens <stevens.garrett.j@gmail.com>
2020-03-17 20:12:52 -0600

    Add cascading menu component to core

--- packages/core/util/index.ts
+++ packages/core/util/index.ts
@@ -429,3 +429,26 @@ export function mergeConfigs(A: Config, B: Config) {
   else if (A.defaultSession) merged.defaultSession = A.defaultSession
   return merged
 }
+
+// https://stackoverflow.com/a/53187807
+/**
+ * Returns the index of the last element in the array where predicate is true,
+ * and -1 otherwise.
+ * @param array The source array to search in
+ * @param predicate find calls predicate once for each element of the array, in
+ * descending order, until it finds one where predicate returns true. If such an
+ * element is found, findLastIndex immediately returns that element index.
+ * Otherwise, findLastIndex returns -1.
+ */
+export function findLastIndex<T>(
+  array: Array<T>,
+  predicate: (value: T, index: number, obj: T[]) => boolean,
+): number {
+  let l = array.length
+  while ((l -= 1)) {
+    if (predicate(array[l], l, array)) {
+      return l
+    }
+  }
+  return -1
+}
