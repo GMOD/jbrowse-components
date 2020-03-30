4a31c97e4892cf028c732103eca5e2675fcc8f88
Colin <colin.diesh@gmail.com>
2020-03-29 21:09:44 -0400

    Typescriptify WiggleRendering to get useRef to behave right, otherwise gives 'Object is possible null' errors

--- packages/wiggle/src/WiggleRendering.tsx
+++ packages/wiggle/src/WiggleRendering.tsx
@@ -1,21 +1,27 @@
 /* eslint-disable no-nested-ternary */
 import { observer } from 'mobx-react'
 import ReactPropTypes from 'prop-types'
-import React, { useState, useRef } from 'react'
-import { PropTypes as CommonPropTypes } from '@gmod/jbrowse-core/mst-types'
+import React, { useState, useRef, MouseEvent } from 'react'
+import { Feature } from '@gmod/jbrowse-core/util/simpleFeature'
+import {
+  IRegion,
+  PropTypes as CommonPropTypes,
+} from '@gmod/jbrowse-core/mst-types'
 import { PrerenderedCanvas } from '@gmod/jbrowse-core/ui'
 import './WiggleRendering.scss'
 
-const toP = s => parseFloat(s.toPrecision(6))
+const toP = (s: number) => parseFloat(s.toPrecision(6))
 
-function Tooltip({ offsetX, feature }) {
+function Tooltip({ offsetX, feature }: { offsetX: number; feature: Feature }) {
   const info = feature.get('snpinfo') ? feature.get('snpinfo') : null
-  const total = info ? info[info.map(e => e.base).indexOf('total')].score : 0
+  const total = info
+    ? info[info.map((e: { base: string }) => e.base).indexOf('total')].score
+    : 0
   const condId = info && info.length >= 5 ? 'smallInfo' : 'info' // readjust table size to fit all
 
   // construct a table with all relevant information
   const renderTableData = info
-    ? info.map(mismatch => {
+    ? info.map((mismatch: { base: string; score: number; strands: any }) => {
         const { base, score, strands } = mismatch
         return (
           <tr key={base}>
@@ -85,7 +91,14 @@ Tooltip.defaultProps = {
   feature: {},
 }
 
-function WiggleRendering(props) {
+function WiggleRendering(props: {
+  region: IRegion
+  features: Map<string, Feature>
+  bpPerPx: number
+  horizontallyFlipped: boolean
+  width: number
+  height: number
+}) {
   const {
     region,
     features,
@@ -94,15 +107,17 @@ function WiggleRendering(props) {
     width,
     height,
   } = props
-  const ref = useRef(null)
-  const [featureUnderMouse, setFeatureUnderMouse] = useState()
+  const ref = useRef<HTMLDivElement>(null)
+  const [featureUnderMouse, setFeatureUnderMouse] = useState<
+    Feature | undefined
+  >()
   const [clientX, setClientX] = useState(0)
 
   let offset = 0
-  if (ref && ref.current && ref.current !== null) {
+  if (ref.current) {
     offset = ref.current.getBoundingClientRect().left
   }
-  function onMouseMove(evt) {
+  function onMouseMove(evt: MouseEvent) {
     const offsetX = evt.clientX - offset
     const px = horizontallyFlipped ? width - offsetX : offsetX
     const clientBp = region.start + bpPerPx * px
