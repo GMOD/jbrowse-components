c2c294bbb719b5a8fe77ad4a304369dfca78cc6a
Colin <colin.diesh@gmail.com>
2020-04-05 15:11:07 -0400

    Add a simple bed3 test

--- packages/bed/src/BedTabixAdapter/BedTabixAdapter.test.ts
+++ packages/bed/src/BedTabixAdapter/BedTabixAdapter.test.ts
@@ -1,7 +1,7 @@
 import { toArray } from 'rxjs/operators'
 import BedTabixAdapter from './BedTabixAdapter'
 
-test('adapter can fetch features from volvox.bb', async () => {
+test('adapter can fetch features from volvox-bed12.bed.gz', async () => {
   const adapter = new BedTabixAdapter({
     bedGzLocation: {
       localPath: require.resolve('./test_data/volvox-bed12.bed.gz'),
@@ -20,6 +20,32 @@ test('adapter can fetch features from volvox.bb', async () => {
     assemblyName: 'volvox',
   })
   expect(await adapter.hasDataForRefName('ctgA')).toBe(true)
+  expect(await adapter.hasDataForRefName('ctgB')).toBe(false)
+
+  const featuresArray = await features.pipe(toArray()).toPromise()
+  const featuresJsonArray = featuresArray.map(f => f.toJSON())
+  expect(featuresJsonArray).toMatchSnapshot()
+})
+
+test('adapter can fetch features from volvox.sort.bed.gz simple bed3', async () => {
+  const adapter = new BedTabixAdapter({
+    bedGzLocation: {
+      localPath: require.resolve('./test_data/volvox.sort.bed.gz'),
+    },
+    index: {
+      location: {
+        localPath: require.resolve('./test_data/volvox.sort.bed.gz.tbi'),
+      },
+    },
+  })
+
+  const features = await adapter.getFeatures({
+    refName: 'contigA',
+    start: 0,
+    end: 20000,
+    assemblyName: 'volvox',
+  })
+  expect(await adapter.hasDataForRefName('contigA')).toBe(true)
   expect(await adapter.hasDataForRefName('ctgB')).toBe(false)
 
   const featuresArray = await features.pipe(toArray()).toPromise()
