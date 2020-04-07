8aeee03e782f0dd5b18aed56377fd376522bae5d
Colin <colin.diesh@gmail.com>
2020-04-07 11:51:05 -0400

    Add comparativeRender

--- packages/jbrowse-web/src/rpcMethods.js
+++ packages/jbrowse-web/src/rpcMethods.js
@@ -204,3 +204,57 @@ export async function render(
   checkAbortSignal(signal)
   return result
 }
+
+/**
+ * call a synteny renderer with the given args
+ * param views: a set of views that each contain a set of regions
+ * used instead of passing regions directly as in render()
+ */
+export async function comparativeRender(
+  pluginManager,
+  {
+    views,
+    sessionId,
+    adapterType,
+    adapterConfig,
+    sequenceAdapterType,
+    sequenceAdapterConfig,
+    rendererType,
+    renderProps,
+    signal,
+  },
+) {
+  if (!sessionId) throw new Error('must pass a unique session id')
+
+  if (isRemoteAbortSignal(signal)) {
+    signal = deserializeAbortSignal(signal)
+  }
+  checkAbortSignal(signal)
+
+  const { dataAdapter } = getAdapter(
+    pluginManager,
+    sessionId,
+    adapterType,
+    adapterConfig,
+    sequenceAdapterType,
+    sequenceAdapterConfig,
+  )
+
+  const RendererType = pluginManager.getRendererType(rendererType)
+  if (!RendererType) throw new Error(`renderer "${rendererType}" not found`)
+  if (!RendererType.ReactComponent)
+    throw new Error(
+      `renderer ${rendererType} has no ReactComponent, it may not be completely implemented yet`,
+    )
+
+  const result = await RendererType.renderInWorker({
+    ...renderProps,
+    pluginManager,
+    sessionId,
+    dataAdapter,
+    views,
+    signal,
+  })
+  checkAbortSignal(signal)
+  return result
+}
