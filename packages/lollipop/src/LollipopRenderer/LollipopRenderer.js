0eaf1af1f7c20ca7c3e266f334ac0f575aba08f0
Robert Buels <rbuels@gmail.com>
2020-04-29 18:45:29 -0700

    prettier

--- packages/lollipop/src/LollipopRenderer/LollipopRenderer.js
+++ packages/lollipop/src/LollipopRenderer/LollipopRenderer.js
@@ -6,6 +6,8 @@ import { FloatingLayout, PrecomputedFloatingLayout } from './Layout'
 
 class FloatingLayoutSession extends LayoutSession {
   makeLayout() {
+    'sequenceAdapter'
+
     const { end, start } = this.regions[0]
     const widthPx = (end - start) / this.bpPerPx
     return new MultiLayout(FloatingLayout, { width: widthPx })
