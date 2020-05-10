1631ae50c2c8c68ddab077791fc68f8e3c84049d
Colin <colin.diesh@gmail.com>
2020-05-10 17:14:41 -0400

    Use extend_eslint CRA env config

--- packages/alignments/src/PileupRenderer/components/PileupRendering.js
+++ packages/alignments/src/PileupRenderer/components/PileupRendering.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-conf-assign */
 import { PropTypes as CommonPropTypes } from '@gmod/jbrowse-core/mst-types'
 import { PrerenderedCanvas, Tooltip } from '@gmod/jbrowse-core/ui'
 import { bpSpanPx } from '@gmod/jbrowse-core/util'
