34b282ae503390d8f03eb16ec66628f1fce8d211
Garrett Stevens <stevens.garrett.j@gmail.com>
2020-05-22 10:55:22 -0600

    Add `getRefNameColor` to assembly
    Gets colors from the assembly config or falls back to a default color
    scheme based on the UCSC color scheme.

--- packages/core/assemblyManager/assembly.ts
+++ packages/core/assemblyManager/assembly.ts
@@ -17,6 +17,39 @@ const refNameAdapterMapSet = types.model('RefNameMappingForAdapter', {
   reverseMap: types.map(types.string),
 })
 
+// Based on the UCSC Genome Browser chromosome color palette:
+// https://github.com/ucscGenomeBrowser/kent/blob/a50ed53aff81d6fb3e34e6913ce18578292bc24e/src/hg/inc/chromColors.h
+// Some colors darkened to have at least a 3:1 contrast ratio on a white
+// background
+const refNameColors = [
+  'rgb(153, 102, 0)',
+  'rgb(102, 102, 0)',
+  'rgb(153, 153, 30)',
+  'rgb(204, 0, 0)',
+  'rgb(255, 0, 0)',
+  'rgb(255, 0, 204)',
+  'rgb(165, 132, 132)', // originally 'rgb(255, 204, 204)'
+  'rgb(204, 122, 0)', // originally rgb(255, 153, 0)'
+  'rgb(178, 142, 0)', // originally 'rgb(255, 204, 0)'
+  'rgb(153, 153, 0)', // originally 'rgb(255, 255, 0)'
+  'rgb(122, 153, 0)', // originally 'rgb(204, 255, 0)'
+  'rgb(0, 165, 0)', // originally 'rgb(0, 255, 0)'
+  'rgb(53, 128, 0)',
+  'rgb(0, 0, 204)',
+  'rgb(96, 145, 242)', // originally 'rgb(102, 153, 255)'
+  'rgb(107, 142, 178)', // originally 'rgb(153, 204, 255)'
+  'rgb(0, 165, 165)', // originally 'rgb(0, 255, 255)'
+  'rgb(122, 153, 153)', // originally 'rgb(204, 255, 255)'
+  'rgb(153, 0, 204)',
+  'rgb(204, 51, 255)',
+  'rgb(173, 130, 216)', // originally 'rgb(204, 153, 255)'
+  'rgb(102, 102, 102)',
+  'rgb(145, 145, 145)', // originally 'rgb(153, 153, 153)'
+  'rgb(142, 142, 142)', // originally 'rgb(204, 204, 204)'
+  'rgb(142, 142, 107)', // originally 'rgb(204, 204, 153)'
+  'rgb(96, 163, 48)', // originally 'rgb(121, 204, 61)'
+]
+
 async function loadRefNameMap(
   assembly: Assembly,
   adapterId: string,
@@ -153,6 +186,16 @@ export default function assemblyFactory(assemblyConfigType: IAnyType) {
       get rpcManager() {
         return getParent(self, 2).rpcManager
       },
+      get refNameColors() {
+        const colors = readConfObject(
+          self.configuration,
+          'refNameColors',
+        ) as string[]
+        if (colors.length === 0) {
+          return refNameColors
+        }
+        return colors
+      },
     }))
     .views(self => ({
       getCanonicalRefName(refName: string) {
@@ -172,6 +215,13 @@ export default function assemblyFactory(assemblyConfigType: IAnyType) {
         throw new Error(
           `unknown reference sequence name ${refName}, this reference does not appear in the assembly`,
         )
+      },
+      getRefNameColor(refName: string) {
+        const idx = self.refNames?.findIndex(r => r === refName)
+        if (idx === undefined || idx === -1) {
+          return undefined
+        }
+        return self.refNameColors[idx % self.refNameColors.length]
       },
       isValidRefName(refName: string) {
         if (!self.allRefNames)
