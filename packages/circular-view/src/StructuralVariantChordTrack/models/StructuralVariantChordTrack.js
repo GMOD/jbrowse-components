98581db846c97ffbe20301b7f9e2acc985fd4d8c
Robert Buels <rbuels@gmail.com>
2020-05-22 16:20:37 -0700

    make onChordClick for chord tracks be a config callback

--- packages/circular-view/src/StructuralVariantChordTrack/models/StructuralVariantChordTrack.js
+++ packages/circular-view/src/StructuralVariantChordTrack/models/StructuralVariantChordTrack.js
@@ -1,7 +1,45 @@
+const defaultOnChordClick = function(feature, self, pluginManager) {
+  const { jbrequire } = pluginManager
+  const { getConf } = jbrequire('@gmod/jbrowse-core/configuration')
+  const { getContainingView, getSession } = jbrequire('@gmod/jbrowse-core/util')
+  const { resolveIdentifier } = jbrequire('mobx-state-tree')
+
+  const session = getSession(self)
+  session.setSelection(feature)
+  const view = getContainingView(self)
+  const viewType = pluginManager.getViewType('BreakpointSplitView')
+  const viewSnapshot = viewType.snapshotFromBreakendFeature(feature, view)
+  const tracks = getConf(self, 'configRelationships')
+    .map(entry => {
+      const type = pluginManager.pluggableConfigSchemaType('track')
+      const trackConfig = resolveIdentifier(type, session, entry.target)
+      return trackConfig
+        ? {
+            type: trackConfig.type,
+            height: 100,
+            configuration: trackConfig.trackId,
+            selectedRendering: '',
+          }
+        : null
+    })
+    .filter(f => !!f)
+
+  // add the specific evidence tracks to the LGVs in the split view
+  viewSnapshot.views[0].tracks = tracks
+  viewSnapshot.views[1].tracks = tracks
+
+  // try to center the offsetPx
+  viewSnapshot.views[0].offsetPx -= view.width / 2 + 100
+  viewSnapshot.views[1].offsetPx -= view.width / 2 + 100
+  viewSnapshot.featureData = feature.data
+
+  session.addView('BreakpointSplitView', viewSnapshot)
+}
+
 export default pluginManager => {
   const { jbrequire } = pluginManager
 
-  const { types, resolveIdentifier } = jbrequire('mobx-state-tree')
+  const { types } = jbrequire('mobx-state-tree')
   const { getConf, ConfigurationSchema, ConfigurationReference } = jbrequire(
     '@gmod/jbrowse-core/configuration',
   )
@@ -25,6 +63,13 @@ export default pluginManager => {
     {
       adapter: pluginManager.pluggableConfigSchemaType('adapter'),
       renderer: pluginManager.pluggableConfigSchemaType('renderer'),
+      onChordClick: {
+        type: 'boolean',
+        description:
+          'callback that should be run when a chord in the track is clicked',
+        defaultValue: defaultOnChordClick.toString(),
+        functionSignature: ['feature', 'track', 'pluginManager'],
+      },
     },
     { baseConfiguration: ChordTrackConfigSchema, explicitlyTyped: true },
   )
@@ -132,36 +177,7 @@ export default pluginManager => {
       },
 
       onChordClick(feature) {
-        const session = getSession(self)
-        session.setSelection(feature)
-        const view = getContainingView(self)
-        const viewType = pluginManager.getViewType('BreakpointSplitView')
-        const viewSnapshot = viewType.snapshotFromBreakendFeature(feature, view)
-        const tracks = getConf(self, 'configRelationships')
-          .map(entry => {
-            const type = pluginManager.pluggableConfigSchemaType('track')
-            const trackConfig = resolveIdentifier(type, session, entry.target)
-            return trackConfig
-              ? {
-                  type: trackConfig.type,
-                  height: 100,
-                  configuration: trackConfig.trackId,
-                  selectedRendering: '',
-                }
-              : null
-          })
-          .filter(f => !!f)
-
-        // add the specific evidence tracks to the LGVs in the split view
-        viewSnapshot.views[0].tracks = tracks
-        viewSnapshot.views[1].tracks = tracks
-
-        // try to center the offsetPx
-        viewSnapshot.views[0].offsetPx -= view.width / 2 + 100
-        viewSnapshot.views[1].offsetPx -= view.width / 2 + 100
-        viewSnapshot.featureData = feature.data
-
-        session.addView('BreakpointSplitView', viewSnapshot)
+        getConf(self, 'onChordClick', [feature, self, pluginManager])
       },
     }))
 
