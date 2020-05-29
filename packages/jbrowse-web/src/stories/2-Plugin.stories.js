45f7532d20a73535957119cc6d7c19fc3c3db9c4
Colin <colin.diesh@gmail.com>
2020-05-29 04:46:11 -0400

    Flesh out story

--- packages/jbrowse-web/src/stories/2-Plugin.stories.js
+++ packages/jbrowse-web/src/stories/2-Plugin.stories.js
@@ -2,12 +2,37 @@ import React from 'react'
 import { action } from '@storybook/addon-actions'
 import { Button } from '@storybook/react/demo'
 import PluginManager from '@gmod/jbrowse-core/PluginManager'
+import corePlugins from '../corePlugins'
+import JBrowse from '../JBrowse'
+import JBrowseRootModelFactory from '../rootModel'
+import configSnapshot from '../../test_data/volvox/config.json'
 
 export default {
   title: 'PluginManager',
   component: Button,
 }
 
+function getPluginManager(initialState) {
+  const pluginManager = new PluginManager(corePlugins.map(P => new P()))
+  pluginManager.createPluggableElements()
+
+  const JBrowseRootModel = JBrowseRootModelFactory(pluginManager)
+  const rootModel = JBrowseRootModel.create({
+    jbrowse: initialState || configSnapshot,
+    assemblyManager: {},
+  })
+  if (rootModel.jbrowse && rootModel.jbrowse.savedSessions.length) {
+    const { name } = rootModel.jbrowse.savedSessions[0]
+    rootModel.activateSession(name)
+  } else {
+    rootModel.setDefaultSession()
+  }
+  pluginManager.setRootModel(rootModel)
+
+  pluginManager.configure()
+  return pluginManager
+}
 export const Text = () => {
-  return <Button onClick={action('clicked')}>Hello Button</Button>
+  const pluginManager = getPluginManager({})
+  return <JBrowse pluginManager={pluginManager} />
 }
