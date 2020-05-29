e21615328084257929479527e5b132955f58f293
Colin <colin.diesh@gmail.com>
2020-05-29 19:12:09 -0400

    Simpler use of mocking query params

--- packages/jbrowse-web/src/Loader.test.tsx
+++ packages/jbrowse-web/src/Loader.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
 import { TextDecoder, TextEncoder } from 'fastestsmallesttextencoderdecoder'
