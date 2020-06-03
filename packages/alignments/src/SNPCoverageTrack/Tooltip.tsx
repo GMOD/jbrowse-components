f00e43096b334aaae01b77c260967de502345393
Colin <colin.diesh@gmail.com>
2020-06-03 12:34:57 -0400

    Use normal tooltip for snpcoverage

--- packages/alignments/src/SNPCoverageTrack/Tooltip.tsx
+++ packages/alignments/src/SNPCoverageTrack/Tooltip.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useRef } from 'react'
 import PropTypes from 'prop-types'
-import Popper from '@material-ui/core/Popper'
+import MUITooltip from '@material-ui/core/Tooltip'
 import Paper from '@material-ui/core/Paper'
 import { observer } from 'mobx-react'
 import { makeStyles } from '@material-ui/core/styles'
@@ -27,47 +27,46 @@ const useStyles = makeStyles(theme => ({
 }))
 
 function TooltipContents({ feature }: { feature: Feature }) {
+  if (!feature) return null
   const info = feature.get('snpinfo')
   const total = info
     ? info[info.map((e: any) => e.base).indexOf('total')].score
     : 0
   const condId = info && info.length >= 5 ? 'smallInfo' : 'info' // readjust table size to fit all
   return (
-    <Paper>
-      <table>
-        <thead>
-          <tr>
-            <th id={condId}>Base</th>
-            <th id={condId}>Count</th>
-            <th id={condId}>% of Total</th>
-            <th id={condId}>Strands</th>
-          </tr>
-        </thead>
-        <tbody>
-          {(info || []).map((mismatch: any) => {
-            const { base, score, strands } = mismatch
-            return (
-              <tr key={base}>
-                <td id={condId}>{base.toUpperCase()}</td>
-                <td id={condId}>{score}</td>
-                <td id={condId}>
-                  {base === 'total'
-                    ? '---'
-                    : `${Math.floor((score / total) * 100)}%`}
-                </td>
-                <td id={condId}>
-                  {base === 'total'
-                    ? '---'
-                    : (strands['+']
-                        ? `+:${strands['+']} ${strands['-'] ? `,\t` : `\t`} `
-                        : ``) + (strands['-'] ? `-:${strands['-']}` : ``)}
-                </td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
-    </Paper>
+    <table>
+      <thead>
+        <tr>
+          <th id={condId}>Base</th>
+          <th id={condId}>Count</th>
+          <th id={condId}>% of Total</th>
+          <th id={condId}>Strands</th>
+        </tr>
+      </thead>
+      <tbody>
+        {(info || []).map((mismatch: any) => {
+          const { base, score, strands } = mismatch
+          return (
+            <tr key={base}>
+              <td id={condId}>{base.toUpperCase()}</td>
+              <td id={condId}>{score}</td>
+              <td id={condId}>
+                {base === 'total'
+                  ? '---'
+                  : `${Math.floor((score / total) * 100)}%`}
+              </td>
+              <td id={condId}>
+                {base === 'total'
+                  ? '---'
+                  : (strands['+']
+                      ? `+:${strands['+']} ${strands['-'] ? `,\t` : `\t`} `
+                      : ``) + (strands['-'] ? `-:${strands['-']}` : ``)}
+              </td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
   )
 }
 
@@ -88,43 +87,29 @@ const Tooltip = observer(
   }) => {
     const { featureUnderMouse } = model
     const classes = useStyles()
-    const ref = useRef<HTMLDivElement>(null)
 
     return (
       <>
-        {ref.current && featureUnderMouse ? (
-          <>
-            <Popper
-              placement="right-start"
-              className={classes.popper}
-              anchorEl={ref.current}
-              modifiers={{
-                offset: {
-                  enabled: true,
-                  offset: '0, 10',
-                },
-              }}
-              open
-            >
-              <TooltipContents feature={featureUnderMouse} />
-            </Popper>
-            <div
-              className={classes.hoverVertical}
-              style={{ left: mouseCoord[0], height }}
-            />
-          </>
-        ) : null}
-
-        <div
-          ref={ref}
-          style={{
-            position: 'absolute',
-            left: mouseCoord[0],
-            top: 0,
-          }}
+        <MUITooltip
+          placement="right-start"
+          className={classes.popper}
+          open={Boolean(featureUnderMouse)}
+          title={<TooltipContents feature={featureUnderMouse} />}
         >
-          {' '}
-        </div>
+          <div
+            style={{
+              position: 'absolute',
+              left: mouseCoord[0],
+              top: 0,
+            }}
+          >
+            {' '}
+          </div>
+        </MUITooltip>
+        <div
+          className={classes.hoverVertical}
+          style={{ left: mouseCoord[0], height }}
+        />
       </>
     )
   },
