90f073cc3e440d27240c282b718f243e3073df26
Colin <colin.diesh@gmail.com>
2020-06-05 14:47:33 -0400

    Add PileupTrack component for drawing sort text

---
+++ packages/alignments/src/PileupTrack/components/PileupTrack.tsx
@@ -0,0 +1,54 @@
+import { observer } from 'mobx-react'
+import { getConf } from '@gmod/jbrowse-core/configuration'
+import React, { useEffect } from 'react'
+import { ResizeHandle } from '@gmod/jbrowse-core/ui'
+import { AlignmentsTrackModel } from '../model'
+
+function AlignmentsTrackComponent({ model }: { model: AlignmentsTrackModel }) {
+  const { PileupTrack, SNPCoverageTrack, showPileup, showCoverage } = model
+
+  // determine height of the model when toggling pileuptrack
+  useEffect(() => {
+    SNPCoverageTrack.setHeight(!showPileup ? model.height : 40)
+  }, [SNPCoverageTrack, model, showPileup])
+
+  return (
+    <div
+      data-testid={`track-${getConf(model, 'trackId')}`}
+      style={{ position: 'relative' }}
+    >
+      <div data-testid="Blockset-snpcoverage">
+        {showCoverage ? (
+          <SNPCoverageTrack.ReactComponent model={SNPCoverageTrack} />
+        ) : null}
+      </div>
+      <ResizeHandle
+        onDrag={delta => {
+          if (SNPCoverageTrack) {
+            SNPCoverageTrack.setHeight(SNPCoverageTrack.height + delta)
+            return delta
+          }
+          return 0
+        }}
+        style={{
+          position: 'absolute',
+          top: showCoverage ? SNPCoverageTrack.height + 2 : 0,
+          height: 3,
+        }}
+      />
+
+      <div
+        data-testid="Blockset-pileup"
+        style={{
+          position: 'absolute',
+          top: showCoverage ? SNPCoverageTrack.height + 5 : 0,
+          height: 3,
+        }}
+      >
+        {showPileup ? <PileupTrack.ReactComponent model={PileupTrack} /> : null}
+      </div>
+    </div>
+  )
+}
+
+export default observer(AlignmentsTrackComponent)
