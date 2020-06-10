aa1ac68b4bd07ef468a3e65312d86dae26dfed1a
Garrett Stevens <stevens.garrett.j@gmail.com>
2020-06-10 11:53:12 -0600

    Use node env and jest matcher for cli tests

--- packages/jbrowse-cli/src/commands/hello.test.ts
+++ packages/jbrowse-cli/src/commands/hello.test.ts
@@ -1,17 +1,21 @@
-import { expect, test } from '@oclif/test'
+/**
+ * @jest-environment node
+ */
+
+import { test } from '@oclif/test'
 
 describe('hello', () => {
   test
     .stdout()
     .command(['hello'])
     .it('runs hello', ctx => {
-      expect(ctx.stdout).to.contain('hello world')
+      expect(ctx.stdout).toMatch('hello world')
     })
 
   test
     .stdout()
     .command(['hello', '--name', 'jeff'])
     .it('runs hello --name jeff', ctx => {
-      expect(ctx.stdout).to.contain('hello jeff')
+      expect(ctx.stdout).toMatch('hello jeff')
     })
 })
