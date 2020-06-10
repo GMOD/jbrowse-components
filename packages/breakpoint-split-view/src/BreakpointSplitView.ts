39fbec8e4b42911d6827683fb83db01cc459c192
Colin <colin.diesh@gmail.com>
2020-06-10 11:28:10 -0400

    Restore old breakpoint split view

---
+++ packages/breakpoint-split-view/src/BreakpointSplitView.ts
@@ -0,0 +1,118 @@
+import { Feature } from '@gmod/jbrowse-core/util/simpleFeature'
+import PluginManager from '@gmod/jbrowse-core/PluginManager'
+import { LinearGenomeViewModel } from '@gmod/jbrowse-plugin-linear-genome-view/src/LinearGenomeView'
+import BreakpointSplitViewComponent from './components/BreakpointSplitView'
+import BreakpointSplitViewModel from './model'
+
+export default (pluginManager: PluginManager) => {
+  const { lib, load } = pluginManager
+  const ViewType = lib['@gmod/jbrowse-core/pluggableElementTypes/ViewType']
+  const { getSession } = lib['@gmod/jbrowse-core/util']
+
+  const ReactComponent = load(BreakpointSplitViewComponent)
+  const { stateModel } = load(BreakpointSplitViewModel)
+
+  class BreakpointSplitViewType extends ViewType {
+    snapshotFromBreakendFeature(feature: Feature, view: LinearGenomeViewModel) {
+      const breakendSpecification = (feature.get('ALT') || [])[0]
+      const startPos = feature.get('start')
+      let endPos
+      const bpPerPx = 10
+
+      // TODO: Figure this out for multiple assembly names
+      const { assemblyName } = view.displayedRegions[0]
+      const assembly = getSession(view).assemblyManager.get(assemblyName)
+      const { getCanonicalRefName } = assembly as any
+      const featureRefName = getCanonicalRefName(feature.get('refName'))
+
+      const topRegion = view.displayedRegions.find(
+        (f: { refName: string }) => f.refName === String(featureRefName),
+      )
+
+      let mateRefName: string | undefined
+      let startMod = 0
+      let endMod = 0
+
+      if (breakendSpecification) {
+        // a VCF breakend feature
+        if (breakendSpecification === '<TRA>') {
+          const INFO = feature.get('INFO') || []
+          endPos = INFO.END[0] - 1
+          mateRefName = getCanonicalRefName(INFO.CHR2[0])
+        } else {
+          const matePosition = breakendSpecification.MatePosition.split(':')
+          endPos = parseInt(matePosition[1], 10) - 1
+          mateRefName = getCanonicalRefName(matePosition[0])
+          if (breakendSpecification.Join === 'left') startMod = -1
+          if (breakendSpecification.MateDirection === 'left') endMod = -1
+        }
+
+        // if (breakendSpecification.Join === 'left') {
+        // marker -1, else 0
+
+        // if (breakendSpecification.MateDirection === 'left') {
+        // marker -1, else 0
+      } else if (feature.get('mate')) {
+        // a generic 'mate' feature
+        const mate = feature.get('mate')
+        mateRefName = getCanonicalRefName(mate.refName)
+        endPos = mate.start
+      }
+
+      if (!mateRefName) {
+        console.warn(
+          `unable to resolve mate refName ${mateRefName} in reference genome`,
+        )
+        return {}
+      }
+
+      const bottomRegion = view.displayedRegions.find(
+        f => f.refName === String(mateRefName),
+      )
+
+      if (!topRegion || !bottomRegion) {
+        console.warn(
+          `unable to find the refName for the top or bottom of the breakpoint view`,
+        )
+        return {}
+      }
+
+      const topMarkedRegion = [{ ...topRegion }, { ...topRegion }]
+      const bottomMarkedRegion = [{ ...bottomRegion }, { ...bottomRegion }]
+      topMarkedRegion[0].end = startPos + startMod
+      topMarkedRegion[1].start = startPos + startMod
+      bottomMarkedRegion[0].end = endPos + endMod
+      bottomMarkedRegion[1].start = endPos + endMod
+      const snapshot = {
+        type: 'BreakpointSplitView',
+        views: [
+          {
+            type: 'LinearGenomeView',
+            displayedRegions: topMarkedRegion,
+            hideHeader: true,
+            bpPerPx,
+            offsetPx: (topRegion.start + feature.get('start')) / bpPerPx,
+          },
+          {
+            type: 'LinearGenomeView',
+            displayedRegions: bottomMarkedRegion,
+            hideHeader: true,
+            bpPerPx,
+            offsetPx: (bottomRegion.start + endPos) / bpPerPx,
+          },
+        ],
+        displayName: `${
+          feature.get('name') || feature.get('id') || 'breakend'
+        } split detail`,
+      }
+      return snapshot
+    }
+  }
+
+  // @ts-ignore
+  return new BreakpointSplitViewType({
+    name: 'BreakpointSplitView',
+    stateModel,
+    ReactComponent,
+  })
+}
---
+++ packages/breakpoint-split-view/src/components/BreakpointSplitView.test.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { createTestSession } from '@gmod/jbrowse-web/src/rootModel'
+import sizeMe from 'react-sizeme'
+import BreakpointSplitView from './BreakpointSplitView'
+
+sizeMe.noPlaceholders = true
+
+const assemblyConf = {
+  name: 'volMyt1',
+  sequence: {
+    trackId: 'sequenceConfigId',
+    adapter: {
+      type: 'FromConfigAdapter',
+      features: [
+        {
+          refName: 'ctgA',
+          uniqueId: 'firstId',
+          start: 0,
+          end: 10,
+          seq: 'cattgttgcg',
+        },
+      ],
+    },
+  },
+}
+
+// mock warnings to avoid unnecessary outputs
+beforeEach(() => {
+  jest.spyOn(console, 'warn').mockImplementation(() => {})
+})
+
+afterEach(() => {
+  console.warn.mockRestore()
+})
+describe('BreakpointSplitView genome view component', () => {
+  it('renders with an empty model', async () => {
+    const session = createTestSession()
+    session.addAssemblyConf(assemblyConf)
+    session.addView('BreakpointSplitView', {
+      views: [
+        {
+          type: 'LinearGenomeView',
+          id: 'lgv1',
+          offsetPx: 0,
+          bpPerPx: 1,
+          tracks: [],
+          displayedRegions: [
+            { assemblyName: 'volMyt1', refName: 'ctgA', start: 0, end: 5 },
+          ],
+        },
+        {
+          type: 'LinearGenomeView',
+          id: 'lgv2',
+          tracks: [],
+          displayedRegions: [
+            { assemblyName: 'volMyt1', refName: 'ctgA', start: 5, end: 10 },
+          ],
+        },
+      ],
+      configuration: {},
+    })
+    const model = session.views[0]
+    const SplitView = new BreakpointSplitView(session.pluginManager)
+    const { container, findAllByText } = render(<SplitView model={model} />)
+    await findAllByText('No tracks active.')
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
