907417fd63bd985a6190cb36db30b2ba44ef9c0e
Colin <colin.diesh@gmail.com>
2020-06-12 15:12:55 -0400

    Testing mdx

--- packages/jbrowse-web/.storybook/main.js
+++ packages/jbrowse-web/.storybook/main.js
@@ -1,3 +1,88 @@
+const plugin = require('rescripts-use-yarn-workspaces')
+const postcssNormalize = require('postcss-normalize')
+const webpack_conf = require('react-scripts/config/webpack.config')
+const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent')
+
+
+const imageInlineSizeLimit = parseInt(
+  process.env.IMAGE_INLINE_SIZE_LIMIT || '10000'
+);
+
+const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
+const sassRegex = /\.(scss|sass)$/;
+const sassModuleRegex = /\.module\.(scss|sass)$/;
+
+const ext = [
+  'web.mjs',
+  'mjs',
+  'web.js',
+  'js',
+  'web.ts',
+  'ts',
+  'web.tsx',
+  'tsx',
+  'json',
+  'web.jsx',
+  'jsx',
+  'mdx'
+];
+
+// common function to get style loaders
+const getStyleLoaders = (cssOptions, preProcessor) => {
+  const loaders = [
+    require.resolve('style-loader'),
+    {
+      loader: require.resolve('css-loader'),
+      options: cssOptions,
+    },
+    {
+      // Options for PostCSS as we reference these options twice
+      // Adds vendor prefixing based on your specified browser support in
+      // package.json
+      loader: require.resolve('postcss-loader'),
+      options: {
+        // Necessary for external CSS imports to work
+        // https://github.com/facebook/create-react-app/issues/2677
+        ident: 'postcss',
+        plugins: () => [
+          require('postcss-flexbugs-fixes'),
+          require('postcss-preset-env')({
+            autoprefixer: {
+              flexbox: 'no-2009',
+            },
+            stage: 3,
+          }),
+          // Adds PostCSS Normalize as the reset css with default options,
+          // so that it honors browserslist config in package.json
+          // which in turn let's users customize the target behavior as per their needs.
+          postcssNormalize(),
+        ],
+        sourceMap: true
+      },
+    },
+  ];
+  if (preProcessor) {
+    loaders.push(
+      {
+        loader: require.resolve('resolve-url-loader'),
+        options: {
+          sourceMap: true,
+        },
+      },
+      {
+        loader: require.resolve(preProcessor),
+        options: {
+          sourceMap: true,
+        },
+      }
+    );
+  }
+  return loaders;
+};
+
+
+
 /**
  * based on https://github.com/linonetwo/rescript-worker-loader, but with
  * custom resolving of babel-plugin-named-asset-import
@@ -6,13 +91,12 @@
 const getCacheIdentifier = require('react-dev-utils/getCacheIdentifier')
 
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  stories: ['../src/**/*.stories.(mdx|js)'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-docs',
     '@storybook/addon-links',
-    'rescripts-use-yarn-workspaces/storybook',
   ],
   webpackFinal: config => {
     const isEnvProduction = config.mode === 'production'
@@ -85,7 +169,116 @@ module.exports = {
         },
       ],
     })
+    const _ = webpack_conf('development')
+    return plugin({
+      ...config,
+      resolve: { ...config.resolve, extensions:ext.map(ext => `.${ext}`) },
+      module: {
+        ...config.module,
+        rules: [
+          {
+            oneOf: [
+              {
+                test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                loader: require.resolve('url-loader'),
+                options: {
+                  limit: imageInlineSizeLimit,
+                  name: 'static/media/[name].[hash:8].[ext]',
+                },
+              },
+              // "postcss" loader applies autoprefixer to our CSS.
+              // "css" loader resolves paths in CSS and adds assets as dependencies.
+              // "style" loader turns CSS into JS modules that inject <style> tags.
+              // In production, we use MiniCSSExtractPlugin to extract that CSS
+              // to a file, but in development "style" loader enables hot editing
+              // of CSS.
+              // By default we support CSS Modules with the extension .module.css
+              {
+                test: cssRegex,
+                exclude: cssModuleRegex,
+                use: getStyleLoaders({
+                  importLoaders: 1,
+                  sourceMap: true,
+                }),
+                // Don't consider CSS imports dead code even if the
+                // containing package claims to have no side effects.
+                // Remove this when webpack adds a warning or an error for this.
+                // See https://github.com/webpack/webpack/issues/6571
+                sideEffects: true,
+              },
+              // Adds support for CSS Modules (https://github.com/css-modules/css-modules)
+              // using the extension .module.css
+              {
+                test: cssModuleRegex,
+                use: getStyleLoaders({
+                  importLoaders: 1,
+                  sourceMap: true,
+                  modules: {
+                    getLocalIdent: getCSSModuleLocalIdent,
+                  },
+                }),
+              },
+              // Opt-in support for SASS (using .scss or .sass extensions).
+              // By default we support SASS Modules with the
+              // extensions .module.scss or .module.sass
+              {
+                test: sassRegex,
+                exclude: sassModuleRegex,
+                use: getStyleLoaders(
+                  {
+                    importLoaders: 3,
+                    sourceMap: true,
+                  },
+                  'sass-loader'
+                ),
+                // Don't consider CSS imports dead code even if the
+                // containing package claims to have no side effects.
+                // Remove this when webpack adds a warning or an error for this.
+                // See https://github.com/webpack/webpack/issues/6571
+                sideEffects: true,
+              },
+              // Adds support for CSS Modules, but using SASS
+              // using the extension .module.scss or .module.sass
+              {
+                test: sassModuleRegex,
+                use: getStyleLoaders(
+                  {
+                    importLoaders: 3,
+                    sourceMap: true,
+                    modules: {
+                      getLocalIdent: getCSSModuleLocalIdent,
+                    },
+                  },
+                  'sass-loader'
+                ),
+              },
+    {
+      test: /\.mdx?$/,
+      use: ['babel-loader', '@mdx-js/loader']
+    },
+              // "file" loader makes sure those assets get served by WebpackDevServer.
+            // When you `import` an asset, you get its (virtual) filename.
+            // In production, they would get copied to the `build` folder.
+            // This loader doesn't use a "test" so it will catch all modules
+            // that fall through the other loaders.
+            {
+              loader: require.resolve('file-loader'),
+              // Exclude `js` files to keep "css" loader working as it injects
+              // its runtime that would otherwise be processed through "file" loader.
+              // Also exclude `html` and `json` extensions so they get processed
+              // by webpacks internal loaders.
+              exclude: [/\.(js|mjs|jsx|ts|tsx|ejs|mdx)$/, /\.html$/, /\.json$/],
+              options: {
+                name: 'static/media/[name].[hash:8].[ext]',
+              },
+            },
+            // ** STOP ** Are you adding a new loader?
+            // Make sure to add the new loader(s) before the "file" loader.
+            ]
+          }
+        ]
+      }
+    })
 
-    return config
   },
 }
