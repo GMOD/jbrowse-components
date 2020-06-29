9f7d1d7e5fbbd3c9c7b1bb47d8607633bf1f4e81
Colin <colin.diesh@gmail.com>
2020-06-29 16:27:59 -0400

    Clamp SVG values to the current block area

--- packages/svg/src/SvgFeatureRenderer/components/Box.js
+++ packages/svg/src/SvgFeatureRenderer/components/Box.js
@@ -6,7 +6,8 @@ import ReactPropTypes from 'prop-types'
 import React from 'react'
 
 function Box(props) {
-  const { feature, config, featureLayout, selected } = props
+  const { feature, region, config, featureLayout, selected, bpPerPx } = props
+  const screenWidth = (region.end - region.start) / bpPerPx
 
   const color1 = readConfObject(config, 'color1', [feature])
   let emphasizedColor1
@@ -19,15 +20,20 @@ function Box(props) {
 
   const { left, top, width, height } = featureLayout.absolute
 
+  // clamp the SVG boxes to the current block area
+  const x = Math.max(left, 0)
+  const diff = x - left
+  const w = width - diff
+
   // the below Math.min/Math.max 10000px limits are because SVG produces
   // silent non-rendered elements if it's like millions of px
   // renders -50000 to screenwidth (up to 100000) +50000
   return (
     <rect
       data-testid={feature.id()}
-      x={Math.max(left, -50000)}
+      x={x}
       y={top}
-      width={Math.min(Math.max(width, 1), 200000)}
+      width={Math.min(w, screenWidth)}
       height={height}
       fill={selected ? emphasizedColor1 : color1}
       stroke={selected ? color2 : undefined}
