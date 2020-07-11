94d60bbb659d46ca3681f9c6e4a7a7cff88bedcc
Colin <colin.diesh@gmail.com>
2020-07-11 09:49:05 -0400

    Remove statsCache which was already removed from wiggle, the stats are based on dynamicblocks so are very rarely cache-hit

--- packages/alignments/src/SNPCoverageAdapter/SNPCoverageAdapter.ts
+++ packages/alignments/src/SNPCoverageAdapter/SNPCoverageAdapter.ts
@@ -6,11 +6,8 @@ import { Region } from '@gmod/jbrowse-core/util/types'
 import { ObservableCreate } from '@gmod/jbrowse-core/util/rxjs'
 import SimpleFeature, { Feature } from '@gmod/jbrowse-core/util/simpleFeature'
 import { toArray } from 'rxjs/operators'
-import AbortablePromiseCache from 'abortable-promise-cache'
-import QuickLRU from '@gmod/jbrowse-core/util/QuickLRU'
 import {
   blankStats,
-  FeatureStats,
   rectifyStats,
   scoresToStats,
 } from '@gmod/jbrowse-plugin-wiggle/src/statsUtil'
@@ -75,24 +72,6 @@ export default (pluginManager: PluginManager) => {
   return class SNPCoverageAdapter extends BaseFeatureDataAdapter {
     private subadapter: BaseFeatureDataAdapter
 
-    private statsCache = new AbortablePromiseCache({
-      cache: new QuickLRU({ maxSize: 1000 }),
-      fill: async (
-        args: {
-          region: Region
-          bpPerPx: number
-        },
-        abortSignal?: AbortSignal,
-      ): Promise<FeatureStats> => {
-        const { region, bpPerPx } = args
-        const feats = this.getFeatures(region, {
-          signal: abortSignal,
-          basesPerSpan: bpPerPx,
-        })
-        return scoresToStats(region, feats)
-      },
-    })
-
     public constructor(
       config: Instance<typeof MyConfigSchema>,
       getSubAdapter: getSubAdapterType,
@@ -109,13 +88,8 @@ export default (pluginManager: PluginManager) => {
     }
 
     public getRegionStats(region: Region, opts: BaseOptions = {}) {
-      const { refName, start, end } = region
-      const { bpPerPx, signal } = opts
-      return this.statsCache.get(
-        `${refName}_${start}_${end}_${bpPerPx}`,
-        { region, bpPerPx: bpPerPx || 0 },
-        signal,
-      )
+      const feats = this.getFeatures(region, opts)
+      return scoresToStats(region, feats)
     }
 
     public async getMultiRegionStats(
@@ -188,8 +162,8 @@ export default (pluginManager: PluginManager) => {
       }, opts.signal)
     }
 
-    async getRefNames() {
-      return this.subadapter.getRefNames()
+    async getRefNames(opts: BaseOptions = {}) {
+      return this.subadapter.getRefNames(opts)
     }
 
     freeResources(/* { region } */): void {}
