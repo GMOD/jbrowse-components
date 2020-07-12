5b22e2587321a68ed74b5f36ee57aca0f58c32a9
peterkxie <peterkxie@yahoo.com>
2020-07-12 13:25:56 -0700

    add 'jbrowse create' and 'jbrowse update' CLI commands (#1050)
    * create the two commands, comments
    
    * start coding run, write test file
    
    * start w the fetch -> unzip
    
    * fix up checkPath errors, get a real example zip to download to file, test out using pako to unzip
    
    * finish create, upgrade, and tests for both. TODO: determine the final URL for fetching
    
    * make create/upgrade link dynamic by fetching version number, add flags
    
    * improve error messages
    
    * naming
    
    * hide placeholder
    
    * naming
    
    * more text fixes
    
    * add description string to latest version
    
    * need to use dummy calls in tests
    
    * most tests mocked
    
    * allow endpoint to be called twice
    
    * persist, use initial version and cleanup nock after
    
    * persist and cleanup in upgrade
    
    * no need for testdir
    
    * make the before all also async
    
    * remove beforeAll, afterall asyncs and done
    
    * del over rmdir
    
    * see if skipping add-assembly passes
    
    * comment out to skip
    
    * bring add-assembly back, add cwd to create/upgrade tests
    
    * remove usage, make create and upgrade fetch from github releases rather than aws, upgrade now can specify a version to upgrade to
    
    * fix test to work with filter, change jbversion to tag
    
    * formatting
    
    * use full flag name

---
+++ packages/jbrowse-cli/src/commands/create.ts
@@ -0,0 +1,202 @@
+import { Command, flags } from '@oclif/command'
+import * as fs from 'fs'
+import { promises as fsPromises } from 'fs'
+import * as path from 'path'
+import fetch from 'node-fetch'
+import * as unzip from 'unzipper'
+
+interface GithubRelease {
+  tag_name: string
+  prerelease: boolean
+  assets: [
+    {
+      browser_download_url: string
+    },
+  ]
+}
+export default class Create extends Command {
+  static description = 'Downloads and installs the latest JBrowse 2 release'
+
+  static examples = [
+    '$ jbrowse create /path/to/new/installation',
+    '$ jbrowse create /path/to/new/installation --force',
+    '$ jbrowse create /path/to/new/installation --url url.com/directjbrowselink.zip',
+    '$ jbrowse create /path/to/new/installation --tag JBrowse-2@v0.0.1',
+    '$ jbrowse create --listVersion',
+  ]
+
+  static args = [
+    {
+      name: 'localPath',
+      required: true,
+      description: `Location where JBrowse 2 will be installed`,
+    },
+  ]
+
+  static flags = {
+    help: flags.help({ char: 'h' }),
+    force: flags.boolean({
+      char: 'f',
+      description:
+        'Overwrites existing JBrowse 2 installation if present in path',
+    }),
+    listVersions: flags.boolean({
+      char: 'l',
+      description: 'Lists out all versions of JBrowse 2',
+    }),
+    url: flags.string({
+      char: 'u',
+      description: 'A direct URL to a JBrowse 2 release',
+    }),
+    tag: flags.string({
+      char: 't',
+      description:
+        'Version of JBrowse 2 to install. Format is JBrowse-2@v1.2.3. Defaults to latest',
+    }),
+  }
+
+  async run() {
+    const { args: runArgs, flags: runFlags } = this.parse(Create)
+    const { localPath: argsPath } = runArgs as { localPath: string }
+    this.debug(`Want to install path at: ${argsPath}`)
+
+    const { force, url, listVersions, tag } = runFlags
+
+    if (listVersions) {
+      try {
+        const versions = (await this.fetchGithubVersions()).map(
+          (version: GithubRelease) => version.tag_name,
+        )
+        this.log(`All JBrowse versions: ${versions.join(', ')}`)
+        this.exit()
+      } catch (error) {
+        this.error(error)
+      }
+    }
+
+    // mkdir will do nothing if dir exists
+    try {
+      await fsPromises.mkdir(argsPath, { recursive: true })
+    } catch (error) {
+      this.error(error)
+    }
+
+    if (!force) await this.checkPath(argsPath)
+
+    let locationUrl
+    let versionRes
+    if (url) locationUrl = url
+    else {
+      try {
+        versionRes = await this.fetchGithubVersions()
+      } catch (error) {
+        this.error(error)
+      }
+      const versionObj = tag
+        ? versionRes.find((version: GithubRelease) => version.tag_name === tag)
+        : versionRes[0]
+
+      locationUrl = versionObj
+        ? versionObj.assets[0].browser_download_url
+        : this.error(
+            'Could not find version specified. Use --listVersions to see all available versions',
+            { exit: 40 },
+          )
+    }
+
+    let response
+    try {
+      response = await fetch(locationUrl, {
+        method: 'GET',
+      })
+    } catch (error) {
+      this.error(error)
+    }
+    if (!response.ok)
+      this.error(`Failed to fetch JBrowse2 from server`, { exit: 50 })
+
+    if (url && response.headers.get('content-type') !== 'application/zip')
+      this.error(
+        'The URL provided does not seem to be a JBrowse installation URL',
+      )
+
+    response.body
+      .pipe(unzip.Parse())
+      .on('entry', async entry => {
+        const { path: fileName, type } = entry
+        if (type === 'Directory') {
+          try {
+            await fsPromises.mkdir(path.join(argsPath, fileName), {
+              recursive: true,
+            })
+          } catch (error) {
+            this.error(error)
+          }
+        }
+        entry.pipe(fs.createWriteStream(path.join(argsPath, fileName)))
+      })
+      .on('error', err => {
+        fs.unlink(argsPath, () => {})
+        this.error(
+          `Failed to download JBrowse 2 with ${err}. Please try again later`,
+        )
+      })
+      .on('close', () => {
+        this.log(`Your JBrowse 2 setup has been created at ${argsPath}`)
+      })
+  }
+
+  async checkPath(userPath: string) {
+    let allFiles
+    try {
+      allFiles = await fsPromises.readdir(userPath)
+    } catch (error) {
+      this.error('Directory does not exist', { exit: 20 })
+    }
+    if (allFiles.length > 0)
+      this.error(
+        `${userPath} This directory has existing files and could cause conflicts with create. Please choose another directory or use the force flag to overwrite existing files`,
+        { exit: 10 },
+      )
+  }
+
+  async fetchGithubVersions() {
+    let versionResponse
+    try {
+      versionResponse = await fetch(
+        'https://api.github.com/repos/GMOD/jbrowse-components/releases',
+        {
+          method: 'GET',
+        },
+      )
+    } catch (error) {
+      this.error(error)
+    }
+
+    if (!versionResponse.ok) this.error('Failed to fetch version from server')
+    // use all release only if there are only pre-release in repo
+    const allReleaseArray = (
+      await versionResponse.json()
+    ).filter((release: GithubRelease) =>
+      release.tag_name.includes('JBrowse-2@v'),
+    )
+
+    const releaseArray = allReleaseArray.filter(
+      (release: GithubRelease) => release.prerelease === false,
+    )
+
+    return releaseArray.length === 0 ? allReleaseArray : releaseArray
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async catch(error: any) {
+    if (error.parse && error.parse.output.flags.listVersions) {
+      const versions = (await this.fetchGithubVersions()).map(
+        (version: GithubRelease) => version.tag_name,
+      )
+      this.log(`All JBrowse versions: ${versions.join(', ')}`)
+      this.exit()
+    }
+    throw error
+  }
+}
