3dcc13cc01ca08e57bf616f583f257f79bc3503e
Colin <colin.diesh@gmail.com>
2020-08-11 23:45:15 -0400

    Add a import form where arbitrary number of species can be added

--- packages/linear-comparative-view/src/LinearSyntenyView/components/ImportForm.tsx
+++ packages/linear-comparative-view/src/LinearSyntenyView/components/ImportForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { observer } from 'mobx-react'
+import AddIcon from '@material-ui/icons/Add'
 import { getSnapshot } from 'mobx-state-tree'
 import { getSession, when } from '@gmod/jbrowse-core/util'
 import Button from '@material-ui/core/Button'
@@ -8,6 +9,7 @@ import Grid from '@material-ui/core/Grid'
 import MenuItem from '@material-ui/core/MenuItem'
 import TextField from '@material-ui/core/TextField'
 import { makeStyles } from '@material-ui/core/styles'
+import IconButton from '@material-ui/core/IconButton'
 import { LinearSyntenyViewModel } from '../model'
 
 const useStyles = makeStyles(theme => ({
@@ -23,12 +25,49 @@ const useStyles = makeStyles(theme => ({
     paddingBottom: theme.spacing(1),
   },
 }))
-
+const FormRow = observer(
+  ({
+    model,
+    selected,
+    onChange,
+    error,
+  }: {
+    model: LinearSyntenyViewModel
+    selected: number
+    onChange: (arg0: number) => void
+    error?: string
+  }) => {
+    const classes = useStyles()
+    const { assemblyNames } = getSession(model) as { assemblyNames: string[] }
+    return (
+      <Grid container item justify="center" spacing={2} alignItems="center">
+        <TextField
+          select
+          variant="outlined"
+          value={assemblyNames[selected] ? selected : ''}
+          onChange={event => {
+            onChange(Number(event.target.value))
+          }}
+          error={Boolean(error)}
+          disabled={Boolean(error)}
+          margin="dense"
+          className={classes.importFormEntry}
+        >
+          {assemblyNames.map((name, idx) => (
+            <MenuItem key={name} value={idx}>
+              {name}
+            </MenuItem>
+          ))}
+        </TextField>
+      </Grid>
+    )
+  },
+)
 const ImportForm = observer(({ model }: { model: LinearSyntenyViewModel }) => {
   const classes = useStyles()
-  const [selectedAssemblyIdx1, setSelectedAssemblyIdx1] = useState(0)
-  const [selectedAssemblyIdx2, setSelectedAssemblyIdx2] = useState(0)
+  const [selected, setSelected] = useState([0, 0])
   const [error, setError] = useState('')
+  const [numRows, setNumRows] = useState(2)
   const { assemblyNames } = getSession(model) as { assemblyNames: string[] }
   if (!assemblyNames.length) {
     setError('No configured assemblies')
@@ -36,96 +75,64 @@ const ImportForm = observer(({ model }: { model: LinearSyntenyViewModel }) => {
 
   async function onOpenClick() {
     const { assemblyManager } = getSession(model)
-    const asm1 = assemblyNames[selectedAssemblyIdx1]
-    const asm2 = assemblyNames[selectedAssemblyIdx2]
-    const assembly1 = assemblyManager.get(asm1)
-    const assembly2 = assemblyManager.get(asm2)
-    if (assembly1 && assembly2) {
-      await when(() => Boolean(assembly1.regions) && Boolean(assembly2.regions))
-      if (assembly1.regions && assembly2.regions) {
-        const regions1 = getSnapshot(assembly1.regions)
-        const regions2 = getSnapshot(assembly2.regions)
-        model.setViews([
-          {
-            type: 'LinearGenomeView',
-            bpPerPx: 1,
-            offsetPx: 0,
-            hideHeader: true,
-            displayedRegions: regions1,
-          },
-          {
-            type: 'LinearGenomeView',
-            bpPerPx: 1,
-            offsetPx: 0,
-            hideHeader: true,
-            displayedRegions: regions2,
-          },
-        ])
-        model.views.forEach(view => view.showAllRegions())
-      }
-    }
+
+    model.setViews(
+      // @ts-ignore
+      await Promise.all(
+        selected
+          .map(async selection => {
+            const assembly = assemblyManager.get(assemblyNames[selection])
+            if (assembly) {
+              await when(() => Boolean(assembly.regions))
+              return {
+                type: 'LinearGenomeView',
+                bpPerPx: 1,
+                offsetPx: 0,
+                hideHeader: true,
+                // @ts-ignore
+                displayedRegions: getSnapshot(assembly.regions),
+              }
+            }
+            return null
+          })
+          .filter(f => !!f),
+      ),
+    )
+
+    model.views.forEach(view => view.showAllRegions())
   }
 
   return (
     <Container className={classes.importFormContainer}>
-      <Grid container spacing={1} justify="center" alignItems="center">
-        <Grid item>
-          <TextField
-            select
-            variant="outlined"
-            value={
-              assemblyNames[selectedAssemblyIdx1] && !error
-                ? selectedAssemblyIdx1
-                : ''
-            }
-            onChange={event => {
-              setSelectedAssemblyIdx1(Number(event.target.value))
-            }}
-            label="Assembly"
-            helperText={error || 'Select assembly to view'}
-            error={Boolean(error)}
-            disabled={Boolean(error)}
-            margin="normal"
-            className={classes.importFormEntry}
-          >
-            {assemblyNames.map((name, idx) => (
-              <MenuItem key={name} value={idx}>
-                {name}
-              </MenuItem>
-            ))}
-          </TextField>
-        </Grid>
-        <Grid item>
-          <TextField
-            select
-            variant="outlined"
-            value={
-              assemblyNames[selectedAssemblyIdx2] && !error
-                ? selectedAssemblyIdx2
-                : ''
-            }
-            onChange={event => {
-              setSelectedAssemblyIdx2(Number(event.target.value))
-            }}
-            label="Assembly"
-            helperText={error || 'Select assembly to view'}
-            error={Boolean(error)}
-            disabled={Boolean(error)}
-            margin="normal"
-            className={classes.importFormEntry}
-          >
-            {assemblyNames.map((name, idx) => (
-              <MenuItem key={name} value={idx}>
-                {name}
-              </MenuItem>
-            ))}
-          </TextField>
-        </Grid>
-        <Grid item>
-          <Button onClick={onOpenClick} variant="contained" color="primary">
-            Open
-          </Button>
-        </Grid>
+      <p style={{ textAlign: 'center' }}>Select assemblies for synteny view</p>
+      {[...new Array(numRows)].map((element, index) => (
+        <FormRow
+          key={`row_${index}_${selected[index]}`}
+          error={error}
+          selected={selected[index]}
+          onChange={val => {
+            const copy = selected.slice(0)
+            copy[index] = val
+            setSelected(copy)
+          }}
+          model={model}
+        />
+      ))}
+
+      <Grid container item justify="center" spacing={4} alignItems="center">
+        Add another assembly...
+        <IconButton
+          onClick={() => setNumRows(rows => rows + 1)}
+          color="primary"
+        >
+          <AddIcon />
+        </IconButton>
+      </Grid>
+
+      <Grid container item justify="center" spacing={4} alignItems="center">
+        <Button onClick={onOpenClick} variant="contained" color="primary">
+          Open
+        </Button>
       </Grid>
     </Container>
   )
