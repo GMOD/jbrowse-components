0fb2051a85fa35db00fff92f60808be9436d28ce
Colin <colin.diesh@gmail.com>
2020-08-25 19:05:58 -0400

    Change tabular data to spreadsheet view

--- packages/spreadsheet-view/src/index.ts
+++ packages/spreadsheet-view/src/index.ts
@@ -18,7 +18,7 @@ export default class SpreadsheetViewPlugin extends Plugin {
   configure(pluginManager: PluginManager) {
     if (isAbstractMenuManager(pluginManager.rootModel)) {
       pluginManager.rootModel.appendToSubMenu(['File', 'Add'], {
-        label: 'Tabular data',
+        label: 'Spreadsheet view',
         icon: ViewComfyIcon,
         onClick: (session: AbstractViewContainer) => {
           session.addView('SpreadsheetView', {})
