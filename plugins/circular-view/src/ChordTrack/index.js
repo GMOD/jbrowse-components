a07af33507cf4dc8ae7854a5de2357b93b20516b
Garrett Stevens <stevens.garrett.j@gmail.com>
2020-09-11 15:56:16 -0600

    Build Circular View plugin

--- plugins/circular-view/src/ChordTrack/index.js
+++ plugins/circular-view/src/ChordTrack/index.js
@@ -1,10 +1,13 @@
+import ChordTrackComponentFactory from './components/ChordTrack'
+import ChordTrackModelFactory from './models/ChordTrack'
+
 export default ({ jbrequire }) => {
   const TrackType = jbrequire(
     '@gmod/jbrowse-core/pluggableElementTypes/TrackType',
   )
 
-  const ReactComponent = jbrequire(require('./components/ChordTrack'))
-  const { stateModel, configSchema } = jbrequire(require('./models/ChordTrack'))
+  const ReactComponent = jbrequire(ChordTrackComponentFactory)
+  const { stateModel, configSchema } = jbrequire(ChordTrackModelFactory)
 
   return new TrackType({
     name: 'ChordTrack',
