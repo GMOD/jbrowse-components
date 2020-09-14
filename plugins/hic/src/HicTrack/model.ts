db11ff9891a208fc53d95b2dd9d0924b76904199
Colin <colin.diesh@gmail.com>
2020-09-14 18:41:51 -0400

    Testing

--- plugins/hic/src/HicTrack/model.ts
+++ plugins/hic/src/HicTrack/model.ts
@@ -2,6 +2,7 @@ import {
   ConfigurationReference,
   getConf,
 } from '@gmod/jbrowse-core/configuration'
+import { getParentRenderProps } from '@gmod/jbrowse-core/util/tracks'
 import { blockBasedTrackModel } from '@gmod/jbrowse-plugin-linear-genome-view'
 import { types } from 'mobx-state-tree'
 import { AnyConfigurationSchemaType } from '@gmod/jbrowse-core/configuration/configurationSchema'
@@ -20,5 +21,26 @@ export default (configSchema: AnyConfigurationSchemaType) =>
     .views(self => ({
       get blockType() {
         return 'dynamicBlocks'
+      },
+      get rendererTypeName() {
+        return 'HicRenderer'
+      },
+
+      /**
+       * the react props that are passed to the Renderer when data
+       * is rendered in this track
+       */
+      get renderProps() {
+        // view -> [tracks] -> [blocks]
+        const config = getConf(self, 'renderer')
+        // rendererType.configSchema.create(
+        //   getConf(self, ['renderers', self.rendererTypeName]) || {},
+        // )
+        return {
+          ...self.composedRenderProps,
+          ...getParentRenderProps(self),
+          config,
+          trackModel: self,
+        }
       },
     }))
