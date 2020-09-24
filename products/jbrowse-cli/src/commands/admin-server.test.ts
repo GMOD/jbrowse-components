b7c26477cfceabd62d56c1857981f19b07fee1b1
Elliot Hershberg <eahershberg@gmail.com>
2020-09-23 17:04:00 -0700

    Update admin-server test suite

--- products/jbrowse-cli/src/commands/admin-server.test.ts
+++ products/jbrowse-cli/src/commands/admin-server.test.ts
@@ -4,12 +4,46 @@
 
 import fs from 'fs'
 import * as path from 'path'
+import fetch from 'node-fetch'
 import { setup } from '../testUtil'
 
 const fsPromises = fs.promises
 
 const defaultConfig = {
   assemblies: [],
+  configuration: {},
+  connections: [],
+  defaultSession: {
+    name: 'New Session',
+  },
+  tracks: [],
+}
+
+const testConfig = path.join(
+  __dirname,
+  '..',
+  '..',
+  'test',
+  'data',
+  'test_config.json',
+)
+
+const testConfigContents = {
+  assemblies: [
+    {
+      name: 'testAssembly',
+      sequence: {
+        type: 'testSequenceTrack',
+        trackId: '',
+        adapter: {
+          type: 'testSeqAdapter',
+          twoBitLocation: {
+            uri: 'test.2bit',
+          },
+        },
+      },
+    },
+  ],
   configuration: {},
   connections: [],
   defaultSession: {
@@ -34,20 +68,8 @@ const setupWithCreate = setup.do(async ctx => {
   )
 })
 
-// make sure if no config written, default gets written
-
-// testing port
-
-// fails w/ wrong or no adminKey
-
-// succesful test: URL is correct
-
-// test by making change to root model (figure out how to change rootModel, or just do POST directly)
-
-// successful test: if correct should match what expected (try w/ out nock first)
-
-// remember to kill server in tests (afterEach)
-
+// need to use --forceExit if running just this test suite to kill express servers
+// (doesn't seem to be an issue when running entire test suite)
 describe('admin-server', () => {
   setupWithCreate
     .do(async () => {
@@ -57,12 +79,105 @@ describe('admin-server', () => {
     .exit(10)
     .it('fails if no manifest.json found in cwd')
   setupWithCreate
-    .command(['admin-server'])
+    .command(['admin-server', '--port', '9091'])
     .it('creates a default config', async ctx => {
       const contents = await fsPromises.readFile(
         path.join(ctx.dir, 'config.json'),
         { encoding: 'utf8' },
       )
-      expect(JSON.parse(contents).toEqual(defaultConfig))
+      expect(JSON.parse(contents)).toEqual(defaultConfig)
+    })
+  setupWithCreate
+    .do(async ctx => {
+      await fsPromises.copyFile(
+        testConfig,
+        path.join(ctx.dir, path.basename(testConfig)),
+      )
+
+      await fsPromises.rename(
+        path.join(ctx.dir, path.basename(testConfig)),
+        path.join(ctx.dir, 'config.json'),
+      )
+    })
+    .command(['admin-server', '--port', '9092'])
+    .it('does not overwrite an existing config', async ctx => {
+      const contents = await fsPromises.readFile(
+        path.join(ctx.dir, 'config.json'),
+        { encoding: 'utf8' },
+      )
+      expect(JSON.parse(contents)).toEqual(testConfigContents)
+    })
+  setupWithCreate
+    .command(['admin-server'])
+    .it('uses port 9090 if not specified', async ctx => {
+      expect(ctx.stdoutWrite.mock.calls[0][0]).toMatch(
+        /Navigate to http:\/\/localhost:9090\?adminKey=[a-zA-Z0-9]{10,12} to configure your JBrowse installation graphically\./,
+      )
+    })
+  setupWithCreate
+    .command(['admin-server', '--port', '-10'])
+    .catch('-10 is not a valid port')
+    .it('throws an error with a negative port')
+  setupWithCreate
+    .command(['admin-server', '--port', '66666'])
+    .catch('66666 is not a valid port')
+    .it('throws an error with a port greater than 65535')
+  setupWithCreate
+    .command(['admin-server', '--port', '9093'])
+    .it('notifies the user if adminKey is incorrect', async () => {
+      const payload = { adminKey: 'badKey' }
+      const response = await fetch('http://localhost:9093/updateConfig', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      })
+      expect(response.status).toBe(403)
+      expect(await response.text()).toBe('Admin key does not match')
+    })
+  setupWithCreate
+    .command(['admin-server', '--port', '9094'])
+    .it('writes the config to disk if adminKey is valid', async ctx => {
+      // grab the correct admin key from URL
+      const adminKey = ctx.stdoutWrite.mock.calls[0][0].match(
+        /adminKey=([a-zA-Z0-9]{10,12}) /,
+      )[1]
+      const config = { foo: 'bar' }
+      const payload = { adminKey, config }
+      const response = await fetch('http://localhost:9094/updateConfig', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      })
+      const contents = await fsPromises.readFile(
+        path.join(ctx.dir, 'config.json'),
+        { encoding: 'utf8' },
+      )
+      expect(await response.text()).toBe('Config written to disk')
+      expect(JSON.parse(contents)).toEqual(config)
+    })
+  setupWithCreate
+    .command(['admin-server', '--port', '9095'])
+    .do(async () => {
+      await fsPromises.chmod('config.json', '444')
+    })
+    .it('throws an error if unable to write to config.json', async ctx => {
+      const adminKey = ctx.stdoutWrite.mock.calls[0][0].match(
+        /adminKey=([a-zA-Z0-9]{10,12}) /,
+      )[1]
+      const config = { foo: 'bar' }
+      const payload = { adminKey, config }
+      const response = await fetch('http://localhost:9095/updateConfig', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      })
+      expect(response.status).toBe(500)
+      expect(await response.text()).toBe('Could not write config file')
     })
 })
