856ff73631dc6faf67a3b633b59dcf4d90a897ff
Colin <colin.diesh@gmail.com>
2020-09-29 11:40:50 -0400

    Add scaleFactor to the linked actions

--- plugins/breakpoint-split-view/src/model.ts
+++ plugins/breakpoint-split-view/src/model.ts
@@ -241,7 +241,8 @@ export default function stateModelFactory(pluginManager: any) {
               args: any[]
             }) => {
               if (self.linkViews) {
-                if (['horizontalScroll', 'zoomTo'].includes(name)) {
+                const actions = ['horizontalScroll', 'zoomTo', 'setScaleFactor']
+                if (actions.includes(name) && path) {
                   this.onSubviewAction(name, path, args)
                 }
               }
