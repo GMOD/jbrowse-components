8fbc33fa90335e9407e3a3ca2ebe1584decb99ea
Colin <colin.diesh@gmail.com>
2020-10-11 10:07:42 -0400

    Add blog_template

--- scripts/release.sh
+++ scripts/release.sh
@@ -15,12 +15,12 @@ NOTES=`cat $1`
 DATE=$(date +"%Y-%m-%d")
 BLOGPOST_FILENAME=products/website/blog/$(date +"%Y-%m-%d")-release.md
 
-yarn run lerna-publish $2
+# yarn run lerna-publish $2
 
 ## Blogpost run after lerna publish, to get the accurate tags
 JBROWSE_WEB_TAG=$(git tag --sort=-creatordate -l "@gmod/jbrowse-web*"|head -n1)
-JBROWSE_DESKTOP_TAG=$(git tag --sort=-creatordate -l "@gmod/jbrowse-desktop*"|head -n1)
-DATE=$DATE NOTES=$NOTES perl -p -i -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' < build/blog_template.txt > $BLOGPOST_FILENAME
-git add $BLOGPOST_FILENAME
-git commit -m "[update docs] release"
+JBROWSE_DESKTOP_TAG=$(git tag --sort=-creatordate -l "jbrowse-desktop*"|head -n1)
+DATE=$DATE NOTES=$NOTES perl -p -i -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' < scripts/blog_template.txt > $BLOGPOST_FILENAME
+# git add $BLOGPOST_FILENAME
+# git commit -m "[update docs] release"
 
