db1b54599cdc5f33c352adb59fbd193b57e191ba
Colin <colin.diesh@gmail.com>
2020-10-11 19:29:27 -0400

    Silence useLayoutEffect test error globally

--- products/jbrowse-web/src/tests/util.js
+++ products/jbrowse-web/src/tests/util.js
@@ -87,14 +87,3 @@ export function setup() {
 
 // eslint-disable-next-line no-native-reassign,no-global-assign
 window = Object.assign(window, { innerWidth: 800 })
-
-const originalError = console.error
-
-// this is here to silence a warning related to useStaticRendering
-// xref https://github.com/GMOD/jbrowse-components/issues/1277
-jest.spyOn(console, 'error').mockImplementation((...args) => {
-  if (typeof args[0] === 'string' && args[0].includes('useLayoutEffect')) {
-    return undefined
-  }
-  return originalError.call(console, args)
-})
