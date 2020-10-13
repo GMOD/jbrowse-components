b2fc19d0b4da7e9a9adb3346c2a1b3a4615ea236
Colin <colin.diesh@gmail.com>
2020-10-13 15:29:53 -0400

    Unused

--- plugins/legacy-jbrowse/src/JBrowse1Connection/jb1ConfigLoad.ts
+++ plugins/legacy-jbrowse/src/JBrowse1Connection/jb1ConfigLoad.ts
@@ -21,11 +21,11 @@ function isLocalPathLocation(
 }
 
 export async function fetchJb1(
-  dataRoot: JBLocation = { uri: '', baseUri: '' },
+  dataRoot: JBLocation = { uri: '' },
   baseConfig: Config = {
     include: ['{dataRoot}/trackList.json', '{dataRoot}/tracks.conf'],
   },
-  baseConfigRoot: JBLocation = { uri: '', baseUri: '' },
+  baseConfigRoot: JBLocation = { uri: '' },
 ): Promise<Config> {
   const protocol = 'uri' in dataRoot ? 'uri' : 'localPath'
   const dataRootReg = JSON.parse(JSON.stringify(dataRoot))
@@ -201,7 +201,6 @@ async function loadIncludes(inputConfig: Config): Promise<Config> {
         include.cacheBuster = inputConfig.cacheBuster
         const includedData = await fetchConfigFile({
           uri: new URL(include.url, sourceUrl).href,
-          baseUri: '',
         })
         return loadRecur(includedData, newUpstreamConf)
       },
