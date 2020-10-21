254f43ddc28c573d88b3d42a522ad6d076bd5d2a
Colin <colin.diesh@gmail.com>
2020-10-21 11:33:06 -0400

    Add a baseColor and color for coloring the hi-c track

--- plugins/hic/src/HicRenderer/configSchema.ts
+++ plugins/hic/src/HicRenderer/configSchema.ts
@@ -3,14 +3,18 @@ import { ConfigurationSchema } from '@jbrowse/core/configuration'
 export default ConfigurationSchema(
   'HicRenderer',
   {
+    baseColor: {
+      type: 'color',
+      description: 'base color to be used in the hic alignment',
+      defaultValue: 'red',
+    },
     color: {
       type: 'color',
       description: 'the color of each feature in a hic alignment',
-      defaultValue: `function(feature) {
-  var s = feature.get('strand');
-  return s === -1 ? '#8F8FD8': '#EC8B8B'
+      defaultValue: `function(count, maxScore, baseColor, lighten) {
+      return lighten(baseColor, counts/(maxScore/20))
 }`,
-      functionSignature: ['feature'],
+      functionSignature: ['count, maxScore, baseColor, lighten'],
     },
 
     maxHeight: {
