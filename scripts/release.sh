df5a94963665c73d1b00b035d7feadbd03fa608f
Colin <colin.diesh@gmail.com>
2020-10-21 17:31:20 -0400

    Update release

--- scripts/release.sh
+++ scripts/release.sh
@@ -16,14 +16,14 @@ DATE=$(date +"%Y-%m-%d")
 BLOGPOST_FILENAME=products/website/blog/$(date +"%Y-%m-%d")-release.md
 FRONTPAGE_FILENAME=products/website/src/pages/index.js
 
-# yarn run lerna-publish $2
+yarn run lerna-publish $2
 
-# ## This pushes only the @jbrowse/web tag first because a flood of tags causes
-# ## the CI system to skip the build
-# git push origin tag \"@jbrowse/web*\"
+## This pushes only the @jbrowse/web tag first because a flood of tags causes
+## the CI system to skip the build
+git push origin tag \"@jbrowse/web*\"
 
-# ## Push the rest of the tags
-# git push --follow-tags
+## Push the rest of the tags
+git push --follow-tags
 
 ## Blogpost run after lerna publish, to get the accurate tags
 
@@ -33,10 +33,10 @@ INSTANCE=https://s3.amazonaws.com/jbrowse.org/code/jb2/alpha/$JBROWSE_WEB_TAG/in
 JBROWSE_WEB_TAG=$JBROWSE_WEB_TAG JBROWSE_DESKTOP_TAG=$JBROWSE_DESKTOP_TAG DATE=$DATE NOTES=$NOTES perl -p -i -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' < scripts/blog_template.txt > $BLOGPOST_FILENAME
 
 
-INSTANCE=$INSTANCE node -p "JSON.stringify({...require('./products/website/docusaurus.config.json'), currentLink: process.env.INSTANCE }, null, 2)" > tmp.json
+INSTANCE=$INSTANCE node -p "const config = require('./products/website/docusaurus.config.json'); config.customFields.currentLink = process.env.INSTANCE; JSON.stringify(config,0,2)" > tmp.json
 mv tmp.json products/website/docusaurus.config.json
 
 
-# git add $BLOGPOST_FILENAME
-# git commit -m "[update docs] release"
+git add .
+git commit -m "[update docs] release"
 
