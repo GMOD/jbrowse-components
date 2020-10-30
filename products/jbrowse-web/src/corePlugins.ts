af1ec9c5243f5de8a688d3ad945d894bf7740fb6
Garrett Stevens <stevens.garrett.j@gmail.com>
2020-10-30 09:33:00 -0600

    Update volvox config

--- products/jbrowse-web/src/corePlugins.ts
+++ products/jbrowse-web/src/corePlugins.ts
@@ -1,15 +1,15 @@
 import Alignments from '@jbrowse/plugin-alignments'
 import Bed from '@jbrowse/plugin-bed'
-import BreakpointSplitView from '@jbrowse/plugin-breakpoint-split-view'
+// import BreakpointSplitView from '@jbrowse/plugin-breakpoint-split-view'
 import CircularView from '@jbrowse/plugin-circular-view'
 import Config from '@jbrowse/plugin-config'
 import DataManagement from '@jbrowse/plugin-data-management'
-import DotplotView from '@jbrowse/plugin-dotplot-view'
+// import DotplotView from '@jbrowse/plugin-dotplot-view'
 import Filtering from '@jbrowse/plugin-filtering'
 import Gff3Tabix from '@jbrowse/plugin-gff3'
 import LegacyJBrowse from '@jbrowse/plugin-legacy-jbrowse'
 import LinearGenomeView from '@jbrowse/plugin-linear-genome-view'
-import LinearComparativeView from '@jbrowse/plugin-linear-comparative-view'
+// import LinearComparativeView from '@jbrowse/plugin-linear-comparative-view'
 import Lollipop from '@jbrowse/plugin-lollipop'
 import Menus from '@jbrowse/plugin-menus'
 import RDF from '@jbrowse/plugin-rdf'
@@ -18,9 +18,9 @@ import SVG from '@jbrowse/plugin-svg'
 import TrackHubRegistry from '@jbrowse/plugin-trackhub-registry'
 import Variants from '@jbrowse/plugin-variants'
 import Wiggle from '@jbrowse/plugin-wiggle'
-import SpreadsheetViewPlugin from '@jbrowse/plugin-spreadsheet-view'
-import SvInspectorPlugin from '@jbrowse/plugin-sv-inspector'
-import HicPlugin from '@jbrowse/plugin-hic'
+// import SpreadsheetViewPlugin from '@jbrowse/plugin-spreadsheet-view'
+// import SvInspectorPlugin from '@jbrowse/plugin-sv-inspector'
+// import HicPlugin from '@jbrowse/plugin-hic'
 
 export default [
   Alignments,
@@ -28,12 +28,12 @@ export default [
   CircularView,
   Config,
   DataManagement,
-  DotplotView,
+  // DotplotView,
   Filtering,
   Gff3Tabix,
   LegacyJBrowse,
   LinearGenomeView,
-  LinearComparativeView,
+  // LinearComparativeView,
   Lollipop,
   Menus,
   RDF,
@@ -42,8 +42,8 @@ export default [
   TrackHubRegistry,
   Variants,
   Wiggle,
-  SpreadsheetViewPlugin,
-  SvInspectorPlugin,
-  BreakpointSplitView,
-  HicPlugin,
+  // SpreadsheetViewPlugin,
+  // SvInspectorPlugin,
+  // BreakpointSplitView,
+  // HicPlugin,
 ]
