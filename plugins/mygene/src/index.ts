a47365094347e294d65da8b454d43d806d37c8ce
Colin <colin.diesh@gmail.com>
2020-11-27 17:15:56 -0500

    Use extent of transcripts

--- plugins/mygene/src/index.ts
+++ plugins/mygene/src/index.ts
@@ -24,6 +24,108 @@ const configSchema = ConfigurationSchema(
   { explicitlyTyped: true },
 )
 
+// translate thickStart/thickEnd to utr's
+// adapted from BigBedAdapter for ucsc thickStart/thickEnd
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function cdsStartEndProcessor(feature: any) {
+  // split the blocks into UTR, CDS, and exons
+  const { thickStart, thickEnd, strand, subfeatures: children } = feature
+
+  if (!thickStart && !thickEnd) {
+    return feature
+  }
+
+  const blocks = children
+    ? children.sort(
+        (a: { start: number }, b: { start: number }) => a.start - b.start,
+      )
+    : []
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const newChildren: Record<string, any> = []
+  blocks.forEach((block: { start: number; end: number }) => {
+    const { start, end } = block
+    if (thickStart >= end) {
+      // left-side UTR
+      const prime = strand > 0 ? 'five' : 'three'
+      newChildren.push({
+        type: `${prime}_prime_UTR`,
+        start,
+        end,
+      })
+    } else if (thickStart > start && thickStart < end && thickEnd >= end) {
+      // UTR | CDS
+      const prime = strand > 0 ? 'five' : 'three'
+      newChildren.push(
+        {
+          type: `${prime}_prime_UTR`,
+          start,
+          end: thickStart,
+        },
+        {
+          type: 'CDS',
+          start: thickStart,
+          end,
+        },
+      )
+    } else if (thickStart <= start && thickEnd >= end) {
+      // CDS
+      newChildren.push({
+        type: 'CDS',
+        start,
+        end,
+      })
+    } else if (thickStart > start && thickStart < end && thickEnd < end) {
+      // UTR | CDS | UTR
+      const leftPrime = strand > 0 ? 'five' : 'three'
+      const rightPrime = strand > 0 ? 'three' : 'five'
+      newChildren.push(
+        {
+          type: `${leftPrime}_prime_UTR`,
+          start,
+          end: thickStart,
+        },
+        {
+          type: `CDS`,
+          start: thickStart,
+          end: thickEnd,
+        },
+        {
+          type: `${rightPrime}_prime_UTR`,
+          start: thickEnd,
+          end,
+        },
+      )
+    } else if (thickStart <= start && thickEnd > start && thickEnd < end) {
+      // CDS | UTR
+      const prime = strand > 0 ? 'three' : 'five'
+      newChildren.push(
+        {
+          type: `CDS`,
+          start,
+          end: thickEnd,
+        },
+        {
+          type: `${prime}_prime_UTR`,
+          start: thickEnd,
+          end,
+        },
+      )
+    } else if (thickEnd <= start) {
+      // right-side UTR
+      const prime = strand > 0 ? 'three' : 'five'
+      newChildren.push({
+        type: `${prime}_prime_UTR`,
+        start,
+        end,
+      })
+    }
+  })
+  return { ...feature, subfeatures: newChildren, type: 'mRNA' }
+}
+
+// random notes for possible email to team: cdk11a/cdk11b return pretty bad data
+// for their transcripts, so they are filtered out
 class AdapterClass extends BaseFeatureDataAdapter {
   private featureCache = new AbortablePromiseCache({
     cache: new QuickLRU({ maxSize: 100 }),
@@ -53,24 +155,22 @@ class AdapterClass extends BaseFeatureDataAdapter {
       }
       await Promise.all(
         // @ts-ignore
-        chunks.map(chunk => {
-          return this.featureCache
-            .get(
-              `${chunk.assemblyName},${chunk.refName},${chunk.start},${chunk.end}`,
-              chunk,
-              opts.signal,
-            )
-            .then(features => {
-              // @ts-ignore
-              features.forEach(feature => {
-                if (
-                  !(feature.get('start') > query.end) &&
-                  feature.get('end') >= query.start
-                ) {
-                  observer.next(feature)
-                }
-              })
-            })
+        chunks.map(async chunk => {
+          const features = await this.featureCache.get(
+            `${chunk.assemblyName},${chunk.refName},${chunk.start},${chunk.end}`,
+            chunk,
+            opts.signal,
+          )
+          // @ts-ignore
+          features.forEach(feature => {
+            if (
+              feature &&
+              !(feature.get('start') > query.end) &&
+              feature.get('end') >= query.start
+            ) {
+              observer.next(feature)
+            }
+          })
         }),
       )
 
@@ -85,7 +185,7 @@ class AdapterClass extends BaseFeatureDataAdapter {
   }) {
     const { start, end, refName } = query
     const ref = refName.startsWith('chr') ? refName : `chr${refName}`
-    const url = `https://mygene.info/v3/query?q=hg19.${ref}:${start}-${end}&fields=all&size=1000&email=colin.diesh@gmail.com`
+    const url = `https://mygene.info/v3/query?q=hg19.${ref}:${start}-${end}&size=1000&fields=all&size=1000&email=colin.diesh@gmail.com`
 
     const hg19 = 1 /// might be config+(this.config.hg19 || 0)
     const response = await fetch(url)
@@ -114,65 +214,88 @@ class AdapterClass extends BaseFeatureDataAdapter {
       }
 
       let transcriptData = [exons, exons_hg19][hg19]
+
+      // this is a weird hack because mygene.info returns features on other
+      // chromosomes that are close homologues, and the homologues aren't even
+      // clear on whether they are located on the chromosome you are querying
+      // on because it returns a set of locations of all the other homologues,
+      // so this tries to filter those out
+      if (
+        feature.map_location &&
+        !feature.map_location.match(`^${genomicPos.chr}(p|q)`)
+      ) {
+        return null
+      }
+
       if (transcriptData) {
         // @ts-ignore
         transcriptData = transcriptData.filter(transcript => {
-          return transcript.chr === refName.replace('chr', '')
+          return feature.map_location?.startsWith(transcript.chr)
         })
       }
 
-      return transcriptData
-        ? new SimpleFeature({
-            id: _id,
-            data: {
-              ...rest,
-              refName: genomicPos.chr,
-              start: genomicPos.start,
-              end: genomicPos.end,
-              strand: genomicPos.strand,
-              name: feature.symbol,
-              description: feature.name,
-              type: 'gene',
+      if (transcriptData && transcriptData.length) {
+        const transcripts = transcriptData
+          // @ts-ignore
+          .map((transcript, index) => {
+            return {
+              start: transcript.txstart,
+              end: transcript.txend,
+              name: transcript.transcript,
+              strand: transcript.strand,
+              thickStart: transcript.cdsstart,
+              thickEnd: transcript.cdsend,
               // @ts-ignore
-              subfeatures: transcriptData.map(transcript => {
-                return {
-                  start: transcript.txstart,
-                  end: transcript.txend,
-                  name: transcript.transcript,
-                  strand: transcript.strand,
-                  type: 'mRNA',
-                  // @ts-ignore
-                  subfeatures: transcript.position.map(pos => ({
-                    start: pos[0],
-                    end: pos[1],
-                    strand: transcript.strand,
-                    type: 'exon',
-                  })),
-                }
-              }),
-            },
+              subfeatures: transcript.position.map(pos => ({
+                start: pos[0],
+                end: pos[1],
+                strand: transcript.strand,
+                type: 'exon',
+              })),
+            }
           })
-        : new SimpleFeature({
-            id: _id,
-            data: {
-              ...rest,
-              refName: genomicPos.chr,
-              start: genomicPos.start,
-              end: genomicPos.end,
-              strand: genomicPos.strand,
-              name: feature.symbol,
-              description: feature.name,
-              type: 'gene',
-              subfeatures: [
-                {
-                  type: 'mRNA',
-                  start: genomicPos.start,
-                  end: genomicPos.end,
-                  strand: genomicPos.strand,
-                },
-              ],
-            },
+          // @ts-ignore
+          .filter(t => {
+            // another weird filter to avoid transcripts that are outside the
+            // range of the genomic pos. the +/-1000 added for ATAD3C, SKI2, MEGF6
+            return (
+              t.start >= genomicPos.start - 2000 &&
+              t.end <= genomicPos.end + 2000
+            )
           })
+          // @ts-ignore
+          .map(feat => {
+            return feature.type_of_gene === 'protein-coding'
+              ? cdsStartEndProcessor(feat)
+              : feat
+          })
+
+        // maybe worth reviewing but SvgFeatureRenderer has very bad behavior
+        // if subfeatures go outside of the bounds of the parent feature so
+        // this is needed
+        const [min, max] = [
+          // @ts-ignore
+          Math.min(...[genomicPos.start, ...transcripts.map(t => t.start)]),
+          // @ts-ignore
+          Math.max(...[genomicPos.end, ...transcripts.map(t => t.end)]),
+        ]
+
+        return new SimpleFeature({
+          id: _id,
+          data: {
+            ...rest,
+            refName: genomicPos.chr,
+            start: min,
+            end: max,
+            strand: genomicPos.strand,
+            name: feature.symbol,
+            description: feature.name,
+            type: 'gene',
+            subfeatures: transcripts,
+          },
+        })
+      }
+      return null
     })
   }
 
