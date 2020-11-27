f7cc6000ab09f9191c8728dd703e50f022c38ebe
Colin <colin.diesh@gmail.com>
2020-11-27 15:59:57 -0500

    Misc

---
+++ plugins/mygene/src/declare.d.ts
@@ -0,0 +1,1 @@
+declare module '@jbrowse/core/util/QuickLRU'
--- plugins/mygene/src/index.ts
+++ plugins/mygene/src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/camelcase,no-underscore-dangle */
 import AdapterType from '@jbrowse/core/pluggableElementTypes/AdapterType'
 import PluginManager from '@jbrowse/core/PluginManager'
 import Plugin from '@jbrowse/core/Plugin'
@@ -5,14 +6,12 @@ import {
   BaseFeatureDataAdapter,
   BaseOptions,
 } from '@jbrowse/core/data_adapters/BaseAdapter'
-import { NoAssemblyRegion } from '@jbrowse/core/util/types'
+import { Region } from '@jbrowse/core/util/types'
 import { ObservableCreate } from '@jbrowse/core/util/rxjs'
 import SimpleFeature, { Feature } from '@jbrowse/core/util/simpleFeature'
 import AbortablePromiseCache from 'abortable-promise-cache'
 import QuickLRU from '@jbrowse/core/util/QuickLRU'
-
 import { ConfigurationSchema } from '@jbrowse/core/configuration'
-import { AnyConfigurationModel } from '@jbrowse/core/configuration/configurationSchema'
 
 const configSchema = ConfigurationSchema(
   'MyGeneV3Adapter',
@@ -28,149 +27,138 @@ const configSchema = ConfigurationSchema(
 class AdapterClass extends BaseFeatureDataAdapter {
   private featureCache = new AbortablePromiseCache({
     cache: new QuickLRU({ maxSize: 100 }),
-    fill: async args => this.readChunk(key),
+    fill: async args => this.readChunk(args),
   })
 
   public async getRefNames(opts: BaseOptions = {}) {
     return []
   }
 
-  public getFeatures(query: NoAssemblyRegion, opts: BaseOptions = {}) {
+  public getFeatures(query: Region, opts: BaseOptions = {}) {
     return ObservableCreate<Feature>(async observer => {
-      const { refName, start, end } = query
-
-      query.toString = () => `${refName},${start},${end}`
       const chunkSize = 100000
       const s = query.start - (query.start % chunkSize)
       const e = query.end + (chunkSize - (query.end % chunkSize))
       const chunks = []
-      let chunksProcessed = 0
-      let haveError = false
       for (let start = s; start < e; start += chunkSize) {
-        const chunk = { ref: query.ref, start, end: start + chunkSize }
-        chunk.toString = () => `${refName},${start},${end}`
-        chunks.push(chunk)
-      }
-      chunks.forEach(c => {
-        this.featureCache.get(c, (f, err) => {
-          if (err && !haveError) {
-            observer.error(err)
-          }
-          haveError = haveError || err
-          if (haveError) {
-            return
-          }
-          f.forEach(feature => {
-            if (feature.get('start') > query.end) {
-            } else if (feature.get('end') >= query.start) {
-              observer.next(feature)
-            }
-          })
-          if (++chunksProcessed == chunks.length) {
-            observer.complete()
-          }
+        chunks.push({
+          refName: query.refName,
+          start,
+          end: start + chunkSize,
+          assemblyName: query.assemblyName,
         })
-      })
+      }
+      await Promise.all(
+        chunks.map(chunk => {
+          return this.featureCache
+            .get(
+              `${chunk.assemblyName},${chunk.refName},${chunk.start},${chunk.end}`,
+              chunk,
+              opts.signal,
+            )
+            .then(features => {
+              console.log({ features })
+              features.forEach(feature => {
+                if (feature.get('start') > query.end) {
+                } else if (feature.get('end') >= query.start) {
+                  observer.next(feature)
+                }
+              })
+            })
+        }),
+      )
+
+      observer.complete()
     }, opts.signal)
   }
 
   private async readChunk(query) {
     const { start, end, refName } = query
-    const url = `https://mygene.info/v3/query?q=hg19.${refName}:${start}-${end}&fields=all&size=1000&email=colin.diesh@gmail.com`
+    const ref = refName.startsWith('chr') ? refName : `chr${refName}`
+    const url = `https://mygene.info/v3/query?q=hg19.${ref}:${start}-${end}&fields=all&size=1000&email=colin.diesh@gmail.com`
 
     const hg19 = 1 /// might be config+(this.config.hg19 || 0)
-    const feats = []
     const response = await fetch(url)
     if (!response.ok) {
       throw new Error(response.statusText)
     }
-    const featuredata = await response.json()
+    const featureData = await response.json()
+    return featureData.hits.map(feature => {
+      const {
+        genomic_pos,
+        genomic_pos_hg19,
+        exons,
+        exons_hg19,
+        _id,
+        _score,
+        _license,
+        ...rest
+      } = feature
 
-    featuredata.hits.forEach(f => {
-      let genomic_pos = [f.genomic_pos, f.genomic_pos_hg19][hg19]
-      const exons = [f.exons, f.exons_hg19][hg19]
-      if (Array.isArray(genomic_pos)) {
-        genomic_pos = genomic_pos[0]
+      let genomicPos = [genomic_pos, genomic_pos_hg19][hg19]
+      if (Array.isArray(genomicPos)) {
+        genomicPos = genomicPos.find(pos => {
+          return refName.replace('chr', '') === pos.chr
+        })
       }
-      // if (f.exac) {
-      //   f.exac_nontcga = f.exac.nontcga
-      //   f.exac_nonpsych = f.exac.nonpsych
-      //   f.exac_all = f.exac.all
-      //   delete f.exac
-      // }
-      // if (f.reagent) {
-      //   Object.keys(f.reagent).forEach(key => {
-      //     f[`reagent_${key}`] = f.reagent[key]
-      //   })
-      //   delete f.reagent
-      // }
-      // if (f.reporter) {
-      //   Object.keys(f.reporter).forEach(key => {
-      //     f[`reporter_${key}`] = f.reporter[key]
-      //   })
-      //   delete f.reporter
-      // }
 
-      const { exons: trash, exons_hg19, ...rest } = f
-      const superfeat = new SimpleFeature({
-        id: f._id,
-        data: {
-          ...rest,
-          start: genomic_pos.start,
-          end: genomic_pos.end,
-          strand: genomic_pos.strand,
-          name: f.symbol,
-          description: f.name,
-          type: 'gene',
-          subfeatures: [],
-        },
-      })
-      // if (!exons) {
-      //   const feature = new SimpleFeature({
-      //     id: `${f._id}-transcript`,
-      //     data: {
-      //       start: genomic_pos.start,
-      //       end: genomic_pos.end,
-      //       strand: genomic_pos.strand,
-      //       type: 'mRNA',
-      //       name: `${f.name}-transcript`,
-      //       subfeatures: [],
-      //     },
-      //     parent: superfeat,
-      //   })
-      //   superfeat.data.subfeatures.push(feature)
-      // }
-      // const transcripts = {}
-      // if (exons) {
-      //   const transcripts = exons.map(exon => {
-      //     const tname = exon.transcript
-      //     const ts = {
-      //       start: exon.txstart,
-      //       end: exon.txend,
-      //       strand: exon.strand,
-      //       type: 'mRNA',
-      //       name: tname,
-      //       subfeatures: [],
-      //     }
-      //     const exons = exon.position.map(pos => {
-      //       return {
-      //         data: {
-      //           ...exon,
-      //           start: pos[0],
-      //           end: pos[1],
-      //           strand: genomic_pos.strand,
-      //           position: null,
-      //           txstart: null,
-      //           txend: null,
-      //           transcript: null,
-      //           type: 'exon',
-      //         },
-      //         parent: ts,
-      //       }
-      //     })
-      //   })
-      // }
-      feats.push(superfeat)
+      let transcriptData = [exons, exons_hg19][hg19]
+      if (transcriptData) {
+        transcriptData = transcriptData.filter(transcript => {
+          return transcript.chr === refName.replace('chr', '')
+        })
+      }
+
+      return transcriptData
+        ? new SimpleFeature({
+            id: _id,
+            data: {
+              ...rest,
+              refName: genomicPos.chr,
+              start: genomicPos.start,
+              end: genomicPos.end,
+              strand: genomicPos.strand,
+              name: feature.symbol,
+              description: feature.name,
+              type: 'gene',
+              subfeatures: transcriptData.map(transcript => {
+                return {
+                  start: transcript.txstart,
+                  end: transcript.txend,
+                  name: transcript.transcript,
+                  strand: transcript.strand,
+                  type: 'mRNA',
+                  subfeatures: transcript.position.map(([start, end]) => ({
+                    start,
+                    end,
+                    strand: transcript.strand,
+                    type: 'exon',
+                  })),
+                }
+              }),
+            },
+          })
+        : new SimpleFeature({
+            id: _id,
+            data: {
+              ...rest,
+              refName: genomicPos.chr,
+              start: genomicPos.start,
+              end: genomicPos.end,
+              strand: genomicPos.strand,
+              name: feature.symbol,
+              description: feature.name,
+              type: 'gene',
+              subfeatures: [
+                {
+                  type: 'mRNA',
+                  start: genomicPos.start,
+                  end: genomicPos.end,
+                  strand: genomicPos.strand,
+                },
+              ],
+            },
+          })
     })
   }
 
