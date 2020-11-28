7b4765da51a922056ddda6ee9c817770e5cfe4c8
Colin <colin.diesh@gmail.com>
2020-11-28 13:35:01 -0500

    Add datagrid

--- packages/core/BaseFeatureWidget/BaseFeatureDetail.tsx
+++ packages/core/BaseFeatureWidget/BaseFeatureDetail.tsx
@@ -8,6 +8,7 @@ import Divider from '@material-ui/core/Divider'
 import Paper from '@material-ui/core/Paper'
 import Tooltip from '@material-ui/core/Tooltip'
 import { makeStyles } from '@material-ui/core/styles'
+import { DataGrid } from '@material-ui/data-grid'
 import { observer } from 'mobx-react'
 import React, { FunctionComponent } from 'react'
 import isObject from 'is-object'
@@ -212,7 +213,19 @@ const ArrayValue = ({
     </div>
   )
 }
-
+function measureText(str: string, fontSize = 10) {
+  // prettier-ignore
+  const widths = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.2796875,0.2765625,0.3546875,0.5546875,0.5546875,0.8890625,0.665625,0.190625,0.3328125,0.3328125,0.3890625,0.5828125,0.2765625,0.3328125,0.2765625,0.3015625,0.5546875,0.5546875,0.5546875,0.5546875,0.5546875,0.5546875,0.5546875,0.5546875,0.5546875,0.5546875,0.2765625,0.2765625,0.584375,0.5828125,0.584375,0.5546875,1.0140625,0.665625,0.665625,0.721875,0.721875,0.665625,0.609375,0.7765625,0.721875,0.2765625,0.5,0.665625,0.5546875,0.8328125,0.721875,0.7765625,0.665625,0.7765625,0.721875,0.665625,0.609375,0.721875,0.665625,0.94375,0.665625,0.665625,0.609375,0.2765625,0.3546875,0.2765625,0.4765625,0.5546875,0.3328125,0.5546875,0.5546875,0.5,0.5546875,0.5546875,0.2765625,0.5546875,0.5546875,0.221875,0.240625,0.5,0.221875,0.8328125,0.5546875,0.5546875,0.5546875,0.5546875,0.3328125,0.5,0.2765625,0.5546875,0.5,0.721875,0.5,0.5,0.5,0.3546875,0.259375,0.353125,0.5890625]
+  const avg = 0.5279276315789471
+  return (
+    str
+      .split('')
+      .map(c =>
+        c.charCodeAt(0) < widths.length ? widths[c.charCodeAt(0)] : avg,
+      )
+      .reduce((cur, acc) => acc + cur) * fontSize
+  )
+}
 export const Attributes: FunctionComponent<AttributeProps> = props => {
   const { attributes, omit = [], descriptions = {} } = props
   const omits = [...omit, ...globalOmit]
@@ -222,7 +235,46 @@ export const Attributes: FunctionComponent<AttributeProps> = props => {
       {Object.entries(attributes)
         .filter(([k, v]) => v !== undefined && !omits.includes(k))
         .map(([key, value]) => {
-          if (Array.isArray(value)) {
+          if (Array.isArray(value) && value.length) {
+            const keys = Object.keys(value[0]).sort()
+            if (
+              value.length > 2 &&
+              value.every(
+                val =>
+                  isObject(val) &&
+                  Object.keys(val)
+                    .sort()
+                    .every((key, index) => key === keys[index]),
+              )
+            ) {
+              return (
+                <div
+                  key={key}
+                  style={{ height: value.length * 20 + 100, width: '100%' }}
+                >
+                  <FieldName name={key} />
+                  <DataGrid
+                    rowHeight={20}
+                    headerHeight={25}
+                    hideFooter
+                    autoHeight
+                    rows={Object.entries(value).map(([key, val]) => ({
+                      id: key,
+                      ...val,
+                    }))}
+                    columns={keys.map(val => ({
+                      field: val,
+                      width: Math.max(
+                        ...value.map(row => {
+                          const result = String(row[val])
+                          return Math.max(measureText(result, 14) + 50, 80)
+                        }),
+                      ),
+                    }))}
+                  />
+                </div>
+              )
+            }
             return (
               <ArrayValue
                 key={key}
