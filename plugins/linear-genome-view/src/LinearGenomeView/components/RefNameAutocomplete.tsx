965392e945b8e08446b0a5497aeedf12c40f5c42
teresam856 <tere486martinez@gmail.com>
2020-12-03 02:52:33 -0800

    removed list component and added test id

--- plugins/linear-genome-view/src/LinearGenomeView/components/RefNameAutocomplete.tsx
+++ plugins/linear-genome-view/src/LinearGenomeView/components/RefNameAutocomplete.tsx
@@ -5,7 +5,7 @@
 import { Region } from '@jbrowse/core/util/types'
 import { getSession } from '@jbrowse/core/util'
 import CircularProgress from '@material-ui/core/CircularProgress'
-import ListSubheader from '@material-ui/core/ListSubheader'
+// import ListSubheader from '@material-ui/core/ListSubheader'
 import TextField, { TextFieldProps as TFP } from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 import Autocomplete, {
@@ -14,82 +14,17 @@ import Autocomplete, {
 import { observer } from 'mobx-react'
 import { getSnapshot } from 'mobx-state-tree'
 import React, { useEffect } from 'react'
-import { ListChildComponentProps, VariableSizeList } from 'react-window'
+// import { ListChildComponentProps, VariableSizeList } from 'react-window'
 import { LinearGenomeViewModel } from '..'
 
 // filter for options that were fetched
 const filter = createFilterOptions({ trim: true, limit: 36 })
-
-function renderRow(props: ListChildComponentProps) {
-  const { data, index, style } = props
-  return React.cloneElement(data[index], {
-    style: {
-      ...style,
-    },
-  })
-}
-
-const OuterElementContext = React.createContext({})
-
-const OuterElementType = React.forwardRef<HTMLDivElement>((props, ref) => {
-  console.log(props)
-  const outerProps = React.useContext(OuterElementContext)
-  return <div ref={ref} {...props} {...outerProps} />
-})
-
-// Adapter for react-window
-const ListboxComponent = React.forwardRef<HTMLDivElement>(
-  function ListboxComponent(props, ref) {
-    // eslint-disable-next-line react/prop-types
-    const { children, ...other } = props
-    console.log(props)
-    const itemData = React.Children.toArray(children)
-    const itemCount = itemData.length
-    const itemSize = 36
-
-    const getChildSize = (child: React.ReactNode) => {
-      if (React.isValidElement(child) && child.type === ListSubheader) {
-        return 48
-      }
-
-      return itemSize
-    }
-
-    const getHeight = () => {
-      if (itemCount > 8) {
-        return 8 * itemSize
-      }
-      return itemData.map(getChildSize).reduce((a, b) => a + b, 0)
-    }
-
-    return (
-      <div ref={ref}>
-        <OuterElementContext.Provider value={other}>
-          <VariableSizeList
-            itemData={itemData}
-            height={getHeight()}
-            width="100%"
-            key={itemCount}
-            outerElementType={OuterElementType}
-            innerElementType="ul"
-            itemSize={(index: number) => getChildSize(itemData[index])}
-            overscanCount={5}
-            itemCount={itemCount}
-          >
-            {renderRow}
-          </VariableSizeList>
-        </OuterElementContext.Provider>
-      </div>
-    )
-  },
-)
 
 function RefNameAutocomplete({
   model,
   onSelect,
   assemblyName,
   style,
-  value,
   TextFieldProps = {},
 }: {
   model: LinearGenomeViewModel
@@ -174,6 +109,8 @@ function RefNameAutocomplete({
   return (
     <Autocomplete
       id={`refNameAutocomplete-${model.id}`}
+      ListboxProps={{ style: { maxHeight: 250 } }}
+      data-testid="autocomplete"
       autoComplete
       freeSolo
       selectOnFocus
@@ -181,11 +118,6 @@ function RefNameAutocomplete({
       disableClearable
       style={style}
       loading={loaded}
-      // ListboxComponent={
-      //   ListboxComponent as React.ComponentType<
-      //     React.HTMLAttributes<HTMLElement>
-      //   >
-      // }
       value={visibleLocStrings || ''}
       includeInputInList
       disabled={!assemblyName || !loaded}
