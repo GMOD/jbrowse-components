fd1ca295be9cb24c38289fefbc4a35212fbaad30
Colin <colin.diesh@gmail.com>
2020-12-19 21:29:30 -0500

    Attempt to improve loader error

--- products/jbrowse-web/src/Loader.tsx
+++ products/jbrowse-web/src/Loader.tsx
@@ -350,82 +350,106 @@ const Renderer = observer(
     const [, setPassword] = useQueryParam('password', StringParam)
     const { sessionError, configError, ready } = loader
     const [pm, setPluginManager] = useState<PluginManager>()
+    const [error, setError] = useState('')
+    const [snapshotError, setSnapshotError] = useState('')
     // only create the pluginManager/rootModel "on mount"
     useEffect(() => {
-      const {
-        plugins,
-        adminKey,
-        configSnapshot,
-        sessionSnapshot,
-        configPath,
-      } = loader
+      try {
+        const {
+          plugins,
+          adminKey,
+          configSnapshot,
+          sessionSnapshot,
+          configPath,
+        } = loader
 
-      if (ready) {
-        // it is ready when a session has loaded and when there is no config error
-        // Assuming that the query changes self.sessionError or self.sessionSnapshot or self.blankSession
-        const pluginManager = new PluginManager(plugins.map(P => new P()))
+        if (ready) {
+          // it is ready when a session has loaded and when there is no config error
+          // Assuming that the query changes self.sessionError or self.sessionSnapshot or self.blankSession
+          const pluginManager = new PluginManager(plugins.map(P => new P()))
 
-        pluginManager.createPluggableElements()
+          pluginManager.createPluggableElements()
 
-        const JBrowseRootModel = JBrowseRootModelFactory(
-          pluginManager,
-          !!adminKey,
-        )
+          const JBrowseRootModel = JBrowseRootModelFactory(
+            pluginManager,
+            !!adminKey,
+          )
 
-        if (loader.configSnapshot) {
-          const rootModel = JBrowseRootModel.create({
-            jbrowse: configSnapshot,
-            assemblyManager: {},
-            version: packagedef.version,
-            configPath,
-          })
+          if (loader.configSnapshot) {
+            const rootModel = JBrowseRootModel.create({
+              jbrowse: configSnapshot,
+              assemblyManager: {},
+              version: packagedef.version,
+              configPath,
+            })
 
-          // in order: saves the previous autosave for recovery, tries to load
-          // the local session if session in query, or loads the default
-          // session
-          if (sessionError) {
-            rootModel.setDefaultSession()
-            // make typescript happy by checking for session after
-            // setDefaultSession, even though we know this exists now
-            if (rootModel.session) {
-              rootModel.session.notify(
-                `Error loading session: ${sessionError.message}. If you
+            // in order: saves the previous autosave for recovery, tries to load
+            // the local session if session in query, or loads the default
+            // session
+            if (sessionError) {
+              rootModel.setDefaultSession()
+              // make typescript happy by checking for session after
+              // setDefaultSession, even though we know this exists now
+              if (rootModel.session) {
+                rootModel.session.notify(
+                  `Error loading session: ${sessionError.message}. If you
                 received this URL from another user, request that they send you
                 a session generated with the "Share" button instead of copying
                 and pasting their URL`,
-              )
-            }
-          } else if (sessionSnapshot) {
-            rootModel.setSession(loader.sessionSnapshot)
-          } else {
-            const defaultJBrowseSession = rootModel.jbrowse.defaultSession
-            if (defaultJBrowseSession?.views) {
-              if (defaultJBrowseSession.views.length > 0) {
-                rootModel.setDefaultSession()
+                )
+              }
+            } else if (sessionSnapshot) {
+              rootModel.setSession(loader.sessionSnapshot)
+            } else {
+              const defaultJBrowseSession = rootModel.jbrowse.defaultSession
+              if (defaultJBrowseSession?.views) {
+                if (defaultJBrowseSession.views.length > 0) {
+                  rootModel.setDefaultSession()
+                }
               }
             }
+
+            // send analytics
+            if (
+              rootModel &&
+              !readConfObject(
+                rootModel.jbrowse.configuration,
+                'disableAnalytics',
+              )
+            ) {
+              writeAWSAnalytics(
+                rootModel,
+                initialTimestamp,
+                initialSessionQuery,
+              )
+              writeGAAnalytics(rootModel, initialTimestamp)
+            }
+
+            // TODO use UndoManager
+            // rootModel.setHistory(
+            //   UndoManager.create({}, { targetStore: rootModel.session }),
+            // )
+            pluginManager.setRootModel(rootModel)
+            pluginManager.configure()
+            setPluginManager(pluginManager)
+
+            // automatically clear password field once loaded
+            setPassword(undefined)
           }
-
-          // send analytics
-          if (
-            rootModel &&
-            !readConfObject(rootModel.jbrowse.configuration, 'disableAnalytics')
-          ) {
-            writeAWSAnalytics(rootModel, initialTimestamp, initialSessionQuery)
-            writeGAAnalytics(rootModel, initialTimestamp)
-          }
-
-          // TODO use UndoManager
-          // rootModel.setHistory(
-          //   UndoManager.create({}, { targetStore: rootModel.session }),
-          // )
-          pluginManager.setRootModel(rootModel)
-          pluginManager.configure()
-          setPluginManager(pluginManager)
-
-          // automatically clear password field once loaded
-          setPassword(undefined)
         }
+      } catch (e) {
+        const match = e.message.match(
+          /.*at path "(.*)" snapshot `(.*)` is not assignable/,
+        )
+        // best effort to make a better error message than the default
+        // mobx-state-tree
+        if (match) {
+          setError(`Failed to load element at ${match[1]}`)
+          setSnapshotError(match[2])
+        } else {
+          setError(e.message.slice(0, 10000))
+        }
+        console.error(e)
       }
     }, [
       loader,
@@ -436,19 +460,33 @@ const Renderer = observer(
       initialSessionQuery,
     ])
 
-    if (configError) {
+    const err = configError || error
+
+    if (err) {
       return (
         <div>
           <NoConfigMessage />
-          {configError ? (
+          {err ? (
             <div
               style={{
                 border: '1px solid black',
+
                 padding: 2,
                 margin: 2,
                 backgroundColor: '#ff8888',
               }}
-            >{`${configError}`}</div>
+            >
+              {`${err}`}
+              {snapshotError ? (
+                <>
+                  {' '}
+                  ... Failed element had snapshot:
+                  <pre>
+                    {JSON.stringify(JSON.parse(snapshotError), null, 2)}
+                  </pre>
+                </>
+              ) : null}
+            </div>
           ) : null}
         </div>
       )
