4bb285b937cca81bae16421a42bb9949168317fa
Colin <colin.diesh@gmail.com>
2020-12-21 13:34:33 -0500

    Silence the useResizeContainer warn from @material-ui/data-grid, appears to only happen during test

--- config/jest/console.js
+++ config/jest/console.js
@@ -1,4 +1,5 @@
 const originalError = console.error
+const originalWarn = console.warn
 
 // this is here to silence a warning related to useStaticRendering
 // xref https://github.com/GMOD/jbrowse-components/issues/1277
@@ -7,4 +8,13 @@ jest.spyOn(console, 'error').mockImplementation((...args) => {
     return undefined
   }
   return originalError.call(console, args)
+})
+
+// this is here to silence a warning related to @material-ui/data-grid
+// not precisely sure why it warns but this error is silenced during test
+jest.spyOn(console, 'warn').mockImplementation((...args) => {
+  if (typeof args[0] === 'string' && args[0].includes('useResizeContainer')) {
+    return undefined
+  }
+  return originalWarn.call(console, args)
 })
