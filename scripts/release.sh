4d3d25f9f00fe6a470cd4ca193f92290001cec31
peterkxie <peterkxie@yahoo.com>
2021-01-12 14:18:19 -0800

    check if master is updated, yarn format

--- scripts/release.sh
+++ scripts/release.sh
@@ -19,6 +19,8 @@ BRANCH=$(git branch --show-current)
 [[ "$BRANCH" != "master" ]] && { echo "Current branch is not master, please switch to master branch" && exit 1; }
 NPMUSER=$(npm whoami)
 [[ -n "$NPMUSER" ]] || { echo "No NPM user detected, please run 'npm adduser'" && exit 1; }
+MASTERUPDATED=$(git rev-list --left-only --count origin/master...master)
+[[ "$MASTERUPDATED" != 0 ]] && { echo "Master is not up to date with origin/master. Please fetch and try again" && exit 1; }
 
 # Get the version before release from lerna.json
 PREVIOUS_VERSION=$(node --print "const lernaJson = require('./lerna.json'); lernaJson.version")
@@ -49,6 +51,7 @@ DATE=$(date +"%Y-%m-%d")
 BLOGPOST_FILENAME=website/blog/${DATE}-${RELEASE_TAG}-release.md
 RELEASE_TAG=$RELEASE_TAG DATE=$DATE NOTES=$NOTES CHANGELOG=$CHANGELOG perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' <scripts/blog_template.txt >"$BLOGPOST_FILENAME"
 
+yarn format
 git add .
 git commit --message "Prepare for $RELEASE_TAG release"
 
