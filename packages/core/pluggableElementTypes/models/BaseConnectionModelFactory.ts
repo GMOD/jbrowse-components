b612a7f37003398a89bee35252ab90ce2fa0204c
Garrett Stevens <stevens.garrett.j@gmail.com>
2021-02-03 15:53:02 -0700

    Convert BaseConnectionModel to TS

--- packages/core/pluggableElementTypes/models/BaseConnectionModelFactory.ts
+++ packages/core/pluggableElementTypes/models/BaseConnectionModelFactory.ts
@@ -1,6 +1,8 @@
-import { types } from 'mobx-state-tree'
+import { types, SnapshotOrInstance, cast } from 'mobx-state-tree'
+import PluginManager from '../../PluginManager'
+import { AnyConfigurationModel } from '../../configuration/configurationSchema'
 
-export default pluginManager => {
+export default (pluginManager: PluginManager) => {
   return types
     .model('Connection', {
       name: types.identifier,
@@ -8,18 +10,21 @@ export default pluginManager => {
     })
     .actions(self => ({
       afterAttach() {
+        // @ts-ignore
         self.connect(self.configuration)
       },
-      addTrackConf(trackConf) {
+      addTrackConf(trackConf: AnyConfigurationModel) {
         const length = self.tracks.push(trackConf)
         return self.tracks[length - 1]
       },
-      addTrackConfs(trackConfs) {
+      addTrackConfs(
+        trackConfs: NonNullable<SnapshotOrInstance<typeof self.tracks>>,
+      ) {
         const length = self.tracks.push(...trackConfs)
         return self.tracks.slice(length - 1 - trackConfs.length, length - 1)
       },
-      setTrackConfs(trackConfs) {
-        self.tracks = trackConfs
+      setTrackConfs(trackConfs: SnapshotOrInstance<typeof self.tracks>) {
+        self.tracks = cast(trackConfs)
         return self.tracks
       },
       clear() {},
