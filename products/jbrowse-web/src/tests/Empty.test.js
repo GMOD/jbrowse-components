637b6edc011c75eba2d0fa576ebfc234daa92f04
Colin <colin.diesh@gmail.com>
2021-02-05 18:48:20 -0700

    Add test

--- products/jbrowse-web/src/tests/Empty.test.js
+++ products/jbrowse-web/src/tests/Empty.test.js
@@ -1,7 +1,7 @@
 // library
 import '@testing-library/jest-dom/extend-expect'
 
-import { cleanup, fireEvent, render, waitFor } from '@testing-library/react'
+import { cleanup, render } from '@testing-library/react'
 import { toMatchImageSnapshot } from 'jest-image-snapshot'
 import React from 'react'
 import { LocalFile } from 'generic-filehandle'
@@ -11,36 +11,11 @@ import { TextEncoder } from 'fastestsmallesttextencoderdecoder'
 import { clearCache } from '@jbrowse/core/util/io/rangeFetcher'
 import { clearAdapterCache } from '@jbrowse/core/data_adapters/dataAdapterCache'
 import JBrowse from '../JBrowse'
-import { setup, getPluginManager, generateReadBuffer } from './util'
+import { setup, getPluginManager } from './util'
+import emptyConfig from '../../test_data/empty.json'
 
-window.TextEncoder = TextEncoder
-
-expect.extend({ toMatchImageSnapshot })
-
-setup()
-
-afterEach(cleanup)
-
-beforeEach(() => {
-  clearCache()
-  clearAdapterCache()
-  fetch.resetMocks()
-  fetch.mockResponse(
-    generateReadBuffer(url => {
-      return new LocalFile(require.resolve(`../../test_data/volvox/${url}`))
-    }),
-  )
-})
-
-describe('<JBrowse />', () => {
-  it('renders with an empty config', async () => {
-    const pluginManager = getPluginManager({})
-    const { findByText } = render(<JBrowse pluginManager={pluginManager} />)
-    expect(await findByText('Help')).toBeTruthy()
-  })
-  it('renders with an initialState', async () => {
-    const pluginManager = getPluginManager()
-    const { findByText } = render(<JBrowse pluginManager={pluginManager} />)
-    expect(await findByText('Help')).toBeTruthy()
-  })
+test('catches no assemblies with empty config', async () => {
+  const pluginManager = getPluginManager(emptyConfig)
+  const { findByText } = render(<JBrowse pluginManager={pluginManager} />)
+  expect(await findByText('No configured assemblies')).toBeTruthy()
 })
