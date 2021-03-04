b40c9f171748e937bfad0b49b012e41a72bf3379
Colin <colin.diesh@gmail.com>
2021-03-04 12:49:55 -0700

    Testing

--- plugins/data-management/src/AddTrackWidget/index.test.jsx
+++ plugins/data-management/src/AddTrackWidget/index.test.jsx
@@ -12,8 +12,10 @@ function setWindowLoc(loc) {
   delete window.location
   window.location = new URL(loc)
 }
-const FakeViewModel = types.model({
+const FakeViewModel = types.model('FakeView', {
+  id: types.identifier,
   type: types.literal('FakeView'),
+  assemblyNames: types.array(types.string),
 })
 
 // register a true fake view plugin because the widget.view is a true
@@ -38,13 +40,19 @@ describe('tests on an LGV type system with view.assemblyNames, using URL', () =>
     const pluginManager = new PluginManager([new FakeViewPlugin()])
     pluginManager.createPluggableElements()
     pluginManager.configure()
+
     const SessionModel = types.model({
       view: FakeViewModel,
       widget: stateModelFactory(pluginManager),
     })
 
+    // assemblyNames is defined on the view, which is done in LGV for example
+    // this is really just used for convenience to automatically fill in the
+    // assembly field in the form
+
     session = SessionModel.create({
       view: {
+        id: 'testing',
         type: 'FakeView',
         assemblyNames: ['volvox'],
       },
@@ -59,19 +67,21 @@ describe('tests on an LGV type system with view.assemblyNames, using URL', () =>
     window.location = realLocation
   })
 
-  it('adds url', () => {
+  it('adds relative URL', () => {
     const { widget } = session
     widget.setTrackData({ uri: 'volvox-sorted.bam' })
     expect(widget.trackName).toBe('volvox-sorted.bam')
     expect(widget.isFtp).toBe(false)
     expect(widget.isRelativeUrl).toBe(true)
+    expect(widget.assembly).toBe('volvox')
   })
 
-  it('adds url from wrong protocol', () => {
+  it('adds full URL', () => {
     const { widget } = session
     widget.setTrackData({ uri: 'http://google.com/volvox-sorted.bam' })
     expect(widget.trackName).toBe('volvox-sorted.bam')
     expect(widget.isRelativeUrl).toBe(false)
+    expect(widget.assembly).toBe('volvox')
   })
 
   it('test wrongProtocol returning false', () => {
@@ -101,8 +111,13 @@ describe('tests on an view without view.assemblyNames', () => {
       widget: stateModelFactory(pluginManager),
     })
 
+    // no assemblyNames on the view, just in case some view does not implement
+    // this
     session = SessionModel.create({
-      view: { type: 'FakeView' },
+      view: {
+        type: 'FakeView',
+        id: 'testing',
+      },
       widget: {
         type: 'AddTrackWidget',
         view: 'testing',
