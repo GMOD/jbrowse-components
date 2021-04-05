2c73dba349ea650147552c07af23aff6c4d86785
teresam856 <tere486martinez@gmail.com>
2021-04-05 15:50:46 -0700

    add types to http map and to crc32 implementations

--- packages/core/TextSearch/JbrowseTextSeachAdapter/httpMap.ts
+++ packages/core/TextSearch/JbrowseTextSeachAdapter/httpMap.ts
@@ -1,11 +1,10 @@
 // Adapted from https://github.com/GMOD/jbrowse/blob/master/src/JBrowse/Store/Hash.js
-import objectHash from 'object-hash'
-import CRC32 from 'crc-32'
+import { crc32 } from './Crc32'
+
 export default class HttpMap {
   constructor(args) {
     // make sure url has a trailing slash
     this.url = /\/$/.test(args.url) ? args.url : `${args.url}/`
-
     this.browser = args.browser
     //   const isElectron = typeof window !== 'undefined' && Boolean(window.electron)
     this.isElectron = args.isElectron
@@ -15,32 +14,33 @@ export default class HttpMap {
     this.ready = this.readMeta()
   }
 
-  private async loadFile(id: string) {
-    const response = await fetch(`${this.url}/${id}`)
-    return response.json()
-  }
-
   /**
    * loads meta.json file from names directory and reads number of hash_bits used
    */
-  private async readMeta() {
-    this.meta = await this.loadFile('meta.json')
-    this.meta.hash_hex_characters = Math.ceil(this.meta.hash_bits / 4)
-    return true
+  async readMeta() {
+    try {
+      this.meta = await this.loadFile('meta.json')
+      this.meta.hash_hex_characters = Math.ceil(this.meta.hash_bits / 4)
+      return true
+    } catch (err) {
+      throw Error(err)
+    }
   }
 
   /**
-   * Returns the contents of the bucket given a key
+   * Returns contents of a bucket given a key
+   * @param key - string
    */
-  async get(key) {
+  async get(key: string) {
     const bucket = await this.getBucket(key)
     return bucket[key]
   }
 
   /**
    * Returns a bucket given a key
+   * @param key - string
    */
-  async getBucket(key) {
+  async getBucket(key: string) {
     await this.ready
     const bucketIdent = this.hash(key)
     try {
@@ -55,7 +55,21 @@ export default class HttpMap {
     return {}
   }
 
-  public hexToDirPath(hex: string) {
+  /**
+   * Loads a file using the url and provided id.
+   * Returns response object with contents of the file
+   * @param id - string
+   */
+  async loadFile(id: string) {
+    const response = await fetch(`${this.url}/${id}`)
+    return response.json()
+  }
+
+  /**
+   * Returns the corresponding path of the file given a hex string
+   * @param hex - hex string
+   */
+  hexToDirPath(hex: string) {
     // zero-pad the hex string to be 8 chars if necessary
     while (hex.length < 8) hex = `0${hex}`
     hex = hex.substr(8 - this.meta.hash_hex_characters)
@@ -63,13 +77,15 @@ export default class HttpMap {
     for (let i = 0; i < hex.length; i += 3) {
       dirpath.push(hex.substring(i, i + 3))
     }
-    console.log(`${dirpath.join('/')}.json${this.meta.compress ? 'z' : ''}`)
     return `${dirpath.join('/')}.json${this.meta.compress ? 'z' : ''}`
   }
 
-  public hash(data) {
-    // TODO replace with crc32 from util in jbrowse1 legacy plugin once this gets moved to that plugin
-    const result = CRC32.str(data)
-    return result.toString(16).toLowerCase().replace('-', 'n')
+  /**
+   * Returns crc32 hash given a string.
+   * (Note: this is using JBrowse1 implementation of crc32.)
+   * @param data - string
+   */
+  hash(data: string) {
+    return crc32(data).toString(16).toLowerCase().replace('-', 'n')
   }
 }
