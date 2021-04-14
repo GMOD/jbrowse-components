5308e393a2f7382a953c4db79c9534a922429dbb
Elliot Hershberg <eahershberg@gmail.com>
2021-04-14 13:10:48 -0700

    Fetch plugin data

--- plugins/data-management/src/PluginStoreWidget/components/PluginStoreWidget.tsx
+++ plugins/data-management/src/PluginStoreWidget/components/PluginStoreWidget.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { observer, PropTypes as MobxPropTypes } from 'mobx-react'
 
 import { Typography } from '@material-ui/core'
@@ -6,6 +6,27 @@ import { Typography } from '@material-ui/core'
 import { PluginStoreModel } from '../model'
 
 function PluginStoreWidget({ model }: { model: PluginStoreModel }) {
+  const [pluginArray, setPluginArray] = useState([])
+
+  useEffect(() => {
+    if (pluginArray.length === 0) {
+      fetchPluginArray()
+    }
+  })
+
+  const fetchPluginArray = async () => {
+    const fetchResult = await fetch(
+      'https://s3.amazonaws.com/jbrowse.org/plugin-store/plugins.json',
+    )
+    if (fetchResult.status !== 200) {
+      throw new Error('Failed to fetch plugin data')
+    }
+    const array = await fetchResult.json()
+    setPluginArray(array.plugins)
+  }
+
+  console.log(pluginArray)
+
   return <Typography>Plugin Store lol</Typography>
 }
 
