913cb48b440aeb7b12bbfa4e833b6ef187255e47
Colin <colin.diesh@gmail.com>
2021-04-26 16:03:50 -0400

    Add bed.gz example

--- products/jbrowse-web/src/tests/Spreadsheet.test.js
+++ products/jbrowse-web/src/tests/Spreadsheet.test.js
@@ -1,7 +1,7 @@
 // library
 import '@testing-library/jest-dom/extend-expect'
 
-import { cleanup, fireEvent, render, waitFor } from '@testing-library/react'
+import { cleanup, fireEvent, render } from '@testing-library/react'
 import { toMatchImageSnapshot } from 'jest-image-snapshot'
 import React from 'react'
 import { LocalFile } from 'generic-filehandle'
@@ -50,5 +50,25 @@ test('opens a vcf.gz file in the spreadsheet view', async () => {
 
   fireEvent.click(await findByTestId('open_spreadsheet'))
   fireEvent.click(await findByText('ctgA:9602..9603'))
+  expect(pluginManager.rootModel.session.views.length).toBe(2)
+}, 15000)
+
+test('opens a bed.gz file in the spreadsheet view', async () => {
+  const pluginManager = getPluginManager()
+  const { findByTestId, findByText } = render(
+    <JBrowse pluginManager={pluginManager} />,
+  )
+  fireEvent.click(await findByText('File'))
+  fireEvent.click(await findByText('Add'))
+  fireEvent.click(await findByText('Spreadsheet view'))
+
+  await findByTestId('spreadsheet_view_open')
+  fireEvent.click(await findByText('URL'))
+  fireEvent.change(await findByTestId('urlInput'), {
+    target: { value: 'volvox-bed12.bed.gz' },
+  })
+
+  fireEvent.click(await findByTestId('open_spreadsheet'))
+  fireEvent.click(await findByText('ctgA:1299..9000'))
   expect(pluginManager.rootModel.session.views.length).toBe(2)
 }, 15000)
