01b2a5a5c2d68ac19d35abe39f33e66f49306529
Colin <colin.diesh@gmail.com>
2021-04-26 23:56:09 -0400

    Add await

--- products/jbrowse-web/src/tests/Spreadsheet.test.js
+++ products/jbrowse-web/src/tests/Spreadsheet.test.js
@@ -1,7 +1,7 @@
 // library
 import '@testing-library/jest-dom/extend-expect'
 
-import { cleanup, fireEvent, render } from '@testing-library/react'
+import { cleanup, fireEvent, render, waitFor } from '@testing-library/react'
 import { toMatchImageSnapshot } from 'jest-image-snapshot'
 import React from 'react'
 import { LocalFile } from 'generic-filehandle'
@@ -35,7 +35,7 @@ beforeEach(() => {
 
 test('opens a vcf.gz file in the spreadsheet view', async () => {
   const pluginManager = getPluginManager()
-  const { findByTestId, findByText } = render(
+  const { findByTestId, getByTestId, findByText } = render(
     <JBrowse pluginManager={pluginManager} />,
   )
   fireEvent.click(await findByText('File'))
@@ -47,8 +47,11 @@ test('opens a vcf.gz file in the spreadsheet view', async () => {
   fireEvent.change(await findByTestId('urlInput'), {
     target: { value: 'volvox.filtered.vcf.gz' },
   })
-
-  expect(await findByTestId('open_spreadsheet')).not.toBeDisabled()
+  await waitFor(() =>
+    expect(
+      getByTestId('open_spreadsheet').closest('button'),
+    ).not.toBeDisabled(),
+  )
   fireEvent.click(await findByTestId('open_spreadsheet'))
   fireEvent.click(await findByText('ctgA:276..277'))
   expect(pluginManager.rootModel.session.views.length).toBe(2)
@@ -56,7 +59,7 @@ test('opens a vcf.gz file in the spreadsheet view', async () => {
 
 test('opens a bed.gz file in the spreadsheet view', async () => {
   const pluginManager = getPluginManager()
-  const { findByTestId, findByText } = render(
+  const { findByTestId, getByTestId, findByText } = render(
     <JBrowse pluginManager={pluginManager} />,
   )
   fireEvent.click(await findByText('File'))
@@ -68,8 +71,11 @@ test('opens a bed.gz file in the spreadsheet view', async () => {
   fireEvent.change(await findByTestId('urlInput'), {
     target: { value: 'volvox-bed12.bed.gz' },
   })
-
-  expect(await findByTestId('open_spreadsheet')).not.toBeDisabled()
+  await waitFor(() =>
+    expect(
+      getByTestId('open_spreadsheet').closest('button'),
+    ).not.toBeDisabled(),
+  )
   fireEvent.click(await findByTestId('open_spreadsheet'))
   fireEvent.click(await findByText('ctgA:1299..9000'))
   expect(pluginManager.rootModel.session.views.length).toBe(2)
