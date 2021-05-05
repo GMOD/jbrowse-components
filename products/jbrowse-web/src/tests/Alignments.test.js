726f7c8ff35e0766376fa5aff6d3bca13092ef72
Colin <colin.diesh@gmail.com>
2021-05-05 17:51:50 -0400

    Increase timeout

--- products/jbrowse-web/src/tests/Alignments.test.js
+++ products/jbrowse-web/src/tests/Alignments.test.js
@@ -31,7 +31,7 @@ beforeEach(() => {
   )
 })
 
-const delay = { timeout: 10000 }
+const delay = { timeout: 20000 }
 
 describe('alignments track', () => {
   it('opens an alignments track', async () => {
