5a009379c872f7830de0a9c3f32e8ad1110ceecd
Colin <colin.diesh@gmail.com>
2021-05-06 01:29:15 -0400

    Allow snpcoverage track to filter on it's own

--- plugins/alignments/src/LinearSNPCoverageDisplay/models/model.ts
+++ plugins/alignments/src/LinearSNPCoverageDisplay/models/model.ts
@@ -1,4 +1,4 @@
-import { types, cast } from 'mobx-state-tree'
+import { types, cast, getParent } from 'mobx-state-tree'
 import { getConf, readConfObject } from '@jbrowse/core/configuration'
 import { linearWiggleDisplayModelFactory } from '@jbrowse/plugin-wiggle'
 import {
@@ -8,6 +8,12 @@ import {
 import PluginManager from '@jbrowse/core/PluginManager'
 import SerializableFilterChain from '@jbrowse/core/pluggableElementTypes/renderers/util/serializableFilterChain'
 import Tooltip from '../components/Tooltip'
+import {
+  colorSchemeMenu,
+  filterByMenu,
+  filterBy,
+  colorBy,
+} from '../../shared/models'
 
 // using a map because it preserves order
 const rendererTypes = new Map([['snpcoverage', 'SNPCoverageRenderer']])
@@ -24,17 +30,8 @@ const stateModelFactory = (
         type: types.literal('LinearSNPCoverageDisplay'),
         drawInterbaseCounts: types.maybe(types.boolean),
         drawIndicators: types.maybe(types.boolean),
-        filterBy: types.optional(
-          types.model({
-            flagInclude: types.optional(types.number, 0),
-            flagExclude: types.optional(types.number, 1536),
-            readName: types.maybe(types.string),
-            tagFilter: types.maybe(
-              types.model({ tag: types.string, value: types.string }),
-            ),
-          }),
-          {},
-        ),
+        colorBy,
+        filterBy,
       }),
     )
     .actions(self => ({
@@ -133,6 +130,12 @@ const stateModelFactory = (
                 self.toggleDrawInterbaseCounts()
               },
             },
+            ...(getParent(self).type !== 'LinearAlignmentsDisplay'
+              ? [colorSchemeMenu(self)]
+              : []),
+            ...(getParent(self).type !== 'LinearAlignmentsDisplay'
+              ? [filterByMenu(self)]
+              : []),
           ]
         },
         get trackMenuItems() {
