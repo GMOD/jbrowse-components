75743eefcbb6cfea6772da9c2420a7bc6ef0eb30
Colin <colin.diesh@gmail.com>
2021-05-07 19:44:26 -0400

    Add hic integration test

--- products/jbrowse-web/src/tests/BigWig.test.js
+++ products/jbrowse-web/src/tests/BigWig.test.js
@@ -10,6 +10,7 @@ import { toMatchImageSnapshot } from 'jest-image-snapshot'
 
 import { setup, generateReadBuffer, getPluginManager } from './util'
 import JBrowse from '../JBrowse'
+import hicConfig from '../../test_data/hic_integration_test.json'
 
 expect.extend({ toMatchImageSnapshot })
 setup()
@@ -86,6 +87,36 @@ describe('bigwig', () => {
     fireEvent.click(
       await findByTestId('htsTrackEntry-volvox_microarray_density'),
     )
+    const canvas = await findAllByTestId(
+      'prerendered_canvas',
+      {},
+      {
+        timeout: 10000,
+      },
+    )
+    const bigwigImg = canvas[0].toDataURL()
+    const bigwigData = bigwigImg.replace(/^data:image\/\w+;base64,/, '')
+    const bigwigBuf = Buffer.from(bigwigData, 'base64')
+    expect(bigwigBuf).toMatchImageSnapshot({
+      failureThreshold: 0.05,
+      failureThresholdType: 'percent',
+    })
+  }, 15000)
+
+  it('hic track', async () => {
+    const pluginManager = getPluginManager(hicConfig)
+    fetch.mockResponse(
+      generateReadBuffer(url => {
+        return new LocalFile(require.resolve(`../../test_data/${url}`))
+      }),
+    )
+
+    const state = pluginManager.rootModel
+    const { findByTestId, findAllByTestId } = render(
+      <JBrowse pluginManager={pluginManager} />,
+    )
+    state.session.views[0].setNewView(5000, 0)
+    fireEvent.click(await findByTestId('htsTrackEntry-hic_test'))
     const canvas = await findAllByTestId(
       'prerendered_canvas',
       {},
