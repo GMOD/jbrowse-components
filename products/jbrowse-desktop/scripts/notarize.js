35ddd4656569b17a7baab80be48caa9d0e7d012a
Elliot Hershberg <eahershberg@gmail.com>
2021-06-04 12:57:40 -0700

    Revert "Avoid notarizing unless on CI"
    This reverts commit f331704ab30a48d186bf2734968478338519e803.

--- products/jbrowse-desktop/scripts/notarize.js
+++ products/jbrowse-desktop/scripts/notarize.js
@@ -3,7 +3,7 @@ const { notarize } = require('electron-notarize')
 
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context
-  if (!process.env.GITHUB_ACTIONS || electronPlatformName !== 'darwin') {
+  if (electronPlatformName !== 'darwin') {
     return
   }
 
