bdcc0d305202b307e791d03c017ac1be9538e2a1
Colin <colin.diesh@gmail.com>
2021-06-22 10:27:16 -0400

    Add a template for making a plain-text VCF adapter

---
+++ plugins/variants/src/VcfAdapter/VcfAdapter.ts
@@ -0,0 +1,94 @@
+import {
+  BaseFeatureDataAdapter,
+  BaseOptions,
+} from '@jbrowse/core/data_adapters/BaseAdapter'
+import { FileLocation, Region } from '@jbrowse/core/util/types'
+import { openLocation } from '@jbrowse/core/util/io'
+import { ObservableCreate } from '@jbrowse/core/util/rxjs'
+import { Feature } from '@jbrowse/core/util/simpleFeature'
+import { readConfObject } from '@jbrowse/core/configuration'
+import { AnyConfigurationModel } from '@jbrowse/core/configuration/configurationSchema'
+import VcfFeature from '../VcfTabixAdapter/VcfFeature'
+
+import VCF from '@gmod/vcf'
+
+const readVcf = (f: string) => {
+  const lines = f.split('\n')
+  const header: string[] = []
+  const rest: string[] = []
+  lines.forEach(line => {
+    if (line.startsWith('#')) {
+      header.push(line)
+    } else if (line) {
+      rest.push(line)
+    }
+  })
+  return { header: header.join('\n'), lines: rest }
+}
+
+export default class SegmentCNVAdapter extends BaseFeatureDataAdapter {
+  public static capabilities = ['getFeatures', 'getRefNames']
+
+  public config: any
+
+  private setupP?: Promise<Feature[]>
+
+  public constructor(config: AnyConfigurationModel) {
+    super(config)
+  }
+
+  public async getLines() {
+    const vcfLocation = readConfObject(
+      this.config,
+      'vcfLocation',
+    ) as FileLocation
+    const entireFileText = (await openLocation(vcfLocation).readFile(
+      'utf8',
+    )) as string
+
+    const { header, lines } = readVcf(entireFileText)
+
+    const parser = new VCF({ header: header })
+
+    return lines.map((line, index) => {
+      return new VcfFeature({
+        variant: parser.parseLine(line),
+        parser,
+        id: `${this.id}-vcf-${index}`,
+      })
+    })
+  }
+
+  public async setup() {
+    if (!this.setupP) {
+      this.setupP = this.getLines()
+    }
+    return this.setupP
+  }
+
+  public async getRefNames(_: BaseOptions = {}) {
+    const l = []
+    for (let i = 0; i < 23; i++) {
+      l.push('' + i)
+    }
+    return l
+  }
+
+  public getFeatures(region: Region, opts: BaseOptions = {}) {
+    return ObservableCreate<Feature>(async observer => {
+      const feats = await this.setup()
+      feats.forEach(f => {
+        if (
+          f.get('refName') === region.refName &&
+          f.get('end') > region.start &&
+          f.get('start') < region.end
+        ) {
+          observer.next(f)
+        }
+      })
+      observer.complete()
+    }, opts.signal)
+  }
+
+  public freeResources(): void {}
+}
