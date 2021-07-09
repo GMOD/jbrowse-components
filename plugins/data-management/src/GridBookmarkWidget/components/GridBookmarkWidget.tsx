0cbadd69496fc747d467c6ee0a10bc5fac7299f3
Elliot Hershberg <eahershberg@gmail.com>
2021-07-09 12:02:59 -0700

    Make bookmark table more compact

--- plugins/data-management/src/GridBookmarkWidget/components/GridBookmarkWidget.tsx
+++ plugins/data-management/src/GridBookmarkWidget/components/GridBookmarkWidget.tsx
@@ -39,19 +39,14 @@ function GridBookmarkWidget({ model }: { model: GridBookmarkModel }) {
   const bookmarkRows = bookmarkedRegions.toJS().map((region: Region) => ({
     ...region,
     id: `${region.refName}:${region.start}..${region.end}`,
-    chrom: region.refName,
     delete: `${region.refName}:${region.start}..${region.end}`,
   }))
 
   const columns = [
-    { field: 'chrom', headerName: 'chrom', width: 100 },
-    { field: 'start', headerName: 'start', width: 100 },
-    { field: 'end', headerName: 'end', width: 100 },
-    { field: 'assemblyName', headerName: 'assembly', width: 100 },
     {
       field: 'id',
-      headerName: 'link',
-      width: 100,
+      headerName: 'bookmark link',
+      width: 300,
       renderCell: (params: GridCellParams) => {
         const { value } = params
         return (
--- plugins/data-management/src/GridBookmarkWidget/model.ts
+++ plugins/data-management/src/GridBookmarkWidget/model.ts
@@ -2,24 +2,15 @@ import { types, Instance } from 'mobx-state-tree'
 
 import PluginManager from '@jbrowse/core/PluginManager'
 import { ElementId } from '@jbrowse/core/util/types/mst'
-import { getSession } from '@jbrowse/core/util'
 
 export default function f(pluginManager: PluginManager) {
-  return types
-    .model('GridBookmarkModel', {
-      id: ElementId,
-      type: types.literal('GridBookmarkWidget'),
-      view: types.safeReference(
-        pluginManager.pluggableMstType('view', 'stateModel'),
-      ),
-    })
-    .actions(self => ({
-      async afterCreate() {
-        const session = getSession(self)
-        // @ts-ignore
-        session.updateDrawerWidth(575)
-      },
-    }))
+  return types.model('GridBookmarkModel', {
+    id: ElementId,
+    type: types.literal('GridBookmarkWidget'),
+    view: types.safeReference(
+      pluginManager.pluggableMstType('view', 'stateModel'),
+    ),
+  })
 }
 
 export type GridBookmarkStateModel = ReturnType<typeof f>
