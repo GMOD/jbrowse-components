7de14c4896dd552fb0059772b3e28418dbcfaa6c
Bebe <tia.simone01@gmail.com>
2021-08-04 12:11:44 -0700

    Updated config.json + other small changes

--- products/jbrowse-cli/src/commands/text-index.ts
+++ products/jbrowse-cli/src/commands/text-index.ts
@@ -75,7 +75,7 @@ export default class TextIndex extends JBrowseCommand {
       assemblies?.split(',') || config.assemblies?.map(a => a.name) || []
     const adapters = config.aggregateTextSearchAdapters || []
 
-    const metaDir = path.join(dir, 'trix', `meta.json`)
+    const metaFile = path.join(dir, 'trix', `meta.json`)
 
     const trixDir = path.join(dir, 'trix')
     if (!fs.existsSync(trixDir)) {
@@ -148,9 +148,9 @@ export default class TextIndex extends JBrowseCommand {
     )
 
     fs.writeFileSync(
-      metaDir,
+      metaFile,
       JSON.stringify(
-        { attributes: attributesToIndex, IndexedTracks: TrackIds },
+        { attributes: attributesToIndex, indexedTracks: TrackIds },
         null,
         2,
       ),
