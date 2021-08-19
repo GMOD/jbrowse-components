2df95ebb72dbe59608ccb6effb29b806ffe2a623
Bebe <tia.simone01@gmail.com>
2021-08-19 16:06:09 -0700

    function stubs

--- plugins/gtf/src/GtfTabixAdapter/GtfTabixAdapter.ts
+++ plugins/gtf/src/GtfTabixAdapter/GtfTabixAdapter.ts
@@ -1,6 +1,47 @@
+/* eslint-disable no-underscore-dangle */
 import {
   BaseFeatureDataAdapter,
   BaseOptions,
 } from '@jbrowse/core/data_adapters/BaseAdapter'
+import { doesIntersect2 } from '@jbrowse/core/util/range'
+import { NoAssemblyRegion, Region } from '@jbrowse/core/util/types'
+import { openLocation } from '@jbrowse/core/util/io'
+import { ObservableCreate } from '@jbrowse/core/util/rxjs'
+import SimpleFeature, { Feature } from '@jbrowse/core/util/simpleFeature'
+import { TabixIndexedFile } from '@gmod/tabix'
+import { Observer, Observable } from 'rxjs'
 
-export default class extends BaseFeatureDataAdapter {}
+import { Instance } from 'mobx-state-tree'
+import { readConfObject } from '@jbrowse/core/configuration'
+import MyConfigSchema from './configSchema'
+
+type Strand = '+' | '-' | '.' | '?'
+interface FeatureLoc {
+  [key: string]: unknown
+  seq_name: string
+  source: string
+  feature: string
+  start: number
+  end: number
+  score: number
+  strand: Strand
+  frame: number
+  attributes: { [key: string]: unknown[] }
+}
+
+export default class extends BaseFeatureDataAdapter {
+  protected gtf: TabixIndexedFile | undefined
+
+  public getRefNames(opts?: BaseOptions | undefined): Promise<string[]> {
+    throw new Error("Method not implemented.")
+  }
+  public getFeatures(region: Region, opts?: BaseOptions | undefined): Observable<Feature> {
+    throw new Error("Method not implemented.")
+  }
+
+  private formatFeatures(featureLocs: FeatureLoc[]) {
+
+  }
+
+  public freeResources(/* { region } */): void {}
+}
--- plugins/gtf/src/GtfTabixAdapter/configSchema.ts
+++ plugins/gtf/src/GtfTabixAdapter/configSchema.ts
@@ -2,22 +2,27 @@ import { types } from 'mobx-state-tree'
 import { ConfigurationSchema } from '@jbrowse/core/configuration'
 
 export default ConfigurationSchema(
-    'GTFTabixAdapter',
-    {
-        gtfGzLocation: {
-            type: 'fileLocation',
-            defaultValue: {uri: '/path/to/my.gtf.gz'},
-        },
-        index: ConfigurationSchema('GtfTabixIndex', {
-            model: types.enumeration('IndexType', ['TBI', 'CSI']),
-            type: 'stringEnum',
-            defaultValue: 'TBI',
-            },
-            location: {
-            type: 'fileLocation',
-            defaultValue: { uri: '/path/to/my.gtf.gz.tbi' },
-            },
-        })
-    }
-    { explicitlyTyped: true },
+  'GTFTabixAdapter',
+  {
+    gtfGzLocation: {
+      type: 'fileLocation',
+      defaultValue: { uri: '/path/to/my.gtf.gz' },
+    },
+    index: ConfigurationSchema('GtfTabixIndex', {
+      indexType: {
+        model: types.enumeration('IndexType', ['TBI', 'CSI']),
+        type: 'stringEnum',
+        defaultValue: 'TBI',
+      },
+      location: {
+        type: 'fileLocation',
+        defaultValue: { uri: '/path/to/my.gtf.gz.tbi' },
+      },
+    }),
+    dontRedispatch: {
+      type: 'stringArray',
+      defaultValue: ['chromosome', 'region'],
+    },
+  },
+  { explicitlyTyped: true },
 )
