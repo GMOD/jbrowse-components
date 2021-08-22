f030fad0963381e7acd91cfd88cdaf4de8962a22
Colin <colin.diesh@gmail.com>
2021-08-21 20:36:33 -0400

    Start model for getting theme information elsewhere...prolly not config

--- products/jbrowse-desktop/src/Loader.tsx
+++ products/jbrowse-desktop/src/Loader.tsx
@@ -1,14 +1,21 @@
 import React, { useState } from 'react'
 import PluginManager from '@jbrowse/core/PluginManager'
+import { CssBaseline, ThemeProvider } from '@material-ui/core'
 
 import JBrowse from './JBrowse'
 import StartScreen from './StartScreen'
+import { createJBrowseTheme } from '@jbrowse/core/ui'
 
 export default function Loader() {
   const [pluginManager, setPluginManager] = useState<PluginManager>()
-  return pluginManager ? (
-    <JBrowse pluginManager={pluginManager} />
-  ) : (
-    <StartScreen setPluginManager={setPluginManager} />
+  return (
+    <ThemeProvider theme={createJBrowseTheme()}>
+      <CssBaseline />
+      {pluginManager ? (
+        <JBrowse pluginManager={pluginManager} />
+      ) : (
+        <StartScreen setPluginManager={setPluginManager} />
+      )}
+    </ThemeProvider>
   )
 }
