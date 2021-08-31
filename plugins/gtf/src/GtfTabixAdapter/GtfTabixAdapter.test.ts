bfbd3e73e572059a079e0dd2b2353283fb98032d
Bebe <tia.simone01@gmail.com>
2021-08-31 13:19:16 -0700

    gtf adapter base test

--- plugins/gtf/src/GtfTabixAdapter/GtfTabixAdapter.test.ts
+++ plugins/gtf/src/GtfTabixAdapter/GtfTabixAdapter.test.ts
@@ -1,6 +1,33 @@
 import configSchema from './configSchema'
 import Adapter from './GtfTabixAdapter'
+import path from 'path'
 
 test('adapter can fetch variants form demo.gtf', async () => {
-    
+  const adapter = new Adapter(
+    configSchema.create({
+      gtfGzLocation: {
+        localPath: path.join(
+          __dirname,
+          '..',
+          '..',
+          '..',
+          'test_data',
+          'demo.gtf',
+        ),
+      },
+      index: {
+        indexType: 'TBI',
+        location: {
+          localPath: path.join(
+            __dirname,
+            '..',
+            '..',
+            '..',
+            'test_data',
+            'demo.gtf.tbi',
+          ),
+        },
+      },
+    }),
+  )
 })
