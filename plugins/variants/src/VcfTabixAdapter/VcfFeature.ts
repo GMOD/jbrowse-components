dcdf7b76ed42728475fc022117ff4c371c9d9759
Colin <colin.diesh@gmail.com>
2021-09-05 16:07:53 -0400

    Breakend parsing in jbrowse-web

--- plugins/variants/src/VcfTabixAdapter/VcfFeature.ts
+++ plugins/variants/src/VcfTabixAdapter/VcfFeature.ts
@@ -1,4 +1,5 @@
 import { Feature } from '@jbrowse/core/util/simpleFeature'
+import { parseBreakend } from '@gmod/vcf'
 
 /* eslint-disable no-underscore-dangle */
 
@@ -10,12 +11,7 @@ interface Samples {
     [key: string]: { values: string[] | number[] | null }
   }
 }
-export interface Breakend {
-  MateDirection: string
-  Replacement: string
-  MatePosition: string
-  Join: string
-}
+
 interface FeatureData {
   [key: string]: unknown
   refName: string
@@ -49,10 +45,9 @@ export default class VCFFeature implements Feature {
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   get(field: string): any {
-    if (field === 'samples') {
-      return this.variant.SAMPLES
-    }
-    return this.data[field] || this.variant[field]
+    return field === 'samples'
+      ? this.variant.SAMPLES
+      : this.data[field] || this.variant[field]
   }
 
   set(): void {}
@@ -81,7 +76,7 @@ export default class VCFFeature implements Feature {
   dataFromVariant(variant: {
     REF: string
     POS: number
-    ALT: (string | Breakend)[]
+    ALT: string[]
     CHROM: string
     INFO: any // eslint-disable-line @typescript-eslint/no-explicit-any
     ID: string[]
@@ -90,16 +85,15 @@ export default class VCFFeature implements Feature {
     const start = POS - 1
     const [SO_term, description] = this._getSOTermAndDescription(REF, ALT)
     const isTRA = ALT?.some(f => f === '<TRA>')
-    const isSymbolic = ALT?.some(
-      f => typeof f === 'string' && f.indexOf('<') !== -1,
-    )
+    const isSymbolic = ALT?.some(f => f.indexOf('<') !== -1)
+
     return {
       refName: CHROM,
       start,
       end: isSymbolic && INFO.END && !isTRA ? +INFO.END[0] : start + REF.length,
       description,
       type: SO_term,
-      name: ID ? ID[0] : undefined,
+      name: ID?.join(','),
       aliases: ID && ID.length > 1 ? variant.ID.slice(1) : undefined,
     }
   }
@@ -109,15 +103,15 @@ export default class VCFFeature implements Feature {
    */
   _getSOTermAndDescription(
     ref: string,
-    alt: (string | Breakend)[],
+    alt: string[],
   ): [string, string] | [undefined, undefined] {
     // it's just a remark if there are no alternate alleles
     if (!alt || alt === []) {
       return ['remark', 'no alternative alleles']
     }
 
-    const soTerms: Set<string> = new Set()
-    let descriptions: Set<string> = new Set()
+    const soTerms = new Set<string>()
+    let descriptions = new Set<string>()
     alt.forEach(a => {
       let [soTerm, description] = this._getSOAndDescFromAltDefs(ref, a)
 
@@ -175,13 +169,8 @@ export default class VCFFeature implements Feature {
 
   _getSOAndDescFromAltDefs(
     ref: string,
-    alt: string | Breakend,
+    alt: string,
   ): [string, string] | [undefined, undefined] {
-    // not a symbolic ALT if doesn't begin with '<', so we'll have no definition
-    if (typeof alt === 'object') {
-      return ['breakend', alt.toString()]
-    }
-
     if (typeof alt === 'string' && !alt.startsWith('<')) {
       return [undefined, undefined]
     }
@@ -193,10 +182,6 @@ export default class VCFFeature implements Feature {
       soTerm = 'sequence_variant'
     }
     if (soTerm) {
-      // const metaDescription = this.parser.getMetadata('ALT', alt, 'Description')
-      // const description = metaDescription
-      //   ? `${alt} - ${metaDescription}`
-      //   : this._makeDescriptionString(soTerm, ref, alt)
       return [soTerm, alt]
     }
 
@@ -213,20 +198,16 @@ export default class VCFFeature implements Feature {
     return [undefined, undefined]
   }
 
-  _getSOAndDescByExamination(
-    ref: string,
-    alt: string | Breakend,
-  ): [string, string] {
-    if (typeof alt === 'object') {
-      return ['breakend', this._makeDescriptionString('breakend', ref, alt)]
-    }
-
-    if (ref.length === 1 && alt.length === 1) {
-      // use SNV because SO definition of SNP says abundance must be at
-      // least 1% in population, and can't be sure we meet that
+  // note: term SNV is used instead of SNP because SO definition of SNP says
+  // abundance must be at least 1% in population, and can't be sure we meet
+  // that
+  _getSOAndDescByExamination(ref: string, alt: string): [string, string] {
+    const bnd = parseBreakend(alt)
+    if (bnd) {
+      return ['breakend', alt]
+    } else if (ref.length === 1 && alt.length === 1) {
       return ['SNV', this._makeDescriptionString('SNV', ref, alt)]
-    }
-    if (alt === '<INS>') {
+    } else if (alt === '<INS>') {
       return ['insertion', alt]
     } else if (alt === '<DEL>') {
       return ['deletion', alt]
@@ -244,24 +225,16 @@ export default class VCFFeature implements Feature {
         'substitution',
         this._makeDescriptionString('substitution', ref, alt),
       ]
-    }
-
-    if (ref.length <= alt.length) {
+    } else if (ref.length <= alt.length) {
       return ['insertion', this._makeDescriptionString('insertion', ref, alt)]
-    }
-
-    if (ref.length > alt.length) {
+    } else if (ref.length > alt.length) {
       return ['deletion', this._makeDescriptionString('deletion', ref, alt)]
     }
 
     return ['indel', this._makeDescriptionString('indel', ref, alt)]
   }
 
-  _makeDescriptionString(
-    soTerm: string,
-    ref: string,
-    alt: string | Breakend,
-  ): string {
+  _makeDescriptionString(soTerm: string, ref: string, alt: string): string {
     return `${soTerm} ${ref} -> ${alt}`
   }
 
