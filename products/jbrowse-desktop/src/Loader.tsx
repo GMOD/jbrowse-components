56d67edeb75022e4e1e757d2e095195ec7284322
Colin <colin.diesh@gmail.com>
2021-09-22 02:12:24 -0400

    Rearrange some error handling

--- products/jbrowse-desktop/src/Loader.tsx
+++ products/jbrowse-desktop/src/Loader.tsx
@@ -1,24 +1,85 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
 import { observer } from 'mobx-react'
 import PluginManager from '@jbrowse/core/PluginManager'
-import { CssBaseline, ThemeProvider, Typography } from '@material-ui/core'
+import { CssBaseline, ThemeProvider, makeStyles } from '@material-ui/core'
 import { createJBrowseTheme } from '@jbrowse/core/ui'
-import {
-  StringParam,
-  QueryParamProvider,
-  useQueryParam,
-} from 'use-query-params'
+import { StringParam, useQueryParam } from 'use-query-params'
 import { ipcRenderer } from 'electron'
-
 import { createPluginManager } from './StartScreen/util'
 
 import JBrowse from './JBrowse'
 import StartScreen from './StartScreen'
 
+const useStyles = makeStyles(theme => ({
+  message: {
+    border: '1px solid black',
+    overflow: 'auto',
+    maxHeight: 200,
+    margin: theme.spacing(1),
+    padding: theme.spacing(1),
+  },
+
+  errorBox: {
+    background: 'lightgrey',
+    border: '1px solid black',
+    margin: 20,
+  },
+}))
+
+const ErrorMessage = ({
+  error,
+  snapshotError,
+}: {
+  error: Error
+  snapshotError?: string
+}) => {
+  const classes = useStyles()
+  return (
+    <div className={classes.message} style={{ background: '#f88' }}>
+      {`${error}`}
+      {snapshotError ? (
+        <>
+          ... Failed element had snapshot:
+          <pre className={classes.errorBox}>
+            {JSON.stringify(JSON.parse(snapshotError), null, 2)}
+          </pre>
+        </>
+      ) : null}
+    </div>
+  )
+}
+
 const Loader = observer(() => {
   const [pluginManager, setPluginManager] = useState<PluginManager>()
   const [config, setConfig] = useQueryParam('config', StringParam)
   const [error, setError] = useState<Error>()
+  const [snapshotError, setSnapshotError] = useState('')
+
+  function handleError(e: Error) {
+    const match = e.message.match(
+      /.*at path "(.*)" snapshot `(.*)` is not assignable/,
+    )
+
+    // best effort to make a better error message than the default
+    // mobx-state-tree
+    if (match) {
+      setError(new Error(`Failed to load element at ${match[1]}`))
+      setSnapshotError(match[2])
+    } else {
+      setError(new Error(e.message.slice(0, 10000)))
+    }
+    console.error(e)
+  }
+
+  const handleSetPluginManager = useCallback(
+    (pm: PluginManager) => {
+      setPluginManager(pm)
+      setError(undefined)
+      setSnapshotError('')
+      setConfig('')
+    },
+    [setConfig],
+  )
 
   useEffect(() => {
     ;(async () => {
@@ -26,37 +87,31 @@ const Loader = observer(() => {
         try {
           const data = await ipcRenderer.invoke('loadSession', config)
           const pm = await createPluginManager(JSON.parse(data))
-          setPluginManager(pm)
-          setConfig('')
+          handleSetPluginManager(pm)
         } catch (e) {
-          console.error(e)
-          setError(e)
+          handleError(e)
         }
       }
     })()
-  }, [config, setConfig])
+  }, [config, handleSetPluginManager])
 
   return (
     <ThemeProvider theme={createJBrowseTheme()}>
       <CssBaseline />
+
       {error ? (
-        <Typography variant="h6" color="error">{`${error}`}</Typography>
+        <ErrorMessage error={error} snapshotError={snapshotError} />
       ) : null}
       {pluginManager?.rootModel?.session ? (
         <JBrowse pluginManager={pluginManager} />
       ) : !config || error ? (
-        <StartScreen setPluginManager={setPluginManager} />
+        <StartScreen
+          setError={handleError}
+          setPluginManager={handleSetPluginManager}
+        />
       ) : null}
     </ThemeProvider>
   )
 })
 
-function Wrapper() {
-  return (
-    <QueryParamProvider>
-      <Loader />
-    </QueryParamProvider>
-  )
-}
-
-export default Wrapper
+export default Loader
