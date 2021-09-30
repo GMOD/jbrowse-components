5e725d023d6ec94756bcef7c2bbd82c864d4467b
Shihab Dider <shihabdider@gmail.com>
2021-09-29 21:11:37 -0700

    Added a sequence search dialog

--- plugins/linear-genome-view/src/LinearGenomeView/components/SequenceSearchButton.tsx
+++ plugins/linear-genome-view/src/LinearGenomeView/components/SequenceSearchButton.tsx
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react'
+import { getSession } from '@jbrowse/core/util'
 import { observer } from 'mobx-react'
 import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/core/styles'
-import { LinearGenomeViewModel } from '..'
 import Search from '@material-ui/icons/Search'
 import { fade } from '@material-ui/core/styles/colorManipulator'
+import { LinearGenomeViewModel } from '..'
+
+/* eslint-disable no-nested-ternary */
+import {
+  Dialog,
+} from '@material-ui/core'
 
 const WIDGET_HEIGHT = 32
 const SPACING = 7
@@ -19,16 +25,51 @@ const useStyles = makeStyles(theme => ({
 
 function SequenceSearchButton({ model }: { model: LinearGenomeViewModel }) {
   const classes = useStyles()
+
+  const session = getSession(model)
+  const { rpcManager, assemblyManager } = session
+
+  const [trigger, setTrigger] = useState(false);
+  const [sequence, setSequence] = useState("");
+  const [results, setResults] = useState();
+
   return (
-    <>
+    <div>
       <Button
         variant="outlined"
         className={classes.sequenceSearchButton}
-        onClick={model.activateTrackSelector}
+        onClick={() => setTrigger(true)}
       >
         <Search />
       </Button>
-    </>
+      <Dialog open={trigger} onClose={() => setTrigger(false)}>
+        <p>Enter your sequence here</p>
+        <input
+          type="text"
+          value={sequence}
+          onChange={() => setSequence(event.target.value)}
+        />
+        <button
+          onClick={async () => {
+            const sessionId = 'bigsiQuery'
+            const params = {
+                sessionId,
+                sequence
+            }
+            const results = await rpcManager.call(
+              sessionId,
+              "BigsiQueryRPC",
+              params
+            );
+            console.log(results);
+            setResults(results);
+            setTrigger(false) // closes the dialog if you want, or skip this and display the results in the dialog
+          }}
+        >
+          Submit
+        </button>
+      </Dialog>
+    </div>
   )
 }
 
