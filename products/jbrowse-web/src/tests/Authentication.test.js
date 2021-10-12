179bcc3700383e9d417c1a5654a6020070982de2
Colin <colin.diesh@gmail.com>
2021-10-12 11:40:10 -0400

    Within query in test

--- products/jbrowse-web/src/tests/Authentication.test.js
+++ products/jbrowse-web/src/tests/Authentication.test.js
@@ -1,5 +1,5 @@
 // library
-import { cleanup, fireEvent, render } from '@testing-library/react'
+import { cleanup, fireEvent, render, within } from '@testing-library/react'
 import React from 'react'
 import { LocalFile, RemoteFile } from 'generic-filehandle'
 
@@ -119,11 +119,13 @@ describe('authentication', () => {
     fireEvent.click(
       await findByTestId('htsTrackEntry-volvox_microarray_externaltoken'),
     )
-    await findByTestId('externalToken-form')
+    const { findByText: findByTextWithin } = within(
+      await findByTestId('externalToken-form'),
+    )
     fireEvent.change(await findByTestId('entry-externalToken'), {
       target: { value: 'testentry' },
     })
-    fireEvent.click(await findByText('Add'))
+    fireEvent.click(await findByTextWithin('Add'))
 
     expect(Object.keys(sessionStorage)).toContain('ExternalTokenTest-token')
     expect(Object.values(sessionStorage)).toContain('testentry')
