0c34544a84b2c7ba6ace391c7ef863811849fde9
Caroline Bridge <cbridge@oicr.on.ca>
2021-10-20 10:40:51 -0400

    lint

--- plugins/data-management/src/AddTrackWidget/components/ConfirmTrack.tsx
+++ plugins/data-management/src/AddTrackWidget/components/ConfirmTrack.tsx
@@ -217,14 +217,8 @@ const TrackAssemblySelector = observer(
 
 function ConfirmTrack({ model }: { model: AddTrackModel }) {
   const classes = useStyles()
-  const {
-    trackName,
-    trackAdapter,
-    trackType,
-    assembly,
-    warningMessage,
-    adapterHint,
-  } = model
+  const { trackName, trackAdapter, trackType, warningMessage, adapterHint } =
+    model
 
   if (model.unsupported) {
     return (
