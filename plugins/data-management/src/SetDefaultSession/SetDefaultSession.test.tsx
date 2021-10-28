982daf8aa09ad478feec6d7cf1be4c49101e1256
Colin <colin.diesh@gmail.com>
2021-10-28 00:59:43 -0400

    Update

--- plugins/data-management/src/SetDefaultSession/SetDefaultSession.test.tsx
+++ plugins/data-management/src/SetDefaultSession/SetDefaultSession.test.tsx
@@ -20,37 +20,48 @@ const mockRootModel = {
         },
       },
     ],
+    setDefaultSessionConf: jest.fn(),
   },
   session: {
-    savedSessions: [],
-    setDefaultSession: jest.fn(),
-    rpcManager: {},
-    configuration: {},
+    savedSessions: {},
   },
 }
-
-const session = mockRootModel.session
 
 describe('SetDefaultSession GUI', () => {
   it('renders succesfully', () => {
     const { getByText } = render(
-      <SetDefaultSession session={session} open onClose={() => {}} />,
+      <SetDefaultSession
+        rootModel={mockRootModel}
+        open
+        onClose={() => {}}
+        currentDefault="New session"
+      />,
     )
-    expect(getByText('Set default session')).toBeTruthy()
+    expect(getByText('Set Default Session')).toBeTruthy()
   })
 
   it('closes when the return button is clicked', () => {
     const onClose = jest.fn()
     const { getByText } = render(
-      <SetDefaultSession session={session} open onClose={onClose} />,
+      <SetDefaultSession
+        rootModel={mockRootModel}
+        open
+        onClose={onClose}
+        currentDefault="New session"
+      />,
     )
-    fireEvent.click(getByText('Cancel'))
+    fireEvent.click(getByText('Return'))
     expect(onClose).toHaveBeenCalled()
   })
 
   it('shows no sessions if none are saved', () => {
     const { getByText } = render(
-      <SetDefaultSession session={session} open onClose={() => {}} />,
+      <SetDefaultSession
+        rootModel={mockRootModel}
+        open
+        onClose={() => {}}
+        currentDefault="New session"
+      />,
     )
     expect(getByText('No saved sessions found')).toBeTruthy()
   })
@@ -59,7 +70,6 @@ describe('SetDefaultSession GUI', () => {
     const MockSavedSessions = {
       ...mockRootModel,
       session: {
-        ...mockRootModel.session,
         savedSessions: [
           {
             name: `New session`,
@@ -69,19 +79,40 @@ describe('SetDefaultSession GUI', () => {
     }
     const { getByText } = render(
       <SetDefaultSession
-        session={MockSavedSessions.session}
+        rootModel={MockSavedSessions}
         open
         onClose={() => {}}
+        currentDefault="New session"
       />,
     )
     expect(getByText('New session')).toBeTruthy()
+  })
+
+  it('sets to the default session when checked', () => {
+    const MockSession = {
+      ...mockRootModel,
+      session: {
+        name: `Moo session`,
+        savedSessions: [],
+        notify: jest.fn(),
+      },
+    }
+    const { getByRole } = render(
+      <SetDefaultSession
+        rootModel={MockSession}
+        open
+        onClose={() => {}}
+        currentDefault="New session"
+      />,
+    )
+    fireEvent.click(getByRole('radio'))
+    expect(MockSession.jbrowse.setDefaultSessionConf).toHaveBeenCalled()
   })
 
   it('unsets to the default session with reset button', () => {
     const MockSession = {
       ...mockRootModel,
       session: {
-        ...mockRootModel.session,
         name: `Moo session`,
         savedSessions: [],
         notify: jest.fn(),
@@ -89,13 +120,14 @@ describe('SetDefaultSession GUI', () => {
     }
     const { getByText } = render(
       <SetDefaultSession
-        session={MockSession.session}
+        rootModel={MockSession}
         open
         onClose={() => {}}
+        currentDefault="New session"
       />,
     )
     fireEvent.click(getByText('Clear default session'))
-    expect(MockSession.session.setDefaultSession).toHaveBeenCalledWith({
+    expect(MockSession.jbrowse.setDefaultSessionConf).toHaveBeenCalledWith({
       name: `New session`,
     })
   })
