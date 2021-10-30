fa2634689b0aac39e8555fad852a46c683404933
Colin <colin.diesh@gmail.com>
2021-10-30 11:49:22 -0400

    Add getRefNames to VcfAdapter

--- plugins/variants/src/VcfAdapter/VcfAdapter.ts
+++ plugins/variants/src/VcfAdapter/VcfAdapter.ts
@@ -2,40 +2,38 @@ import {
   BaseFeatureDataAdapter,
   BaseOptions,
 } from '@jbrowse/core/data_adapters/BaseAdapter'
-import { FileLocation, Region } from '@jbrowse/core/util/types'
+import { Region } from '@jbrowse/core/util/types'
 import { openLocation } from '@jbrowse/core/util/io'
 import { ObservableCreate } from '@jbrowse/core/util/rxjs'
 import { Feature } from '@jbrowse/core/util/simpleFeature'
 import { readConfObject } from '@jbrowse/core/configuration'
 import { AnyConfigurationModel } from '@jbrowse/core/configuration/configurationSchema'
 import IntervalTree from '@flatten-js/interval-tree'
-import VcfFeature from '../VcfTabixAdapter/VcfFeature'
-import VCF from '@gmod/vcf'
 import { unzip } from '@gmod/bgzf-filehandle'
 import PluginManager from '@jbrowse/core/PluginManager'
 import { getSubAdapterType } from '@jbrowse/core/data_adapters/dataAdapterCache'
+import VCF from '@gmod/vcf'
+import VcfFeature from '../VcfTabixAdapter/VcfFeature'
 
 const readVcf = (f: string) => {
   const lines = f.split('\n')
   const header: string[] = []
-  const refNames: string[] = []
   const rest: string[] = []
   lines.forEach(line => {
-    if (line.startsWith('##contig')) {
-      refNames.push(line.split('##contig=<ID=')[1].split(',')[0])
-    } else if (line.startsWith('#')) {
+    if (line.startsWith('#')) {
       header.push(line)
     } else if (line) {
       rest.push(line)
     }
   })
-  return { header: header.join('\n'), lines: rest, refNames }
+  return { header: header.join('\n'), lines: rest }
 }
 
 export default class VcfAdapter extends BaseFeatureDataAdapter {
   public static capabilities = ['getFeatures', 'getRefNames']
 
   protected vcfFeatures?: Promise<Record<string, IntervalTree>>
+  protected unzipped?: Promise<{ header: string; lines: string[] }>
 
   public constructor(
     config: AnyConfigurationModel,
@@ -46,30 +44,25 @@ export default class VcfAdapter extends BaseFeatureDataAdapter {
   }
 
   private async decodeFileContents() {
-    const vcfLocation = readConfObject(
-      this.config,
-      'vcfLocation',
-    ) as FileLocation
+    if (!this.unzipped) {
+      const loc = readConfObject(this.config, 'vcfLocation')
+      this.unzipped = openLocation(loc, this.pluginManager)
+        .readFile()
+        .then(async fileContents => {
+          if (
+            fileContents[0] === 31 &&
+            fileContents[1] === 139 &&
+            fileContents[2] === 8
+          ) {
+            fileContents = new TextDecoder().decode(await unzip(fileContents))
+          } else {
+            fileContents = fileContents.toString()
+          }
 
-    let fileContents = await openLocation(
-      vcfLocation,
-      this.pluginManager,
-    ).readFile()
-
-    if (
-      typeof fileContents[0] === 'number' &&
-      fileContents[0] === 31 &&
-      typeof fileContents[1] === 'number' &&
-      fileContents[1] === 139 &&
-      typeof fileContents[2] === 'number' &&
-      fileContents[2] === 8
-    ) {
-      fileContents = new TextDecoder().decode(await unzip(fileContents))
-    } else {
-      fileContents = fileContents.toString()
+          return readVcf(fileContents)
+        })
     }
-
-    return readVcf(fileContents)
+    return this.unzipped
   }
 
   public async getHeader() {
@@ -87,14 +80,22 @@ export default class VcfAdapter extends BaseFeatureDataAdapter {
     const { lines } = await this.decodeFileContents()
 
     return lines.map(line => {
-      const [refName, start, _id, ref, _alt, _qual, _filter, info] =
+      const [refName, startOneBased, _id, ref, _alt, _qual, _filter, info] =
         line.split('\t')
-      const end = info
-        .split(';')
-        .filter(elt => elt.startsWith('END='))?.[0]
-        .split('=')[1]
-        .trim()
-      return { line, refName, start, end: end ? end : start + ref.length }
+      const start = +startOneBased - 1
+      const end =
+        info
+          .split(';')
+          .filter(elt => elt.startsWith('END='))?.[0]
+          ?.split('=')[1]
+          .trim() || start + ref.length
+
+      return {
+        line,
+        refName,
+        start,
+        end: +end,
+      }
     })
   }
 
@@ -124,52 +125,26 @@ export default class VcfAdapter extends BaseFeatureDataAdapter {
   }
 
   public async getRefNames(_: BaseOptions = {}) {
-    const { refNames } = await this.decodeFileContents()
-    if (refNames.length === 0) {
-      return [
-        'chr1',
-        'chr2',
-        'chr3',
-        'chr4',
-        'chr5',
-        'chr6',
-        'chr7',
-        'chr8',
-        'chr9',
-        'chr10',
-        'chr11',
-        'chr12',
-        'chr13',
-        'chr14',
-        'chr15',
-        'chr16',
-        'chr17',
-        'chr18',
-        'chr19',
-        'chr20',
-        'chr21',
-        'chr22',
-        'chr23',
-        'chrX',
-        'chrY',
-        'chrMT',
-      ]
-    }
-    return refNames
+    const lines = await this.getLines()
+    return [...new Set(lines.map(line => line.refName))]
   }
 
   public getFeatures(region: Region, opts: BaseOptions = {}) {
     return ObservableCreate<Feature>(async observer => {
       try {
         const { header } = await this.decodeFileContents()
-        const parser = new VCF({ header: header })
         const { start, end, refName } = region
+        const parser = new VCF({ header: header })
         const vcfFeatures = await this.setup()
-        const tree = vcfFeatures[refName]
-        const feats = tree.search([start, end]) //  expected array ['val1']
-        feats.forEach(f => {
-          observer.next(parser.parseLine(f.line))
-        })
+        vcfFeatures[refName]?.search([start, end]).forEach(f =>
+          observer.next(
+            new VcfFeature({
+              variant: parser.parseLine(f.line),
+              parser,
+              id: `${this.id}-${f.line}`,
+            }),
+          ),
+        )
         observer.complete()
       } catch (e) {
         observer.error(e)
