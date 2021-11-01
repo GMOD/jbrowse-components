6f8be54fa72b59e32bf1a4dc5dd04f42b0c0368e
Colin <colin.diesh@gmail.com>
2021-11-01 17:52:52 -0400

    Checkout main branch VcfAdapter

--- plugins/variants/src/VcfAdapter/VcfAdapter.ts
+++ plugins/variants/src/VcfAdapter/VcfAdapter.ts
@@ -2,32 +2,34 @@ import {
   BaseFeatureDataAdapter,
   BaseOptions,
 } from '@jbrowse/core/data_adapters/BaseAdapter'
-import { Region } from '@jbrowse/core/util/types'
+import { FileLocation, Region } from '@jbrowse/core/util/types'
 import { openLocation } from '@jbrowse/core/util/io'
 import { ObservableCreate } from '@jbrowse/core/util/rxjs'
 import { Feature } from '@jbrowse/core/util/simpleFeature'
 import { readConfObject } from '@jbrowse/core/configuration'
+import { AnyConfigurationModel } from '@jbrowse/core/configuration/configurationSchema'
 import IntervalTree from '@flatten-js/interval-tree'
-import { unzip } from '@gmod/bgzf-filehandle'
-import VCF from '@gmod/vcf'
 import VcfFeature from '../VcfTabixAdapter/VcfFeature'
+import VCF from '@gmod/vcf'
+import { unzip } from '@gmod/bgzf-filehandle'
+import PluginManager from '@jbrowse/core/PluginManager'
+import { getSubAdapterType } from '@jbrowse/core/data_adapters/dataAdapterCache'
 
 const readVcf = (f: string) => {
   const lines = f.split('\n')
   const header: string[] = []
+  const refNames: string[] = []
   const rest: string[] = []
   lines.forEach(line => {
-    if (line.startsWith('#')) {
+    if (line.startsWith('##contig')) {
+      refNames.push(line.split('##contig=<ID=')[1].split(',')[0])
+    } else if (line.startsWith('#')) {
       header.push(line)
     } else if (line) {
       rest.push(line)
     }
   })
-  return { header: header.join('\n'), lines: rest }
-}
-
-function isGzip(buf: Buffer) {
-  return buf[0] === 31 && buf[1] === 139 && buf[2] === 8
+  return { header: header.join('\n'), lines: rest, refNames }
 }
 
 export default class VcfAdapter extends BaseFeatureDataAdapter {
@@ -35,30 +37,39 @@ export default class VcfAdapter extends BaseFeatureDataAdapter {
 
   protected vcfFeatures?: Promise<Record<string, IntervalTree>>
 
-  protected unzipped?: Promise<{ header: string; lines: string[] }>
+  public constructor(
+    config: AnyConfigurationModel,
+    getSubAdapter?: getSubAdapterType,
+    pluginManager?: PluginManager,
+  ) {
+    super(config, getSubAdapter, pluginManager)
+  }
 
   private async decodeFileContents() {
-    if (!this.unzipped) {
-      this.unzipped = openLocation(
-        readConfObject(this.config, 'vcfLocation'),
-        this.pluginManager,
-      )
-        .readFile()
-        .then(async buffer => {
-          const buf = isGzip(buffer as Buffer) ? await unzip(buffer) : buffer
-          // 512MB  max chrome string length is 512MB
-          if (buf.length > 536_870_888) {
-            throw new Error('Data exceeds maximum string length (512MB)')
-          }
-          return new TextDecoder().decode(buf)
-        })
-        .then(str => readVcf(str))
-        .catch(e => {
-          this.unzipped = undefined
-          throw e
-        })
+    const vcfLocation = readConfObject(
+      this.config,
+      'vcfLocation',
+    ) as FileLocation
+
+    let fileContents = await openLocation(
+      vcfLocation,
+      this.pluginManager,
+    ).readFile()
+
+    if (
+      typeof fileContents[0] === 'number' &&
+      fileContents[0] === 31 &&
+      typeof fileContents[1] === 'number' &&
+      fileContents[1] === 139 &&
+      typeof fileContents[2] === 'number' &&
+      fileContents[2] === 8
+    ) {
+      fileContents = new TextDecoder().decode(await unzip(fileContents))
+    } else {
+      fileContents = fileContents.toString()
     }
-    return this.unzipped
+
+    return readVcf(fileContents)
   }
 
   public async getHeader() {
@@ -73,69 +84,83 @@ export default class VcfAdapter extends BaseFeatureDataAdapter {
   }
 
   public async getLines() {
-    const { lines } = await this.decodeFileContents()
+    const { header, lines } = await this.decodeFileContents()
 
-    return lines.map((line, id) => {
-      const [refName, startP, , ref, , , , info] = line.split('\t')
-      const start = +startP - 1
-      const end = +(info.match(/END=(\d+)/)?.[1].trim() || start + ref.length)
-      return { line, refName, start, end, id }
+    const parser = new VCF({ header: header })
+
+    return lines.map((line, index) => {
+      return new VcfFeature({
+        variant: parser.parseLine(line),
+        parser,
+        id: `${this.id}-vcf-${index}`,
+      })
     })
   }
 
   public async setup() {
     if (!this.vcfFeatures) {
-      this.vcfFeatures = this.getLines()
-        .then(feats =>
-          feats.reduce(
-            (
-              acc: Record<string, IntervalTree>,
-              obj: {
-                refName: string
-                start: number
-                end: number
-                line: string
-              },
-            ) => {
-              const key = obj.refName
-              if (!acc[key]) {
-                acc[key] = new IntervalTree()
-              }
-              acc[key].insert([obj.start, obj.end], obj)
-              return acc
-            },
-            {},
-          ),
+      this.vcfFeatures = this.getLines().then(feats => {
+        return feats.reduce(
+          (acc: Record<string, IntervalTree>, obj: VcfFeature) => {
+            const key = obj.get('refName')
+            if (!acc[key]) {
+              acc[key] = new IntervalTree()
+            }
+            acc[key].insert([obj.get('start'), obj.get('end')], obj)
+            return acc
+          },
+          {},
         )
-        .catch(e => {
-          this.vcfFeatures = undefined
-          throw e
-        })
+      })
     }
     return this.vcfFeatures
   }
 
   public async getRefNames(_: BaseOptions = {}) {
-    const lines = await this.getLines()
-    return [...new Set(lines.map(line => line.refName))]
+    const { refNames } = await this.decodeFileContents()
+    if (refNames.length === 0) {
+      return [
+        'chr1',
+        'chr2',
+        'chr3',
+        'chr4',
+        'chr5',
+        'chr6',
+        'chr7',
+        'chr8',
+        'chr9',
+        'chr10',
+        'chr11',
+        'chr12',
+        'chr13',
+        'chr14',
+        'chr15',
+        'chr16',
+        'chr17',
+        'chr18',
+        'chr19',
+        'chr20',
+        'chr21',
+        'chr22',
+        'chr23',
+        'chrX',
+        'chrY',
+        'chrMT',
+      ]
+    }
+    return refNames
   }
 
   public getFeatures(region: Region, opts: BaseOptions = {}) {
     return ObservableCreate<Feature>(async observer => {
       try {
-        const { header } = await this.decodeFileContents()
         const { start, end, refName } = region
-        const parser = new VCF({ header: header })
         const vcfFeatures = await this.setup()
-        vcfFeatures[refName]?.search([start, end]).forEach(f =>
-          observer.next(
-            new VcfFeature({
-              variant: parser.parseLine(f.line),
-              parser,
-              id: `${this.id}-${f.id}`,
-            }),
-          ),
-        )
+        const tree = vcfFeatures[refName]
+        const feats = tree.search([start, end]) //  expected array ['val1']
+        feats.forEach(f => {
+          observer.next(f)
+        })
         observer.complete()
       } catch (e) {
         observer.error(e)
