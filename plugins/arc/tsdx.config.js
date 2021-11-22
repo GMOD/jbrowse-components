d30c5d5930e40424bf9e14f0cdeb56ef2c712aac
Caroline Bridge <cbridge@oicr.on.ca>
2021-11-22 10:37:32 -0500

    changing configs attempt to compile

--- plugins/arc/tsdx.config.js
+++ plugins/arc/tsdx.config.js
@@ -1,8 +1,8 @@
-const globals = require('@jbrowse/core/ReExports/list').default
-const { createJBrowsePluginTsdxConfig } = require('@jbrowse/development-tools')
-
+// Not transpiled with TypeScript or Babel, so use plain Es6/Node.js!
 module.exports = {
-  rollup(config, options) {
-    return createJBrowsePluginTsdxConfig(config, options, globals)
+  // This function will run for each entry/format/env combination
+  rollup(config) {
+    config.inlineDynamicImports = true
+    return config // always return a config.
   },
 }
