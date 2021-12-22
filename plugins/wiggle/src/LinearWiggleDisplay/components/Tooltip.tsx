5ecb9a3e7301e03598401e2b758fefaae9478b2f
Colin <colin.diesh@gmail.com>
2021-12-22 12:01:53 -0700

    Fix wiggle tooltip

--- plugins/wiggle/src/LinearWiggleDisplay/components/Tooltip.tsx
+++ plugins/wiggle/src/LinearWiggleDisplay/components/Tooltip.tsx
@@ -7,8 +7,9 @@ import { Feature } from '@jbrowse/core/util/simpleFeature'
 import { YSCALEBAR_LABEL_OFFSET } from '../models/model'
 import { usePopper } from 'react-popper'
 
+// convert to number, apply shortened precision, and render
 function toP(s = 0) {
-  return parseFloat(s.toPrecision(6))
+  return +(+s).toPrecision(6)
 }
 
 function round(value: number) {
