b33d0d25904a86252bc395a7de26764ce66789fc
teresam856 <tere486martinez@gmail.com>
2022-01-10 03:20:05 -0800

    initial architecture

--- packages/core/TextSearch/TextSearchManager.ts
+++ packages/core/TextSearch/TextSearchManager.ts
@@ -16,14 +16,41 @@ export interface BaseArgs {
 }
 
 // TODO: implement Text background indexer class
-// start with a way to monitor the config for any changes
-// in order to get the right things you need to index
+// 1) detect changes in the config
+// 2) determine wether to index changes or not
+// 3) index changes in a separate thread
+//    create a temp dir to store index
 export interface SearchScope {
   includeAggregateIndexes: boolean
   assemblyName: string
   tracks?: Array<string>
 }
 
+export class Jobs {
+  param: number
+  constructor(param: number) {
+    // add a queue here to handle job requests
+    // keep the state of the jobs
+    this.param = param
+    console.log(param)
+  }
+  // we can have a way to detect type of job
+  // for our case we would want an indexing job
+  // handle aborting jobs
+  cancelJob() {
+    // will use abort signals to cancel jobs
+    // if a job is cancelled, do not create another one unitl JBrowse restarts
+    console.log('handling error...')
+  }
+
+  runJob() {
+    console.log('running the job in another thread')
+  }
+  // provide progress updates from workers
+  provideStatus() {
+    console.log('this is the status of the worker')
+  }
+}
 export default class TextSearchManager {
   adapterCache: QuickLRU
 
@@ -37,6 +64,7 @@ export default class TextSearchManager {
    * Instantiate/initialize list of relevant adapters
    */
   loadTextSearchAdapters(searchScope: SearchScope): BaseTextSearchAdapter[] {
+    // this.changesInConf()
     return this.relevantAdapters(searchScope).map(adapterConfig => {
       const adapterId = readConfObject(adapterConfig, 'textSearchAdapterId')
       if (this.adapterCache.has(adapterId)) {
@@ -92,6 +120,16 @@ export default class TextSearchManager {
     )
   }
 
+  changesInConf() {
+    const config = this.pluginManager.rootModel?.jbrowse
+    console.log('config ===>', 'hi')
+    // const { aggregateTextSearchAdapters, tracks } = this.pluginManager.rootModel
+    //   ?.jbrowse as {
+    //   tracks: AnyConfigurationModel[]
+    //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    //   aggregateTextSearchAdapters: any
+    // }
+  }
   /**
    * Returns list of relevant results given a search query and options
    * @param args - search options/arguments include: search query
