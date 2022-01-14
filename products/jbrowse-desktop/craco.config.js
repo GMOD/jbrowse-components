b660532551890636dc8f791a1b5f70e38df3dc87
Colin <colin.diesh@gmail.com>
2022-01-14 08:52:40 -0700

    Add craco to desktop

---
+++ products/jbrowse-desktop/craco.config.js
@@ -0,0 +1,21 @@
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  webpack: {
+    target: 'node',
+
+    plugins: [
+      new NodePolyfillPlugin({
+        excludeAliases: ['console'],
+      }),
+      new webpack.ContextReplacementPlugin(/any-promise/),
+    ],
+    configure: {
+      resolve: { fallback: { fs: false } },
+      output: {
+        publicPath: 'auto',
+      },
+    },
+  },
+}
