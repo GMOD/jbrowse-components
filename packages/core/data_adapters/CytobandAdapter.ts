31a850d2f31809bb7bce346f6009e8a758c469ed
Colin <colin.diesh@gmail.com>
2022-01-18 08:01:24 -0700

    Add babel config back to core

--- packages/core/data_adapters/CytobandAdapter.ts
+++ packages/core/data_adapters/CytobandAdapter.ts
@@ -1,7 +1,6 @@
-import SimpleFeature from '@jbrowse/core/util/simpleFeature'
-import { ConfigurationSchema } from '@jbrowse/core/configuration'
-import { readConfObject } from '@jbrowse/core/configuration'
-import { openLocation } from '@jbrowse/core/util/io'
+import SimpleFeature from '../util/simpleFeature'
+import { ConfigurationSchema, readConfObject } from '../configuration'
+import { openLocation } from '../util/io'
 import { BaseAdapter } from './BaseAdapter'
 
 const configSchema = ConfigurationSchema(
