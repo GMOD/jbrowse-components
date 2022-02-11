5b4bdcf83f75d86f03ed263f7dea584a2b40d894
Colin <colin.diesh@gmail.com>
2022-02-11 13:16:04 -0700

    Avoid typescript any

--- plugins/dotplot-view/src/DotplotDisplay/components/DotplotDisplay.tsx
+++ plugins/dotplot-view/src/DotplotDisplay/components/DotplotDisplay.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { observer } from 'mobx-react'
 import { getContainingView } from '@jbrowse/core/util'
 import { DotplotDisplayModel } from '..'
+import { DotplotViewModel } from '../../DotplotView/model'
 
 const DotplotDisplay: React.FC<{
   model: DotplotDisplayModel
@@ -9,8 +10,7 @@ const DotplotDisplay: React.FC<{
 }> = props => {
   const { model, children } = props
   const { offsetX = 0, offsetY = 0 } = model.data || {}
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const view = getContainingView(model) as any
+  const view = getContainingView(model) as DotplotViewModel
   const top = view.vview.offsetPx - offsetY
   const left = -(view.hview.offsetPx - offsetX)
   return (
