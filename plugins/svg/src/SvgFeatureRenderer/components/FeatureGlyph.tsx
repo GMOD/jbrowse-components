269e51f415fd485b9a3c20b7631c20292edd8dce
Colin <colin.diesh@gmail.com>
2022-03-01 12:34:26 -0700

    More stuff

--- plugins/svg/src/SvgFeatureRenderer/components/FeatureGlyph.tsx
+++ plugins/svg/src/SvgFeatureRenderer/components/FeatureGlyph.tsx
@@ -3,7 +3,7 @@ import {
   AnyConfigurationModel,
   readConfObject,
 } from '@jbrowse/core/configuration'
-import { getContainingView } from '@jbrowse/core/util'
+import { getContainingView, Region } from '@jbrowse/core/util'
 import { observer } from 'mobx-react'
 import { isStateTreeNode } from 'mobx-state-tree'
 import FeatureLabel from './FeatureLabel'
@@ -25,6 +25,7 @@ function FeatureGlyph(props: {
   selected?: boolean
   reversed?: boolean
   topLevel?: boolean
+  region: Region
 }) {
   const {
     feature,
@@ -37,6 +38,7 @@ function FeatureGlyph(props: {
     shouldShowDescription,
     reversed,
     displayModel,
+    region,
   } = props
 
   const featureLayout = rootLayout.getSubRecord(String(feature.id()))
@@ -45,6 +47,26 @@ function FeatureGlyph(props: {
   }
   const { GlyphComponent } = featureLayout.data || {}
 
+  let offsetPx = rootLayout.getSubRecord('nameLabel')?.absolute.left || 0
+  if (isStateTreeNode(displayModel)) {
+    const view = getContainingView(displayModel)
+    // @ts-ignore
+    const { dynamicBlocks, offsetPx: viewOffsetPx } = view
+
+    const { start: viewStart } = dynamicBlocks?.contentBlocks[0] || {}
+    // @ts-ignore
+    const blockOffsetPx = view.bpToPx({
+      refName: region.refName,
+      coord: region.start,
+    })?.offsetPx
+    const baseOffsetPx = viewOffsetPx - blockOffsetPx
+    if (
+      feature.get('start') < viewStart + featureLayout.width &&
+      viewStart - featureLayout.width < feature.get('end')
+    ) {
+      offsetPx = baseOffsetPx
+    }
+  }
   return (
     <>
       <GlyphComponent
@@ -56,7 +78,7 @@ function FeatureGlyph(props: {
       {shouldShowName ? (
         <FeatureLabel
           text={name}
-          x={rootLayout.getSubRecord('nameLabel')?.absolute.left || 0}
+          x={offsetPx}
           y={rootLayout.getSubRecord('nameLabel')?.absolute.top || 0}
           color={readConfObject(config, ['labels', 'nameColor'], { feature })}
           reversed={reversed}
@@ -67,7 +89,7 @@ function FeatureGlyph(props: {
       {shouldShowDescription ? (
         <FeatureLabel
           text={description}
-          x={rootLayout.getSubRecord('descriptionLabel')?.absolute.left || 0}
+          x={offsetPx}
           y={rootLayout.getSubRecord('descriptionLabel')?.absolute.top || 0}
           color={readConfObject(config, ['labels', 'descriptionColor'], {
             feature,
