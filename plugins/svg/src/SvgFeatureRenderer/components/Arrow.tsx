ea0337d2503921b2d5af03dd90744d42ef115175
Colin <colin.diesh@gmail.com>
2022-03-09 17:07:36 -0700

    No arrow if no strand

--- plugins/svg/src/SvgFeatureRenderer/components/Arrow.tsx
+++ plugins/svg/src/SvgFeatureRenderer/components/Arrow.tsx
@@ -22,10 +22,10 @@ const Arrow = ({
   const offset = 7 * strand
   const { left = 0, top = 0, width = 0, height = 0 } = featureLayout.absolute
   const color2 = readConfObject(config, 'color2', { feature })
-  const p = strand === -1 ? left : left + width
+  const p = strand === -1 ? left : strand === 1 ? left + width : null
   const y = top + height / 2
 
-  return (
+  return p ? (
     <>
       <line x1={p} x2={p + offset} y1={y} y2={y} stroke={color2} />
       <polygon
@@ -37,7 +37,7 @@ const Arrow = ({
         stroke={color2}
       />
     </>
-  )
+  ) : null
 }
 
 export default observer(Arrow)
