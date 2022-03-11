0335957a444b6e3f12ad03880dc44e6a06b63994
Colin <colin.diesh@gmail.com>
2022-03-10 20:06:00 -0700

    Accordion config editor

--- plugins/config/src/ConfigurationEditorWidget/components/ConfigurationEditor.js
+++ plugins/config/src/ConfigurationEditorWidget/components/ConfigurationEditor.js
@@ -1,32 +1,45 @@
+import React from 'react'
 import {
   readConfObject,
   getTypeNamesFromExplicitlyTypedUnion,
   isConfigurationSchemaType,
   isConfigurationSlotType,
 } from '@jbrowse/core/configuration'
-
-import { iterMap } from '@jbrowse/core/util'
-import FormGroup from '@material-ui/core/FormGroup'
-import FormLabel from '@material-ui/core/FormLabel'
-import { makeStyles } from '@material-ui/core/styles'
+import {
+  FormGroup,
+  FormLabel,
+  Accordion,
+  AccordionSummary,
+  Typography,
+  makeStyles,
+} from '@material-ui/core'
 import { observer } from 'mobx-react'
 import { getMembers } from 'mobx-state-tree'
 import { singular } from 'pluralize'
-import React from 'react'
+
+//icons
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+
+//locals
 import SlotEditor from './SlotEditor'
 import TypeSelector from './TypeSelector'
 
 const useStyles = makeStyles(theme => ({
   subSchemaContainer: {
-    marginLeft: theme.spacing(1),
-    borderLeft: `1px solid ${theme.palette.secondary.main}`,
-    paddingLeft: theme.spacing(1),
-    marginBottom: theme.spacing(1),
+    margin: theme.spacing(1),
+    padding: theme.spacing(1),
+  },
+  expandIcon: {
+    color: '#fff',
   },
   root: {
     padding: theme.spacing(1, 3, 1, 1),
-    background: theme.palette.background.default,
-    overflowX: 'hidden',
+    width: '100%',
+  },
+  accordion: {
+    maxWidth: '100%',
+    display: 'grid',
+    gridTemplateColumn: '100%',
   },
 }))
 
@@ -36,14 +49,10 @@ const Member = observer(props => {
   let typeSelector
   if (isConfigurationSchemaType(slotSchema)) {
     if (slot.length) {
-      return (
-        <>
-          {slot.map((subslot, slotIndex) => {
-            const key = `${singular(slotName)} ${slotIndex + 1}`
-            return <Member {...props} key={key} slot={subslot} slotName={key} />
-          })}
-        </>
-      )
+      return slot.map((subslot, slotIndex) => {
+        const key = `${singular(slotName)} ${slotIndex + 1}`
+        return <Member {...props} key={key} slot={subslot} slotName={key} />
+      })
     }
     // if this is an explicitly typed schema, make a type-selecting dropdown
     // that can be used to change its type
@@ -64,13 +73,23 @@ const Member = observer(props => {
     }
     return (
       <>
-        <FormLabel>{slotName}</FormLabel>
-        <div className={classes.subSchemaContainer}>
-          {typeSelector}
-          <FormGroup>
-            <Schema schema={slot} />
-          </FormGroup>
-        </div>
+        <Accordion
+          defaultExpanded
+          TransitionProps={{ unmountOnExit: true, timeout: 0 }}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon className={classes.expandIcon} />}
+          >
+            <Typography>Config {slotName}</Typography>
+          </AccordionSummary>
+          <FormLabel>{slotName}</FormLabel>
+          <div className={classes.subSchemaContainer}>
+            {typeSelector}
+            <FormGroup>
+              <Schema schema={slot} />
+            </FormGroup>
+          </div>
+        </Accordion>
       </>
     )
   }
@@ -84,12 +103,15 @@ const Member = observer(props => {
 })
 
 const Schema = observer(({ schema }) => {
-  return iterMap(
-    Object.entries(getMembers(schema).properties),
-    ([slotName, slotSchema]) => (
-      <Member key={slotName} {...{ slotName, slotSchema, schema }} />
-    ),
-  )
+  const properties = getMembers(schema).properties
+  return Object.entries(properties).map(([slotName, slotSchema]) => (
+    <Member
+      key={slotName}
+      slotName={slotName}
+      slotSchema={slotSchema}
+      schema={schema}
+    />
+  ))
 })
 
 const ConfigurationEditor = observer(({ model }) => {
@@ -98,10 +120,22 @@ const ConfigurationEditor = observer(({ model }) => {
   // for different tracks since only the backing model changes for example
   // see pr #804
   const key = model.target && readConfObject(model.target, 'trackId')
+  const name = model.target && readConfObject(model.target, 'name')
   return (
-    <div className={classes.root} key={key} data-testid="configEditor">
-      {!model.target ? 'no target set' : <Schema schema={model.target} />}
-    </div>
+    <Accordion
+      defaultExpanded
+      className={classes.accordion}
+      TransitionProps={{ unmountOnExit: true, timeout: 0 }}
+    >
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon className={classes.expandIcon} />}
+      >
+        <Typography>Configuration{name ? ' - ' + name : ''}</Typography>
+      </AccordionSummary>
+      <div className={classes.root} key={key} data-testid="configEditor">
+        {!model.target ? 'no target set' : <Schema schema={model.target} />}
+      </div>
+    </Accordion>
   )
 })
 
