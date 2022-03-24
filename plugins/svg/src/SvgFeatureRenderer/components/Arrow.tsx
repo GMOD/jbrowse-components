ac436fc3958f6b00e79cb34d50cd21aa08cb5c31
Colin <colin.diesh@gmail.com>
2022-03-24 09:01:50 -0600

    git checkout origin/main arrow

--- plugins/svg/src/SvgFeatureRenderer/components/Arrow.tsx
+++ plugins/svg/src/SvgFeatureRenderer/components/Arrow.tsx
@@ -12,23 +12,20 @@ const Arrow = ({
   feature,
   featureLayout,
   config,
-  reversed,
 }: {
   feature: Feature
   featureLayout: SceneGraph
   config: AnyConfigurationModel
-  reversed: boolean
 }) => {
   const strand = feature.get('strand')
   const size = 5
-  const rev = reversed ? -1 : 1
-  const offset = 7 * strand * rev
+  const offset = 7 * strand
   const { left = 0, top = 0, width = 0, height = 0 } = featureLayout.absolute
   const color2 = readConfObject(config, 'color2', { feature })
-  const p = strand * rev === -1 ? left : left + width
+  const p = strand === -1 ? left : strand === 1 ? left + width : null
   const y = top + height / 2
 
-  return (
+  return p ? (
     <>
       <line x1={p} x2={p + offset} y1={y} y2={y} stroke={color2} />
       <polygon
@@ -40,7 +37,7 @@ const Arrow = ({
         stroke={color2}
       />
     </>
-  )
+  ) : null
 }
 
 export default observer(Arrow)
