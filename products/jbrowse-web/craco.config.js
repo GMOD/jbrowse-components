681bd37459da64c6be394b5eb5382b0a9bd4ead9
Colin <colin.diesh@gmail.com>
2022-03-30 14:53:37 -0600

    Fix static options for fallthrough 404 in dev server

--- products/jbrowse-web/craco.config.js
+++ products/jbrowse-web/craco.config.js
@@ -2,6 +2,10 @@ const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 const webpack = require('webpack')
 
 module.exports = {
+  devServer: config => {
+    config.static.staticOptions = { fallthrough: false }
+    return config
+  },
   webpack: {
     target: 'node',
 
