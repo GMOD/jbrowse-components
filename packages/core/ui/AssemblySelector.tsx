696b4686555f0419ad3fbdce499563b33bd21f04
Colin <colin.diesh@gmail.com>
2022-04-08 15:04:12 -0600

    Remember assembly selector selections based on host, and avoid infinite loop selecting from synteny dialogs

--- packages/core/ui/AssemblySelector.tsx
+++ packages/core/ui/AssemblySelector.tsx
@@ -43,15 +43,25 @@ const AssemblySelector = observer(
     session,
     onChange,
     selected,
+    extra = 0,
   }: {
     session: AbstractSessionModel
     onChange: (arg: string) => void
     selected: string | undefined
+    extra?: unknown
   }) => {
     const classes = useStyles()
     const { assemblyNames, assemblyManager } = session
+
+    // constructs a localstorage key based on host/path/config to help
+    // remember. non-config assists usage with e.g. embedded apps
+    const config = new URLSearchParams(window.location.search).get('config')
     const [lastSelected, setLastSelected] = useLocalStorage(
-      'lastSelectedAsm',
+      `lastAssembly-${[
+        window.location.host + window.location.pathname,
+        config,
+        extra,
+      ].join('-')}`,
       selected,
     )
 
@@ -60,7 +70,7 @@ const AssemblySelector = observer(
       : selected
 
     useEffect(() => {
-      if (selection) {
+      if (selection && selection !== selected) {
         onChange(selection)
       }
     }, [selection, onChange])
