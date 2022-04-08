295ddaa5be5aa78ce6bed94e0e414067f1dba6db
Colin <colin.diesh@gmail.com>
2022-04-08 16:27:29 -0600

    Add ENABLE_TYPE_CHECK to mobx production builds

--- products/jbrowse-desktop/craco.config.js
+++ products/jbrowse-desktop/craco.config.js
@@ -16,6 +16,12 @@ module.exports = {
         excludeAliases: ['console'],
       }),
       new webpack.ContextReplacementPlugin(/any-promise/),
+      new webpack.DefinePlugin({
+        // Global mobx-state-tree configuration.
+        // Force type checking in production for easier debugging:
+        // xref https://github.com/GMOD/jbrowse-components/pull/1575
+        'process.env.ENABLE_TYPE_CHECK': '"true"',
+      }),
     ],
     configure: config => {
       const { isFound, match } = getLoader(config, loaderByName('babel-loader'))
--- products/jbrowse-web/craco.config.js
+++ products/jbrowse-web/craco.config.js
@@ -18,6 +18,12 @@ module.exports = {
         excludeAliases: ['console'],
       }),
       new webpack.ContextReplacementPlugin(/any-promise/),
+      new webpack.DefinePlugin({
+        // Global mobx-state-tree configuration.
+        // Force type checking in production for easier debugging:
+        // xref https://github.com/GMOD/jbrowse-components/pull/1575
+        'process.env.ENABLE_TYPE_CHECK': '"true"',
+      }),
     ],
     configure: webpackConfig => {
       const { isFound, match } = getLoader(
