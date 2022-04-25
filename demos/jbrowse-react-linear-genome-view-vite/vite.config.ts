201b0ab773d9a1e2e4a6afda0ccd0556ed3851c2
Colin <colin.diesh@gmail.com>
2022-04-25 16:58:02 -0600

    [skip ci] Use base ./ for relative path/sub-URI deployment

--- demos/jbrowse-react-linear-genome-view-vite/vite.config.ts
+++ demos/jbrowse-react-linear-genome-view-vite/vite.config.ts
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   resolve: {
     alias: {
       stream: 'stream-browserify',
