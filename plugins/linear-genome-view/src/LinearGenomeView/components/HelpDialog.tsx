4d1d84697311a7e664844f562c330637e38c8f91
Colin Diesh <colin.diesh@gmail.com>
2022-06-08 12:59:46 -0600

    Allow whitespace separated refname,start,end (#3016)

--- plugins/linear-genome-view/src/LinearGenomeView/components/HelpDialog.tsx
+++ plugins/linear-genome-view/src/LinearGenomeView/components/HelpDialog.tsx
@@ -32,11 +32,8 @@ export default function HelpDialog({
         Using the search box
         {handleClose ? (
           <IconButton
-            data-testid="close-resultsDialog"
             className={classes.closeButton}
-            onClick={() => {
-              handleClose()
-            }}
+            onClick={() => handleClose()}
           >
             <CloseIcon />
           </IconButton>
@@ -80,6 +77,10 @@ export default function HelpDialog({
           <li>
             <code>chr1:1-100[rev] chr2:1-100</code> - open up the first region
             in the horizontally flipped orientation
+          </li>
+          <li>
+            <code>chr1 100 200</code> - use whitespace separated refname, start,
+            end
           </li>
         </ul>
       </DialogContent>
--- plugins/linear-genome-view/src/LinearGenomeView/index.tsx
+++ plugins/linear-genome-view/src/LinearGenomeView/index.tsx
@@ -667,13 +667,38 @@ export function stateModelFactory(pluginManager: PluginManager) {
         const { assemblyManager } = getSession(self)
         const { isValidRefName } = assemblyManager
         const assemblyName = optAssemblyName || assemblyNames[0]
+        let parsedLocStrings
+        const inputs = locString
+          .split(/(\s+)/)
+          .map(f => f.trim())
+          .filter(f => !!f)
 
-        const parsedLocStrings = locString
-          .split(' ')
-          .filter(f => !!f.trim())
-          .map(l => parseLocString(l, ref => isValidRefName(ref, assemblyName)))
+        // first try interpreting as a whitespace-separated sequence of
+        // multiple locstrings
+        try {
+          parsedLocStrings = inputs.map(l =>
+            parseLocString(l, ref => isValidRefName(ref, assemblyName)),
+          )
+        } catch (e) {
+          // if this fails, try interpreting as a whitespace-separated refname,
+          // start, end if start and end are integer inputs
+          const [refName, start, end] = inputs
+          if (
+            `${e}`.match(/Unknown reference sequence/) &&
+            Number.isInteger(+start) &&
+            Number.isInteger(+end)
+          ) {
+            parsedLocStrings = [
+              parseLocString(refName + ':' + start + '..' + end, ref =>
+                isValidRefName(ref, assemblyName),
+              ),
+            ]
+          } else {
+            throw e
+          }
+        }
 
-        const locations = parsedLocStrings.map(region => {
+        const locations = parsedLocStrings?.map(region => {
           const asmName = region.assemblyName || assemblyName
           const asm = assemblyManager.get(asmName)
           const { refName } = region
