e13fafc26470645591345e6597b553673a17d900
Colin <colin.diesh@gmail.com>
2022-06-09 15:50:46 -0600

    Remove @mui/styles

--- packages/__mocks__/@testing-library/react.js
+++ packages/__mocks__/@testing-library/react.js
@@ -1,19 +1,11 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { StylesProvider } from '@mui/styles'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 // eslint-disable-next-line import/no-extraneous-dependencies
 import React from 'react'
 
 const react = jest.requireActual('@testing-library/react')
-const generateClassName = (rule, styleSheet) =>
-  `${styleSheet.options.classNamePrefix}-${rule.key}`
 const render = args => {
   return react.render(
-    <ThemeProvider theme={createTheme()}>
-      <StylesProvider generateClassName={generateClassName}>
-        {args}
-      </StylesProvider>
-    </ThemeProvider>,
+    <ThemeProvider theme={createTheme()}>{args}</ThemeProvider>,
   )
 }
 
