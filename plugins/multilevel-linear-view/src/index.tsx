11d42326afb3d35a3ea36f87cebde6485634a2ef
Caroline Bridge <cbridge@oicr.on.ca>
2022-08-23 11:11:59 -0400

    lint

--- plugins/multilevel-linear-view/src/index.tsx
+++ plugins/multilevel-linear-view/src/index.tsx
@@ -1,12 +1,9 @@
 import Plugin from '@jbrowse/core/Plugin'
 import PluginManager from '@jbrowse/core/PluginManager'
 import { AbstractSessionModel, isAbstractMenuManager } from '@jbrowse/core/util'
-import { LinearGenomeViewModel } from '@jbrowse/plugin-linear-genome-view'
 import DynamicFeedIcon from '@mui/icons-material/DynamicFeed'
 import MultilevelLinearViewF from './MultilevelLinearView'
 import LinearGenomeMultilevelViewF from './LinearGenomeMultilevelView'
-import MiniControls from './MultilevelLinearView/components/MiniControls'
-import Header from './MultilevelLinearView/components/Header'
 
 export default class extends Plugin {
   name = 'MultilevelLinearViewPlugin'
