694c0d95f742f08b356e3b0c9488c26a79389ca4
Colin <colin.diesh@gmail.com>
2022-09-21 12:51:49 -0600

    Add hardcoded width

--- component_test/cypress/integration/basic.spec.ts
+++ component_test/cypress/integration/basic.spec.ts
@@ -5,9 +5,10 @@ describe('JBrowse embedded view', () => {
   })
   it('track loads', () => {
     cy.visit('/')
+
     // eslint-disable-next-line testing-library/await-async-query,testing-library/prefer-screen-queries
     cy.findByTestId('Blockset-pileup').findByTestId(
-      'prerendered_canvas_{GRCh38}10:29,838,733..29,838,818-0_done',
+      'prerendered_canvas_{GRCh38}10:29,838,637..29,838,705-0_done',
       { timeout: 10000 },
     )
   })
