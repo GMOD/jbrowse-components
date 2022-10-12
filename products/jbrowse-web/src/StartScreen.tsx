998362b929e818163e1e7966757c2de03c73399d
Colin <colin.diesh@gmail.com>
2022-10-12 13:04:35 -0600

    Misc

--- products/jbrowse-web/src/StartScreen.tsx
+++ products/jbrowse-web/src/StartScreen.tsx
@@ -19,10 +19,13 @@ import {
   Typography,
 } from '@mui/material'
 import { makeStyles } from 'tss-react/mui'
+import { LogoFull, FactoryResetDialog, ErrorMessage } from '@jbrowse/core/ui'
+
+// icons
 import WarningIcon from '@mui/icons-material/Warning'
 import SettingsIcon from '@mui/icons-material/Settings'
 
-import { LogoFull, FactoryResetDialog, ErrorMessage } from '@jbrowse/core/ui'
+// locals
 import {
   NewEmptySession,
   NewLinearGenomeViewSession,
@@ -33,8 +36,8 @@ import RecentSessionCard from './RecentSessionCard'
 const useStyles = makeStyles()(theme => ({
   newSession: {
     backgroundColor: theme.palette?.grey['300'],
-    padding: 8, // theme.spacing(2),
-    marginTop: 8, // theme.spacing(6),
+    padding: 8,
+    marginTop: 8,
   },
   header: {
     margin: 8, // theme.spacing(2),
@@ -106,7 +109,7 @@ export default function StartScreen({
 }) {
   const { classes } = useStyles()
 
-  const [sessions, setSessions] = useState<Record<string, any>>()
+  const [sessionNames, setSessionNames] = useState<string[]>([])
   const [sessionToDelete, setSessionToDelete] = useState<string>()
   const [sessionToLoad, setSessionToLoad] = useState<string>()
   const [error, setError] = useState<unknown>()
@@ -114,7 +117,6 @@ export default function StartScreen({
   const [menuAnchorEl, setMenuAnchorEl] = useState<null | HTMLElement>(null)
   const [reset, setReset] = useState(false)
 
-  const sessionNames = sessions !== undefined ? sessions : []
   useEffect(() => {
     ;(async () => {
       try {
@@ -132,37 +134,26 @@ export default function StartScreen({
       try {
         if (updateSessionsList) {
           setUpdateSessionsList(false)
-          const savedRootSessions = rootModel.savedSessions.map(
-            (rootSavedSession: any) => {
-              return JSON.parse(JSON.stringify(rootSavedSession))?.name
-            },
-          )
-          setSessions(savedRootSessions)
+          setSessionNames(rootModel.savedSessions.map((s: any) => s?.name))
         }
       } catch (e) {
-        setSessions(() => {
-          throw e
-        })
+        setError(e)
       }
     })()
   }, [rootModel.savedSessions, updateSessionsList])
 
-  if (!sessions) {
-    return (
-      <CircularProgress
-        style={{
-          position: 'fixed',
-          top: '50%',
-          left: '50%',
-          marginTop: -25,
-          marginLeft: -25,
-        }}
-        size={50}
-      />
-    )
-  }
-
-  return (
+  return !sessionNames ? (
+    <CircularProgress
+      style={{
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        marginTop: -25,
+        marginLeft: -25,
+      }}
+      size={50}
+    />
+  ) : (
     <>
       <FactoryResetDialog
         open={reset}
@@ -216,12 +207,12 @@ export default function StartScreen({
               maxHeight: 200,
             }}
           >
-            {sessionNames?.map((sessionName: string) => (
+            {sessionNames?.map((name: string) => (
               <RecentSessionCard
-                key={sessionName}
-                sessionName={sessionName}
-                onClick={() => setSessionToLoad(sessionName)}
-                onDelete={() => setSessionToDelete(sessionName)}
+                key={name}
+                sessionName={name}
+                onClick={() => setSessionToLoad(name)}
+                onDelete={() => setSessionToDelete(name)}
               />
             ))}
           </List>
@@ -233,9 +224,7 @@ export default function StartScreen({
         anchorEl={menuAnchorEl}
         keepMounted
         open={Boolean(menuAnchorEl)}
-        onClose={() => {
-          setMenuAnchorEl(null)
-        }}
+        onClose={() => setMenuAnchorEl(null)}
       >
         <ListSubheader>Advanced Settings</ListSubheader>
         <MenuItem
