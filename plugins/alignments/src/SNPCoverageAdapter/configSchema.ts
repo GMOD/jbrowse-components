53b97e4f1d8893c53650f32bf36dcc4d693488a2
Colin <colin.diesh@gmail.com>
2022-11-01 03:11:16 -0600

    Use frozen

--- plugins/alignments/src/SNPCoverageAdapter/configSchema.ts
+++ plugins/alignments/src/SNPCoverageAdapter/configSchema.ts
@@ -1,22 +1,23 @@
 import { ConfigurationSchema } from '@jbrowse/core/configuration'
-import PluginManager from '@jbrowse/core/PluginManager'
 
 /**
  * #config SNPCoverageAdapter
  */
 function x() {} // eslint-disable-line @typescript-eslint/no-unused-vars
 
-const SNPCoverageAdapter = (pluginManager: PluginManager) =>
-  ConfigurationSchema(
-    'SNPCoverageAdapter',
-    {
-      /**
-       * #slot
-       * normally refers to a BAM or CRAM adapter
-       */
-      subadapter: pluginManager.pluggableConfigSchemaType('adapter'),
+const configSchema = ConfigurationSchema(
+  'SNPCoverageAdapter',
+  {
+    /**
+     * #slot
+     * normally refers to a BAM or CRAM adapter
+     */
+    subadapter: {
+      type: 'frozen',
+      defaultValue: null,
     },
-    { explicitlyTyped: true },
-  )
+  },
+  { explicitlyTyped: true },
+)
 
-export default SNPCoverageAdapter
+export default configSchema
