643087ec7f36e6cda96b40c64a9b0e7b67e31448
Colin <colin.diesh@gmail.com>
2022-11-02 09:18:26 -0600

    Loading ellipses component

--- plugins/linear-genome-view/src/BaseLinearDisplay/components/ServerSideRenderedBlockContent.tsx
+++ plugins/linear-genome-view/src/BaseLinearDisplay/components/ServerSideRenderedBlockContent.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { makeStyles } from 'tss-react/mui'
 import { observer } from 'mobx-react'
+import { keyframes } from 'tss-react'
 import { getParent } from 'mobx-state-tree'
 import { LoadingEllipses } from '@jbrowse/core/ui'
 import RefreshIcon from '@mui/icons-material/Refresh'
