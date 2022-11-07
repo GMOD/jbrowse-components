d54632d5a513d0d89aff6639b5d08910505e68ad
Colin <colin.diesh@gmail.com>
2022-11-07 12:25:17 -0700

    Fix warning during Empty.test.tsx when no assemblies configured

--- packages/core/ui/AssemblySelector.tsx
+++ packages/core/ui/AssemblySelector.tsx
@@ -60,7 +60,7 @@ const AssemblySelector = observer(
         label="Assembly"
         variant="outlined"
         helperText={error || 'Select assembly to view'}
-        value={selection}
+        value={selection || ''}
         inputProps={{ 'data-testid': 'assembly-selector' }}
         onChange={event => setLastSelected(event.target.value)}
         error={!!error}
