bc6adcb7e6cceb1b4243948c0cd206cf605a795b
Colin Diesh <colin.diesh@gmail.com>
2022-11-08 15:58:48 -0700

    Fix pileup "sort by" setting being lost on zoom level change (#3319)
    * Fix sort on zoom
    
    * Sort by base pair instead of by strand, easier to see result, and test zoom out retains the sort

--- products/jbrowse-web/src/tests/AlignmentsFeatures.test.tsx
+++ products/jbrowse-web/src/tests/AlignmentsFeatures.test.tsx
@@ -47,54 +47,32 @@ test('opens the track menu and enables soft clipping', async () => {
   )
 }, 30000)
 
-test('selects a sort, sort by strand', async () => {
+test('selects a sort, sort by base pair', async () => {
   console.error = jest.fn()
   const { view, findByTestId, findByText, findAllByTestId } = createView()
   await findByText('Help')
-  view.setNewView(0.02, 2086500)
+  view.setNewView(0.043688891869634636, 301762)
+  const track = 'volvox_cram_alignments_ctga'
 
   // load track
-  fireEvent.click(await findByTestId(hts('volvox-long-reads-cram'), {}, delay))
-  await findByTestId(
-    'display-volvox-long-reads-cram-LinearAlignmentsDisplay',
-    {},
-    delay,
-  )
+  fireEvent.click(await findByTestId(hts(track), {}, delay))
+  await findByTestId(`display-${track}-LinearAlignmentsDisplay`, {}, delay)
   expect(view.tracks[0]).toBeTruthy()
 
   fireEvent.click(await findByTestId('track_menu_icon'))
   fireEvent.click(await findByText('Sort by'))
-  fireEvent.click(await findByText('Read strand'))
+  fireEvent.click(await findByText('Base pair'))
 
   // wait for pileup track to render with sort
-  await findAllByTestId('pileup-Read strand', {}, delay)
+  await findAllByTestId('pileup-Base pair', {}, delay)
   const { findByTestId: find1 } = within(await findByTestId('Blockset-pileup'))
-  expectCanvasMatch(await find1(pc('{volvox}ctgA:41729..41744-0'), {}, delay))
+  expectCanvasMatch(await find1(pc('{volvox}ctgA:13196..13230-0'), {}, delay))
+
+  fireEvent.click(await findByTestId('zoom_out'))
+  await findAllByTestId('pileup-Base pair', {}, delay)
+  const { findByTestId: find2 } = within(await findByTestId('Blockset-pileup'))
+  expectCanvasMatch(await find2(pc('{volvox}ctgA:13161..13230-0'), {}, delay))
 }, 35000)
-
-test('color by strand', async () => {
-  console.error = jest.fn()
-  const { view, findByTestId, findByText, findAllByTestId } = createView()
-  await findByText('Help')
-  view.setNewView(0.02, 2086500)
-
-  // load track
-  fireEvent.click(await findByTestId(hts('volvox-long-reads-cram'), {}, delay))
-  await findByTestId(
-    'display-volvox-long-reads-cram-LinearAlignmentsDisplay',
-    {},
-    delay,
-  )
-
-  fireEvent.click(await findByTestId('track_menu_icon'))
-  fireEvent.click(await findByText('Color scheme'))
-  fireEvent.click(await findByText('Strand'))
-
-  // wait for pileup track to render with color
-  await findAllByTestId('pileup-strand', {}, delay)
-  const { findByTestId: find1 } = within(await findByTestId('Blockset-pileup'))
-  expectCanvasMatch(await find1(pc('{volvox}ctgA:41729..41744-0'), {}, delay))
-}, 30000)
 
 test('color by tag', async () => {
   console.error = jest.fn()
