537cc90a987917d0712502fbaa909807ae583894
Colin Diesh <colin.diesh@gmail.com>
2022-11-17 15:52:31 -0700

    Create new "Add track workflow" that allows pasting JSON track configs (#3277)

--- plugins/data-management/src/AddTrackWidget/components/AddTrackWidget.tsx
+++ plugins/data-management/src/AddTrackWidget/components/AddTrackWidget.tsx
@@ -7,6 +7,7 @@ import { getEnv, useLocalStorage } from '@jbrowse/core/util'
 // locals
 import { AddTrackModel } from '../model'
 import DefaultAddTrackWorkflow from './DefaultAddTrackWorkflow'
+import PasteConfigWorkflow from './PasteConfigWorkflow'
 
 function AddTrackSelector({ model }: { model: AddTrackModel }) {
   const [val, setVal] = useLocalStorage('trackSelector-choice', 'Default')
@@ -16,6 +17,7 @@ function AddTrackSelector({ model }: { model: AddTrackModel }) {
   ) as AddTrackWorkflowType[]
   const ComponentMap = {
     Default: DefaultAddTrackWorkflow,
+    'Add track JSON': PasteConfigWorkflow,
     ...Object.fromEntries(widgets.map(w => [w.name, w.ReactComponent])),
   } as { [key: string]: React.FC<{ model: AddTrackModel }> }
 
