d509556f8d05f62e67b77a31d746a2cc799dba9a
Colin Diesh <colin.diesh@gmail.com>
2022-12-20 10:02:46 -0700

    Auto-adjust height of reference sequence track to current settings (#3422)
    Autorun to auto-adjust the height of reference sequence track

--- plugins/gccontent/src/LinearGCContentDisplay/stateModel.ts
+++ plugins/gccontent/src/LinearGCContentDisplay/stateModel.ts
@@ -6,6 +6,10 @@ import PluginManager from '@jbrowse/core/PluginManager'
 import { linearWiggleDisplayModelFactory } from '@jbrowse/plugin-wiggle'
 import { types } from 'mobx-state-tree'
 
+/**
+ * #stateModel LinearGCContentDisplay
+ * base model BaseWiggleDisplayModel
+ */
 export default function stateModelFactory(
   pluginManager: PluginManager,
   configSchema: AnyConfigurationSchemaType,
@@ -15,12 +19,20 @@ export default function stateModelFactory(
       'LinearGCContentDisplay',
       linearWiggleDisplayModelFactory(pluginManager, configSchema),
       types.model({
+        /**
+         * #property
+         */
         type: types.literal('LinearGCContentDisplay'),
       }),
     )
     .views(self => {
       const { renderProps: superRenderProps } = self
       return {
+        /**
+         * #method
+         * retrieves the sequence adapter from parent track, and puts it as a
+         * subadapter on a GCContentAdapter
+         */
         renderProps() {
           const sequenceAdapter = getConf(self.parentTrack, 'adapter')
           return {
