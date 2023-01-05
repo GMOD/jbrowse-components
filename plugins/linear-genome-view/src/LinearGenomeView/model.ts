015294fecb2af7e018186329dad51a76305faa18
Colin <colin.diesh@gmail.com>
2023-01-05 11:12:13 -0700

    Save track data

--- plugins/linear-genome-view/src/LinearGenomeView/model.ts
+++ plugins/linear-genome-view/src/LinearGenomeView/model.ts
@@ -1706,6 +1706,10 @@ export function stateModelFactory(pluginManager: PluginManager) {
           ? this.pxToBp(self.width / 2)
           : undefined
       },
+
+      get visibleRegions() {
+        return self.dynamicBlocks.contentBlocks
+      },
     }))
     .actions(self => ({
       afterCreate() {
