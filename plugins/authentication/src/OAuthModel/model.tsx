1b8aca386d3cf1696ca684548853f5733b4e3fe1
grzelaka <andrzej.grzelak@contractors.roche.com>
2023-01-18 17:58:28 +0100

    Make refresh token not found message more clear.

--- plugins/authentication/src/OAuthModel/model.tsx
+++ plugins/authentication/src/OAuthModel/model.tsx
@@ -361,7 +361,7 @@ const stateModelFactory = (configSchema: OAuthInternetAccountConfigModel) => {
               }
             }
             throw new Error(
-              `Token could not be refreshed. No refresh token found`,
+              `Token could not be refreshed. No refresh token found. Please reload the page.`,
             )
           } else {
             refreshTokenPromise = undefined
