d21fd50d7023817dccf49468ed0e05f8faf0bcfc
Colin <colin.diesh@gmail.com>
2023-01-27 15:27:11 -0700

    Misc

--- products/jbrowse-react-linear-genome-view/.storybook/preview.js
+++ products/jbrowse-react-linear-genome-view/.storybook/preview.js
@@ -1,11 +1,4 @@
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
   options: {
     storySort: {
       order: [
