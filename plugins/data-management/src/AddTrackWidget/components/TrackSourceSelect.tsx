8e664f347ae4ab05a12a010436ec018a0687d73e
Colin Diesh <colin.diesh@gmail.com>
2023-01-26 23:55:48 -0700

    Larger margins to add track workflow to visually clarify next button behavior (#3477)

--- plugins/data-management/src/AddTrackWidget/components/TrackSourceSelect.tsx
+++ plugins/data-management/src/AddTrackWidget/components/TrackSourceSelect.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FileSelector } from '@jbrowse/core/ui'
+import { AbstractRootModel } from '@jbrowse/core/util'
 import { Paper } from '@mui/material'
 import { makeStyles } from 'tss-react/mui'
 import { getRoot } from 'mobx-state-tree'
@@ -10,9 +11,7 @@ import { AddTrackModel } from '../model'
 
 const useStyles = makeStyles()(theme => ({
   paper: {
-    display: 'flex',
-    flexDirection: 'column',
-    padding: theme.spacing(1),
+    padding: theme.spacing(2),
   },
   spacer: {
     height: theme.spacing(8),
@@ -21,8 +20,7 @@ const useStyles = makeStyles()(theme => ({
 
 function TrackSourceSelect({ model }: { model: AddTrackModel }) {
   const { classes } = useStyles()
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const rootModel = getRoot<any>(model)
+  const rootModel = getRoot<AbstractRootModel>(model)
 
   return (
     <Paper className={classes.paper}>
