1711e86f46ded88cd2a73e12d8c05291ff662f81
Colin <colin.diesh@gmail.com>
2023-01-28 12:19:03 -0700

    Misc

---
+++ plugins/spreadsheet-view/src/SpreadsheetView/components/NumberEditor.tsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react'
+
+import { TextField } from '@mui/material'
+import { makeStyles } from 'tss-react/mui'
+import { observer } from 'mobx-react'
+import { ImportWizardModel } from '../models/ImportWizard'
+
+const useStyles = makeStyles()({
+  textField: {
+    width: '2rem',
+    verticalAlign: 'baseline',
+  },
+})
+
+export default observer(function ({
+  model,
+  disabled,
+  modelPropName,
+  modelSetterName,
+}: {
+  model: ImportWizardModel
+  disabled: boolean
+  modelPropName: string
+  modelSetterName: string
+}) {
+  // @ts-ignore
+  const [val, setVal] = useState(model[modelPropName])
+  const { classes } = useStyles()
+  useEffect(() => {
+    const num = parseInt(val, 10)
+    if (!Number.isNaN(num)) {
+      if (num > 0) {
+        // @ts-ignore
+        model[modelSetterName](num)
+      } else {
+        setVal(1)
+      }
+    }
+  }, [model, modelSetterName, val])
+  return (
+    <TextField
+      value={val}
+      disabled={disabled}
+      type="number"
+      onChange={evt => setVal(evt.target.value)}
+      className={classes.textField}
+    />
+  )
+})
