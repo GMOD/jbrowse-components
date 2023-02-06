86f8ee4e17f4a6c5bd54213df2a758acc1c44658
Colin <colin.diesh@gmail.com>
2023-02-06 11:08:16 -0700

    Revert

--- products/jbrowse-web/src/JBrowse.tsx
+++ products/jbrowse-web/src/JBrowse.tsx
@@ -15,74 +15,74 @@ import ShareButton from './ShareButton'
 import AdminComponent from './AdminComponent'
 import { SessionModel } from './sessionModelFactory'
 
-const JBrowse = observer(function ({
-  pluginManager,
-}: {
-  pluginManager: PluginManager
-}) {
-  const [adminKey] = useQueryParam('adminKey', StringParam)
-  const [adminServer] = useQueryParam('adminServer', StringParam)
-  const [configPath] = useQueryParam('config', StringParam)
-  const [, setSessionId] = useQueryParam('session', StringParam)
-  const { rootModel } = pluginManager
-  const { error, jbrowse } = rootModel || {}
-  const session = rootModel?.session as SessionModel
-  const currentSessionId = session.id
+import './JBrowse.css'
 
-  useEffect(() => {
-    setSessionId(`local-${currentSessionId}`)
-    // @ts-ignore
-    window.JBrowseRootModel = rootModel
-    // @ts-ignore
-    window.JBrowseSession = session
-  }, [currentSessionId, rootModel, session, setSessionId])
+const JBrowse = observer(
+  ({ pluginManager }: { pluginManager: PluginManager }) => {
+    const [adminKey] = useQueryParam('adminKey', StringParam)
+    const [adminServer] = useQueryParam('adminServer', StringParam)
+    const [configPath] = useQueryParam('config', StringParam)
+    const [, setSessionId] = useQueryParam('session', StringParam)
+    const { rootModel } = pluginManager
+    const { error, jbrowse } = rootModel || {}
+    const session = rootModel?.session as SessionModel
+    const currentSessionId = session.id
 
-  useEffect(() => {
-    if (!jbrowse || !adminKey) {
-      return
-    }
-    return onSnapshot(jbrowse, async snapshot => {
-      try {
-        const response = await fetch(adminServer || `/updateConfig`, {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            adminKey,
-            configPath,
-            config: snapshot,
-          }),
-        })
-        if (!response.ok) {
-          const message = await response.text()
-          throw new Error(`HTTP ${response.status} (${message})`)
-        }
-      } catch (e) {
-        session?.notify(`Admin server error: ${e}`)
+    useEffect(() => {
+      setSessionId(`local-${currentSessionId}`)
+      // @ts-ignore
+      window.JBrowseRootModel = rootModel
+      // @ts-ignore
+      window.JBrowseSession = session
+    }, [currentSessionId, rootModel, session, setSessionId])
+
+    useEffect(() => {
+      if (!jbrowse || !adminKey) {
+        return
       }
-    })
-  }, [jbrowse, session, adminKey, adminServer, configPath])
+      return onSnapshot(jbrowse, async snapshot => {
+        try {
+          const response = await fetch(adminServer || `/updateConfig`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              adminKey,
+              configPath,
+              config: snapshot,
+            }),
+          })
+          if (!response.ok) {
+            const message = await response.text()
+            throw new Error(`HTTP ${response.status} (${message})`)
+          }
+        } catch (e) {
+          session?.notify(`Admin server error: ${e}`)
+        }
+      })
+    }, [jbrowse, session, adminKey, adminServer, configPath])
 
-  if (error) {
-    throw error
-  }
-  if (!rootModel) {
-    throw new Error('No rootModel found')
-  }
-  if (!session) {
-    throw new Error('No session found')
-  }
+    if (error) {
+      throw error
+    }
+    if (!rootModel) {
+      throw new Error('No rootModel found')
+    }
+    if (!session) {
+      throw new Error('No session found')
+    }
 
-  const theme = getConf(rootModel.jbrowse, 'theme')
-  return (
-    <ThemeProvider theme={createJBrowseTheme(theme)}>
-      <CssBaseline />
-      <App
-        session={session}
-        HeaderButtons={<ShareButton session={session} />}
-      />
-      {adminKey ? <AdminComponent pluginManager={pluginManager} /> : null}
-    </ThemeProvider>
-  )
-})
+    const theme = getConf(rootModel.jbrowse, 'theme')
+    return (
+      <ThemeProvider theme={createJBrowseTheme(theme)}>
+        <CssBaseline />
+        <App
+          session={session}
+          HeaderButtons={<ShareButton session={session} />}
+        />
+        {adminKey ? <AdminComponent pluginManager={pluginManager} /> : null}
+      </ThemeProvider>
+    )
+  },
+)
 
 export default JBrowse
