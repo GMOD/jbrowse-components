b4cf0f88480cba9e478f207b1f4411cdef3da8ee
Colin <colin.diesh@gmail.com>
2023-02-10 13:00:06 -0700

    Render multibigwig in test

--- plugins/wiggle/src/MultiLinearWiggleDisplay/models/configSchema.ts
+++ plugins/wiggle/src/MultiLinearWiggleDisplay/models/configSchema.ts
@@ -122,6 +122,10 @@ export default function WiggleConfigFactory(pluginManager: PluginManager) {
         MultiLineRenderer: MultiLineRendererConfigSchema,
         MultiRowLineRenderer: MultiRowLineRendererConfigSchema,
       }),
+
+      /**
+       * #slot
+       */
       height: {
         type: 'number',
         defaultValue: 200,
