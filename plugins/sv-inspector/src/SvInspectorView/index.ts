4f6f9269ceb00db383d3312bd02fa5fc5009bc8e
Colin <colin.diesh@gmail.com>
2023-03-06 22:35:03 -0700

    Add ability to show all regions with data from TRA features with CHR2

--- plugins/sv-inspector/src/SvInspectorView/index.ts
+++ plugins/sv-inspector/src/SvInspectorView/index.ts
@@ -3,10 +3,34 @@ import ViewType from '@jbrowse/core/pluggableElementTypes/ViewType'
 
 import ReactComponent from './components/SvInspectorView'
 import stateModelFactory from './models/SvInspectorView'
+import { Feature, getContainingView, getSession } from '@jbrowse/core/util'
+import { IAnyStateTreeNode } from 'mobx-state-tree'
+
+function defaultOnChordClick(
+  feature: Feature,
+  chordTrack: IAnyStateTreeNode,
+  pluginManager: PluginManager,
+) {
+  const session = getSession(chordTrack)
+  session.setSelection(feature)
+  const view = getContainingView(chordTrack)
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const viewType = pluginManager.getViewType('BreakpointSplitView') as any
+  const viewSnapshot = viewType.snapshotFromBreakendFeature(feature, view)
+
+  // try to center the offsetPx
+  viewSnapshot.views[0].offsetPx -= view.width / 2 + 100
+  viewSnapshot.views[1].offsetPx -= view.width / 2 + 100
+  viewSnapshot.featureData = feature.toJSON()
+
+  session.addView('BreakpointSplitView', viewSnapshot)
+}
 
 export default (pluginManager: PluginManager) => {
+  pluginManager.jexl.addFunction('defaultOnChordClick', defaultOnChordClick)
+
   pluginManager.addViewType(() => {
-    const { stateModel } = stateModelFactory(pluginManager)
+    const stateModel = stateModelFactory(pluginManager)
     return new ViewType({
       name: 'SvInspectorView',
       displayName: 'SV inspector',
