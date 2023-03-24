504cb1d7893b9c2532c25191801981d8866a0671
Colin <colin.diesh@gmail.com>
2023-03-23 21:58:13 -0600

    Remove unneeded ui from cgv test too

--- component_tests/lgv/cypress/integration/basic.spec.ts
+++ component_tests/lgv/cypress/integration/basic.spec.ts
@@ -1,14 +1,10 @@
 describe('JBrowse embedded view', () => {
-  it('page loads', () => {
-    cy.visit('/')
-    cy.contains('JBrowse 2 React Linear Genome View Demo')
-  })
   it('track loads', () => {
     cy.visit('/')
 
     // eslint-disable-next-line testing-library/await-async-query,testing-library/prefer-screen-queries
     cy.findByTestId('Blockset-pileup', { timeout: 30000 }).findByTestId(
-      'prerendered_canvas_{volvox}ctgA:1..51-0_done',
+      'prerendered_canvas_{volvox}ctgA:1..82-0_done',
       { timeout: 30000 },
     )
   })
