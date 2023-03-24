b766f037fd1854cd3a276391f7e95da7f54eeed0
Colin Diesh <colin.diesh@gmail.com>
2023-03-23 23:27:44 -0600

    Use local data instead of remote data for LGV component test  (#3611)

--- component_tests/lgv/cypress/integration/basic.spec.ts
+++ component_tests/lgv/cypress/integration/basic.spec.ts
@@ -1,20 +1,10 @@
 describe('JBrowse embedded view', () => {
-  it('page loads', () => {
-    Cypress.on(
-      // from https://github.com/cypress-io/cypress/issues/8418
-      'uncaught:exception',
-      // @ts-expect-error thinks we are es5 and don't have includes, may be able to update
-      err => !err.message.includes('ResizeObserver loop limit exceeded'),
-    )
-    cy.visit('/')
-    cy.contains('JBrowse 2 React Linear Genome View Demo')
-  })
   it('track loads', () => {
     cy.visit('/')
 
     // eslint-disable-next-line testing-library/await-async-query,testing-library/prefer-screen-queries
     cy.findByTestId('Blockset-pileup', { timeout: 30000 }).findByTestId(
-      'prerendered_canvas_{GRCh38}10:29838637..29838705-0_done',
+      'prerendered_canvas_{volvox}ctgA:1..82-0_done',
       { timeout: 30000 },
     )
   })
--- component_tests/lgv/src/tracks.ts
+++ component_tests/lgv/src/tracks.ts
@@ -1,94 +1,25 @@
 const tracks = [
   {
-    type: 'BasicTrack',
-    trackId:
-      'GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.sorted.gff',
-    name: 'NCBI RefSeq Genes',
-    assemblyNames: ['GRCh38'],
-    category: ['Genes'],
-    adapter: {
-      type: 'Gff3TabixAdapter',
-      gffGzLocation: {
-        uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/ncbi_refseq/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.sorted.gff.gz',
-      },
-      index: {
-        location: {
-          uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/ncbi_refseq/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.sorted.gff.gz.tbi',
-        },
-      },
-    },
-  },
-  {
     type: 'AlignmentsTrack',
-    trackId: 'NA12878.alt_bwamem_GRCh38DH.20150826.CEU.exome',
-    name: 'NA12878 Exome',
-    assemblyNames: ['GRCh38'],
-    category: ['1000 Genomes', 'Alignments'],
+    trackId: 'volvox_cram',
+    name: 'volvox-sorted.cram',
+    assemblyNames: ['volvox'],
+    category: ['Alignments'],
     adapter: {
       type: 'CramAdapter',
       cramLocation: {
-        uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/alignments/NA12878/NA12878.alt_bwamem_GRCh38DH.20150826.CEU.exome.cram',
+        uri: 'volvox-sorted.cram',
+        locationType: 'UriLocation',
       },
       craiLocation: {
-        uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/alignments/NA12878/NA12878.alt_bwamem_GRCh38DH.20150826.CEU.exome.cram.crai',
+        uri: 'volvox-sorted.cram.crai',
+        locationType: 'UriLocation',
       },
       sequenceAdapter: {
-        type: 'BgzipFastaAdapter',
-        fastaLocation: {
-          uri: 'https://jbrowse.org/genomes/GRCh38/fasta/hg38.prefix.fa.gz',
-        },
-        faiLocation: {
-          uri: 'https://jbrowse.org/genomes/GRCh38/fasta/hg38.prefix.fa.gz.fai',
-        },
-        gziLocation: {
-          uri: 'https://jbrowse.org/genomes/GRCh38/fasta/hg38.prefix.fa.gz.gzi',
-        },
-      },
-    },
-  },
-  {
-    type: 'FeatureTrack',
-    trackId: 'repeats_hg38',
-    name: 'Repeats',
-    assemblyNames: ['hg38'],
-    category: ['Annotation'],
-    adapter: {
-      type: 'BigBedAdapter',
-      bigBedLocation: {
-        uri: 'https://jbrowse.org/genomes/GRCh38/repeats.bb',
-        locationType: 'UriLocation',
-      },
-    },
-  },
-  {
-    type: 'FeatureTrack',
-    trackId: 'clinvar_cnv_hg38',
-    name: 'ClinVar CNV (UCSC)',
-    assemblyNames: ['hg38'],
-    category: ['Annotation'],
-    adapter: {
-      type: 'BigBedAdapter',
-      bigBedLocation: {
-        uri: 'https://hgdownload.soe.ucsc.edu/gbdb/hg38/bbi/clinvar/clinvarCnv.bb',
-        locationType: 'UriLocation',
-      },
-    },
-  },
-  {
-    type: 'VariantTrack',
-    trackId:
-      'ALL.wgs.shapeit2_integrated_snvindels_v2a.GRCh38.27022019.sites.vcf',
-    name: '1000 Genomes Variant Calls',
-    assemblyNames: ['GRCh38'],
-    category: ['1000 Genomes', 'Variants'],
-    adapter: {
-      type: 'VcfTabixAdapter',
-      vcfGzLocation: {
-        uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/variants/ALL.wgs.shapeit2_integrated_snvindels_v2a.GRCh38.27022019.sites.vcf.gz',
-      },
-      index: {
-        location: {
-          uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/variants/ALL.wgs.shapeit2_integrated_snvindels_v2a.GRCh38.27022019.sites.vcf.gz.tbi',
+        type: 'TwoBitAdapter',
+        twoBitLocation: {
+          uri: 'volvox.2bit',
+          locationType: 'UriLocation',
         },
       },
     },
