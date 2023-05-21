12399719251b6f7639b2840b615d74b02e6e0b51
Colin <colin.diesh@gmail.com>
2023-05-20 18:31:36 -0600

    Refactor app component

--- packages/app-core/src/ui/App.tsx
+++ packages/app-core/src/ui/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { AppBar, Fab, Tooltip } from '@mui/material'
+import React, { Suspense, lazy } from 'react'
+import { AppBar } from '@mui/material'
 import { makeStyles } from 'tss-react/mui'
 import { observer } from 'mobx-react'
 import {
@@ -10,15 +10,14 @@ import {
 import Snackbar from '@jbrowse/core/ui/Snackbar'
 import { MenuItem as JBMenuItem } from '@jbrowse/core/ui/Menu'
 
-// icons
-import LaunchIcon from '@mui/icons-material/Launch'
-
 // locals
-import DrawerWidget from './DrawerWidget'
 import AppToolbar from './AppToolbar'
 import ViewLauncher from './ViewLauncher'
 import ViewPanel from './ViewPanel'
 import DialogQueue from './DialogQueue'
+import AppFab from './AppFab'
+
+const DrawerWidget = lazy(() => import('./DrawerWidget'))
 
 const useStyles = makeStyles()(theme => ({
   root: {
@@ -28,33 +27,19 @@ const useStyles = makeStyles()(theme => ({
     width: '100%',
     colorScheme: theme.palette.mode,
   },
-  fabLeft: {
-    zIndex: 10000,
-    position: 'fixed',
-    bottom: theme.spacing(2),
-    left: theme.spacing(2),
-  },
-  fabRight: {
-    zIndex: 10000,
-    position: 'fixed',
-    bottom: theme.spacing(2),
-    right: theme.spacing(2),
-  },
-  menuBarAndComponents: {
+  appContainer: {
     gridColumn: 'main',
     display: 'grid',
     gridTemplateRows: '[menubar] min-content [components] auto',
     height: '100vh',
   },
-  menuBar: {
-    gridRow: 'menubar',
-  },
-  components: {
+  viewContainer: {
     overflowY: 'auto',
     gridRow: 'components',
   },
   appBar: {
     flexGrow: 1,
+    gridRow: 'menubar',
   },
 }))
 
@@ -71,79 +56,21 @@ type Props = {
   }
 }
 
-const App = observer(function (props: Props) {
+const LazyDrawerWidget = observer(function (props: Props) {
   const { session } = props
-  const { classes } = useStyles()
-
-  const {
-    minimized,
-    visibleWidget,
-    drawerWidth,
-    activeWidgets,
-    drawerPosition,
-  } = session
-
-  const drawerVisible = visibleWidget && !minimized
-
-  const d = drawerVisible ? `[drawer] ${drawerWidth}px` : undefined
-  const grid =
-    drawerPosition === 'right' ? ['[main] 1fr', d] : [d, '[main] 1fr']
-
   return (
-    <div
-      className={classes.root}
-      style={{ gridTemplateColumns: grid?.filter(f => !!f).join(' ') }}
-    >
-      {drawerVisible && drawerPosition === 'left' ? (
-        <DrawerWidget session={session} />
-      ) : null}
-      <DialogQueue session={session} />
-      <AppContainer {...props} />
-
-      {activeWidgets.size > 0 && minimized ? (
-        <Tooltip title="Open drawer widget">
-          <Fab
-            className={
-              drawerPosition === 'right' ? classes.fabRight : classes.fabLeft
-            }
-            color="primary"
-            data-testid="drawer-maximize"
-            onClick={() => session.showWidgetDrawer()}
-          >
-            <LaunchIcon />
-          </Fab>
-        </Tooltip>
-      ) : null}
-
-      {drawerVisible && drawerPosition === 'right' ? (
-        <DrawerWidget session={session} />
-      ) : null}
-
-      <Snackbar session={session} />
-    </div>
+    <Suspense fallback={<React.Fragment />}>
+      <DrawerWidget session={session} />
+    </Suspense>
   )
 })
 
-const AppContainer = observer(function AppContainer(props: Props) {
-  const { classes } = useStyles()
-  return (
-    <div className={classes.menuBarAndComponents}>
-      <div className={classes.menuBar}>
-        <AppBar className={classes.appBar} position="static">
-          <AppToolbar {...props} />
-        </AppBar>
-      </div>
-      <ViewContainer {...props} />
-    </div>
-  )
-})
-
-const ViewContainer = observer(function ViewContainer(props: Props) {
+const ViewContainer = observer(function (props: Props) {
   const { session } = props
   const { views } = session
   const { classes } = useStyles()
   return (
-    <div className={classes.components}>
+    <div className={classes.viewContainer}>
       {views.length > 0 ? (
         views.map(view => (
           <ViewPanel key={`view-${view.id}`} view={view} session={session} />
@@ -154,6 +81,41 @@ const ViewContainer = observer(function ViewContainer(props: Props) {
 
       {/* blank space at the bottom of screen allows scroll */}
       <div style={{ height: 300 }} />
+    </div>
+  )
+})
+
+const App = observer(function (props: Props) {
+  const { session } = props
+  const { classes } = useStyles()
+  const { minimized, visibleWidget, drawerWidth, drawerPosition } = session
+  const drawerVisible = visibleWidget && !minimized
+  const d = drawerVisible ? `[drawer] ${drawerWidth}px` : undefined
+  const grid =
+    drawerPosition === 'right' ? ['[main] 1fr', d] : [d, '[main] 1fr']
+
+  return (
+    <div
+      className={classes.root}
+      style={{ gridTemplateColumns: grid?.filter(f => !!f).join(' ') }}
+    >
+      {drawerVisible && drawerPosition === 'left' ? (
+        <LazyDrawerWidget session={session} />
+      ) : null}
+      <DialogQueue session={session} />
+      <div className={classes.appContainer}>
+        <AppBar className={classes.appBar} position="static">
+          <AppToolbar {...props} />
+        </AppBar>
+        <ViewContainer {...props} />
+      </div>
+      <AppFab session={session} />
+
+      {drawerVisible && drawerPosition === 'right' ? (
+        <LazyDrawerWidget session={session} />
+      ) : null}
+
+      <Snackbar session={session} />
     </div>
   )
 })
