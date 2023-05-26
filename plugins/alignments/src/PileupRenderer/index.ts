e930e15c2b48df6263f752bd1f37868046da375d
Colin <colin.diesh@gmail.com>
2023-05-25 19:42:49 -0600

    Bump aws github action version and don't render lazyload rendering class (possibly causes rare errors related to not having a suspense)

--- plugins/alignments/src/PileupRenderer/index.ts
+++ plugins/alignments/src/PileupRenderer/index.ts
@@ -1,14 +1,14 @@
 import PluginManager from '@jbrowse/core/PluginManager'
 import PileupRenderer from './PileupRenderer'
 import configSchema from './configSchema'
-import { lazy } from 'react'
+import ReactComponent from './components/PileupRendering'
 
 export default function register(pluginManager: PluginManager) {
   pluginManager.addRendererType(() => {
     return new PileupRenderer({
       name: 'PileupRenderer',
       displayName: 'Pileup renderer',
-      ReactComponent: lazy(() => import('./components/PileupRendering')),
+      ReactComponent,
       configSchema,
       pluginManager,
     })
