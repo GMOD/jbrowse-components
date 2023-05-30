c4acc915a428f3f81dff6dee37685bed06d675ab
Colin <colin.diesh@gmail.com>
2023-05-30 13:12:49 -0600

    [skip ci] Bad style from 'html' level css bleeds into our all:initial container

--- products/jbrowse-react-linear-genome-view/stories/examples/WithOutsideStyling.tsx
+++ products/jbrowse-react-linear-genome-view/stories/examples/WithOutsideStyling.tsx
@@ -11,21 +11,26 @@ export const WithOutsideStyling = () => {
   })
 
   return (
-    <div style={{ textAlign: 'center', fontFamily: 'monospace' }}>
-      <p>
-        This parent container has textAlign:&apos;center&apos; and a monospace
-        font, but these attributes are not affecting the internal LGV
-      </p>
-      <p>
-        The react component takes measures to avoid being affected by styles
-        outside of it on the page. In this case, a font and a text align, which
-        without measures could affect the jbrowse-react-linear-genome-view
-        component, is fine here.
-      </p>
-      <JBrowseLinearGenomeView viewState={state} />
-      <a href="https://github.com/GMOD/jbrowse-components/blob/main/products/jbrowse-react-linear-genome-view/stories/examples/WithOutsideStyling.tsx">
-        Source code
-      </a>
-    </div>
+    <>
+      <style
+        dangerouslySetInnerHTML={{ __html: `html { font-size: 10px; }` }}
+      ></style>
+      <div style={{ textAlign: 'center', fontFamily: 'monospace' }}>
+        <p>
+          This parent container has textAlign:&apos;center&apos; and a monospace
+          font, but these attributes are not affecting the internal LGV
+        </p>
+        <p>
+          The react component takes measures to avoid being affected by styles
+          outside of it on the page. In this case, a font and a text align,
+          which without measures could affect the
+          jbrowse-react-linear-genome-view component, is fine here.
+        </p>
+        <JBrowseLinearGenomeView viewState={state} />
+        <a href="https://github.com/GMOD/jbrowse-components/blob/main/products/jbrowse-react-linear-genome-view/stories/examples/WithOutsideStyling.tsx">
+          Source code
+        </a>
+      </div>
+    </>
   )
 }
