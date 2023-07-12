a9e2283fb043d88b03bff0eb1dca6d7c359e3c78
Garrett Stevens <stevens.garrett.j@gmail.com>
2023-07-12 10:55:49 -0600

    Allow workers to load CJS plugins
    Co-authored-by: Colin <colin.diesh@gmail.com>

--- products/jbrowse-desktop/src/util.tsx
+++ products/jbrowse-desktop/src/util.tsx
@@ -1,12 +1,11 @@
+import fsPromises from 'fs/promises'
+import path from 'path'
+import os from 'os'
+
 import { LoadedPlugin } from '@jbrowse/core/PluginLoader'
 import sanitize from 'sanitize-filename'
 
 export async function fetchCJS(url: string): Promise<LoadedPlugin> {
-  const fs: typeof import('fs') = window.require('fs')
-  const path: typeof import('path') = window.require('path')
-  const os: typeof import('os') = window.require('os')
-  const http: typeof import('http') = window.require('http')
-  const fsPromises = fs.promises
   // On macOS `os.tmpdir()` returns the path to a symlink, see:
   // https://github.com/nodejs/node/issues/11422
   const tmpDir = await fsPromises.mkdtemp(
@@ -16,19 +15,20 @@ export async function fetchCJS(url: string): Promise<LoadedPlugin> {
     const pluginLocation = path.join(tmpDir, sanitize(url))
     const pluginLocationRelative = path.relative('.', pluginLocation)
 
-    await new Promise((resolve, reject) => {
-      const file = fs.createWriteStream(pluginLocation)
-      http
-        .get(url, res => {
-          res.pipe(file)
-          file.on('finish', resolve)
-        })
-        .on('error', err => {
-          fs.unlinkSync(pluginLocation)
-          reject(err)
-        })
-    })
-    return window.require(pluginLocationRelative) as LoadedPlugin
+    let pluginText = ''
+    try {
+      const pluginResponse = await fetch(url)
+      if (!pluginResponse.ok) {
+        throw new Error('Bad response')
+      }
+      pluginText = await pluginResponse.text()
+    } catch (error) {
+      console.error(error)
+      await fsPromises.unlink(pluginLocation)
+      throw error
+    }
+    await fsPromises.writeFile(pluginLocation, pluginText)
+    return globalThis.require(pluginLocationRelative) as LoadedPlugin
   } finally {
     await fsPromises.rmdir(tmpDir, { recursive: true })
   }
