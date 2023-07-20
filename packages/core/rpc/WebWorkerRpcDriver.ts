45f48d863fbfd2b2e29e7435b71050faa14dee4e
Colin <colin.diesh@gmail.com>
2023-07-20 11:36:38 -0400

    Silence warnings from jbrowse-web startup

--- packages/core/rpc/WebWorkerRpcDriver.ts
+++ packages/core/rpc/WebWorkerRpcDriver.ts
@@ -59,6 +59,9 @@ export default class WebWorkerRpcDriver extends BaseRpcDriver {
     // load balancing rather than using librpc's builtin round-robin
     const instance = this.makeWorkerInstance()
 
+    // @ts-ignore this is used to avoid warning in jbrowse-web startup
+    // (because this file is referred to via src, declare.d.ts file is in
+    // e.g. products/jbrowse-web)
     const worker = new WebWorkerHandle({ workers: [instance] })
     const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
     if (isSafari) {
