9e468172d9409a608dcf50c8b0419314b6ec3714
Colin <colin.diesh@gmail.com>
2023-08-24 10:15:22 -0400

    Back to reasonable chunk sizes

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -96,7 +96,7 @@ module.exports = function (webpackEnv) {
                   [
                     require.resolve('babel-preset-react-app'),
                     {
-                      runtime: hasJsxRuntime ? 'automatic' : 'classic',
+                      runtime: 'automatic',
                     },
                   ],
                 ],
