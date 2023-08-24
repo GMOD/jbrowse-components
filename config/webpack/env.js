4d5e853e57ca8c6f4cb0c01c612fa6d5b8bd0168
Colin <colin.diesh@gmail.com>
2023-08-24 10:12:13 -0400

    Revert "Remove root babel config"
    This reverts commit 10d1821d76050d7e2a7caeefb3e758b0932aadfc.

--- config/webpack/env.js
+++ config/webpack/env.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const fs = require('fs')
 const path = require('path')
 const paths = require('./paths')
@@ -30,7 +32,7 @@ const dotenvFiles = [
 // https://github.com/motdotla/dotenv-expand
 dotenvFiles.forEach(dotenvFile => {
   if (fs.existsSync(dotenvFile)) {
-    require('dotenv-expand').expand(
+    require('dotenv-expand')(
       require('dotenv').config({
         path: dotenvFile,
       }),
--- products/jbrowse-web/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [
-    '@babel/preset-react',
-    '@babel/preset-env',
-    '@babel/preset-typescript',
-  ],
-}
