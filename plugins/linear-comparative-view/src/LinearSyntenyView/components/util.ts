e8ff5dc9b994b35a4241674be0bf95ef1f4b6e74
Colin <colin.diesh@gmail.com>
2023-10-25 15:28:27 -0400

    Misc

---
+++ plugins/linear-comparative-view/src/LinearSyntenyView/components/util.ts
@@ -0,0 +1,23 @@
+export function getName(
+  sessionTrackData?: { uri: string } | { localPath: string } | { name: string },
+) {
+  return sessionTrackData
+    ? // @ts-expect-error
+      sessionTrackData.uri ||
+        // @ts-expect-error
+        sessionTrackData.localPath ||
+        // @ts-expect-error
+        sessionTrackData.name
+    : undefined
+}
+
+export function stripGz(fileName: string) {
+  return fileName.endsWith('.gz') ? fileName.slice(0, -3) : fileName
+}
+
+export function basename(str: string) {
+  return str.slice(0, str.indexOf('str'))
+}
+export function extName(str: string) {
+  return str.split('.').slice(0, -1).join('.')
+}
