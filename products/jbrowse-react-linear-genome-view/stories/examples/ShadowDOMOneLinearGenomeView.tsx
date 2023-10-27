7d61fd00e53166ba84e50fda4a013c68e77e1132
Colin <colin.diesh@gmail.com>
2023-10-27 12:29:01 -0400

    Small storybook simplification

--- products/jbrowse-react-linear-genome-view/stories/examples/ShadowDOMOneLinearGenomeView.tsx
+++ products/jbrowse-react-linear-genome-view/stories/examples/ShadowDOMOneLinearGenomeView.tsx
@@ -18,80 +18,74 @@ const ShadowComponent = () => {
   const [cacheNode, setCacheNode] = useState(null)
   const [config, setConfig] = useState(null)
   useEffect(() => {
-    if (node.current) {
-      const { assembly, tracks } = getVolvoxConfig()
-      const root = node.current.attachShadow({ mode: 'open' })
-      setRootNode(root)
-      setCacheNode(
-        createCache({
-          key: 'react-shadow',
-          prepend: true,
-          container: root,
-        }),
-      )
-      setConfig(
-        createViewState({
-          assembly: assembly,
-          tracks: tracks,
-          location: 'ctgA:1105..1221',
-          onChange: patch => {
-            console.log('patch', patch)
-          },
+    if (!node.current) {
+      return
+    }
+    const { assembly, tracks } = getVolvoxConfig()
+    const root = node.current.attachShadow({ mode: 'open' })
+    setRootNode(root)
+    setCacheNode(
+      createCache({
+        key: 'react-shadow',
+        prepend: true,
+        container: root,
+      }),
+    )
 
-          configuration: {
-            theme: {
-              palette: {
-                primary: {
-                  main: '#4400a6',
+    // see https://mui.com/material-ui/guides/shadow-dom/ for
+    // defaultProps.container note
+    setConfig(
+      createViewState({
+        assembly: assembly,
+        tracks: tracks,
+        location: 'ctgA:1105..1221',
+        onChange: patch => {
+          console.log('patch', patch)
+        },
+
+        configuration: {
+          theme: {
+            palette: {
+              primary: {
+                main: '#4400a6',
+              },
+            },
+            components: {
+              MuiPopover: {
+                defaultProps: {
+                  container: () => nodeForPin.current,
                 },
               },
-              components: {
-                MuiPopover: {
-                  defaultProps: {
-                    container: () => {
-                      return nodeForPin.current
-                    },
+              MuiPopper: {
+                defaultProps: {
+                  container: () => nodeForPin.current,
+                },
+              },
+              MuiTooltip: {
+                defaultProps: {
+                  popperprops: {
+                    container: () => nodeForPin.current,
                   },
                 },
-                MuiPopper: {
-                  defaultProps: {
-                    container: () => {
-                      return nodeForPin.current
-                    },
-                  },
+              },
+              MuiModal: {
+                defaultProps: {
+                  container: () => nodeForPin.current,
                 },
-                MuiTooltip: {
-                  defaultProps: {
-                    popperprops: {
-                      container: () => nodeForPin.current,
-                    },
-                  },
+              },
+              MuiMenu: {
+                defaultProps: {
+                  container: () => nodeForPin.current,
                 },
-                MuiModal: {
-                  defaultProps: {
-                    container: () => {
-                      return nodeForPin.current
-                    },
-                  },
-                },
-                MuiMenu: {
-                  defaultProps: {
-                    container: () => {
-                      return nodeForPin.current
-                    },
-                  },
-                },
-                MuiPaper: {
-                  defaultProps: {
-                    component: nodeForPin.current,
-                  },
-                },
+              },
+              MuiPaper: {
+                defaultProps: () => nodeForPin.current,
               },
             },
           },
-        }),
-      )
-    }
+        },
+      }),
+    )
   }, [])
   return (
     <Fragment>
@@ -99,9 +93,7 @@ const ShadowComponent = () => {
         {rootNode &&
           createPortal(
             <CacheProvider value={cacheNode}>
-              <JBrowseLinearGenomeView
-                viewState={config}
-              ></JBrowseLinearGenomeView>
+              <JBrowseLinearGenomeView viewState={config} />
               <div ref={nodeForPin} />
             </CacheProvider>,
             rootNode,
@@ -116,7 +108,6 @@ const JBrowseCustom = () => {
 }
 
 export const ShadowDOMOneLinearGenomeView = () => {
-  window.Buffer = Buffer
   if (customElements.get('jbrowse-linear-view') === undefined) {
     customElements.define('jbrowse-linear-view', r2wc(JBrowseCustom))
   }
