16d8e4d124b20ec2629b09eba63878190a507181
Colin <colin.diesh@gmail.com>
2023-11-21 23:27:25 -0500

    Use cascading menu button for shopping cart

--- plugins/data-management/src/HierarchicalTrackSelectorWidget/components/ShoppingCart.tsx
+++ plugins/data-management/src/HierarchicalTrackSelectorWidget/components/ShoppingCart.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react'
-import { Badge, IconButton } from '@mui/material'
-import { makeStyles } from 'tss-react/mui'
+import React from 'react'
+import { Badge } from '@mui/material'
 import { observer } from 'mobx-react'
-import JBrowseMenu, { MenuItem } from '@jbrowse/core/ui/Menu'
+import { MenuItem } from '@jbrowse/core/ui/Menu'
 import { getSession, getEnv } from '@jbrowse/core/util'
 
 // icons
@@ -10,66 +9,37 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
 
 // locals
 import { HierarchicalTrackSelectorModel } from '../model'
-
-const useStyles = makeStyles()(theme => ({
-  searchBox: {
-    margin: theme.spacing(2),
-  },
-  menuIcon: {
-    marginRight: theme.spacing(1),
-    marginBottom: 0,
-  },
-}))
+import CascadingMenuButton from '@jbrowse/core/ui/CascadingMenuButton'
 
 const ShoppingCart = observer(function ({
   model,
 }: {
   model: HierarchicalTrackSelectorModel
 }) {
-  const { classes } = useStyles()
   const { selection } = model
   const { pluginManager } = getEnv(model)
   const session = getSession(model)
-  const [selectionEl, setSelectionEl] = useState<HTMLButtonElement>()
   const items = pluginManager.evaluateExtensionPoint(
     'TrackSelector-multiTrackMenuItems',
     [],
     { session },
   ) as MenuItem[]
 
-  return (
-    <>
-      {selection.length ? (
-        <IconButton
-          className={classes.menuIcon}
-          onClick={event => setSelectionEl(event.currentTarget)}
-        >
-          <Badge badgeContent={selection.length} color="primary">
-            <ShoppingCartIcon />
-          </Badge>
-        </IconButton>
-      ) : null}
-
-      <JBrowseMenu
-        anchorEl={selectionEl}
-        open={Boolean(selectionEl)}
-        onMenuItemClick={(_, callback) => {
-          callback()
-          setSelectionEl(undefined)
-        }}
-        onClose={() => setSelectionEl(undefined)}
-        menuItems={[
-          { label: 'Clear', onClick: () => model.clearSelection() },
-          ...items.map(item => ({
-            ...item,
-            ...('onClick' in item
-              ? { onClick: () => item.onClick(model) }
-              : {}),
-          })),
-        ]}
-      />
-    </>
-  )
+  return selection.length ? (
+    <CascadingMenuButton
+      menuItems={[
+        { label: 'Clear', onClick: () => model.clearSelection() },
+        ...items.map(item => ({
+          ...item,
+          ...('onClick' in item ? { onClick: () => item.onClick(model) } : {}),
+        })),
+      ]}
+    >
+      <Badge badgeContent={selection.length} color="primary">
+        <ShoppingCartIcon />
+      </Badge>
+    </CascadingMenuButton>
+  ) : null
 })
 
 export default ShoppingCart
