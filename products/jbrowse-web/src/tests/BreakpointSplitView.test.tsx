bd2c2b2a149ee5f2d6678ce7c0ea79e353c5e39d
Colin <colin.diesh@gmail.com>
2023-11-30 09:55:20 -0500

    Remove curlies to try to fix flaky breakpoint split view test

--- products/jbrowse-web/src/tests/BreakpointSplitView.test.tsx
+++ products/jbrowse-web/src/tests/BreakpointSplitView.test.tsx
@@ -20,15 +20,18 @@ test(
         await createView(breakpointConfig)
 
       // the breakpoint could be partially loaded so explicitly wait for two items
-      await waitFor(() => {
-        expect(queryAllByTestId('r1').length).toBe(2)
-      }, delay)
-      await waitFor(() => {
-        expect(getByTestId('pacbio_hg002_breakpoints-loaded')).toMatchSnapshot()
-      }, delay)
-      await waitFor(() => {
-        expect(getByTestId('pacbio_vcf-loaded')).toMatchSnapshot()
-      }, delay)
+      await waitFor(() => expect(queryAllByTestId('r1').length).toBe(2), delay)
+      await waitFor(
+        () =>
+          expect(
+            getByTestId('pacbio_hg002_breakpoints-loaded'),
+          ).toMatchSnapshot(),
+        delay,
+      )
+      await waitFor(
+        () => expect(getByTestId('pacbio_vcf-loaded')).toMatchSnapshot(),
+        delay,
+      )
     }),
   40000,
 )
