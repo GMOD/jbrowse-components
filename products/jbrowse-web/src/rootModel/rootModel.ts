4292bf8fb215e9412acb880992e8c49fa62ad2c9
Colin <colin.diesh@gmail.com>
2023-12-02 11:00:55 -0500

    [update docs] Some more unused

--- products/jbrowse-web/src/rootModel/rootModel.ts
+++ products/jbrowse-web/src/rootModel/rootModel.ts
@@ -70,11 +70,10 @@ type SessionModelFactory = (args: {
  * #stateModel JBrowseWebRootModel
  *
  * composed of
- * - BaseRootModel
- * - InternetAccountsMixin
- * - HistoryManagementMixin
- * - AppFocusMixin
- * - RootAppMenuMixin
+ * - [BaseRootModel](../baserootmodel)
+ * - [InternetAccountsMixin](../internetaccountsmixin)
+ * - [HistoryManagementMixin](../historymanagementmixin)
+ * - [RootAppMenuMixin](../rootappmenumixin)
  *
  * note: many properties of the root model are available through the session,
  * and we generally prefer using the session model (via e.g. getSession) over
--- webpack/config/webpack.config.js
+++ webpack/config/webpack.config.js
@@ -43,10 +43,6 @@ function getWorkspaces(fromDir) {
 // the chunk makes for a smoother build process.
 const shouldInlineRuntimeChunk = process.env.INLINE_RUNTIME_CHUNK !== 'false'
 
-const imageInlineSizeLimit = parseInt(
-  process.env.IMAGE_INLINE_SIZE_LIMIT || '10000',
-)
-
 // style files regexes
 const cssRegex = /\.css$/
 const cssModuleRegex = /\.module\.css$/
@@ -148,28 +144,29 @@ module.exports = function (webpackEnv) {
       level: 'none',
     },
     resolve: {
-      // This allows you to set a fallback for where webpack should look for modules.
-      // We placed these paths second because we want `node_modules` to "win"
-      // if there are any conflicts. This matches Node resolution mechanism.
+      // This allows you to set a fallback for where webpack should look for
+      // modules. We placed these paths second because we want `node_modules`
+      // to "win" if there are any conflicts. This matches Node resolution
+      // mechanism.
       // https://github.com/facebook/create-react-app/issues/253
       modules: ['node_modules', paths.appNodeModules].concat(
         modules.additionalModulePaths || [],
       ),
-      // These are the reasonable defaults supported by the Node ecosystem.
-      // We also include JSX as a common component filename extension to support
+      // These are the reasonable defaults supported by the Node ecosystem. We
+      // also include JSX as a common component filename extension to support
       // some tools, although we do not recommend using it, see:
-      // https://github.com/facebook/create-react-app/issues/290
-      // `web` extension prefixes have been added for better support
-      // for React Native Web.
-      extensions: paths.moduleFileExtensions
-        .map(ext => `.${ext}`)
-        .filter(ext => useTypeScript || !ext.includes('ts')),
+      // https://github.com/facebook/create-react-app/issues/290 `web`
+      // extension prefixes have been added for better support for React Native
+      // Web.
+      extensions: paths.moduleFileExtensions.map(ext => `.${ext}`),
       plugins: [
-        // Prevents users from importing files from outside of src/ (or node_modules/).
-        // This often causes confusion because we only process files within src/ with babel.
-        // To fix this, we prevent you from importing files out of src/ -- if you'd like to,
-        // please link the files into your node_modules/ and let module-resolution kick in.
-        // Make sure your source files are compiled, as they will not be processed in any way.
+        // Prevents users from importing files from outside of src/ (or
+        // node_modules/). This often causes confusion because we only process
+        // files within src/ with babel. To fix this, we prevent you from
+        // importing files out of src/ -- if you'd like to, please link the
+        // files into your node_modules/ and let module-resolution kick in.
+        // Make sure your source files are compiled, as they will not be
+        // processed in any way.
         new ModuleScopePlugin(paths.appSrc, [
           paths.appPackageJson,
           reactRefreshRuntimeEntry,
