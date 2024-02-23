62d08b4d0f2f857513f4e7cfc233eca2b1b1edd9
Garrett Stevens <stevens.garrett.j@gmail.com>
2024-02-23 10:23:55 -0700

    Pin view menu for all views

--- packages/app-core/src/ui/App/ViewContainer.tsx
+++ packages/app-core/src/ui/App/ViewContainer.tsx
@@ -2,18 +2,24 @@ import React, { useEffect } from 'react'
 import { Paper, useTheme } from '@mui/material'
 import { makeStyles } from 'tss-react/mui'
 import { observer } from 'mobx-react'
-import { getSession, useWidthSetter } from '@jbrowse/core/util'
-import { IBaseViewModel } from '@jbrowse/core/pluggableElementTypes/models'
-import { SessionWithFocusedViewAndDrawerWidgets } from '@jbrowse/core/util'
+import {
+  AbstractViewModel,
+  SessionWithFocusedViewAndDrawerWidgets,
+  useWidthSetter,
+} from '@jbrowse/core/util'
 
 // locals
 import ViewHeader from './ViewHeader'
+import ViewWrapper from './ViewWrapper'
 
 const useStyles = makeStyles()(theme => ({
   viewContainer: {
-    overflow: 'hidden',
     margin: theme.spacing(0.5),
     padding: `0 ${theme.spacing(1)} ${theme.spacing(1)}`,
+    overflow: 'visible',
+  },
+  viewContentsContainer: {
+    overflow: 'visible',
   },
   focusedView: {
     background: theme.palette.secondary.main,
@@ -25,19 +31,14 @@ const useStyles = makeStyles()(theme => ({
 
 const ViewContainer = observer(function ({
   view,
-  onClose,
-  onMinimize,
-  children,
+  session,
 }: {
-  view: IBaseViewModel
-  onClose: () => void
-  onMinimize: () => void
-  children: React.ReactNode
+  view: AbstractViewModel
+  session: SessionWithFocusedViewAndDrawerWidgets
 }) {
   const theme = useTheme()
   const ref = useWidthSetter(view, theme.spacing(1))
   const { classes, cx } = useStyles()
-  const session = getSession(view) as SessionWithFocusedViewAndDrawerWidgets
 
   useEffect(() => {
     function handleSelectView(e: Event) {
@@ -56,20 +57,29 @@ const ViewContainer = observer(function ({
     }
   }, [ref, session, view])
 
+  const backgroundColorClassName =
+    session.focusedViewId === view.id
+      ? classes.focusedView
+      : classes.unfocusedView
+  const viewContainerClassName = cx(
+    classes.viewContainer,
+    backgroundColorClassName,
+  )
+
   return (
-    <Paper
-      ref={ref}
-      elevation={12}
-      className={cx(
-        classes.viewContainer,
-        session.focusedViewId === view.id
-          ? classes.focusedView
-          : classes.unfocusedView,
-      )}
-    >
-      <ViewHeader view={view} onClose={onClose} onMinimize={onMinimize} />
-      <Paper>{children}</Paper>
-    </Paper>
+    <>
+      <Paper ref={ref} elevation={12} className={viewContainerClassName}>
+        <ViewHeader
+          view={view}
+          onClose={() => session.removeView(view)}
+          onMinimize={() => view.setMinimized(!view.minimized)}
+          className={backgroundColorClassName}
+        />
+        <Paper elevation={0} className={classes.viewContentsContainer}>
+          <ViewWrapper view={view} session={session} />
+        </Paper>
+      </Paper>
+    </>
   )
 })
 
