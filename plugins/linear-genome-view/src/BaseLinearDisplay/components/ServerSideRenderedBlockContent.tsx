ce58a829ef3931da3b897440473f70ced93e66e7
Colin <colin.diesh@gmail.com>
2024-03-04 10:57:53 -0500

    Slightly improved typescripting

--- plugins/linear-genome-view/src/BaseLinearDisplay/components/ServerSideRenderedBlockContent.tsx
+++ plugins/linear-genome-view/src/BaseLinearDisplay/components/ServerSideRenderedBlockContent.tsx
@@ -1,10 +1,10 @@
 import React, { lazy } from 'react'
 import { Tooltip, IconButton } from '@mui/material'
-
 import { makeStyles } from 'tss-react/mui'
 import { observer } from 'mobx-react'
 import { getParent } from 'mobx-state-tree'
 import { LoadingEllipses } from '@jbrowse/core/ui'
+import { getSession } from '@jbrowse/core/util'
 
 // icons
 import RefreshIcon from '@mui/icons-material/Refresh'
@@ -12,7 +12,6 @@ import ReportIcon from '@mui/icons-material/Report'
 
 // locals
 import BlockMsg from './BlockMsg'
-import { getSession } from '@jbrowse/core/util'
 
 const ErrorMessageStackTraceDialog = lazy(
   () => import('@jbrowse/core/ui/ErrorMessageStackTraceDialog'),
@@ -30,12 +29,10 @@ const useStyles = makeStyles()(theme => {
   }
 })
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const LoadingMessage = observer(({ model }: { model: any }) => {
+const LoadingMessage = observer(({ model }: { model: { status?: string } }) => {
   const { classes } = useStyles()
   const { status: blockStatus } = model
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const { message: displayStatus } = getParent<any>(model, 2)
+  const { message: displayStatus } = getParent<{ message?: string }>(model, 2)
   const status = displayStatus || blockStatus
   return (
     <div className={classes.loading}>
@@ -47,8 +44,14 @@ const LoadingMessage = observer(({ model }: { model: any }) => {
 const ServerSideRenderedBlockContent = observer(function ({
   model,
 }: {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  model: any
+  model: {
+    error?: unknown
+    reload: () => void
+    message: React.ReactNode
+    filled?: boolean
+    status?: string
+    reactElement?: React.ReactElement
+  }
 }) {
   if (model.error) {
     return (
@@ -57,7 +60,7 @@ const ServerSideRenderedBlockContent = observer(function ({
         severity="error"
         action={
           <>
-            <Tooltip title="Reload">
+            <Tooltip title="Reload track">
               <IconButton
                 data-testid="reload_button"
                 onClick={() => model.reload()}
@@ -70,7 +73,7 @@ const ServerSideRenderedBlockContent = observer(function ({
                 onClick={() => {
                   getSession(model).queueDialog(onClose => [
                     ErrorMessageStackTraceDialog,
-                    { onClose, error: model.error },
+                    { onClose, error: model.error as Error },
                   ])
                 }}
               >
