ca7488fa28d25fce9f95303bb4c56bdef74277d4
Colin <colin.diesh@gmail.com>
2024-03-25 12:18:44 -0400

    Misc

--- plugins/bed/src/BigBedAdapter/BigBedAdapter.ts
+++ plugins/bed/src/BigBedAdapter/BigBedAdapter.ts
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { BigBed } from '@gmod/bbi'
+import { BigBed, Header } from '@gmod/bbi'
 import BED from '@gmod/bed'
 import {
   BaseFeatureDataAdapter,
@@ -10,14 +9,12 @@ import { openLocation } from '@jbrowse/core/util/io'
 import { ObservableCreate } from '@jbrowse/core/util/rxjs'
 import SimpleFeature, { Feature } from '@jbrowse/core/util/simpleFeature'
 import { map, mergeAll } from 'rxjs/operators'
-import { ucscProcessedTranscript } from '../util'
 
-function isUCSC(f: Feature) {
-  return f.get('thickStart') && f.get('blockCount') && f.get('strand') !== 0
-}
+// locals
+import { isUCSC, makeBlocks, ucscProcessedTranscript } from '../util'
 
 export default class BigBedAdapter extends BaseFeatureDataAdapter {
-  private cached?: Promise<{ bigbed: BigBed; header: any; parser: BED }>
+  private cached?: Promise<{ bigbed: BigBed; header: Header; parser: BED }>
 
   public async configurePre(opts?: BaseOptions) {
     const pm = this.pluginManager
@@ -46,6 +43,7 @@ export default class BigBedAdapter extends BaseFeatureDataAdapter {
 
   async getHeader(opts?: BaseOptions) {
     const { parser, header } = await this.configure(opts)
+    // @ts-expect-error
     const { version, fileType } = header
     const { fields, ...rest } = parser.autoSql
     return {
@@ -78,43 +76,62 @@ export default class BigBedAdapter extends BaseFeatureDataAdapter {
               },
             )
 
-            const { blockCount, blockSizes, blockStarts, chromStarts } = data
-            if (blockCount) {
-              const starts = chromStarts || blockStarts || []
-              const sizes = blockSizes
-              const blocksOffset = r.start
-              data.subfeatures = []
-
-              for (let b = 0; b < blockCount; b += 1) {
-                const bmin = (starts[b] || 0) + blocksOffset
-                const bmax = bmin + (sizes[b] || 0)
-                data.subfeatures.push({
-                  uniqueId: `${r.uniqueId}-${b}`,
-                  start: bmin,
-                  end: bmax,
-                  type: 'block',
-                })
-              }
-            }
             if (r.uniqueId === undefined) {
               throw new Error('invalid bbi feature')
             }
-            const { chromStart, chromEnd, chrom, ...rest } = data
+            const {
+              uniqueId,
+              type,
+              chromStart,
+              chromStarts,
+              blockStarts,
+              blockCount,
+              blockSizes,
+              chromEnd,
+              thickStart,
+              thickEnd,
+              chrom,
+              ...rest
+            } = data
 
-            const f = new SimpleFeature({
-              id: `${this.id}-${r.uniqueId}`,
-              data: {
-                ...rest,
-                start: r.start,
-                end: r.end,
-                refName,
-              },
+            const f = {
+              ...rest,
+              uniqueId,
+              type,
+              start: r.start,
+              end: r.end,
+              refName,
+            }
+            const subfeatures = blockCount
+              ? makeBlocks({
+                  chromStarts,
+                  blockStarts,
+                  blockCount,
+                  blockSizes,
+                  start,
+                  uniqueId,
+                  refName,
+                })
+              : []
+
+            // collection of heuristics for suggesting that this feature should
+            // be converted to a gene, CNV bigbed has many gene like features
+            // including thickStart and blockCount but no strand
+            return new SimpleFeature({
+              id: `${this.id}-${uniqueId}`,
+              // @ts-expect-error
+              data: isUCSC(f)
+                ? ucscProcessedTranscript({
+                    ...f,
+                    chromStarts,
+                    blockCount,
+                    blockSizes,
+                    thickStart,
+                    thickEnd,
+                    subfeatures,
+                  })
+                : { ...f, subfeatures },
             })
-
-            // collection of heuristics for suggesting that this feature
-            // should be converted to a gene, CNV bigbed has many gene like
-            // features including thickStart and blockCount but no strand
-            return isUCSC(f) ? ucscProcessedTranscript(f) : f
           }),
         ).subscribe(observer)
       } catch (e) {
