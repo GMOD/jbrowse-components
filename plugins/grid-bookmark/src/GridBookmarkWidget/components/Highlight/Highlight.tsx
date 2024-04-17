4735180afc6fab6233ba87596774b83ffaa45db5
Colin <colin.diesh@gmail.com>
2024-04-17 12:21:15 -0400

    Local storage track settings

--- plugins/grid-bookmark/src/GridBookmarkWidget/components/Highlight/Highlight.tsx
+++ plugins/grid-bookmark/src/GridBookmarkWidget/components/Highlight/Highlight.tsx
@@ -23,6 +23,7 @@ const useStyles = makeStyles()({
 })
 
 const Highlight = observer(function Highlight({ model }: { model: LGV }) {
+  return null
   const { classes } = useStyles()
 
   const session = getSession(model) as SessionWithWidgets
