f2feb4eb2b5ba718be78cc200c198220331f01bb
Colin Diesh <colin.diesh@gmail.com>
2024-04-23 12:45:28 -0400

    Fix bookmark showing up on wrong assembly (#4357)

--- plugins/grid-bookmark/src/GridBookmarkWidget/components/Highlight/Highlight.tsx
+++ plugins/grid-bookmark/src/GridBookmarkWidget/components/Highlight/Highlight.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 import { observer } from 'mobx-react'
 import { makeStyles } from 'tss-react/mui'
 import { SessionWithWidgets, getSession, notEmpty } from '@jbrowse/core/util'
@@ -24,42 +24,33 @@ const useStyles = makeStyles()({
 
 const Highlight = observer(function Highlight({ model }: { model: LGV }) {
   const { classes } = useStyles()
-
   const session = getSession(model) as SessionWithWidgets
-  const { showBookmarkHighlights, showBookmarkLabels } = model
   const { assemblyManager } = session
-  const assemblyNames = new Set(session.assemblyNames)
+  const { showBookmarkHighlights, showBookmarkLabels } = model
 
   const bookmarkWidget = session.widgets.get(
     'GridBookmark',
   ) as GridBookmarkModel
 
-  const bookmarks = useRef(bookmarkWidget?.bookmarks ?? [])
-
   useEffect(() => {
     if (!bookmarkWidget) {
-      const newBookmarkWidget = session.addWidget(
+      session.addWidget(
         'GridBookmarkWidget',
         'GridBookmark',
       ) as GridBookmarkModel
-      bookmarks.current = newBookmarkWidget.bookmarks
     }
   }, [session, bookmarkWidget])
 
-  return showBookmarkHighlights && bookmarks.current
-    ? bookmarks.current
-        .filter(value => assemblyNames.has(value.assemblyName))
+  const set = new Set(model.assemblyNames)
+
+  return showBookmarkHighlights
+    ? bookmarkWidget?.bookmarks
+        ?.filter(value => set.has(value.assemblyName))
         .map(r => {
           const asm = assemblyManager.get(r.assemblyName)
           const refName = asm?.getCanonicalRefName(r.refName) ?? r.refName
-          const s = model.bpToPx({
-            refName: refName,
-            coord: r.start,
-          })
-          const e = model.bpToPx({
-            refName: refName,
-            coord: r.end,
-          })
+          const s = model.bpToPx({ refName, coord: r.start })
+          const e = model.bpToPx({ refName, coord: r.end })
           return s && e
             ? {
                 width: Math.max(Math.abs(e.offsetPx - s.offsetPx), 3),
