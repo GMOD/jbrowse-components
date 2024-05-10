2d9fd8569d276f31ab81b8cb6fa511823b78066d
Colin <colin.diesh@gmail.com>
2024-05-10 19:54:51 +0000

    [skip ci] Misc embedded demo updater script improvements

--- embedded_demos/build_demos.sh
+++ embedded_demos/build_demos.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e;
+cd  $JB2TMP
 for i in jbrowse*; do
   cd $i;
   yarn;
   yarn build;
   cd -;
 done;
+cd -
--- embedded_demos/update_demos.sh
+++ embedded_demos/update_demos.sh
@@ -3,6 +3,8 @@ set -e;
 cd  $JB2TMP
 for i in jbrowse-react*; do
   cd $i;
+  git stash;
+  git pull;
   yarn;
   yarn upgrade;
   cd -;
