99e06fbba995e3c4b0a10c7295b4cb518e6bf011
Colin <colin.diesh@gmail.com>
2024-06-14 15:52:49 -0400

    Revert "Remove --tracks and --view options from set-default-session CLI (#4146)"
    This reverts commit d38f51139314c21b6660ff8da22373a49db7c2f3.

--- products/jbrowse-cli/src/commands/set-default-session.ts
+++ products/jbrowse-cli/src/commands/set-default-session.ts
@@ -6,11 +6,14 @@ import JBrowseCommand from '../base'
 
 const fsPromises = fs.promises
 
-type DefaultSession = Record<string, unknown>
-type Track = Record<string, unknown>
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type DefaultSession = Record<string, any>
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Track = Record<string, any>
 
 interface Config {
-  assemblies?: { name: string; sequence: Record<string, unknown> }[]
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  assemblies?: { name: string; sequence: Record<string, any> }[]
   configuration?: {}
   connections?: unknown[]
   defaultSession?: DefaultSession
@@ -25,8 +28,8 @@ export default class SetDefaultSession extends JBrowseCommand {
 
   static examples = [
     '$ jbrowse set-default-session --session /path/to/default/session.json',
-    '$ jbrowse set-default-session --target /path/to/jb2/installation/config.json',
-    '$ jbrowse set-default-session --view LinearGenomeView, --name newName',
+    '$ jbrowse set-default-session --target /path/to/jb2/installation/config.json --view LinearGenomeView --tracks track1, track2, track3',
+    '$ jbrowse set-default-session --view LinearGenomeView, --name newName --viewId view-no-tracks',
     '$ jbrowse set-default-session --currentSession # Prints out current default session',
   ]
 
@@ -39,6 +42,19 @@ export default class SetDefaultSession extends JBrowseCommand {
       char: 'n',
       description: 'Give a name for the default session',
       default: 'New Default Session',
+    }),
+    view: Flags.string({
+      char: 'v',
+      description:
+        'View type in config to be added as default session, i.e LinearGenomeView, CircularView, DotplotView.\nMust be provided if no default session file provided',
+    }),
+    viewId: Flags.string({
+      description: 'Identifier for the view. Will be generated on default',
+    }),
+    tracks: Flags.string({
+      char: 't',
+      description:
+        'Track id or track ids as comma separated string to put into default session',
     }),
     currentSession: Flags.boolean({
       char: 'c',
@@ -61,7 +77,15 @@ export default class SetDefaultSession extends JBrowseCommand {
 
   async run() {
     const { flags: runFlags } = await this.parse(SetDefaultSession)
-    const { session, currentSession, delete: deleteDefaultSession } = runFlags
+    const {
+      session,
+      name,
+      tracks,
+      currentSession,
+      view,
+      viewId,
+      delete: deleteDefaultSession,
+    } = runFlags
     const output = runFlags.target || runFlags.out || '.'
     const isDir = (await fsPromises.lstat(output)).isDirectory()
     this.target = isDir ? `${output}/config.json` : output
@@ -69,22 +93,86 @@ export default class SetDefaultSession extends JBrowseCommand {
 
     if (deleteDefaultSession) {
       delete configContents.defaultSession
+      this.debug(`Writing configuration to file ${this.target}`)
       await this.writeJsonFile(this.target, configContents)
-    } else if (currentSession) {
+      this.log(`Deleted defaultSession from ${this.target}`)
+      return
+    }
+
+    // if user passes current session flag, print out and exit
+    if (currentSession) {
       this.log(
         `The current default session is ${JSON.stringify(
           configContents.defaultSession,
         )}`,
       )
       this.exit()
-    } else if (!session) {
-      this.error(`Please either provide a default session file`, { exit: 120 })
-    } else if (session) {
-      await this.writeJsonFile(this.target, {
-        ...configContents,
-        defaultSession: await this.readDefaultSessionFile(session),
-      })
     }
+
+    const foundTracks: Track[] = []
+    const existingDefaultSession = configContents.defaultSession?.length > 0
+
+    // must provide default session, or view, or tracks + view
+    if (!session && !view && !tracks) {
+      this.error(
+        `No default session information provided, Please either provide a default session file or enter information to build a default session`,
+        { exit: 120 },
+      )
+    } else if (session) {
+      // if user provides a file, process and set as default session and exit
+      const defaultJson = await this.readDefaultSessionFile(session)
+      configContents.defaultSession = defaultJson
+    } else {
+      // use trackids if any to match to tracks in the config
+      let trackIds = []
+      if (tracks && configContents.tracks) {
+        if (!view) {
+          this.error(
+            'Tracks must have a view type specified. Please rerun using the --view flag',
+            { exit: 130 },
+          )
+        }
+        trackIds = tracks.split(',').map(c => c.trim())
+        trackIds.forEach(trackId => {
+          this.log(trackId)
+          const matchingTrack = configContents.tracks?.find(
+            track => trackId === track.trackId,
+          )
+          if (!matchingTrack) {
+            this.error(
+              `Track ${trackId} has not been added to config yet.\nPlease add the track with the add-track command before adding to the default session`,
+              { exit: 140 },
+            )
+          } else {
+            foundTracks.push({
+              type: matchingTrack.type,
+              configuration: matchingTrack.trackId,
+            })
+          }
+        })
+      }
+
+      configContents.defaultSession = {
+        name,
+        views: [
+          {
+            id: viewId || `${view}-${foundTracks.length}`,
+            type: view,
+            tracks: foundTracks,
+          },
+        ],
+      }
+    }
+    this.debug(`Writing configuration to file ${this.target}`)
+    await this.writeJsonFile(this.target, configContents)
+
+    this.log(
+      `${
+        existingDefaultSession ? 'Overwrote' : 'Added'
+      } defaultSession "${name}" ${existingDefaultSession ? 'in' : 'to'} ${
+        this.target
+      }`,
+    )
   }
 
   async readDefaultSessionFile(defaultSessionFile: string) {
