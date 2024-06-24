3091e05f2c352a216ed787009dde07d5ab86ad94
Colin Diesh <colin.diesh@gmail.com>
2024-06-23 23:29:20 -0400

    [update docs] Add demo of using farm-fe bundler for embedded components (#4466)

--- embedded_demos/clone_demos.sh
+++ embedded_demos/clone_demos.sh
@@ -3,16 +3,16 @@ mkdir $JB2TMP
 cd $JB2TMP
 git clone git@github.com:GMOD/jbrowse-react-linear-genome-view-vanillajs-demo
 git clone git@github.com:GMOD/jbrowse-react-linear-genome-view-cra5-demo
-git clone git@github.com:GMOD/jbrowse-react-linear-genome-view-cra4-demo
 git clone git@github.com:GMOD/jbrowse-react-linear-genome-view-vite-demo
+git clone git@github.com:GMOD/jbrowse-react-linear-genome-view-farm-demo
 git clone git@github.com:GMOD/jbrowse-react-linear-genome-view-nextjs-demo
 
 git clone git@github.com:GMOD/jbrowse-react-circular-genome-view-vanillajs-demo
 git clone git@github.com:GMOD/jbrowse-react-circular-genome-view-cra5-demo
-git clone git@github.com:GMOD/jbrowse-react-circular-genome-view-cra4-demo
 git clone git@github.com:GMOD/jbrowse-react-circular-genome-view-nextjs-demo
 
 git clone git@github.com:GMOD/jbrowse-react-app-nextjs-demo
 git clone git@github.com:GMOD/jbrowse-react-app-vite-demo
+git clone git@github.com:GMOD/jbrowse-react-app-farm-demo
 git clone git@github.com:GMOD/jbrowse-react-app-cra5-demo
 cd -
