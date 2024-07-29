9b57b59c0c4cbf3366ae9023de0ea4db19ff886a
Colin <colin.diesh@gmail.com>
2024-07-29 14:05:27 -0400

    Fix opening track selector causing crash in v2.13.0

--- plugins/data-management/src/PluginStoreWidget/model.ts
+++ plugins/data-management/src/PluginStoreWidget/model.ts
@@ -5,17 +5,35 @@ import { ElementId } from '@jbrowse/core/util/types/mst'
 export default function stateModelFactory(pluginManager: PluginManager) {
   return types
     .model('PluginStoreModel', {
+      /**
+       * #property
+       */
       id: ElementId,
+      /**
+       * #property
+       */
       type: types.literal('PluginStoreWidget'),
+      /**
+       * #property
+       */
       filterText: '',
+      /**
+       * #property
+       */
       view: types.safeReference(
         pluginManager.pluggableMstType('view', 'stateModel'),
       ),
     })
     .actions(self => ({
+      /**
+       * #action
+       */
       clearFilterText() {
         self.filterText = ''
       },
+      /**
+       * #action
+       */
       setFilterText(newText: string) {
         self.filterText = newText
       },
