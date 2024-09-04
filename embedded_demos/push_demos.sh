0523cacff8c9fa48b8dd2ef58030b47b89db1c83
Colin <colin.diesh@gmail.com>
2024-09-04 09:18:18 -0400

    Add messages to embedded_demos deploys

--- embedded_demos/push_demos.sh
+++ embedded_demos/push_demos.sh
@@ -2,10 +2,11 @@
 cd $JB2TMP
 for i in jbrowse-react*; do
   cd $i;
-  echo `pwd`;
+  echo "PUSH $i";
   git add yarn.lock;
   git commit -m "Update yarn.lock"
   git push;
+  echo "DONE PUSH $i"
   cd -;
 done;
 cd -
