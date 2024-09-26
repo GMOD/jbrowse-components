4bb1d24329c3e25b4030832825c2392f43bf05e3
Colin <colin.diesh@gmail.com>
2024-09-26 01:09:26 -0400

    Details about alignments feature details

--- plugins/alignments/src/AlignmentsFeatureDetail/AlignmentsFeatureDetail.tsx
+++ plugins/alignments/src/AlignmentsFeatureDetail/AlignmentsFeatureDetail.tsx
@@ -13,12 +13,84 @@ import { AlignmentFeatureWidgetModel } from './stateModelFactory'
 import Flags from './Flags'
 import PairLink from './PairLink'
 import Formatter from './Formatter'
+import { parseCigar } from '../MismatchParser'
 
 // lazies
 const SupplementaryAlignments = lazy(() => import('./SupplementaryAlignments'))
 const LinkedPairedAlignments = lazy(() => import('./LinkedPairedAlignments'))
 
 const omit = ['clipPos', 'flags']
+
+function CigarSeqDisplayer({ seq, cigar }: { seq: string; cigar: string }) {
+  const opts = parseCigar(cigar)
+  let qpos = 0 // query position, position on the read
+  let tpos = 0 // target position, position on the reference sequence
+  const refSeq = ''
+  const readSeq = seq
+  const ret = []
+
+  // opts will be an array like this ['30','M', '2','I', '50','M', '40','D']
+  // which we parse two elements at a time
+  for (let i = 0; i < opts.length; i += 2) {
+    const len = +opts[i]!
+    const op = opts[i + 1]!
+    // do things. refer to the CIGAR chart in SAMv1.pdf for which operators
+    // "consume reference" to see whether to increment
+    if (op === 'M' || op === '=') {
+      // matches consume query and reference
+      const refMatch = refSeq.slice(tpos, tpos + len)
+      const readMatch = readSeq.slice(qpos, qpos + len)
+      for (let i = 0; i < len; i++) {
+        if (refMatch[i] !== readMatch[i]) {
+          // SNP at this position
+        }
+      }
+      qpos += len
+      tpos += len
+      ret.push(<span style={{ color: 'grey' }}>{readMatch}</span>)
+    }
+    if (op === 'I') {
+      // insertions only consume query
+      // sequence of the insertion from the read is
+      const insSeq = readSeq.slice(qpos, qpos + len)
+      qpos += len
+      ret.push(<span style={{ color: '#f00c' }}>{insSeq}</span>)
+    }
+    if (op === 'D') {
+      // deletions only consume reference
+      // sequence of the deletion from the reference is
+      const delSeq = refSeq.slice(tpos, tpos + len)
+      tpos += len
+      ret.push(<span style={{ color: '#00fc' }}>(DEL{len})</span>)
+    }
+    if (op === 'N') {
+      // skips only consume reference
+      // skips are similar to deletions but are related to spliced alignments
+      tpos += len
+      ret.push(<span style={{ color: '#0ffc' }}>(SKIP{len})</span>)
+    }
+    if (op === 'X') {
+      // mismatch using the extended CIGAR format
+      // could lookup the mismatch letter in a string containing the reference
+      const mismatch = refSeq.slice(tpos, tpos + len)
+      qpos += len
+      tpos += len
+      ret.push(<span style={{ color: '#0f0c' }}>{mismatch}</span>)
+    }
+    if (op === 'H') {
+      // does not consume query or reference
+      // hardclip is just an indicator
+    }
+    if (op === 'S') {
+      // softclip consumes query
+      // below gets the entire soft clipped portion
+      const softClipStr = readSeq.slice(qpos, qpos + len)
+      qpos += len
+      ret.push(<span style={{ color: '#880c' }}>{softClipStr}</span>)
+    }
+  }
+  return <div>{ret}</div>
+}
 
 const AlignmentsFeatureDetails = observer(function (props: {
   model: AlignmentFeatureWidgetModel
@@ -39,6 +111,8 @@ const AlignmentsFeatureDetails = observer(function (props: {
         formatter={(value, key) =>
           key === 'next_segment_position' ? (
             <PairLink model={model} locString={value as string} />
+          ) : key === 'seq' ? (
+            <CigarSeqDisplayer seq={value as string} cigar={feat.CIGAR} />
           ) : (
             <Formatter value={value} />
           )
