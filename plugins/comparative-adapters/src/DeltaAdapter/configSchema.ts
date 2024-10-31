ae65816739a41171344e93bed3582cc659ae28b0
Colin Diesh <colin.diesh@gmail.com>
2024-10-31 11:45:21 -0400

    Fix config description of assembly name ordering for synteny data adapters (#4631)

--- plugins/comparative-adapters/src/DeltaAdapter/configSchema.ts
+++ plugins/comparative-adapters/src/DeltaAdapter/configSchema.ts
@@ -16,7 +16,7 @@ const DeltaAdapter = ConfigurationSchema(
       type: 'stringArray',
       defaultValue: [],
       description:
-        'Array of assembly names to use for this file. The target assembly name is the first value in the array, query assembly name is the second',
+        'Array of assembly names to use for this file. The query assembly name is the first value in the array, target assembly name is the second',
     },
     /**
      * #slot
@@ -41,7 +41,10 @@ const DeltaAdapter = ConfigurationSchema(
      */
     deltaLocation: {
       type: 'fileLocation',
-      defaultValue: { uri: '/path/to/file.delta', locationType: 'UriLocation' },
+      defaultValue: {
+        uri: '/path/to/file.delta',
+        locationType: 'UriLocation',
+      },
     },
   },
   { explicitlyTyped: true },
