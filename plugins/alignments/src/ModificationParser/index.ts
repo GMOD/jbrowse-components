2f733ce08cc3783456f4297698aa670c2b359219
Colin <colin.diesh@gmail.com>
2024-11-13 11:30:59 -0500

    Optimize revcom code

--- plugins/alignments/src/ModificationParser/index.ts
+++ plugins/alignments/src/ModificationParser/index.ts
@@ -11,75 +11,116 @@ const modificationRegex = new RegExp(/([A-Z])([-+])([^,.?]+)([.?])?/)
 // if we have MP or Mp it is phred scaled ASCII, which can go up to 90 but has
 // very high likelihood basecalls at that point, we really only care about low
 // qual calls <20 approx
+// fairly hot function so optimized for speed
 export function getModProbabilities(feature: Feature) {
-  const m = (getTagAlt(feature, 'ML', 'Ml') as number[] | string) || []
-  return m
-    ? (typeof m === 'string' ? m.split(',').map(e => +e) : m).map(e => e / 255)
-    : (getTagAlt(feature, 'MP', 'Mp') as string | undefined)
-        ?.split('')
-        .map(s => s.charCodeAt(0) - 33)
-        .map(elt => Math.min(1, elt / 50))
+  let ML = (getTagAlt(feature, 'ML', 'Ml') as number[] | string) || []
+  if (ML) {
+    if (typeof ML === 'string') {
+      ML = ML.split(',').map(e => +e)
+    }
+    const ret = new Array(ML.length)
+    for (let i = 0; i < ML.length; i++) {
+      ret[i] = ML[i]! / 255
+    }
+    return ret
+  } else {
+    return (getTagAlt(feature, 'MP', 'Mp') as string | undefined)
+      ?.split('')
+      .map(s => s.charCodeAt(0) - 33)
+      .map(elt => Math.min(1, elt / 50))
+  }
 }
 
 export function getModPositions(mm: string, fseq: string, fstrand: number) {
   const seq = fstrand === -1 ? revcom(fseq) : fseq
-  const mods = mm.split(';').filter(mod => !!mod)
+  const mods = mm.split(';')
   const result = []
   for (const mod of mods) {
-    const [basemod, ...skips] = mod.split(',')
+    if (mod) {
+      const [basemod, ...skips] = mod.split(',')
 
-    // regexes based on parse_mm.pl from hts-specs
-    // https://github.com/samtools/hts-specs/blob/master/test/SAMtags/parse_mm.pl
-    const matches = modificationRegex.exec(basemod!)
-    if (!matches) {
-      throw new Error('bad format for MM tag')
-    }
-    const [, base, strand, typestr] = matches
-
-    // can be a multi e.g. C+mh for both meth (m) and hydroxymeth (h) so split,
-    // and they can also be chemical codes (ChEBI) e.g. C+16061
-    const types = typestr!.split(/(\d+|.)/).filter(f => !!f)
-
-    if (strand === '-') {
-      console.warn('unsupported negative strand modifications')
-      result.push({
-        type: 'unsupported',
-        positions: [] as number[],
-        base: base!,
-        strand: strand,
-      })
-    }
-
-    // this logic based on parse_mm.pl from hts-specs
-    for (const type of types) {
-      let i = 0
-      const positions = []
-      for (const d of skips) {
-        let delta = +d
-        do {
-          if (base === 'N' || base === seq[i]) {
-            delta--
+      // regexes based on parse_mm.pl from hts-specs
+      // https://github.com/samtools/hts-specs/blob/master/test/SAMtags/parse_mm.pl
+      const matches = modificationRegex.exec(basemod!)
+      if (!matches) {
+        throw new Error('bad format for MM tag')
+      }
+      const [, base, strand, typestr] = matches
+      if (strand === '-') {
+        console.warn('unsupported negative strand modifications')
+        result.push({
+          type: 'unsupported',
+          positions: [] as number[],
+          base: base!,
+          strand: strand,
+        })
+      } else {
+        if (typestr!.length === 1) {
+          let i = 0
+          const positions = []
+          for (const d of skips) {
+            let delta = +d
+            do {
+              if (base === 'N' || base === seq[i]) {
+                delta--
+              }
+              i++
+            } while (delta >= 0 && i < seq.length)
+            if (fstrand === -1) {
+              const pos = seq.length - i
+              if (pos >= 0) {
+                // avoid negative-number-positions in array, seen in #4629 cause
+                // unknown, could warrant some further investigation
+                positions.unshift(pos)
+              }
+            } else {
+              positions.push(i - 1)
+            }
           }
-          i++
-        } while (delta >= 0 && i < seq.length)
-        if (fstrand === -1) {
-          const pos = seq.length - i
-          if (pos >= 0) {
-            // avoid negative-number-positions in array, seen in #4629 cause
-            // unknown, could warrant some further investigation
-            positions.unshift(pos)
-          }
+
+          result.push({
+            type: typestr!,
+            base: base!,
+            strand: strand!,
+            positions,
+          })
         } else {
-          positions.push(i - 1)
+          // can be a multi e.g. C+mh for both meth (m) and hydroxymeth (h) so split,
+          // and they can also be chemical codes (ChEBI) e.g. C+16061
+          const types = typestr!.split(/(\d+|.)/).filter(f => !!f)
+          // this logic based on parse_mm.pl from hts-specs
+          for (const type of types) {
+            let i = 0
+            const positions = []
+            for (const d of skips) {
+              let delta = +d
+              do {
+                if (base === 'N' || base === seq[i]) {
+                  delta--
+                }
+                i++
+              } while (delta >= 0 && i < seq.length)
+              if (fstrand === -1) {
+                const pos = seq.length - i
+                if (pos >= 0) {
+                  // avoid negative-number-positions in array, seen in #4629 cause
+                  // unknown, could warrant some further investigation
+                  positions.unshift(pos)
+                }
+              } else {
+                positions.push(i - 1)
+              }
+            }
+
+            result.push({
+              type,
+              base: base!,
+              strand: strand!,
+              positions,
+            })
+          }
         }
       }
-
-      result.push({
-        type,
-        base: base!,
-        strand: strand!,
-        positions,
-      })
     }
   }
   return result
