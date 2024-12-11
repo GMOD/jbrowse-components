5d0121284ca620b630a37b795d8b588f32e30cc5
Colin <colin.diesh@gmail.com>
2024-12-10 22:10:22 -0500

    Updat

--- products/jbrowse-web/src/tests/NcbiAliasAdapter.test.tsx
+++ products/jbrowse-web/src/tests/NcbiAliasAdapter.test.tsx
@@ -1,3 +1,5 @@
+import { waitFor } from '@testing-library/dom'
+
 import { createView, doBeforeEach, setup } from './util'
 import config from '../../test_data/cfam2/config.json'
 
@@ -8,7 +10,11 @@ const opts = [{}, delay]
 
 test('ncbi config', async () => {
   doBeforeEach(url => require.resolve(`../../test_data/cfam2/${url}`))
-  const { findByTestId } = await createView(config)
-  const features = await findByTestId('box-test-offset-272549-0-1', ...opts)
+  const { queryAllByTestId, getAllByTestId } = await createView(config)
+
+  await waitFor(() => {
+    expect(queryAllByTestId('svgfeatures').length).toBe(2)
+  }, delay)
+  const features = getAllByTestId('svgfeatures', ...opts)
   expect(features).toMatchSnapshot()
 }, 50000)
