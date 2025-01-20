ffca6a7c14e1e441dfd52c84bb456294ae0aa37d
Colin <colin.diesh@gmail.com>
2025-01-20 14:29:22 -0500

    Fix LGVSynteny test producing a warning on getCanonicalRefName for not-yet-loaded assembly

--- products/jbrowse-web/src/tests/BreakpointSplitView.test.tsx
+++ products/jbrowse-web/src/tests/BreakpointSplitView.test.tsx
@@ -12,27 +12,23 @@ beforeEach(() => {
 
 const delay = { timeout: 40000 }
 
-test(
-  'breakpoint split view',
-  () =>
-    mockConsoleWarn(async () => {
-      const { getByTestId, queryAllByTestId } =
-        await createView(breakpointConfig)
+test('breakpoint split view', () => {
+  return mockConsoleWarn(async () => {
+    const { getByTestId, queryAllByTestId } = await createView(breakpointConfig)
 
-      // the breakpoint could be partially loaded so explicitly wait for two items
-      await waitFor(() => {
-        expect(queryAllByTestId('r1').length).toBe(2)
-      }, delay)
-      await waitFor(() => {
-        expect(queryAllByTestId('r2').length).toBe(1)
-      }, delay)
+    // the breakpoint could be partially loaded so explicitly wait for two items
+    await waitFor(() => {
+      expect(queryAllByTestId('r1').length).toBe(2)
+    }, delay)
+    await waitFor(() => {
+      expect(queryAllByTestId('r2').length).toBe(1)
+    }, delay)
 
-      await waitFor(() => {
-        expect(getByTestId('pacbio_hg002_breakpoints-loaded')).toMatchSnapshot()
-      }, delay)
-      await waitFor(() => {
-        expect(getByTestId('pacbio_vcf-loaded')).toMatchSnapshot()
-      }, delay)
-    }),
-  40000,
-)
+    await waitFor(() => {
+      expect(getByTestId('pacbio_hg002_breakpoints-loaded')).toMatchSnapshot()
+    }, delay)
+    await waitFor(() => {
+      expect(getByTestId('pacbio_vcf-loaded')).toMatchSnapshot()
+    }, delay)
+  })
+}, 40000)
--- products/jbrowse-web/src/tests/LGVSynteny.test.tsx
+++ products/jbrowse-web/src/tests/LGVSynteny.test.tsx
@@ -1,7 +1,14 @@
 import { fireEvent, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
-import { createView, doBeforeEach, expectCanvasMatch, hts, setup } from './util'
+import {
+  createView,
+  doBeforeEach,
+  expectCanvasMatch,
+  hts,
+  mockConsoleWarn,
+  setup,
+} from './util'
 
 import type { LinearSyntenyViewModel } from '@jbrowse/plugin-linear-comparative-view/src/LinearSyntenyView/model'
 
@@ -15,45 +22,49 @@ const delay = { timeout: 50000 }
 const opts = [{}, delay]
 
 test('nav to synteny from right click', async () => {
-  const user = userEvent.setup()
-  const { session, view, findByTestId, findByText, findAllByTestId } =
-    await createView()
+  await mockConsoleWarn(async () => {
+    const user = userEvent.setup()
+    const { session, view, findByTestId, findByText, findAllByTestId } =
+      await createView()
 
-  await view.navToLocString('ctgA:30,222..33,669')
-  await user.click(await findByTestId(hts('volvox_ins.paf'), ...opts))
+    await view.navToLocString('ctgA:30,222..33,669')
+    await user.click(await findByTestId(hts('volvox_ins.paf'), ...opts))
 
-  const track = await findAllByTestId('pileup-overlay-strand', ...opts)
-  fireEvent.mouseMove(track[0]!, { clientX: 200, clientY: 5 })
-  fireEvent.contextMenu(track[0]!, { clientX: 200, clientY: 5 })
-  fireEvent.click(await findByText('Launch synteny view for this position'))
-  fireEvent.click(await findByText('Submit'))
-  await waitFor(() => {
-    const v = session.views[1] as LinearSyntenyViewModel | undefined
-    expect(v?.initialized).toBe(true)
-    expect(v?.views[0]?.coarseVisibleLocStrings).toBe('ctgA:29,221..34,669')
-  }, delay)
-  expectCanvasMatch(await findByTestId('synteny_canvas', ...opts))
+    const track = await findAllByTestId('pileup-overlay-strand', ...opts)
+    fireEvent.mouseMove(track[0]!, { clientX: 200, clientY: 5 })
+    fireEvent.contextMenu(track[0]!, { clientX: 200, clientY: 5 })
+    fireEvent.click(await findByText('Launch synteny view for this position'))
+    fireEvent.click(await findByText('Submit'))
+    await waitFor(() => {
+      const v = session.views[1] as LinearSyntenyViewModel | undefined
+      expect(v?.initialized).toBe(true)
+      expect(v?.views[0]?.coarseVisibleLocStrings).toBe('ctgA:29,221..34,669')
+    }, delay)
+    expectCanvasMatch(await findByTestId('synteny_canvas', ...opts))
+  })
 }, 60000)
 
 test('nav to synteny from feature details', async () => {
-  const user = userEvent.setup()
-  const { session, view, findByTestId, findByText, findAllByTestId } =
-    await createView()
+  await mockConsoleWarn(async () => {
+    const user = userEvent.setup()
+    const { session, view, findByTestId, findByText, findAllByTestId } =
+      await createView()
 
-  await view.navToLocString('ctgA:30,222..33,669')
-  await user.click(await findByTestId(hts('volvox_ins.paf'), ...opts))
+    await view.navToLocString('ctgA:30,222..33,669')
+    await user.click(await findByTestId(hts('volvox_ins.paf'), ...opts))
 
-  const track = await findAllByTestId('pileup-overlay-strand', ...opts)
-  fireEvent.mouseMove(track[0]!, { clientX: 200, clientY: 5 })
-  fireEvent.click(track[0]!, { clientX: 200, clientY: 5 })
-  fireEvent.click(
-    await findByText('Launch new linear synteny view on this feature'),
-  )
-  fireEvent.click(await findByText('Submit'))
-  await waitFor(() => {
-    const v = session.views[1] as LinearSyntenyViewModel | undefined
-    expect(v?.initialized).toBe(true)
-    expect(v?.views[0]?.coarseVisibleLocStrings).toBe('ctgA:1..50,001')
-  }, delay)
-  expectCanvasMatch(await findByTestId('synteny_canvas', ...opts))
+    const track = await findAllByTestId('pileup-overlay-strand', ...opts)
+    fireEvent.mouseMove(track[0]!, { clientX: 200, clientY: 5 })
+    fireEvent.click(track[0]!, { clientX: 200, clientY: 5 })
+    fireEvent.click(
+      await findByText('Launch new linear synteny view on this feature'),
+    )
+    fireEvent.click(await findByText('Submit'))
+    await waitFor(() => {
+      const v = session.views[1] as LinearSyntenyViewModel | undefined
+      expect(v?.initialized).toBe(true)
+      expect(v?.views[0]?.coarseVisibleLocStrings).toBe('ctgA:1..50,001')
+    }, delay)
+    expectCanvasMatch(await findByTestId('synteny_canvas', ...opts))
+  })
 }, 60000)
