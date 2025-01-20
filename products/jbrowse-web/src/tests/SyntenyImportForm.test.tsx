826e3a87a1cf59d49311eb82bcd75f864052aa72
Colin <colin.diesh@gmail.com>
2025-01-20 13:35:03 -0500

    Add a top level assembly selector

---
+++ products/jbrowse-web/src/tests/SyntenyImportForm.test.tsx
@@ -0,0 +1,48 @@
+import { fireEvent, within } from '@testing-library/react'
+
+// local
+import { createView, doBeforeEach, expectCanvasMatch, setup } from './util'
+setup()
+
+jest.mock('../makeWorkerInstance', () => () => {})
+
+const delay = { timeout: 20000 }
+beforeEach(() => {
+  doBeforeEach()
+})
+afterEach(() => {
+  localStorage.clear()
+  sessionStorage.clear()
+})
+
+// onAction listener warning
+console.warn = jest.fn()
+
+test('open tracklist file', async () => {
+  const {
+    session,
+    findByTestId,
+    findByRole,
+    findAllByText,
+    findAllByTestId,
+    findByText,
+  } = await createView()
+
+  fireEvent.click(await findByText('File'))
+  fireEvent.click(await findByText('Add'))
+  fireEvent.click(await findByText('Linear synteny view'))
+  expect(session.views.length).toBe(2)
+  const r = await findAllByTestId('assembly-selector-textfield')
+
+  expect(r.length).toBe(2)
+
+  const combo = within(r[1]!)
+  const entry = await combo.findByText('volvox')
+  fireEvent.mouseDown(entry)
+
+  const listbox = within(await findByRole('listbox'))
+  fireEvent.click(listbox.getByText('volvox_del'))
+  fireEvent.click(await findByText('Launch'))
+
+  expectCanvasMatch(await findByTestId('synteny_canvas', {}, delay))
+}, 40000)
