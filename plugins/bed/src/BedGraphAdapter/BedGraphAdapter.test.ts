aec30f5ef31edc9366debb9cd7b4367948f45d23
Colin Diesh <colin.diesh@gmail.com>
2025-01-22 17:19:32 -0500

    Add tests for BEDPE and BedGraph (#4782)

---
+++ plugins/bed/src/BedGraphAdapter/BedGraphAdapter.test.ts
@@ -0,0 +1,32 @@
+import { firstValueFrom } from 'rxjs'
+import { toArray } from 'rxjs/operators'
+
+import BedGraphAdapter from './BedGraphAdapter'
+import configSchema from './configSchema'
+
+function makeAdapter() {
+  return new BedGraphAdapter(
+    configSchema.create({
+      bedGraphLocation: {
+        localPath: require.resolve('./test_data/test.bg'),
+        locationType: 'LocalPathLocation',
+      },
+    }),
+  )
+}
+test('basic', async () => {
+  const adapter = makeAdapter()
+
+  const features = await firstValueFrom(
+    adapter
+      .getFeatures({
+        assemblyName: 'volvox',
+        refName: 'chr1',
+        start: 0,
+        end: 10000,
+      })
+      .pipe(toArray()),
+  )
+
+  expect(features).toMatchSnapshot()
+})
--- plugins/bed/src/BedGraphTabixAdapter/BedGraphTabixAdapter.ts
+++ plugins/bed/src/BedGraphTabixAdapter/BedGraphTabixAdapter.ts
@@ -95,6 +95,12 @@ export default class BedGraphAdapter extends BaseFeatureDataAdapter {
             const start = +cols[colStart]!
             const end = +(same ? start + 1 : cols[colEnd]!)
             const rest = cols.slice(colEnd + 1)
+            if (Number.isNaN(start) || Number.isNaN(end)) {
+              throw new Error(
+                `start/end NaN on line "${line}", with colStart:${colStart} and colEnd:${colEnd}. run "tabix -p bed" to ensure bed preset`,
+              )
+            }
+
             for (let j = 0; j < rest.length; j++) {
               const uniqueId = `${this.id}-${fileOffset}-${j}`
               const score = Math.abs(+rest[j]!)
