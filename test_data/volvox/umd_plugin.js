7cbc4a1333f2d77d45c0193286c1b2b732381480
Colin <colin.diesh@gmail.com>
2025-02-01 13:19:06 -0500

    Coloring from thread

--- test_data/volvox/umd_plugin.js
+++ test_data/volvox/umd_plugin.js
@@ -135,7 +135,12 @@
     configure(pluginManager) {
       pluginManager.jexl.addFunction('repeatColor', feature => {
         let type = feature.get('repeatClass')
-        return { R: 'red', RC: 'green', F: 'blue', C: 'orange' }[type]
+        return {
+          R: '#00A000',
+          RC: '#FF7F00',
+          F: '#8b0000',
+          C: '#0000FF',
+        }[type]
       })
     }
   }
