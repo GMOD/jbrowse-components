492bbd58e437c4aaddda819325c3405cd87d2abd
Colin <colin.diesh@gmail.com>
2025-02-03 09:17:26 -0500

    Display url of plugin being loaded in warning

--- products/jbrowse-web/src/components/ConfigWarningDialog.tsx
+++ products/jbrowse-web/src/components/ConfigWarningDialog.tsx
@@ -1,12 +1,8 @@
-import { pluginDescriptionString } from '@jbrowse/core/PluginLoader'
+import { useState } from 'react'
+
+import { pluginDescriptionString, pluginUrl } from '@jbrowse/core/PluginLoader'
 import { Dialog } from '@jbrowse/core/ui'
-import WarningIcon from '@mui/icons-material/Warning'
-import {
-  Button,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-} from '@mui/material'
+import { Alert, Button, DialogActions, DialogContent } from '@mui/material'
 
 import type { PluginDefinition } from '@jbrowse/core/PluginLoader'
 
@@ -19,20 +15,37 @@ export default function ConfigWarningDialog({
   onCancel: () => void
   reason: PluginDefinition[]
 }) {
+  const [show, setShow] = useState(false)
   return (
     <Dialog open maxWidth="xl" title="Warning" onClose={onCancel}>
       <DialogContent>
-        <WarningIcon fontSize="large" />
-        <DialogContentText>
+        <Alert severity="warning" style={{ width: 800 }}>
           This link contains a cross origin config that has the following
           unknown plugins:
           <ul>
             {reason.map(r => (
-              <li key={JSON.stringify(r)}>{pluginDescriptionString(r)}</li>
+              <li key={JSON.stringify(r)}>
+                {pluginDescriptionString(r)} - ({pluginUrl(r)})
+              </li>
             ))}
           </ul>
-          Please ensure you trust the source of this link.
-        </DialogContentText>
+          Please ensure you trust the source of this link.{' '}
+          <Button
+            onClick={() => {
+              setShow(!show)
+            }}
+          >
+            Why am I seeing this?
+          </Button>
+          {show ? (
+            <div>
+              Config files can load arbitrary javascript files via plugins. For
+              security purposes, we display this message when a cross-origin
+              config is detected to be loading plugins that are not in our
+              plugin store
+            </div>
+          ) : null}
+        </Alert>
       </DialogContent>
       <DialogActions>
         <Button
