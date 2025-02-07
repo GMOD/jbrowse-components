2d294b66bc5a3b5fc84409995c2b68027059b8f8
Colin <colin.diesh@gmail.com>
2025-02-07 12:14:50 -0500

    GTF aggregation

--- plugins/gtf/src/GtfAdapter/GtfAdapter.ts
+++ plugins/gtf/src/GtfAdapter/GtfAdapter.ts
@@ -1,6 +1,12 @@
 import IntervalTree from '@flatten-js/interval-tree'
 import { BaseFeatureDataAdapter } from '@jbrowse/core/data_adapters/BaseAdapter'
-import { SimpleFeature, fetchAndMaybeUnzip } from '@jbrowse/core/util'
+import {
+  SimpleFeature,
+  doesIntersect2,
+  fetchAndMaybeUnzip,
+  max,
+  min,
+} from '@jbrowse/core/util'
 import { openLocation } from '@jbrowse/core/util/io'
 import { ObservableCreate } from '@jbrowse/core/util/rxjs'
 import { parseStringSync } from 'gtf-nostream'
@@ -9,8 +15,9 @@ import { featureData } from '../util'
 
 import type { FeatureLoc } from '../util'
 import type { BaseOptions } from '@jbrowse/core/data_adapters/BaseAdapter'
-import type { Feature } from '@jbrowse/core/util'
-import type { NoAssemblyRegion } from '@jbrowse/core/util/types'
+import type { Feature, SimpleFeatureSerialized } from '@jbrowse/core/util'
+import type { Region } from '@jbrowse/core/util/types'
+import type { Observer } from 'rxjs'
 
 type StatusCallback = (arg: string) => void
 
@@ -70,15 +77,12 @@ export default class GtfAdapter extends BaseFeatureDataAdapter {
             const intervalTree = new IntervalTree()
             ;(parseStringSync(lines) as FeatureLoc[][])
               .flat()
-              .map(
-                (f, i) =>
-                  new SimpleFeature({
-                    data: featureData(f),
-                    id: `${this.id}-${refName}-${i}`,
-                  }),
-              )
+              .map((f, i) => featureData(f, `${this.id}-${refName}-${i}`))
               .forEach(obj =>
-                intervalTree.insert([obj.get('start'), obj.get('end')], obj),
+                intervalTree.insert(
+                  [obj.start as number, obj.end as number],
+                  obj,
+                ),
               )
             this.calculatedIntervalTreeMap[refName] = intervalTree
           }
@@ -114,21 +118,116 @@ export default class GtfAdapter extends BaseFeatureDataAdapter {
     return header
   }
 
-  public getFeatures(query: NoAssemblyRegion, opts: BaseOptions = {}) {
+  public getFeatures(query: Region, opts: BaseOptions = {}) {
     return ObservableCreate<Feature>(async observer => {
       try {
-        const { start, end, refName } = query
-        const { intervalTreeMap } = await this.loadData(opts)
-        intervalTreeMap[refName]?.(opts.statusCallback)
-          .search([start, end])
-          .forEach(f => {
-            observer.next(f)
-          })
-        observer.complete()
+        await this.getFeaturesHelper({
+          query,
+          opts,
+          observer,
+          allowRedispatch: true,
+        })
       } catch (e) {
         observer.error(e)
       }
     }, opts.stopToken)
   }
+
+  public async getFeaturesHelper({
+    query,
+    opts,
+    observer,
+    allowRedispatch,
+    originalQuery = query,
+  }: {
+    query: Region
+    opts: BaseOptions
+    observer: Observer<Feature>
+    allowRedispatch: boolean
+    originalQuery?: Region
+  }) {
+    const aggregateField = this.getConf('aggregateField')
+    const { start, end, refName } = query
+    const { intervalTreeMap } = await this.loadData(opts)
+    const feats = intervalTreeMap[refName]?.(opts.statusCallback).search([
+      start,
+      end,
+    ])
+    if (feats) {
+      if (allowRedispatch && feats.length) {
+        let minStart = Number.POSITIVE_INFINITY
+        let maxEnd = Number.NEGATIVE_INFINITY
+        for (const feat of feats) {
+          if (feat.start < minStart) {
+            minStart = feat.start
+          }
+          if (feat.end > maxEnd) {
+            maxEnd = feat.end
+          }
+        }
+        if (maxEnd > query.end || minStart < query.start) {
+          await this.getFeaturesHelper({
+            query: {
+              ...query,
+              start: minStart,
+              end: maxEnd,
+            },
+            opts,
+            observer,
+            allowRedispatch: false,
+            originalQuery: query,
+          })
+          return
+        }
+      }
+
+      const parentAggregation = {} as Record<string, SimpleFeatureSerialized[]>
+
+      if (feats.some(f => f.uniqueId === undefined)) {
+        throw new Error('found uniqueId undefined')
+      }
+      for (const feat of feats) {
+        const aggr = feat[aggregateField]
+        if (!parentAggregation[aggr]) {
+          parentAggregation[aggr] = []
+        }
+
+        if (aggr) {
+          parentAggregation[aggr].push(feat)
+        } else {
+          observer.next(
+            new SimpleFeature({
+              id: `${this.id}-${feat.uniqueId}`,
+              data: feat,
+            }),
+          )
+        }
+      }
+
+      Object.entries(parentAggregation).map(([name, subfeatures]) => {
+        const s = min(subfeatures.map(f => f.start))
+        const e = max(subfeatures.map(f => f.end))
+        if (doesIntersect2(s, e, originalQuery.start, originalQuery.end)) {
+          const { uniqueId, strand } = subfeatures[0]!
+          observer.next(
+            new SimpleFeature({
+              id: `${this.id}-${uniqueId}-parent`,
+              data: {
+                type: 'gene',
+                subfeatures,
+                strand,
+                name,
+                start: s,
+                end: e,
+                refName: query.refName,
+              },
+            }),
+          )
+        }
+      })
+    }
+    observer.complete()
+  }
+
   public freeResources(/* { region } */) {}
 }
