f2e29801d3235519c186e03607a3dae4b31d26eb
Colin <colin.diesh@gmail.com>
2025-03-10 22:35:08 -0400

    Change the stopPropagation to onMouseDown

--- plugins/linear-genome-view/src/BaseLinearDisplay/components/ServerSideRenderedBlockContent.tsx
+++ plugins/linear-genome-view/src/BaseLinearDisplay/components/ServerSideRenderedBlockContent.tsx
@@ -1,38 +1,12 @@
 import { Suspense, isValidElement, lazy } from 'react'
 
-import { LoadingEllipses } from '@jbrowse/core/ui'
 import { observer } from 'mobx-react'
-import { getParent } from 'mobx-state-tree'
-import { makeStyles } from 'tss-react/mui'
 
+import BlockLoadingMessage from './BlockLoadingMessage'
 import BlockMsg from './BlockMsg'
 
 // lazies
-const BlockError = lazy(() => import('./BlockError'))
-
-const useStyles = makeStyles()(theme => {
-  const bg = theme.palette.action.disabledBackground
-  return {
-    loading: {
-      paddingLeft: '0.6em',
-      backgroundColor: theme.palette.background.default,
-      backgroundImage: `repeating-linear-gradient(45deg, transparent, transparent 5px, ${bg} 5px, ${bg} 10px)`,
-      textAlign: 'center',
-    },
-  }
-})
-
-const LoadingMessage = observer(({ model }: { model: { status?: string } }) => {
-  const { classes } = useStyles()
-  const { status: blockStatus } = model
-  const { message: displayStatus } = getParent<{ message?: string }>(model, 2)
-  const status = displayStatus || blockStatus
-  return (
-    <div className={classes.loading}>
-      <LoadingEllipses message={status} />
-    </div>
-  )
-})
+const BlockErrorMessage = lazy(() => import('./BlockErrorMessage'))
 
 const ServerSideRenderedBlockContent = observer(function ({
   model,
@@ -49,7 +23,7 @@ const ServerSideRenderedBlockContent = observer(function ({
   if (model.error) {
     return (
       <Suspense fallback={null}>
-        <BlockError model={model} />
+        <BlockErrorMessage model={model} />
       </Suspense>
     )
   } else if (model.message) {
@@ -61,7 +35,7 @@ const ServerSideRenderedBlockContent = observer(function ({
       <BlockMsg message={`${model.message}`} severity="info" />
     )
   } else if (!model.filled) {
-    return <LoadingMessage model={model} />
+    return <BlockLoadingMessage model={model} />
   } else {
     return model.reactElement
   }
