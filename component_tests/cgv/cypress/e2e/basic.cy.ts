256c8629995b2eb2d80ea62b1212693584957e66
Colin <colin.diesh@gmail.com>
2025-03-21 23:50:22 -0400

    Update test

--- component_tests/cgv/cypress/e2e/basic.cy.ts
+++ component_tests/cgv/cypress/e2e/basic.cy.ts
@@ -2,6 +2,6 @@ describe('JBrowse embedded circular view', () => {
   it('track loads', () => {
     cy.visit('/')
 
-    cy.findByTestId('chord-adp-351172516-vcf-63101', { timeout: 30000 })
+    cy.findByTestId('chord--351172516-vcf-62853', { timeout: 30000 })
   })
 })
