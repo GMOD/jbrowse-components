d9b6b732152d95909a6c8332187dfc68ed671729
Colin <colin.diesh@gmail.com>
2025-04-18 13:08:45 -0400

    Don't hide undefined

--- plugins/variants/src/VariantFeatureWidget/Formatter.tsx
+++ plugins/variants/src/VariantFeatureWidget/Formatter.tsx
@@ -7,33 +7,31 @@ export default function Formatter({ value }: { value: unknown }) {
   const [show, setShow] = useState(false)
   const [copied, setCopied] = useState(false)
   const display = String(value)
-  return value ? (
-    display.length > 100 ? (
-      <>
-        <button
-          type="button"
-          onClick={() => {
-            copy(display)
-            setCopied(true)
-            setTimeout(() => {
-              setCopied(false)
-            }, 700)
-          }}
-        >
-          {copied ? 'Copied to clipboard' : 'Copy'}
-        </button>
-        <button
-          type="button"
-          onClick={() => {
-            setShow(val => !val)
-          }}
-        >
-          {show ? 'Show less' : 'Show more'}
-        </button>
-        <div>{show ? display : `${display.slice(0, 100)}...`}</div>
-      </>
-    ) : (
-      <div>{display}</div>
-    )
-  ) : null
+  return display.length > 100 ? (
+    <>
+      <button
+        type="button"
+        onClick={() => {
+          copy(display)
+          setCopied(true)
+          setTimeout(() => {
+            setCopied(false)
+          }, 700)
+        }}
+      >
+        {copied ? 'Copied to clipboard' : 'Copy'}
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          setShow(val => !val)
+        }}
+      >
+        {show ? 'Show less' : 'Show more'}
+      </button>
+      <div>{show ? display : `${display.slice(0, 100)}...`}</div>
+    </>
+  ) : (
+    <div>{display}</div>
+  )
 }
