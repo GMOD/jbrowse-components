87558eb3f7c3763f56e760a2362407239a6c7d67
Colin <colin.diesh@gmail.com>
2025-04-20 00:33:25 -0400

    [skip ci] Use dedupe
    Misc
    
    UseSWR for interval polling
    
    Favs
    
    Updates
    
    Add tableContainer
    
    Misc
    
    Lint
    
    Remove update preconfigured sessions
    
    Move go button
    
    Rm
    
    Less progress
    
    Use deferred value
    
    Move files around
    
    Updates
    
    WOrking
    
    Updates
    
    Updates
    
    Updates
    
    Misc
    
    Allow sorting complete genome
    
    Delay bar
    
    Simplified
    
    More info
    
    Simplify
    
    Standardize
    
    Misc
    
    Preference
    
    Re-org
    
    Wow
    
    Aggregate field back to geneName2
    
    Aggregate field

---
+++ products/jbrowse-desktop/src/components/StartScreen/availableGenomes/GenArkDataTable.tsx
@@ -0,0 +1,413 @@
+import { useMemo, useState } from 'react'
+
+import {
+  CascadingMenuButton,
+  ErrorMessage,
+  LoadingEllipses,
+} from '@jbrowse/core/ui'
+import { measureGridWidth, notEmpty, useLocalStorage } from '@jbrowse/core/util'
+import Help from '@mui/icons-material/Help'
+import MoreVert from '@mui/icons-material/MoreVert'
+import StarIcon from '@mui/icons-material/Star'
+import StarBorderIcon from '@mui/icons-material/StarBorder'
+import {
+  Button,
+  IconButton,
+  Link,
+  MenuItem,
+  TextField,
+  Typography,
+} from '@mui/material'
+import { DataGrid } from '@mui/x-data-grid'
+import useSWR from 'swr'
+import { makeStyles } from 'tss-react/mui'
+
+import { fetchjson } from '../util'
+import MoreInfoDialog from './MoreInfoDialog'
+import { useInnerDims } from './util'
+
+import type { Fav, LaunchCallback } from '../types'
+import type { GridColDef, GridRowId } from '@mui/x-data-grid'
+
+const types = [
+  'bacteria',
+  'birds',
+  'fish',
+  'fungi',
+  'invertebrate',
+  'mammals',
+  'plants',
+  'primates',
+  'vertebrate',
+  'viral',
+  'BRC',
+  'CCGP',
+  'globalReference',
+  'HPRC',
+  'legacy',
+  'VGP',
+]
+
+const useStyles = makeStyles()({
+  span: {
+    gap: 10,
+    display: 'flex',
+    marginBottom: 20,
+    marginTop: 20,
+  },
+  panel: {
+    minWidth: 1000,
+    minHeight: 500,
+    position: 'relative',
+  },
+  ml: {
+    margin: 0,
+    marginLeft: 10,
+  },
+})
+
+function applyId(
+  r: {
+    jbrowseConfig: string
+    accession: string
+    commonName: string
+    ncbiAssemblyName: string
+  }[],
+) {
+  return r.map(r => ({
+    ...r,
+    id: r.accession,
+  }))
+}
+
+const order = {
+  'Complete genome': 1,
+  Chromosome: 2,
+  Scaffold: 3,
+  Contig: 4,
+}
+
+const columns = [
+  {
+    field: 'commonName',
+    title: 'Common Name',
+    sortable: true,
+  },
+  {
+    field: 'assemblyStatus',
+    title: 'Assembly status',
+    sortable: true,
+    sortComparator: (v1: string, v2: string) =>
+      (order[v1 as keyof typeof order] || 0) -
+      (order[v2 as keyof typeof order] || 0),
+  },
+  {
+    field: 'submitterOrg',
+    title: 'Submitter',
+    sortable: false,
+    extra: true,
+  },
+  {
+    field: 'seqReleaseDate',
+    title: 'Release date',
+    sortable: true,
+  },
+  {
+    field: 'scientificName',
+    title: 'Scientific name',
+    sortable: true,
+  },
+  {
+    field: 'ncbiAssemblyName',
+    title: 'NCBI assembly name',
+    sortable: true,
+  },
+  {
+    field: 'accession',
+    title: 'Accession',
+    sortable: true,
+    extra: true,
+  },
+
+  {
+    field: 'taxonId',
+    title: 'Taxonomy ID',
+    sortable: true,
+    extra: true,
+  },
+]
+
+export default function GenArkDataTable({
+  favorites,
+  setFavorites,
+  onClose,
+  launch,
+}: {
+  onClose: () => void
+  favorites: Fav[]
+  setFavorites: (arg: Fav[]) => void
+  launch: LaunchCallback
+}) {
+  const [selected, setSelected] = useState<Set<GridRowId>>()
+  const [showOnlyFavs, setShowOnlyFavs] = useState(false)
+  const [filterOption, setFilterOption] = useState('all')
+  const [moreInfoDialogOpen, setMoreInfoDialogOpen] = useState(false)
+  const [typeOption, setTypeOption] = useLocalStorage(
+    'startScreen-genArkChoice',
+    'mammals',
+  )
+  const [showAllColumns, setShowAllColumns] = useState(false)
+  const { height: innerHeight, width: innerWidth } = useInnerDims()
+  const { classes } = useStyles()
+
+  const { data, error } = useSWR(
+    `genark-${typeOption}`,
+    () =>
+      fetchjson(
+        `https://s3.amazonaws.com/jbrowse.org/hubJson2/${typeOption}.json`,
+      ) as Promise<any[]>,
+  )
+
+  const rows = useMemo(() => {
+    if (filterOption === 'refseq') {
+      return data?.filter(r => r.ncbiName.startsWith('GCF_'))
+    } else if (filterOption === 'genbank') {
+      return data?.filter(r => r.ncbiName.startsWith('GCA_'))
+    } else if (filterOption === 'designatedReference') {
+      return data?.filter(r => r.ncbiRefSeqCategory === 'reference genome')
+    } else {
+      return data
+    }
+  }, [filterOption, data])
+
+  const favs = new Set(favorites.map(r => r.id))
+  const colNames = columns.map(c => c.field)
+  const widths = useMemo(
+    () =>
+      rows
+        ? Object.fromEntries(
+            colNames.map(e => [
+              e,
+              measureGridWidth(
+                rows.map(r => r[e as keyof typeof r]),
+                {
+                  maxWidth:
+                    e === 'submitterOrg' ||
+                    e === 'scientificName' ||
+                    e === 'ncbiAssemblyName'
+                      ? 200
+                      : 400,
+                },
+              ),
+            ]),
+          )
+        : undefined,
+    [rows, colNames],
+  )
+
+  const visibleColumns = columns.filter(
+    column => showAllColumns || !column.extra,
+  )
+  const r2 = rows ? applyId(rows) : undefined
+  return (
+    <div className={classes.panel}>
+      <div className={classes.span}>
+        <Typography variant="h6" style={{ display: 'inline', margin: 0 }}>
+          GenArk genome browsers
+        </Typography>
+
+        <Button
+          variant="contained"
+          disabled={!selected?.size}
+          onClick={() => {
+            if (selected && r2) {
+              const r3 = Object.fromEntries(r2.map(r => [r.accession, r]))
+              launch(
+                [...selected]
+                  .map(r => r3[r])
+                  .filter(notEmpty)
+                  .map(r => ({
+                    jbrowseConfig: r.jbrowseConfig,
+                    shortName: r.accession,
+                  })),
+              )
+              onClose()
+            }
+          }}
+        >
+          Go
+        </Button>
+        <TextField
+          select
+          name="typeOption"
+          label="Group"
+          variant="outlined"
+          className={classes.ml}
+          value={typeOption}
+          onChange={event => {
+            setTypeOption(event.target.value)
+          }}
+        >
+          {types.map(key => (
+            <MenuItem key={key} value={key}>
+              {key}
+            </MenuItem>
+          ))}
+        </TextField>
+        <CascadingMenuButton
+          menuItems={[
+            {
+              label: 'Show favorites only?',
+              checked: showOnlyFavs,
+              type: 'checkbox',
+              onClick: () => {
+                setShowOnlyFavs(!showOnlyFavs)
+              },
+            },
+            {
+              label: 'Show all columns',
+              type: 'checkbox',
+              checked: showAllColumns,
+              onClick: () => {
+                setShowAllColumns(!showAllColumns)
+              },
+            },
+            {
+              label: 'Filter by NCBI status',
+              type: 'subMenu',
+              subMenu: [
+                {
+                  label: 'All',
+                  type: 'radio',
+                  checked: filterOption === 'all',
+                  onClick: () => {
+                    setFilterOption('all')
+                  },
+                },
+                {
+                  label: 'RefSeq only',
+                  type: 'radio',
+                  checked: filterOption === 'refseq',
+                  onClick: () => {
+                    setFilterOption('refseq')
+                  },
+                },
+                {
+                  label: 'GenBank only',
+                  type: 'radio',
+                  checked: filterOption === 'genbank',
+                  onClick: () => {
+                    setFilterOption('genbank')
+                  },
+                },
+                {
+                  label: 'Designated reference genome only',
+                  type: 'radio',
+                  checked: filterOption === 'designatedReference',
+                  onClick: () => {
+                    setFilterOption('designatedReference')
+                  },
+                },
+              ],
+            },
+            {
+              label: 'More information',
+              icon: Help,
+              onClick: () => {
+                setMoreInfoDialogOpen(true)
+              },
+            },
+          ]}
+        >
+          <MoreVert />
+        </CascadingMenuButton>
+      </div>
+      <div>
+        These data are based on UCSC GenArk, please see{' '}
+        <Link href="https://hgdownload.soe.ucsc.edu/hubs/">
+          https://hgdownload.soe.ucsc.edu/hubs/
+        </Link>{' '}
+        for more information
+      </div>
+
+      {error ? <ErrorMessage error={error} /> : null}
+
+      {r2 && widths ? (
+        <div
+          style={{
+            width: innerWidth * (3 / 4),
+            height: innerHeight * (1 / 2),
+          }}
+        >
+          <DataGrid
+            rows={r2.filter(f => (showOnlyFavs ? favs.has(f.accession) : true))}
+            showToolbar
+            rowHeight={25}
+            columnHeaderHeight={35}
+            checkboxSelection
+            disableRowSelectionOnClick
+            onRowSelectionModelChange={userSelectedIds => {
+              setSelected(userSelectedIds.ids)
+            }}
+            columns={visibleColumns.map(c => {
+              return c.field === 'commonName'
+                ? ({
+                    field: c.field,
+                    headerName: c.title,
+                    width: widths.commonName! + 40,
+                    renderCell: ({ row, value }) => {
+                      return (
+                        <div>
+                          <IconButton
+                            size="small"
+                            onClick={() => {
+                              if (favs.has(row.id)) {
+                                setFavorites(
+                                  favorites.filter(fav => fav.id !== row.id),
+                                )
+                              } else {
+                                setFavorites([
+                                  ...favorites,
+                                  {
+                                    id: row.id,
+                                    shortName: row.ncbiAssemblyName,
+                                    description: row.commonName,
+                                    jbrowseConfig: row.jbrowseConfig,
+                                  },
+                                ])
+                              }
+                            }}
+                          >
+                            {favs.has(row.id) ? (
+                              <StarIcon />
+                            ) : (
+                              <StarBorderIcon />
+                            )}
+                          </IconButton>{' '}
+                          {value}
+                        </div>
+                      )
+                    },
+                  } satisfies GridColDef<(typeof r2)[0]>)
+                : ({
+                    field: c.field,
+                    headerName: c.title,
+                    width: widths[c.field],
+                    sortComparator: c.sortComparator,
+                  } satisfies GridColDef<(typeof r2)[0]>)
+            })}
+          />
+        </div>
+      ) : (
+        <LoadingEllipses variant="h6" />
+      )}
+      {moreInfoDialogOpen ? (
+        <MoreInfoDialog
+          onClose={() => {
+            setMoreInfoDialogOpen(false)
+          }}
+        />
+      ) : null}
+    </div>
+  )
+}
