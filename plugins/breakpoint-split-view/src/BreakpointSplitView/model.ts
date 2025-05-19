d14f20f9d4232f70c8ca614a3ea0650cd39016c2
Colin <colin.diesh@gmail.com>
2025-05-19 15:10:58 -0400

    Misc

--- plugins/breakpoint-split-view/src/BreakpointSplitView/model.ts
+++ plugins/breakpoint-split-view/src/BreakpointSplitView/model.ts
@@ -8,7 +8,8 @@ import { saveAs } from 'file-saver'
 import { autorun } from 'mobx'
 import { addDisposer, getPath, onAction, types } from 'mobx-state-tree'
 
-import { calc, getBlockFeatures, getClip, intersect } from './util'
+import { calc, getBlockFeatures, intersect } from './util'
+import { getClip } from './getClip'
 
 import type { ExportSvgOptions } from './types'
 import type PluginManager from '@jbrowse/core/PluginManager'
@@ -25,7 +26,6 @@ const ExportSvgDialog = lazy(() => import('./components/ExportSvgDialog'))
  * - [BaseViewModel](../baseviewmodel)
  */
 export default function stateModelFactory(pluginManager: PluginManager) {
-  const minHeight = 40
   const defaultHeight = 400
   return types
     .compose(
@@ -39,14 +39,7 @@ export default function stateModelFactory(pluginManager: PluginManager) {
         /**
          * #property
          */
-        height: types.optional(
-          types.refinement(
-            'viewHeight',
-            types.number,
-            (n: number) => n >= minHeight,
-          ),
-          defaultHeight,
-        ),
+        height: types.optional(types.number, defaultHeight),
         /**
          * #property
          */
@@ -73,7 +66,13 @@ export default function stateModelFactory(pluginManager: PluginManager) {
       }),
     )
     .volatile(() => ({
+      /**
+       * #volatile
+       */
       width: 800,
+      /**
+       * #volatile
+       */
       matchedTrackFeatures: {} as Record<string, Feature[][]>,
     }))
     .views(self => ({
@@ -278,10 +277,16 @@ export default function stateModelFactory(pluginManager: PluginManager) {
               self.setMatchedTrackFeatures(
                 Object.fromEntries(
                   await Promise.all(
-                    self.matchedTracks.map(async track => [
-                      track.configuration.trackId,
-                      await getBlockFeatures(self, track),
-                    ]),
+                    self.matchedTracks.map(async track => {
+                      console.log(
+                        track,
+                        track.displays[0].PileupDisplay?.renderReady(),
+                      )
+                      return [
+                        track.configuration.trackId,
+                        await getBlockFeatures(self, track),
+                      ]
+                    }),
                   ),
                 ),
               )
--- plugins/breakpoint-split-view/src/BreakpointSplitView/util.ts
+++ plugins/breakpoint-split-view/src/BreakpointSplitView/util.ts
@@ -96,12 +96,6 @@ export function intersect<T>(
 const startClip = new RegExp(/(\d+)[SH]$/)
 const endClip = new RegExp(/^(\d+)([SH])/)
 
-export function getClip(cigar: string, strand: number) {
-  return strand === -1
-    ? +(startClip.exec(cigar) || [])[1]! || 0
-    : +(endClip.exec(cigar) || [])[1]! || 0
-}
-
 export function calc(track: Track, f: Feature) {
   return track.displays[0]!.searchFeatureByID?.(f.id())
 }
