06618f7fc4f14ad2f3f9f0bc935df1ad3ef7452c
Colin Diesh <colin.diesh@gmail.com>
2025-05-22 14:18:55 -0400

    Avoid inferring CDS subfeatures from non-coding gene pred features (#5042)

--- plugins/bed/src/BigBedAdapter/BigBedAdapter.ts
+++ plugins/bed/src/BigBedAdapter/BigBedAdapter.ts
@@ -62,12 +62,12 @@ export default class BigBedAdapter extends BaseFeatureDataAdapter {
     const { header } = await this.configure(opts)
     const ret = await Promise.all(
       Object.keys(header.refsByName).map(
-        async r =>
+        async refName =>
           (
             await firstValueFrom(
               this.getFeatures({
                 assemblyName: '',
-                refName: r,
+                refName,
                 start: 0,
                 end: 1,
               }).pipe(toArray()),
@@ -104,11 +104,11 @@ export default class BigBedAdapter extends BaseFeatureDataAdapter {
   async getHeader(opts?: BaseOptions) {
     const { parser, header } = await this.configure(opts)
     const { version, fileType } = header
-    const { fields, ...rest } = parser.autoSql
+    const { fields, ...autoSql } = parser.autoSql
     return {
       version,
       fileType,
-      autoSql: { ...rest },
+      autoSql,
       fields: await this.getMetadata(opts),
     }
   }
@@ -150,126 +150,134 @@ export default class BigBedAdapter extends BaseFeatureDataAdapter {
         }),
     )
 
-    const parentAggregation = {} as Record<string, SimpleFeatureSerialized[]>
-    const parentAggregationFlat = []
+    await updateStatus('Processing features', statusCallback, async () => {
+      const parentAggregation = {} as Record<string, SimpleFeatureSerialized[]>
+      const parentAggregationFlat = []
 
-    if (feats.some(f => f.uniqueId === undefined)) {
-      throw new Error('found uniqueId undefined')
-    }
-    for (const feat of feats) {
-      const splitLine = [
-        query.refName,
-        `${feat.start}`,
-        `${feat.end}`,
-        ...(feat.rest?.split('\t') || []),
-      ]
-      const data = parser.parseLine(splitLine, {
-        uniqueId: feat.uniqueId!,
-      })
-
-      const aggr = data[aggregateField]
-      const aggrIsNotNone = aggr && aggr !== 'none'
-      if (aggrIsNotNone && !parentAggregation[aggr]) {
-        parentAggregation[aggr] = []
+      if (feats.some(f => f.uniqueId === undefined)) {
+        throw new Error('found uniqueId undefined')
       }
-      const {
-        uniqueId,
-        type,
-        chrom,
-        chromStart,
-        chromEnd,
-        description,
-        chromStarts: chromStarts2,
-        blockStarts: blockStarts2,
-        blockSizes: blockSizes2,
-        score: score2,
-        blockCount,
-        thickStart,
-        thickEnd,
-        strand,
-        ...rest
-      } = data
+      for (const feat of feats) {
+        const splitLine = [
+          query.refName,
+          `${feat.start}`,
+          `${feat.end}`,
+          ...(feat.rest?.split('\t') || []),
+        ]
+        const data = parser.parseLine(splitLine, {
+          uniqueId: feat.uniqueId!,
+        })
 
-      const f = featureData2({
-        ...rest,
-        scoreColumn,
-        splitLine,
-        parser,
-        uniqueId,
-        start: feat.start,
-        end: feat.end,
-        refName: query.refName,
-      })
-      if (aggrIsNotNone) {
-        parentAggregation[aggr]!.push(f)
-        parentAggregationFlat.push(f)
-      } else {
-        if (
-          doesIntersect2(f.start, f.end, originalQuery.start, originalQuery.end)
-        ) {
+        const aggr = data[aggregateField]
+        const aggrIsNotNone = aggr && aggr !== 'none'
+        if (aggrIsNotNone && !parentAggregation[aggr]) {
+          parentAggregation[aggr] = []
+        }
+        const {
+          uniqueId,
+          type,
+          chrom,
+          chromStart,
+          chromEnd,
+          description,
+          chromStarts: chromStarts2,
+          blockStarts: blockStarts2,
+          blockSizes: blockSizes2,
+          score: score2,
+          blockCount,
+          thickStart,
+          thickEnd,
+          strand,
+          ...rest
+        } = data
+
+        const f = featureData2({
+          ...rest,
+          scoreColumn,
+          splitLine,
+          parser,
+          uniqueId,
+          start: feat.start,
+          end: feat.end,
+          refName: query.refName,
+        })
+        if (aggrIsNotNone) {
+          parentAggregation[aggr]!.push(f)
+          parentAggregationFlat.push(f)
+        } else {
+          if (
+            doesIntersect2(
+              f.start,
+              f.end,
+              originalQuery.start,
+              originalQuery.end,
+            )
+          ) {
+            observer.next(
+              new SimpleFeature({
+                id: `${this.id}-${uniqueId}`,
+                data: f,
+              }),
+            )
+          }
+        }
+      }
+
+      if (allowRedispatch && parentAggregationFlat.length) {
+        let minStart = Number.POSITIVE_INFINITY
+        let maxEnd = Number.NEGATIVE_INFINITY
+        for (const feat of parentAggregationFlat) {
+          if (feat.start < minStart) {
+            minStart = feat.start
+          }
+          if (feat.end > maxEnd) {
+            maxEnd = feat.end
+          }
+        }
+
+        if (maxEnd > query.end || minStart < query.start) {
+          await this.getFeaturesHelper({
+            query: {
+              ...query,
+              // re-query with 500kb added onto start and end, in order to catch
+              // gene subfeatures that may not overlap your view
+              start: minStart - 500_000,
+              end: maxEnd + 500_000,
+            },
+            opts,
+            observer,
+            allowRedispatch: false,
+            originalQuery: query,
+          })
+          return
+        }
+      }
+
+      Object.entries(parentAggregation).map(([name, subfeatures]) => {
+        const s = min(subfeatures.map(f => f.start))
+        const e = max(subfeatures.map(f => f.end))
+        if (doesIntersect2(s, e, originalQuery.start, originalQuery.end)) {
+          const subs = subfeatures.sort((a, b) =>
+            a.uniqueId.localeCompare(b.uniqueId),
+          )
           observer.next(
             new SimpleFeature({
-              id: `${this.id}-${uniqueId}`,
-              data: f,
+              id: `${this.id}-${subs[0]?.uniqueId}-parent`,
+              data: {
+                type: 'gene',
+                subfeatures: subs,
+                strand: subs[0]?.strand || 1,
+                name,
+                start: s,
+                end: e,
+                refName: query.refName,
+              },
             }),
           )
         }
-      }
-    }
-
-    if (allowRedispatch && parentAggregationFlat.length) {
-      let minStart = Number.POSITIVE_INFINITY
-      let maxEnd = Number.NEGATIVE_INFINITY
-      for (const feat of parentAggregationFlat) {
-        if (feat.start < minStart) {
-          minStart = feat.start
-        }
-        if (feat.end > maxEnd) {
-          maxEnd = feat.end
-        }
-      }
-
-      if (maxEnd > query.end || minStart < query.start) {
-        await this.getFeaturesHelper({
-          query: {
-            ...query,
-            // re-query with 500kb added onto start and end, in order to catch
-            // gene subfeatures that may not overlap your view
-            start: minStart - 500_000,
-            end: maxEnd + 500_000,
-          },
-          opts,
-          observer,
-          allowRedispatch: false,
-          originalQuery: query,
-        })
-        return
-      }
-    }
-
-    Object.entries(parentAggregation).map(([name, subfeatures]) => {
-      const s = min(subfeatures.map(f => f.start))
-      const e = max(subfeatures.map(f => f.end))
-      if (doesIntersect2(s, e, originalQuery.start, originalQuery.end)) {
-        const subs = subfeatures.sort((a, b) =>
-          a.uniqueId.localeCompare(b.uniqueId),
-        )
-        observer.next(
-          new SimpleFeature({
-            id: `${this.id}-${subs[0]?.uniqueId}-parent`,
-            data: {
-              type: 'gene',
-              subfeatures: subs,
-              strand: subs[0]?.strand || 1,
-              name,
-              start: s,
-              end: e,
-              refName: query.refName,
-            },
-          }),
-        )
-      }
+      })
     })
+
     observer.complete()
   }
   public getFeatures(query: Region, opts: BaseOptions = {}) {
