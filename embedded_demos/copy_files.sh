8a97c45d2e1aef4470c0b3fb5589415a99261865
Colin <colin.diesh@gmail.com>
2025-06-07 13:15:42 -0400

    [skip ci] Remove cra5

--- embedded_demos/copy_files.sh
+++ embedded_demos/copy_files.sh
@@ -2,7 +2,6 @@
 
 JB2TMP=${JB2TMP:-~/jb2tmp}
 ## linear
-cp base/linear/config.ts $JB2TMP/jbrowse-react-linear-genome-view-cra5-demo/src/
 cp base/linear/config.ts $JB2TMP/jbrowse-react-linear-genome-view-vite-demo/src/
 cp base/linear/config.ts $JB2TMP/jbrowse-react-linear-genome-view-farm-demo/src/
 cp base/linear/config.ts $JB2TMP/jbrowse-react-linear-genome-view-rsbuild-demo/src/
@@ -10,12 +9,10 @@ cp base/linear/config.ts $JB2TMP/jbrowse-react-linear-genome-view-nextjs-demo/ap
 cp base/linear/config.ts $JB2TMP/jbrowse-react-linear-genome-view-vanillajs-demo/
 
 ## circular
-cp base/circular/config.ts $JB2TMP/jbrowse-react-circular-genome-view-cra5-demo/src/
 cp base/circular/config.ts $JB2TMP/jbrowse-react-circular-genome-view-nextjs-demo/app/
 cp base/circular/config.ts $JB2TMP/jbrowse-react-circular-genome-view-vanillajs-demo/
 
 ## react app
-cp base/app/config.ts $JB2TMP/jbrowse-react-app-cra5-demo/src/
 cp base/app/config.ts $JB2TMP/jbrowse-react-app-vite-demo/src/
 cp base/app/config.ts $JB2TMP/jbrowse-react-app-farm-demo/src/
 cp base/app/config.ts $JB2TMP/jbrowse-react-app-nextjs-demo/app/
