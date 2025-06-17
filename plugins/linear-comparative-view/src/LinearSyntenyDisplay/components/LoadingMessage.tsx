dc0c75add12f4a6c54ec7ddb8d7dea26a31ded87
Colin Diesh <colin.diesh@gmail.com>
2025-06-17 15:44:17 -0400

    Add status message reporting for synteny view loading (#5086)

---
+++ plugins/linear-comparative-view/src/LinearSyntenyDisplay/components/LoadingMessage.tsx
@@ -0,0 +1,24 @@
+import { LoadingEllipses } from '@jbrowse/core/ui'
+import { makeStyles } from 'tss-react/mui'
+
+const useStyles = makeStyles()(theme => {
+  const bg = theme.palette.action.disabledBackground
+  return {
+    loading: {
+      paddingLeft: '0.6em',
+      backgroundColor: theme.palette.background.default,
+      backgroundImage: `repeating-linear-gradient(45deg, transparent, transparent 5px, ${bg} 5px, ${bg} 10px)`,
+      textAlign: 'center',
+    },
+  }
+})
+
+export default function LoadingMessage({ message }: { message?: string }) {
+  const { classes } = useStyles()
+
+  return (
+    <div className={classes.loading}>
+      <LoadingEllipses message={message} />
+    </div>
+  )
+}
