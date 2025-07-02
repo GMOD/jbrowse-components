9acaea232fb7a5db23cbfbee757b72778c37dfd0
Colin <colin.diesh@gmail.com>
2025-07-02 11:59:03 -0400

    Remove import form test from wrong file

--- products/jbrowse-web/src/tests/Dotplot.test.tsx
+++ products/jbrowse-web/src/tests/Dotplot.test.tsx
@@ -21,31 +21,6 @@ test('open a dotplot view', async () => {
   expectCanvasMatch(await findByTestId('prerendered_canvas_done', ...opts))
 }, 50000)
 
-test('open a dotplot view with import form', async () => {
-  const { findByTestId, findAllByTestId, findByText } = await createView(config)
-
-  fireEvent.click(await findByTestId('close_view'))
-  fireEvent.click(await findByText('File'))
-  fireEvent.click(await findByText('Add'))
-  fireEvent.click(await findByText('Dotplot view'))
-  const inputBox = await findAllByTestId('assembly-selector')
-  fireEvent.change(inputBox[0]!, {
-    target: {
-      value: 'peach',
-    },
-  })
-  fireEvent.click(await findByText('New track', ...opts))
-  fireEvent.click(await findByText('.paf'))
-  fireEvent.change(await findByTestId('urlInput', ...opts), {
-    target: {
-      value: 'peach_grape_small.paf.gz',
-    },
-  })
-  fireEvent.click(await findByText('Launch'))
-
-  expectCanvasMatch(await findByTestId('prerendered_canvas_done', ...opts))
-}, 50000)
-
 test('inverted dotplot', async () => {
   const { findByTestId } = await createView({
     ...config,
