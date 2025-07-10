e5ac80a757f9da73415017eb9cb32946e6342b6d
Colin <colin.diesh@gmail.com>
2025-07-10 02:41:53 -0400

    Wow

--- products/jbrowse-cli/src/commands/create.ts
+++ products/jbrowse-cli/src/commands/create.ts
@@ -1,18 +1,144 @@
 import fs from 'fs'
-import { parseArgs } from 'util'
 
 import decompress from 'decompress'
 
+import fetch from '../fetchWithProxy'
 import {
-  debug,
   fetchGithubVersions,
   getBranch,
-  getTag,
   getLatest,
+  getTag,
   printHelp,
 } from '../utils'
+import { parseArgs } from 'util'
 
 const fsPromises = fs.promises
+
+const description = 'Downloads and installs the latest JBrowse 2 release'
+
+const examples = [
+  '# Download latest release from github, and put in specific path',
+  '$ jbrowse create /path/to/new/installation',
+  '',
+  '# Download latest release from github and force overwrite existing contents at path',
+  '$ jbrowse create /path/to/new/installation --force',
+  '',
+  '# Download latest release from a specific URL',
+  '$ jbrowse create /path/to/new/installation --url url.com/directjbrowselink.zip',
+  '',
+  '# Download a specific tag from github',
+  '$ jbrowse create /path/to/new/installation --tag v1.0.0',
+  '',
+  '# List available versions',
+  '$ jbrowse create --listVersions',
+]
+
+const options = {
+  help: {
+    type: 'boolean',
+    short: 'h',
+    description: 'Show help',
+  },
+  force: {
+    type: 'boolean',
+    short: 'f',
+    description:
+      'Overwrites existing JBrowse 2 installation if present in path',
+  },
+  listVersions: {
+    type: 'boolean',
+    short: 'l',
+    description: 'Lists out all versions of JBrowse 2',
+  },
+  branch: {
+    type: 'string',
+    description: 'Download a development build from a named git branch',
+  },
+  nightly: {
+    type: 'boolean',
+    description: 'Download the latest development build from the main branch',
+  },
+  url: {
+    type: 'string',
+    short: 'u',
+    description: 'A direct URL to a JBrowse 2 release',
+  },
+  tag: {
+    type: 'string',
+    short: 't',
+    description:
+      'Version of JBrowse 2 to install. Format is v1.0.0.\nDefaults to latest',
+  },
+} as const
+
+export async function run(args: string[]) {
+  const { positionals, values: runFlags } = parseArgs({
+    options,
+    allowPositionals: true,
+    args,
+  })
+
+  if (runFlags.help) {
+    printHelp({
+      description,
+      examples,
+      usage: 'jbrowse create [localPath] [options]',
+      options,
+    })
+    return
+  }
+
+  const argsPath = positionals[0]!
+  if (!argsPath && !runFlags.listVersions) {
+    console.error(`Missing 1 required arg:
+    localPath  Location where JBrowse 2 will be installed
+    See more help with --help`)
+    return
+  }
+  const { force, url, listVersions, tag, branch, nightly } = runFlags
+  if (listVersions) {
+    const versions = (await fetchGithubVersions()).map(
+      version => version.tag_name,
+    )
+    console.log(`All JBrowse versions:\n${versions.join('\n')}`)
+    process.exit(0)
+  }
+
+  // mkdir will do nothing if dir exists
+  await fsPromises.mkdir(argsPath, { recursive: true })
+
+  if (!force) {
+    await checkPath(argsPath)
+  }
+
+  const locationUrl =
+    url ||
+    (nightly ? await getBranch('main') : '') ||
+    (branch ? await getBranch(branch) : '') ||
+    (tag ? await getTag(tag) : await getLatest())
+
+  console.log(`Fetching ${locationUrl}...`)
+  const response = await fetch(locationUrl)
+  if (!response.ok) {
+    console.error(`Failed to fetch: ${response.statusText}`)
+    process.exit(100)
+  }
+
+  const type = response.headers.get('content-type')
+  if (
+    url &&
+    type !== 'application/zip' &&
+    type !== 'application/octet-stream'
+  ) {
+    console.error(
+      'The URL provided does not seem to be a JBrowse installation URL',
+    )
+    process.exit(1)
+  }
+  await decompress(Buffer.from(await response.arrayBuffer()), argsPath)
+
+  console.log(`Unpacked ${locationUrl} at ${argsPath}`)
+}
 
 async function checkPath(userPath: string) {
   const allFiles = await fsPromises.readdir(userPath)
@@ -24,128 +150,14 @@ async function checkPath(userPath: string) {
   }
 }
 
-export async function run(args?: string[]) {
-  const options = {
-    help: {
-      type: 'boolean',
-      short: 'h',
-    },
-    force: {
-      type: 'boolean',
-      short: 'f',
-      description:
-        'Overwrites existing JBrowse 2 installation if present in path',
-    },
-    listVersions: {
-      type: 'boolean',
-      short: 'l',
-      description: 'Lists out all versions of JBrowse 2',
-    },
-    branch: {
-      type: 'string',
-      description: 'Download a development build from a named git branch',
-    },
-    nightly: {
-      type: 'boolean',
-      description: 'Download the latest development build from the main branch',
-    },
-    url: {
-      type: 'string',
-      short: 'u',
-      description: 'A direct URL to a JBrowse 2 release',
-    },
-    tag: {
-      type: 'string',
-      short: 't',
-      description:
-        'Version of JBrowse 2 to install. Format is v1.0.0. Defaults to latest',
-    },
-  } as const
-  const { values: flags, positionals } = parseArgs({
-    args,
-    options,
-    allowPositionals: true,
-  })
-
-  const description = 'Downloads and installs the latest JBrowse 2 release'
-
-  const examples = [
-    '# Download latest release from github, and put in specific path',
-    '$ jbrowse create /path/to/new/installation',
-    '',
-    '# Download latest release from github and force overwrite existing contents at path',
-    '$ jbrowse create /path/to/new/installation --force',
-    '',
-    '# Download latest release from a specific URL',
-    '$ jbrowse create /path/to/new/installation --url url.com/directjbrowselink.zip',
-    '',
-    '# Download a specific tag from github',
-    '$ jbrowse create /path/to/new/installation --tag v1.0.0',
-    '',
-    '# List available versions',
-    '$ jbrowse create --listVersions',
-  ]
-
-  if (flags.help) {
-    printHelp({
-      description,
-      examples,
-      usage: 'jbrowse create <localPath> [options]',
-      options,
-    })
-    return
-  }
-
-  if (flags.listVersions) {
-    const versions = (await fetchGithubVersions()).map(
-      version => version.tag_name,
-    )
-    console.log(`All JBrowse versions:\n${versions.join('\n')}`)
-    process.exit(0)
-  }
-
-  const localPath = positionals[0]
-  if (!localPath) {
-    console.error('Missing 1 required arg:')
-    console.error('localPath  Location where JBrowse 2 will be installed')
-    console.error('See more help with --help')
-    process.exit(1)
-  }
-
-  debug(`Want to install path at: ${localPath}`)
-
-  // mkdir will do nothing if dir exists
-  await fsPromises.mkdir(localPath, { recursive: true })
-
-  if (!flags.force) {
-    await checkPath(localPath)
-  }
-
-  const locationUrl =
-    flags.url ||
-    (flags.nightly ? await getBranch('main') : '') ||
-    (flags.branch ? await getBranch(flags.branch) : '') ||
-    (flags.tag ? await getTag(flags.tag) : await getLatest())
-
-  console.log(`Fetching ${locationUrl}...`)
-  const response = await fetch(locationUrl)
-  if (!response.ok) {
-    console.error(`Failed to fetch: ${response.statusText}`)
-    process.exit(100)
-  }
-
-  const type = response.headers.get('content-type')
-  if (
-    flags.url &&
-    type !== 'application/zip' &&
-    type !== 'application/octet-stream'
-  ) {
-    console.error(
-      'The URL provided does not seem to be a JBrowse installation URL',
-    )
-    process.exit(1)
-  }
-  await decompress(Buffer.from(await response.arrayBuffer()), localPath)
-
-  console.log(`Unpacked ${locationUrl} at ${localPath}`)
-}
+// async catch(error: unknown) {
+//   // @ts-expect-error
+//   if (error.parse?.output.flags.listVersions) {
+//     const versions = (await this.fetchGithubVersions()).map(
+//       version => version.tag_name,
+//     )
+//     console.log(`All JBrowse versions:\n${versions.join('\n')}`)
+//     this.exit()
+//   }
+//   throw error
+// }
