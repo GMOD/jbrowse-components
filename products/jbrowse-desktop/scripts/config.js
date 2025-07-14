4772f86fdd9e7cdc412268673154286af086d025
Colin Diesh <colin.diesh@gmail.com>
2025-07-14 15:39:33 -0400

    Generic filehandle electron fix (#5109)

--- products/jbrowse-desktop/scripts/config.js
+++ products/jbrowse-desktop/scripts/config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const webpack = require('webpack')
 
 module.exports = function webpackConfigF(config) {
@@ -5,9 +7,9 @@ module.exports = function webpackConfigF(config) {
     // this is needed to properly polyfill buffer in desktop, after the CRA5
     // conversion it was observed cram, twobit, etc that use
     // @gmod/binary-parser complained of buffers not being real buffers
-    new webpack.ProvidePlugin({
-      Buffer: ['buffer', 'Buffer'],
-    }),
+    // new webpack.ProvidePlugin({
+    //   Buffer: ['buffer', 'Buffer'],
+    // }),
     new webpack.DefinePlugin({
       // Global mobx-state-tree configuration.
       // Force type checking in production for easier debugging:
@@ -16,13 +18,19 @@ module.exports = function webpackConfigF(config) {
     }),
   )
 
-  // similar to our webpack 4 rescript, setting target to
-  // 'electron-renderer' helps load 'fs' module for local file access and
-  // avoid browser:{} field from package.json being used (which sometimes
-  // kicks out fs access e.g. in generic-filehandle)
   config.target = 'electron-renderer'
   config.resolve.aliasFields = []
   config.resolve.mainFields = ['module', 'main']
+
+  // Configure webpack to use the non-browser version of generic-filehandle2
+  if (!config.resolve.alias) {
+    config.resolve.alias = {}
+  }
+  config.resolve.alias['generic-filehandle2'] = path.resolve(
+    __dirname,
+    '../../../node_modules/generic-filehandle2/dist/index.js',
+  )
+
   // the 'auto' setting is important for properly resolving the loading of
   // worker chunks xref
   // https://github.com/webpack/webpack/issues/13791#issuecomment-897579223
