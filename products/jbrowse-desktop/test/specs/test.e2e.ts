3761d368eee3bbd27c24ac6d2d784805960d0be0
Colin <colin.diesh@gmail.com>
2025-07-14 23:54:57 -0400

    Reduce

--- products/jbrowse-desktop/test/specs/test.e2e.ts
+++ products/jbrowse-desktop/test/specs/test.e2e.ts
@@ -1,8 +1,8 @@
 import { browser } from '@wdio/globals'
 
 describe('Electron Testing', () => {
-    it('should print application title', async () => {
-        console.log('Hello', await browser.getTitle(), 'application!')
-    })
+  it('should print application title', async () => {
+    const r = await browser.getTitle()
+    expect(r).toBe('Electron')
+  })
 })
-
