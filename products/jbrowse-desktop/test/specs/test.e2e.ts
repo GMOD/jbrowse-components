bcce122f4245de9b4c539df2db3b21b896f05669
Colin <colin.diesh@gmail.com>
2025-07-14 23:17:56 -0400

    Wdio starter

---
+++ products/jbrowse-desktop/test/specs/test.e2e.ts
@@ -0,0 +1,8 @@
+import { browser } from '@wdio/globals'
+
+describe('Electron Testing', () => {
+    it('should print application title', async () => {
+        console.log('Hello', await browser.getTitle(), 'application!')
+    })
+})
+
