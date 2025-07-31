620dd70c658a3b5da5199d67752cd96877540b7e
Colin <colin.diesh@gmail.com>
2025-07-31 14:55:00 -0400

    Updates

--- products/jbrowse-desktop/src/components/StartScreen/availableGenomes/MoreInfoDialog.tsx
+++ products/jbrowse-desktop/src/components/StartScreen/availableGenomes/MoreInfoDialog.tsx
@@ -5,23 +5,30 @@ export default function MoreInfoDialog({ onClose }: { onClose: () => void }) {
   return (
     <Dialog maxWidth="md" onClose={onClose} title="More info" open>
       <DialogContent>
-        <Typography>
-          These JBrowse instances leverage UCSC genome browser data resources,
-          utilizing both their main data browsers and trackhubs. JBrowse sends a
-          huge thank you to the UCSC genome browser team for their data
-          curation, data sharing, and technical innovations. Please visit these
-          links for more info, and consider citing UCSC alongside JBrowse in
-          your work.
-        </Typography>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
+          <Typography>
+            These JBrowse instances leverage UCSC genome browser data resources,
+            utilizing both their main data browsers and GenArk track hubs.
+          </Typography>
+          <Typography>
+            JBrowse sends a huge thank you to the UCSC genome browser team for
+            their data curation, sharing, and innovation!
+          </Typography>
+          <Typography>
+            Please visit these links for more info, and consider citing UCSC
+            alongside JBrowse in your work.
+          </Typography>
+        </div>
+
         <ul>
-          <li>
-            <Link href="https://hgdownload.soe.ucsc.edu/hubs/">
-              UCSC GenArk hubs
-            </Link>
-          </li>
           <li>
             <Link href="https://genome.ucsc.edu/">
               UCSC genome browser home
+            </Link>
+          </li>
+          <li>
+            <Link href="https://hgdownload.soe.ucsc.edu/hubs/">
+              UCSC GenArk home
             </Link>
           </li>
           <li>
@@ -30,9 +37,8 @@ export default function MoreInfoDialog({ onClose }: { onClose: () => void }) {
             </Link>
           </li>
           <li>
-            Our pipeline to import and convert UCSC resources:{' '}
-            <Link href="https://github.com/cmdcolin/ucsc2jbrowse">
-              ucsc2jbrowse
+            <Link href="https://github.com/cmdcolin/jb2hubs">
+              Our pipeline to import and convert UCSC resources
             </Link>
           </li>
         </ul>
