c785aa42bbb54c9d32f37feb10c81cffa04e0772
Colin <colin.diesh@gmail.com>
2025-08-08 14:24:12 -0400

    Custom table

--- products/jbrowse-desktop/src/components/StartScreen/availableGenomes/AvailableGenomesDialog.tsx
+++ products/jbrowse-desktop/src/components/StartScreen/availableGenomes/AvailableGenomesDialog.tsx
@@ -1,20 +1,11 @@
 import { useState } from 'react'
 
 import { Dialog } from '@jbrowse/core/ui'
-import { DialogContent, Tab, Tabs } from '@mui/material'
-import { makeStyles } from 'tss-react/mui'
+import { DialogContent } from '@mui/material'
 
 import GenArkDataTable from './GenArkDataTable'
-import MainGenomesDialogPanel from './MainGenomesDialogPanel'
-import TabPanel from './TabPanel'
 
 import type { Fav, LaunchCallback } from '../types'
-
-const useStyles = makeStyles()({
-  mb: {
-    marginBottom: 5,
-  },
-})
 
 export default function AllGenomesDialog({
   favorites,
@@ -27,41 +18,15 @@ export default function AllGenomesDialog({
   setFavorites: (arg: Fav[]) => void
   launch: LaunchCallback
 }) {
-  const [tabValue, setTabValue] = useState(0)
-  const { classes } = useStyles()
-
   return (
-    <Dialog maxWidth="xl" open title="All available genomes" onClose={onClose}>
+    <Dialog maxWidth="xl" open title="Available genomes" onClose={onClose}>
       <DialogContent>
-        <Tabs
-          className={classes.mb}
-          aria-label="genome source tabs"
-          value={tabValue}
-          onChange={(_, newValue) => {
-            setTabValue(newValue)
-          }}
-        >
-          <Tab label="Main Genome Browsers" />
-          <Tab label="UCSC GenArk" />
-        </Tabs>
-
-        <TabPanel value={tabValue} index={0}>
-          <MainGenomesDialogPanel
-            favorites={favorites}
-            setFavorites={setFavorites}
-            onClose={onClose}
-            launch={launch}
-          />
-        </TabPanel>
-
-        <TabPanel value={tabValue} index={1}>
-          <GenArkDataTable
-            favorites={favorites}
-            setFavorites={setFavorites}
-            onClose={onClose}
-            launch={launch}
-          />
-        </TabPanel>
+        <GenArkDataTable
+          favorites={favorites}
+          setFavorites={setFavorites}
+          onClose={onClose}
+          launch={launch}
+        />
       </DialogContent>
     </Dialog>
   )
