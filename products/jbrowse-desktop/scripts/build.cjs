b7ab8a84b58b303d433aac64b73bfec5627eac1f
Colin <colin.diesh@gmail.com>
2025-10-09 14:29:09 -0400

    Move files to cjs extension

--- products/jbrowse-desktop/scripts/build.cjs
+++ products/jbrowse-desktop/scripts/build.cjs
@@ -2,7 +2,7 @@
 process.env.BABEL_ENV = 'production'
 process.env.NODE_ENV = 'production'
 
-const configTranscript = require('./config')
+const configTranscript = require('./config.cjs')
 const configFactory = require('../../../webpack/config/webpack.config')
 const build = require('../../../webpack/scripts/build')
 build(configTranscript(configFactory('production')))
