22779d19615ae867353ef8298097d4cf5c2d72a0
Colin <colin.diesh@gmail.com>
2025-11-13 22:29:19 -0500

    Updates

--- plugins/alignments/src/LinearReadCloudDisplay/afterAttachRPC.tsx
+++ plugins/alignments/src/LinearReadCloudDisplay/afterAttachRPC.tsx
@@ -17,93 +17,150 @@ interface RenderResult {
 }
 
 export function doAfterAttachRPC(self: LinearReadCloudDisplayModel) {
-  // Autorun to trigger RPC rendering when view changes
+  // Common rendering function shared by both autoruns
+  const performRender = async () => {
+    const view = getContainingView(self) as LGV
+
+    // Check if we have the necessary conditions to render
+    if (
+      !view.initialized ||
+      self.error ||
+      !self.statsReadyAndRegionNotTooLarge
+    ) {
+      return
+    }
+
+    // Don't render if already rendering
+    if (self.isRendering) {
+      return
+    }
+
+    // Get current view state
+    const { bpPerPx, offsetPx } = view
+
+    // Skip if view hasn't changed since last render
+    if (
+      self.lastDrawnBpPerPx === bpPerPx &&
+      self.lastDrawnOffsetPx === offsetPx
+    ) {
+      return
+    }
+
+    try {
+      self.setIsRendering(true)
+
+      const session = getSession(self)
+      const { rpcManager } = session
+      const assemblyName = view.assemblyNames[0]
+      if (!assemblyName) {
+        return
+      }
+
+      // Get current view dimensions and settings
+      const width = view.dynamicBlocks.totalWidthPx
+      // For cloud mode, use fixed height. For stack mode, use trackMaxHeight or large default
+      const height = self.drawCloud
+        ? self.height
+        : (self.trackMaxHeight ?? 10000)
+      const regions = view.dynamicBlocks.contentBlocks
+
+      // Call RPC method - it will fetch chainData internally
+      const result = (await rpcManager.call(
+        self.id,
+        'RenderLinearReadCloudDisplay',
+        {
+          sessionId: session.id,
+          regions,
+          adapterConfig: self.adapterConfig,
+          filterBy: self.filterBy,
+          featureHeight: self.featureHeightSetting,
+          noSpacing: self.noSpacing ?? false,
+          drawCloud: self.drawCloud,
+          colorBy: self.colorBy,
+          drawSingletons: self.drawSingletons,
+          drawProperPairs: self.drawProperPairs,
+          flipStrandLongReadChains: self.flipStrandLongReadChains,
+          trackMaxHeight: self.trackMaxHeight,
+          width,
+          height,
+          bpPerPx,
+          offsetPx,
+          assemblyName,
+          highResolutionScaling: 2,
+        },
+      )) as RenderResult
+
+      // Store the result
+      if (result.imageData) {
+        self.setRenderingImageData(result.imageData)
+        if (result.layoutHeight !== undefined) {
+          self.setLayoutHeight(result.layoutHeight)
+        }
+        if (result.featuresForFlatbush) {
+          buildFlatbushIndex(result.featuresForFlatbush, self)
+        }
+        // Store the offsetPx that was used to render this image
+        if (result.offsetPx !== undefined) {
+          self.setLastDrawnOffsetPx(result.offsetPx)
+        }
+      }
+
+      self.setLastDrawnBpPerPx(view.bpPerPx)
+    } catch (error) {
+      console.error(error)
+      self.setError(error)
+    } finally {
+      self.setIsRendering(false)
+    }
+  }
+
+  // Autorun for cloud mode - observes view changes and height
   createAutorun(
     self,
-    async () => {
+    () => {
+      if (!self.drawCloud) {
+        return
+      }
+
       const view = getContainingView(self) as LGV
+      // Access only the properties we need for cloud mode
+      const { bpPerPx, offsetPx } = view
+      const { height } = self
 
-      // Check if we have the necessary conditions to render
-      if (
-        !view.initialized ||
-        self.error ||
-        !self.statsReadyAndRegionNotTooLarge
-      ) {
+      // These accesses register them as dependencies
+      void bpPerPx
+      void offsetPx
+      void height
+
+      // Fire off the async render but don't await it
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      performRender()
+    },
+    { delay: 1000 },
+  )
+
+  // Autorun for stack mode - observes view changes, trackMaxHeight, noSpacing
+  createAutorun(
+    self,
+    () => {
+      if (self.drawCloud) {
         return
       }
 
-      // Don't render if already rendering
-      if (self.isRendering) {
-        return
-      }
+      const view = getContainingView(self) as LGV
+      // Access only the properties we need for stack mode
+      const { bpPerPx, offsetPx } = view
+      const { trackMaxHeight, noSpacing } = self
 
-      try {
-        self.setIsRendering(true)
+      // These accesses register them as dependencies
+      void bpPerPx
+      void offsetPx
+      void trackMaxHeight
+      void noSpacing
 
-        const session = getSession(self)
-        const { rpcManager } = session
-        const assemblyName = view.assemblyNames[0]
-        if (!assemblyName) {
-          return
-        }
-
-        // Get current view dimensions and settings
-        const width = view.dynamicBlocks.totalWidthPx
-        // For cloud mode, use fixed height. For stack mode, use trackMaxHeight or large default
-        const height = self.drawCloud
-          ? self.height
-          : (self.trackMaxHeight ?? 10000)
-        const { bpPerPx, offsetPx } = view
-        const regions = view.dynamicBlocks.contentBlocks
-
-        // Call RPC method - it will fetch chainData internally
-        const result = (await rpcManager.call(
-          self.id,
-          'RenderLinearReadCloudDisplay',
-          {
-            sessionId: session.id,
-            regions,
-            adapterConfig: self.adapterConfig,
-            filterBy: self.filterBy,
-            featureHeight: self.featureHeightSetting,
-            noSpacing: self.noSpacing ?? false,
-            drawCloud: self.drawCloud,
-            colorBy: self.colorBy,
-            drawSingletons: self.drawSingletons,
-            drawProperPairs: self.drawProperPairs,
-            flipStrandLongReadChains: self.flipStrandLongReadChains,
-            trackMaxHeight: self.trackMaxHeight,
-            width,
-            height,
-            bpPerPx,
-            offsetPx,
-            assemblyName,
-            highResolutionScaling: 2,
-          },
-        )) as RenderResult
-
-        // Store the result
-        if (result.imageData) {
-          self.setRenderingImageData(result.imageData)
-          if (result.layoutHeight !== undefined) {
-            self.setLayoutHeight(result.layoutHeight)
-          }
-          if (result.featuresForFlatbush) {
-            buildFlatbushIndex(result.featuresForFlatbush, self)
-          }
-          // Store the offsetPx that was used to render this image
-          if (result.offsetPx !== undefined) {
-            self.setLastDrawnOffsetPx(result.offsetPx)
-          }
-        }
-
-        self.setLastDrawnBpPerPx(view.bpPerPx)
-      } catch (error) {
-        console.error('RPC rendering error:', error)
-        self.setError(error)
-      } finally {
-        self.setIsRendering(false)
-      }
+      // Fire off the async render but don't await it
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      performRender()
     },
     { delay: 1000 },
   )
