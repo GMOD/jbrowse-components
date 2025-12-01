c05390fd5eba420ac6c73b398561c99b970d17c7
Colin <colin.diesh@gmail.com>
2025-12-01 10:53:10 -0500

    R2

--- component_tests/cgv-vite/cypress.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'cypress'
-
-export default defineConfig({
-  chromeWebSecurity: false,
-  video: false,
-  e2e: {
-    baseUrl: 'http://localhost:5000',
-  },
-})
--- component_tests/cgv-vite/cypress/e2e/basic.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('JBrowse embedded circular view', () => {
-  it('track loads', () => {
-    cy.visit('/')
-
-    cy.findByTestId('chord-adp-1843523680-A-0', { timeout: 30000 })
-  })
-})
