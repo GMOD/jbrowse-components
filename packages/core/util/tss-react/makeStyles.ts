a391ffc77d52adf4d1fa99d364c60dba1a78b41f
Colin <colin.diesh@gmail.com>
2025-12-01 12:53:28 -0500

    Updates

--- packages/core/util/tss-react/makeStyles.ts
+++ packages/core/util/tss-react/makeStyles.ts
@@ -2,11 +2,14 @@
 import { useMemo } from 'react'
 
 import type { EmotionCache } from '@emotion/cache'
-// @ts-expect-error: It's not declared but it's there
+// this is the standard way to access Emotion's cache in a hook-based API when
+// using MUI. MUI's ThemeProvider wraps Emotion's CacheProvider, so the cache is
+// always available. this approach is used by tss-react and other MUI styling
+// libraries
 import { __unsafe_useEmotionCache } from '@emotion/react'
 import { serializeStyles } from '@emotion/serialize'
-import { getRegisteredStyles, insertStyles } from '@emotion/utils'
-import { useTheme } from '@mui/material/styles'
+import { insertStyles } from '@emotion/utils'
+import { useTheme, type Theme } from '@mui/material/styles'
 
 import type { CSSObject as CSSObjectBase } from '@emotion/serialize'
 
@@ -22,66 +25,31 @@ interface CSSObject extends CSSObjectBase {
   label?: string
 }
 
-const useEmotionCache: () => EmotionCache = __unsafe_useEmotionCache
-
-function classnames(args: CxArg[]): string {
+function cx(...args: CxArg[]): string {
   let cls = ''
   for (const arg of args) {
-    if (arg == null || typeof arg === 'boolean') {
+    if (!arg) {
       continue
     }
-    let toAdd: string | undefined
-    if (typeof arg === 'object') {
-      if (Array.isArray(arg)) {
-        toAdd = classnames(arg)
-      } else {
-        toAdd = ''
-        for (const k in arg) {
-          if (arg[k] && k) {
-            toAdd && (toAdd += ' ')
-            toAdd += k
-          }
+    if (typeof arg === 'string') {
+      cls = cls ? `${cls} ${arg}` : arg
+    } else if (Array.isArray(arg)) {
+      const nested = cx(...arg)
+      if (nested) {
+        cls = cls ? `${cls} ${nested}` : nested
+      }
+    } else if (typeof arg === 'object') {
+      for (const k in arg) {
+        if ((arg as Record<string, boolean | null | undefined>)[k]) {
+          cls = cls ? `${cls} ${k}` : k
         }
       }
-    } else {
-      toAdd = arg
-    }
-    if (toAdd) {
-      cls && (cls += ' ')
-      cls += toAdd
     }
   }
   return cls
 }
 
-function createCssAndCx(cache: EmotionCache) {
-  const css = (...args: CSSObject[]) => {
-    const serialized = serializeStyles(args, cache.registered)
-    insertStyles(cache, serialized, false)
-    return `${cache.key}-${serialized.name}`
-  }
-
-  const cx = (...args: CxArg[]) => {
-    const className = classnames(args)
-    const registeredStyles: string[] = []
-    const rawClassName = getRegisteredStyles(
-      cache.registered,
-      registeredStyles,
-      className,
-    )
-    if (registeredStyles.length < 2) {
-      return className
-    }
-    return rawClassName + css(...(registeredStyles as unknown as CSSObject[]))
-  }
-
-  return { css, cx }
-}
-
-type Css = (...args: CSSObject[]) => string
-type Cx = (...args: CxArg[]) => string
-
-export function makeStyles<Theme = ReturnType<typeof useTheme>>() {
+export function makeStyles() {
   return function <RuleName extends string>(
     cssObjectByRuleNameOrGetCssObjectByRuleName:
       | Record<RuleName, CSSObject>
@@ -95,25 +63,24 @@ export function makeStyles<Theme = ReturnType<typeof useTheme>>() {
     return function useStyles(): {
       classes: Record<RuleName, string>
       theme: Theme
-      css: Css
-      cx: Cx
+      cx: typeof cx
     } {
-      const theme = useTheme() as Theme
-      const cache = useEmotionCache()
-
-      const { css, cx } = useMemo(() => createCssAndCx(cache), [cache])
+      const theme = useTheme()
+      const cache = __unsafe_useEmotionCache() as EmotionCache
 
       const classes = useMemo(() => {
         const cssObjectByRuleName = getCssObjectByRuleName(theme)
         const result = {} as Record<RuleName, string>
         for (const ruleName of Object.keys(cssObjectByRuleName) as RuleName[]) {
           const cssObject = cssObjectByRuleName[ruleName]
-          result[ruleName] = css(cssObject)
+          const serialized = serializeStyles([cssObject], cache.registered)
+          insertStyles(cache, serialized, false)
+          result[ruleName] = `${cache.key}-${serialized.name}`
         }
         return result
-      }, [css, theme])
+      }, [cache, theme])
 
-      return { classes, theme, css, cx }
+      return { classes, theme, cx }
     }
   }
 }
