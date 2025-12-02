a1f65916da47ddcafaf6ae3e4db6c719c07cfae2
Colin <colin.diesh@gmail.com>
2025-12-01 22:59:35 -0500

    Back to react-dev-tools

--- webpack/config/webpack.config.js
+++ webpack/config/webpack.config.js
@@ -1,57 +1,29 @@
 const cp = require('child_process')
-const fs = require('fs')
 const path = require('path')
 
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin')
+const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin')
+const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin')
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin')
+const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent')
 const webpack = require('webpack')
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin')
 
-// Resolve paths relative to the app directory
-const appDirectory = fs.realpathSync(process.cwd())
-const resolveApp = relativePath => path.resolve(appDirectory, relativePath)
+const getClientEnvironment = require('./env')
+const modules = require('./modules')
+const paths = require('./paths')
 
-const moduleFileExtensions = ['.js', '.ts', '.tsx', '.jsx', '.mjs', '.json']
-
-// Get public URL from homepage in package.json or PUBLIC_URL env
-function getPublicUrlOrPath(isEnvDevelopment, homepage, envPublicUrl) {
-  if (envPublicUrl) {
-    const url = envPublicUrl.endsWith('/') ? envPublicUrl : `${envPublicUrl}/`
-    return isEnvDevelopment && !url.startsWith('.')
-      ? new URL(url, 'https://x').pathname
-      : url
-  }
-  if (homepage) {
-    const hp = homepage.endsWith('/') ? homepage : `${homepage}/`
-    const pathname = new URL(hp, 'https://x').pathname
-    return isEnvDevelopment && !hp.startsWith('.')
-      ? pathname
-      : hp.startsWith('.')
-        ? hp
-        : pathname
-  }
-  return '/'
-}
-
-const paths = {
-  appBuild: resolveApp(process.env.BUILD_PATH || 'build'),
-  appPublic: resolveApp('public'),
-  appHtml: resolveApp('public/index.html'),
-  appIndexJs: resolveApp('src/index.tsx'),
-  appSrc: resolveApp('src'),
-}
-
-function getClientEnvironment(publicUrl) {
-  return {
-    'process.env': {
-      NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
-      PUBLIC_URL: JSON.stringify(publicUrl),
-    },
-  }
-}
-
+// Source maps are resource heavy and can cause out of memory issue for large
+// source files.
 const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false'
+
+const reactRefreshRuntimeEntry = require.resolve('react-refresh/runtime')
+const reactRefreshWebpackPluginRuntimeEntry = require.resolve(
+  '@pmmmwh/react-refresh-webpack-plugin',
+)
 
 function getWorkspaces(fromDir) {
   const cwd = fromDir || process.cwd()
@@ -63,170 +35,319 @@ function getWorkspaces(fromDir) {
   )
 }
 
+// Some apps do not need the benefits of saving a web request, so not inlining
+// the chunk makes for a smoother build process.
+const shouldInlineRuntimeChunk = process.env.INLINE_RUNTIME_CHUNK !== 'false'
+
+// style files regexes
 const cssRegex = /\.css$/
 const cssModuleRegex = /\.module\.css$/
 
+// This is the production and development configuration.
+// It is focused on developer experience, fast rebuilds, and a minimal bundle.
 module.exports = function webpackBuilder(webpackEnv) {
   const isEnvDevelopment = webpackEnv === 'development'
   const isEnvProduction = webpackEnv === 'production'
 
-  const publicUrlOrPath = getPublicUrlOrPath(
-    isEnvDevelopment,
-    require(resolveApp('package.json')).homepage,
-    process.env.PUBLIC_URL,
-  )
+  // We will provide `paths.publicUrlOrPath` to our app
+  // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
+  // Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
+  // Get environment variables to inject into our app.
+  const env = getClientEnvironment(paths.publicUrlOrPath.slice(0, -1))
 
-  const env = getClientEnvironment(publicUrlOrPath.slice(0, -1))
-  const cssSourceMap = isEnvProduction ? shouldUseSourceMap : true
+  const shouldUseReactRefresh = env.raw.FAST_REFRESH
 
-  const getStyleLoaders = cssOptions =>
-    [
-      isEnvDevelopment && 'style-loader',
+  // common function to get style loaders
+  const getStyleLoaders = cssOptions => {
+    const loaders = [
+      isEnvDevelopment && require.resolve('style-loader'),
       isEnvProduction && {
         loader: MiniCssExtractPlugin.loader,
-        options: publicUrlOrPath.startsWith('.')
+        // css is located in `static/css`, use '../../' to locate index.html folder
+        // in production `paths.publicUrlOrPath` can be a relative path
+        options: paths.publicUrlOrPath.startsWith('.')
           ? { publicPath: '../../' }
           : {},
       },
-      { loader: 'css-loader', options: cssOptions },
+      {
+        loader: require.resolve('css-loader'),
+        options: cssOptions,
+      },
     ].filter(Boolean)
+
+    return loaders
+  }
 
   return {
     ...(process.env.NO_CACHE ? { cache: false } : {}),
     target: ['browserslist'],
+    // Webpack noise constrained to errors and warnings
     stats: 'errors-warnings',
-    mode: isEnvProduction ? 'production' : 'development',
+    mode: isEnvProduction ? 'production' : isEnvDevelopment && 'development',
+    // Stop compilation early in production
     bail: isEnvProduction,
     devtool: isEnvProduction
       ? shouldUseSourceMap
         ? 'source-map'
         : false
-      : 'cheap-module-source-map',
+      : isEnvDevelopment && 'cheap-module-source-map',
+    // These are the "entry points" to our application.
+    // This means they will be the "root" imports that are included in JS bundle.
     entry: paths.appIndexJs,
     output: {
+      // The build folder.
       path: paths.appBuild,
+      // Add /* filename */ comments to generated require()s in the output.
       pathinfo: isEnvDevelopment,
+      // There will be one main bundle, and one file per asynchronous chunk. In
+      // development, it does not produce real files.
       filename: isEnvProduction
         ? 'static/js/[name].[contenthash:8].js'
-        : 'static/js/bundle.js',
+        : isEnvDevelopment && 'static/js/bundle.js',
+      // There are also additional JS chunk files if you use code splitting.
       chunkFilename: isEnvProduction
         ? 'static/js/[name].[contenthash:8].chunk.js'
-        : 'static/js/[name].chunk.js',
+        : isEnvDevelopment && 'static/js/[name].chunk.js',
       assetModuleFilename: 'static/media/[name].[hash][ext]',
+      // webpack uses `publicPath` to determine where the app is being served
+      // from. It requires a trailing slash, or the file assets will get an
+      // incorrect path. We inferred the "public path" (such as / or
+      // /my-project) from homepage.
+
+      // Point sourcemap entries to original disk location (format as URL on
+      // Windows)
       devtoolModuleFilenameTemplate: isEnvProduction
         ? info =>
             path
               .relative(paths.appSrc, info.absoluteResourcePath)
               .replace(/\\/g, '/')
-        : info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/'),
+        : isEnvDevelopment &&
+          (info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')),
     },
+
     resolve: {
       conditionNames: ['mui-modern', '...'],
-      modules: ['node_modules'],
-      extensions: moduleFileExtensions,
+      // This allows you to set a fallback for where webpack should look for
+      // modules. We placed these paths second because we want `node_modules`
+      // to "win" if there are any conflicts. This matches Node resolution
+      // mechanism.
+      // https://github.com/facebook/create-react-app/issues/253
+      modules: ['node_modules', paths.appNodeModules].concat(
+        modules.additionalModulePaths || [],
+      ),
+      // These are the reasonable defaults supported by the Node ecosystem. We
+      // also include JSX as a common component filename extension to support
+      // some tools, although we do not recommend using it, see:
+      // https://github.com/facebook/create-react-app/issues/290 `web`
+      // extension prefixes have been added for better support for React Native
+      // Web.
+      extensions: paths.moduleFileExtensions.map(ext => `.${ext}`),
+      plugins: [
+        // Prevents users from importing files from outside of src/ (or
+        // node_modules/). This often causes confusion because we only process
+        // files within src/ with babel. To fix this, we prevent you from
+        // importing files out of src/ -- if you'd like to, please link the
+        // files into your node_modules/ and let module-resolution kick in.
+        // Make sure your source files are compiled, as they will not be
+        // processed in any way.
+        new ModuleScopePlugin(paths.appSrc, [
+          paths.appPackageJson,
+          reactRefreshRuntimeEntry,
+          reactRefreshWebpackPluginRuntimeEntry,
+        ]),
+      ],
     },
     module: {
       strictExportPresence: true,
       rules: [
+        // Handle node_modules packages that contain sourcemaps
         shouldUseSourceMap && {
           enforce: 'pre',
           test: /\.(js|mjs|jsx|ts|tsx|css)$/,
-          loader: 'source-map-loader',
+          loader: require.resolve('source-map-loader'),
         },
         {
+          // "oneOf" will traverse all following loaders until one will
+          // match the requirements. When no loader matches it will fall
+          // back to the "file" loader at the end of the loader list.
           oneOf: [
+            // Process application JS with Babel.
+            // The preset includes JSX, Flow, TypeScript, and some ESnext features.
             {
               test: /\.(js|mjs|jsx|ts|tsx)$/,
+
+              // Begin: jbrowse-web customizations
+              // technique here similar to
+              // https://github.com/brammitch/monorepo/blob/main/packages/app-one/craco.config.js
+              // and compiles the src directories from the apps
               include: [paths.appSrc, getWorkspaces()],
-              loader: 'babel-loader',
+              // End: jbrowse-web customizations
+
+              loader: require.resolve('babel-loader'),
               options: {
-                rootMode: 'upward',
+                plugins: ['babel-plugin-react-compiler'],
+                presets: [
+                  [
+                    '@babel/preset-react',
+                    {
+                      runtime: 'automatic',
+                    },
+                  ],
+                  '@babel/preset-typescript',
+                ],
               },
             },
+            // "postcss" loader applies autoprefixer to our CSS. "css"
+            // loader resolves paths in CSS and adds assets as
+            // dependencies. "style" loader turns CSS into JS modules that
+            // inject <style> tags.
+            //
+            // In production, we use MiniCSSExtractPlugin to extract that CSS
+            // to a file, but in development "style" loader enables hot editing
+            // of CSS. By default we support CSS Modules with the extension
+            // .module.css
             {
               test: cssRegex,
               exclude: cssModuleRegex,
               use: getStyleLoaders({
                 importLoaders: 1,
-                sourceMap: cssSourceMap,
-                modules: { mode: 'icss' },
+                sourceMap: isEnvProduction
+                  ? shouldUseSourceMap
+                  : isEnvDevelopment,
+                modules: {
+                  mode: 'icss',
+                },
               }),
+              // Don't consider CSS imports dead code even if the
+              // containing package claims to have no side effects.
+              // Remove this when webpack adds a warning or an error for this.
+              // See https://github.com/webpack/webpack/issues/6571
               sideEffects: true,
             },
+            // Adds support for CSS Modules (https://github.com/css-modules/css-modules)
+            // using the extension .module.css
             {
               test: cssModuleRegex,
               use: getStyleLoaders({
                 importLoaders: 1,
-                sourceMap: cssSourceMap,
+                sourceMap: isEnvProduction
+                  ? shouldUseSourceMap
+                  : isEnvDevelopment,
                 modules: {
                   mode: 'local',
-                  localIdentName: '[name]_[local]__[hash:base64:5]',
+                  getLocalIdent: getCSSModuleLocalIdent,
                 },
               }),
             },
+
+            // "file" loader makes sure those assets get served by WebpackDevServer.
+            // When you `import` an asset, you get its (virtual) filename.
+            // In production, they would get copied to the `build` folder.
+            // This loader doesn't use a "test" so it will catch all modules
+            // that fall through the other loaders.
             {
+              // Exclude `js` files to keep "css" loader working as it injects
+              // its runtime that would otherwise be processed through "file" loader.
+              // Also exclude `html` and `json` extensions so they get processed
+              // by webpacks internal loaders.
               exclude: [/^$/, /\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
               type: 'asset/resource',
             },
+            // ** STOP ** Are you adding a new loader?
+            // Make sure to add the new loader(s) before the "file" loader.
           ],
         },
       ].filter(Boolean),
     },
     plugins: [
-      new HtmlWebpackPlugin({
-        inject: true,
-        template: paths.appHtml,
-        publicPath: publicUrlOrPath,
-        ...(isEnvProduction && {
-          minify: {
-            removeComments: true,
-            collapseWhitespace: true,
-            removeRedundantAttributes: true,
-            useShortDoctype: true,
-            removeEmptyAttributes: true,
-            removeStyleLinkTypeAttributes: true,
-            keepClosingSlash: true,
-            minifyJS: true,
-            minifyCSS: true,
-            minifyURLs: true,
+      // Generates an `index.html` file with the <script> injected.
+      new HtmlWebpackPlugin(
+        Object.assign(
+          {},
+          {
+            inject: true,
+            template: paths.appHtml,
           },
+          isEnvProduction
+            ? {
+                minify: {
+                  removeComments: true,
+                  collapseWhitespace: true,
+                  removeRedundantAttributes: true,
+                  useShortDoctype: true,
+                  removeEmptyAttributes: true,
+                  removeStyleLinkTypeAttributes: true,
+                  keepClosingSlash: true,
+                  minifyJS: true,
+                  minifyCSS: true,
+                  minifyURLs: true,
+                },
+              }
+            : undefined,
+        ),
+      ),
+      // Inlines the webpack runtime script. This script is too small to warrant
+      // a network request.
+      // https://github.com/facebook/create-react-app/issues/5358
+      isEnvProduction &&
+        shouldInlineRuntimeChunk &&
+        new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/runtime-.+[.]js/]),
+      // Makes some environment variables available in index.html.
+      // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
+      // <link rel="icon" href="%PUBLIC_URL%/favicon.ico">
+      // It will be an empty string unless you specify "homepage"
+      // in `package.json`, in which case it will be the pathname of that URL.
+      new InterpolateHtmlPlugin(HtmlWebpackPlugin, env.raw),
+      // This gives some necessary context to module not found errors, such as
+      // the requesting resource.
+      new ModuleNotFoundPlugin(paths.appPath),
+      // Makes some environment variables available to the JS code, for example:
+      // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
+      // It is absolutely essential that NODE_ENV is set to production
+      // during a production build.
+      // Otherwise React will be compiled in the very slow development mode.
+      new webpack.DefinePlugin(env.stringified),
+      // Experimental hot reloading for React .
+      // https://github.com/facebook/react/tree/main/packages/react-refresh
+      isEnvDevelopment &&
+        shouldUseReactRefresh &&
+        new ReactRefreshWebpackPlugin({
+          overlay: false,
         }),
-      }),
-      new webpack.DefinePlugin(env),
-      isEnvDevelopment && new ReactRefreshWebpackPlugin({ overlay: false }),
       isEnvProduction &&
         new MiniCssExtractPlugin({
+          // Options similar to the same options in webpackOptions.output
+          // both options are optional
           filename: 'static/css/[name].[contenthash:8].css',
           chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
         }),
+      // Generate an asset manifest file with the following content:
+      // - "files" key: Mapping of all asset filenames to their corresponding
+      //   output file so that tools can pick it up without having to parse
+      //   `index.html`
+      // - "entrypoints" key: Array of files which are included in `index.html`,
+      //   can be used to reconstruct the HTML if necessary
       new WebpackManifestPlugin({
         fileName: 'asset-manifest.json',
-        publicPath: publicUrlOrPath,
-        generate: (seed, files, entrypoints) => ({
-          files: files.reduce((manifest, file) => {
+        publicPath: paths.publicUrlOrPath,
+        generate: (seed, files, entrypoints) => {
+          const manifestFiles = files.reduce((manifest, file) => {
             manifest[file.name] = file.path
             return manifest
-          }, seed),
-          entrypoints: entrypoints.main.filter(f => !f.endsWith('.map')),
-        }),
+          }, seed)
+          const entrypointFiles = entrypoints.main.filter(
+            fileName => !fileName.endsWith('.map'),
+          )
+
+          return {
+            files: manifestFiles,
+            entrypoints: entrypointFiles,
+          }
+        },
       }),
     ].filter(Boolean),
+    // Turn off performance processing because we utilize
+    // our own hints via the FileSizeReporter
     performance: false,
-    ...(isEnvDevelopment && {
-      devServer: {
-        port: process.env.PORT || 3000,
-        hot: true,
-        open: true,
-        client: {
-          logging: 'error',
-        },
-        devMiddleware: {
-          stats: 'errors-only',
-        },
-      },
-    }),
   }
 }
-
-// Export paths for use in build.js
-module.exports.paths = paths
