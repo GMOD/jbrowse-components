acf6a6f68c11ef107400b9f97701e8b1016640d6
Colin <colin.diesh@gmail.com>
2025-12-07 15:45:53 -0500

    Add session snapshot test

---
+++ products/jbrowse-web/src/tests/ExportSession.test.tsx
@@ -0,0 +1,54 @@
+import { fireEvent, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { saveAs } from 'file-saver-es'
+
+import { createView, doBeforeEach, hts, setup } from './util'
+
+// @ts-expect-error
+global.Blob = (content, options) => ({ content, options })
+
+jest.mock('file-saver-es', () => ({
+  ...jest.requireActual('file-saver-es'),
+  saveAs: jest.fn(),
+}))
+
+setup()
+
+beforeEach(() => {
+  jest.clearAllMocks()
+  doBeforeEach()
+})
+
+const delay = { timeout: 30000 }
+const opts = [{}, delay]
+
+test('export session with alignments and gff tracks', async () => {
+  const { view, findByTestId, findByText } = await createView()
+
+  view.setNewView(0.5, 0)
+  fireEvent.click(await findByTestId(hts('volvox_alignments_pileup_coverage'), ...opts))
+  fireEvent.click(await findByTestId(hts('gff3tabix_genes'), ...opts))
+
+  const user = userEvent.setup()
+  await user.click(await findByText('File'))
+  await user.click(await findByText('Export session'))
+
+  await waitFor(() => {
+    // eslint-disable-next-line @typescript-eslint/no-deprecated
+    expect(saveAs).toHaveBeenCalled()
+  }, delay)
+
+  // eslint-disable-next-line @typescript-eslint/no-deprecated
+  const mock = saveAs as jest.Mock
+  const blob = mock.mock.calls[0][0]
+  const filename = mock.mock.calls[0][1]
+
+  expect(filename).toBe('session.json')
+  expect(blob.options.type).toBe('text/plain;charset=utf-8')
+
+  const content = JSON.parse(blob.content[0])
+  // Remove dynamic fields for snapshot stability
+  delete content.session.id
+  delete content.session.name
+  expect(content).toMatchSnapshot()
+}, 40000)
