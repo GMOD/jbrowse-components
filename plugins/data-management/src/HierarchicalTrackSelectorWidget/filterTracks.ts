7706827ebb4c5107485b7b074ffaa99506357753
Colin <colin.diesh@gmail.com>
2025-12-07 23:29:02 -0500

    Implement

--- plugins/data-management/src/HierarchicalTrackSelectorWidget/filterTracks.ts
+++ plugins/data-management/src/HierarchicalTrackSelectorWidget/filterTracks.ts
@@ -26,23 +26,23 @@ export function filterTracks(
     .map(a => assemblyManager.getCanonicalAssemblyName(a))
     .filter(notEmpty)
   const { displayTypes } = pluginManager.getViewType(view.type)!
-  const viewDisplays = displayTypes.map((d: { name: string }) => d.name)
-  return tracks
-    .filter(c => {
-      const trackConfigAssemblyNames = readConfObject(c, 'assemblyNames') as
-        | string[]
-        | undefined
-      const trackCanonicalAssemblyNames = trackConfigAssemblyNames
-        ?.map(name => assemblyManager.getCanonicalAssemblyName(name))
-        .filter(notEmpty)
-      return view.trackSelectorAnyOverlap
-        ? hasAnyOverlap(trackCanonicalAssemblyNames, viewAssemblyNames)
-        : hasAllOverlap(trackCanonicalAssemblyNames, viewAssemblyNames)
-    })
-
-    .filter(conf => {
-      const trackType = pluginManager.getTrackType(conf.type)!
-      const trackDisplays = trackType.displayTypes.map(d => d.name)
-      return hasAnyOverlap(viewDisplays, trackDisplays)
-    })
+  const viewDisplaysSet = new Set(
+    displayTypes.map((d: { name: string }) => d.name),
+  )
+  return tracks.filter(c => {
+    const trackConfigAssemblyNames = readConfObject(c, 'assemblyNames') as
+      | string[]
+      | undefined
+    const trackCanonicalAssemblyNames = trackConfigAssemblyNames
+      ?.map(name => assemblyManager.getCanonicalAssemblyName(name))
+      .filter(notEmpty)
+    const assemblyMatch = view.trackSelectorAnyOverlap
+      ? hasAnyOverlap(trackCanonicalAssemblyNames, viewAssemblyNames)
+      : hasAllOverlap(trackCanonicalAssemblyNames, viewAssemblyNames)
+    if (!assemblyMatch) {
+      return false
+    }
+    const trackType = pluginManager.getTrackType(c.type)!
+    return trackType.displayTypes.some(d => viewDisplaysSet.has(d.name))
+  })
 }
