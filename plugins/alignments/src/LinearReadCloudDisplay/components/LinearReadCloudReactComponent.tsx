75e2d56f5f4cd2b80a86f9ae8f09c70a441d863b
Colin <colin.diesh@gmail.com>
2025-12-15 11:19:34 -0500

    k1

--- plugins/alignments/src/LinearReadCloudDisplay/components/LinearReadCloudReactComponent.tsx
+++ plugins/alignments/src/LinearReadCloudDisplay/components/LinearReadCloudReactComponent.tsx
@@ -13,6 +13,152 @@ import type { LinearGenomeViewModel } from '@jbrowse/plugin-linear-genome-view'
 
 type LGV = LinearGenomeViewModel
 
+function Tooltip({
+  hoveredFeatureData,
+  mousePosition,
+}: {
+  hoveredFeatureData: ReducedFeature
+  mousePosition: {
+    x: number
+    y: number
+  }
+}) {
+  return (
+    <BaseTooltip
+      clientPoint={{
+        x: mousePosition.x,
+        y: mousePosition.y + 20,
+      }}
+      placement="bottom-start"
+    >
+      <div>
+        <div>
+          <strong>{hoveredFeatureData.name}</strong>
+        </div>
+        <div>{assembleLocString(hoveredFeatureData)}</div>
+        {hoveredFeatureData.tlen !== 0 ? (
+          <div>Template length: {hoveredFeatureData.tlen}</div>
+        ) : null}
+      </div>
+    </BaseTooltip>
+  )
+}
+
+const FeatureHighlights = observer(function ({
+  selectedFeatureBounds,
+  hoveredFeature,
+  canvasLeft,
+}: {
+  selectedFeatureBounds: {
+    x: number
+    y: number
+    width: number
+    height: number
+  } | null
+  hoveredFeature:
+    | {
+        x: number
+        y: number
+        width: number
+        height: number
+      }
+    | undefined
+  canvasLeft: number
+}) {
+  return (
+    <>
+      {selectedFeatureBounds ? (
+        <div
+          style={{
+            position: 'absolute',
+            left: selectedFeatureBounds.x + canvasLeft,
+            top: selectedFeatureBounds.y,
+            width: selectedFeatureBounds.width,
+            height: selectedFeatureBounds.height,
+            border: '2px solid #00b8ff',
+            boxSizing: 'border-box',
+            pointerEvents: 'none',
+          }}
+        />
+      ) : null}
+      {hoveredFeature ? (
+        <div
+          style={{
+            position: 'absolute',
+            left: hoveredFeature.x + canvasLeft,
+            top: hoveredFeature.y,
+            width: hoveredFeature.width,
+            height: hoveredFeature.height,
+            backgroundColor: 'rgba(0, 0, 0, 0.2)',
+            pointerEvents: 'none',
+          }}
+        />
+      ) : null}
+    </>
+  )
+})
+
+const CloudCanvases = observer(function ({
+  canvasWidth,
+  height,
+  canvasLeft,
+  model,
+}: {
+  canvasWidth: number
+  height: number
+  canvasLeft: number
+  model: LinearReadCloudDisplayModel
+}) {
+  // biome-ignore lint/correctness/useExhaustiveDependencies:
+  const cb = useCallback(
+    (ref: HTMLCanvasElement) => {
+      model.setRef(ref)
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [model, canvasWidth, height],
+  )
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies:
+  const mouseoverCb = useCallback(
+    (ref: HTMLCanvasElement) => {
+      model.setMouseoverRef(ref)
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [model, canvasWidth, height],
+  )
+  return (
+    <>
+      <canvas
+        data-testid={model.dataTestId}
+        ref={cb}
+        style={{
+          width: canvasWidth,
+          height,
+          position: 'absolute',
+          left: canvasLeft,
+          top: 0,
+        }}
+        width={canvasWidth * 2}
+        height={height * 2}
+      />
+      <canvas
+        data-testid="cloud-mouseover-canvas"
+        ref={mouseoverCb}
+        style={{
+          width: canvasWidth,
+          height,
+          position: 'absolute',
+          left: canvasLeft,
+          top: 0,
+          pointerEvents: 'none',
+        }}
+        width={canvasWidth * 2}
+        height={height * 2}
+      />
+    </>
+  )
+})
+
 const Cloud = observer(function ({
   model,
 }: {
@@ -69,24 +215,6 @@ const Cloud = observer(function ({
 
     return null
   }, [model.selectedFeatureId, model.featuresForFlatbush])
-
-  // biome-ignore lint/correctness/useExhaustiveDependencies:
-  const cb = useCallback(
-    (ref: HTMLCanvasElement) => {
-      model.setRef(ref)
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [model, width, height],
-  )
-
-  // biome-ignore lint/correctness/useExhaustiveDependencies:
-  const mouseoverCb = useCallback(
-    (ref: HTMLCanvasElement) => {
-      model.setMouseoverRef(ref)
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [model, width, height],
-  )
 
   const onMouseMove = useCallback(
     (event: React.MouseEvent) => {
@@ -189,60 +317,17 @@ const Cloud = observer(function ({
       onMouseLeave={onMouseLeave}
       onClick={onClick}
     >
-      <canvas
-        data-testid={model.dataTestId}
-        ref={cb}
-        style={{
-          width: canvasWidth,
-          height,
-          position: 'absolute',
-          left: canvasLeft,
-          top: 0,
-        }}
-        width={canvasWidth * 2}
-        height={height * 2}
+      <CloudCanvases
+        model={model}
+        canvasWidth={canvasWidth}
+        height={height}
+        canvasLeft={canvasLeft}
       />
-      <canvas
-        data-testid="cloud-mouseover-canvas"
-        ref={mouseoverCb}
-        style={{
-          width: canvasWidth,
-          height,
-          position: 'absolute',
-          left: canvasLeft,
-          top: 0,
-          pointerEvents: 'none',
-        }}
-        width={canvasWidth * 2}
-        height={height * 2}
+      <FeatureHighlights
+        selectedFeatureBounds={selectedFeatureBounds}
+        hoveredFeature={hoveredFeature}
+        canvasLeft={canvasLeft}
       />
-      {selectedFeatureBounds ? (
-        <div
-          style={{
-            position: 'absolute',
-            left: selectedFeatureBounds.x + canvasLeft,
-            top: selectedFeatureBounds.y,
-            width: selectedFeatureBounds.width,
-            height: selectedFeatureBounds.height,
-            border: '2px solid #00b8ff',
-            boxSizing: 'border-box',
-            pointerEvents: 'none',
-          }}
-        />
-      ) : null}
-      {hoveredFeature ? (
-        <div
-          style={{
-            position: 'absolute',
-            left: hoveredFeature.x + canvasLeft,
-            top: hoveredFeature.y,
-            width: hoveredFeature.width,
-            height: hoveredFeature.height,
-            backgroundColor: 'rgba(0, 0, 0, 0.2)',
-            pointerEvents: 'none',
-          }}
-        />
-      ) : null}
       {hoveredFeatureData && mousePosition ? (
         <Tooltip
           hoveredFeatureData={hoveredFeatureData}
@@ -252,36 +337,6 @@ const Cloud = observer(function ({
     </div>
   )
 })
-function Tooltip({
-  hoveredFeatureData,
-  mousePosition,
-}: {
-  hoveredFeatureData: ReducedFeature
-  mousePosition: {
-    x: number
-    y: number
-  }
-}) {
-  return (
-    <BaseTooltip
-      clientPoint={{
-        x: mousePosition.x,
-        y: mousePosition.y + 20,
-      }}
-      placement="bottom-start"
-    >
-      <div>
-        <div>
-          <strong>{hoveredFeatureData.name}</strong>
-        </div>
-        <div>{assembleLocString(hoveredFeatureData)}</div>
-        {hoveredFeatureData.tlen !== 0 ? (
-          <div>Template length: {hoveredFeatureData.tlen}</div>
-        ) : null}
-      </div>
-    </BaseTooltip>
-  )
-}
 
 const LinearReadCloudReactComponent = observer(function ({
   model,
