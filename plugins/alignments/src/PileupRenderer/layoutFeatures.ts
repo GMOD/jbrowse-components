47b420030608e92f1364bf53c93069d77276dd2a
Colin Diesh <colin.diesh@gmail.com>
2025-12-16 00:03:13 -0500

    Create layout optimizations for deep RNA-seq data (#5330)

--- plugins/alignments/src/PileupRenderer/layoutFeatures.ts
+++ plugins/alignments/src/PileupRenderer/layoutFeatures.ts
@@ -1,9 +1,9 @@
 import { readConfObject } from '@jbrowse/core/configuration'
-import { iterMap, notEmpty } from '@jbrowse/core/util'
 
 import { layoutFeature } from './layoutFeature'
 import { sortFeature } from './sortUtil'
 
+import type { LayoutRecord } from './layoutFeature'
 import type { PreProcessedRenderArgs } from './types'
 
 // layout determines the height of the canvas that we use to render
@@ -11,27 +11,35 @@ export function layoutFeats(props: PreProcessedRenderArgs) {
   const { layout, features, sortedBy, config, bpPerPx, showSoftClip, regions } =
     props
   const region = regions[0]!
-  const featureMap =
-    sortedBy?.type && region.start === sortedBy.pos
-      ? sortFeature(features, sortedBy)
-      : features
+  const hasSortedBy = sortedBy?.type && region.start === sortedBy.pos
+  const featureMap = hasSortedBy ? sortFeature(features, sortedBy) : features
 
   const heightPx = readConfObject(config, 'height')
   const displayMode = readConfObject(config, 'displayMode')
-  const layoutRecords = iterMap(
-    featureMap.values(),
-    feature =>
-      layoutFeature({
-        feature,
-        layout,
-        bpPerPx,
-        region,
-        showSoftClip,
-        heightPx,
-        displayMode,
-      }),
-    featureMap.size,
-  ).filter(notEmpty)
+
+  // Sort features by start position for PileupLayout's built-in hint optimization,
+  // but only when not using explicit sorting (which has its own order)
+  const featureArr = hasSortedBy
+    ? [...featureMap.values()]
+    : [...featureMap.values()].sort((a, b) => a.get('start') - b.get('start'))
+
+  const layoutRecords: LayoutRecord[] = []
+
+  for (const feature of featureArr) {
+    const result = layoutFeature({
+      feature,
+      layout,
+      bpPerPx,
+      region,
+      showSoftClip,
+      heightPx,
+      displayMode,
+    })
+
+    if (result) {
+      layoutRecords.push(result)
+    }
+  }
 
   return {
     layoutRecords,
