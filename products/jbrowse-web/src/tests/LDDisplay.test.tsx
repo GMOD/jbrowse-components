c9e2188a7f0975052b74fd9c28e17638903af715
Colin <colin.diesh@gmail.com>
2026-01-22 22:21:26 +0000

    Update v4.0.4 to v4.1.0

--- products/jbrowse-web/src/tests/LDDisplay.test.tsx
+++ products/jbrowse-web/src/tests/LDDisplay.test.tsx
@@ -28,7 +28,7 @@ test(
 
     fireEvent.click(await findByTestId('track_menu_icon', ...opts))
     fireEvent.click(await findByText('Display types', ...opts))
-    fireEvent.click(await findByText('Linkage disequilibrium display', ...opts))
+    fireEvent.click(await findByText('LD heatmap display', ...opts))
 
     expectCanvasMatch(await findByTestId('ld_canvas_done', ...opts))
   },
