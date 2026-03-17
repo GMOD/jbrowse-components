GMOD/jbrowse-components

commit fe26ca97e0d14c0b87933548dd388a74a2d1b09a

Make the loading bars occupy full track height (#5451)

plugins/linear-genome-view/src/BaseLinearDisplay/components/LoadingOverlay.tsx
@@ -47,14 +47,16 @@ const useStyles = makeStyles()({
 export default function LoadingOverlay({
   statusMessage,
   children,
+  height,
 }: {
   statusMessage?: string
   children?: React.ReactNode
+  height?: number
 }) {
   const { classes } = useStyles()
   const isLoading = !!statusMessage
   return (
-    <div className={classes.container}>
+    <div className={classes.container} style={height ? { height } : undefined}>
       {children}
       <span
         className={cx(classes.overlay, isLoading && classes.visible)}

plugins/linear-genome-view/src/BaseLinearDisplay/components/NonBlockCanvasDisplayComponent.tsx
@@ -28,6 +28,7 @@ export interface NonBlockCanvasDisplayModel {
   statusMessage?: string
   showLegend?: boolean
   legendItems?: () => LegendItem[]
+  height: number
 }
 
 const useStyles = makeStyles()({
@@ -87,7 +88,8 @@ const DataDisplay = observer(function DataDisplay({
   model: NonBlockCanvasDisplayModel
   children?: React.ReactNode
 }) {
-  const { drawn, loading, showLegend, legendItems, lastDrawnBpPerPx } = model
+  const { drawn, loading, showLegend, legendItems, lastDrawnBpPerPx, height } =
+    model
   const view = getContainingView(model) as LinearGenomeViewModel
   const items = legendItems?.() ?? []
 
@@ -109,7 +111,10 @@ const DataDisplay = observer(function DataDisplay({
   return (
     // this data-testid is located here because changing props on the canvas
     // itself is very sensitive to triggering ref invalidation
-    <div data-testid={`drawn-${drawn}`}>
+    <div
+      data-testid={`drawn-${drawn}`}
+      style={{ position: 'relative', height }}
+    >
       <div
         style={{
           position: 'absolute',

plugins/linear-genome-view/src/BaseLinearDisplay/components/ServerSideRenderedBlockContent.tsx
@@ -18,6 +18,7 @@ const ServerSideRenderedBlockContent = observer(
       statusMessage?: string
       reactElement?: React.ReactElement
       isRenderingPending?: boolean
+      displayHeight?: number
     }
   }) {
     if (model.error) {
@@ -34,7 +35,10 @@ const ServerSideRenderedBlockContent = observer(
       )
     } else {
       return (
-        <LoadingOverlay statusMessage={model.statusMessage}>
+        <LoadingOverlay
+          statusMessage={model.statusMessage}
+          height={model.displayHeight}
+        >
           {model.reactElement}
         </LoadingOverlay>
       )

plugins/linear-genome-view/src/BaseLinearDisplay/models/serverSideRenderedBlock.ts
@@ -258,6 +258,10 @@ const blockState = types
             'Loading'
         : undefined
     },
+    get displayHeight() {
+      // @ts-expect-error
+      return self.cachedDisplay?.height as number | undefined
+    },
   }))
   .actions(self => ({
     afterAttach() {

commit df6897967eedf701a473e21ec10382f848befcdf

Fix scrolling regression introduced by fe26ca97e0d1

plugins/linear-genome-view/src/BaseLinearDisplay/components/LoadingOverlay.tsx
@@ -56,7 +56,10 @@ export default function LoadingOverlay({
   const { classes } = useStyles()
   const isLoading = !!statusMessage
   return (
-    <div className={classes.container} style={height ? { height } : undefined}>
+    <div
+      className={classes.container}
+      style={height ? { minHeight: height } : undefined}
+    >
       {children}
       <span
         className={cx(classes.overlay, isLoading && classes.visible)}