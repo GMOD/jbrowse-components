2407574037aac29dfe96cd1883588961722317ba
Colin <colin.diesh@gmail.com>
2026-03-27 12:02:15 -0400

    Modifications error

--- plugins/alignments/src/ModificationParser/consts.ts
+++ plugins/alignments/src/ModificationParser/consts.ts
@@ -1,6 +1,10 @@
-// Regexes based on parse_mm.pl from hts-specs
-// https://github.com/samtools/hts-specs/blob/f907ead164780fdbcce6a0807c551413dbf18ca1/test/SAMtags/parse_mm.pl#L73
-// Modified to not have the ,(.*) because we saw examples with 'C+m,192,445,157,36,12,39,21,30,5,0,2,1,9,29,6,186,109,8,0,66,57,431,168,16,20,0,58,152,333,8,32,54,12,4,147,2,5,0,1,6,4,1,35,3,282,7,24,4,35,3,42,8,4,81,60,50,37,7,529,138,14,106,327,667;C+h;' and C+h does not have a comma
+// Regexes based on the SAM spec MM:Z tag format:
+// ([ACGTUN][-+]([a-z]+|[0-9]+)[.?]?(,[0-9]+)*;)*
+// The base must be one of ACGTUN per spec. The modification code is lowercase
+// letters or digits per spec, but the spec table also lists uppercase
+// single-letter ambiguity codes (A, C, G, T, U, N) as valid mod codes.
+// Modified to not have the ,(.*) because we saw examples with 'C+m,192,...;C+h;'
+// and C+h does not have a comma
 export const modificationRegex = new RegExp(
-  /([A-Z])([-+])([a-z]+|[0-9]+)([.?]?)/,
+  /([ACGTUN])([-+])([a-z]+|[A-Z]|[0-9]+)([.?]?)/,
 )
