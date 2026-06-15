c9df1bfb0b974c4673c2c4fcfd0330ccb959da45
Colin <colin.diesh@gmail.com>
2026-06-15 11:50:28 -0400

    [skip ci] Custom version reporter string

--- products/jbrowse-desktop/scripts/build.ts
+++ products/jbrowse-desktop/scripts/build.ts
@@ -5,10 +5,30 @@ import desktopConfig from './config.ts'
 import configFactory from '../../../webpack/config/webpack.config.ts'
 import build from '../../../webpack/scripts/build.ts'
 
-void build(desktopConfig(configFactory())).then(() => {
-  const testDataPath = path.resolve(import.meta.dirname, '../build/test_data')
-  if (fs.existsSync(testDataPath)) {
-    fs.rmSync(testDataPath, { recursive: true })
-    console.log('Removed test_data from build')
-  }
-})
+const pkgPath = path.resolve(import.meta.dirname, '../package.json')
+const originalPkgContent = fs.readFileSync(pkgPath, 'utf8')
+const pkg = JSON.parse(originalPkgContent)
+const customVersion = process.env.JBROWSE_VERSION
+
+if (customVersion) {
+  pkg.version = customVersion
+  fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2))
+}
+
+void build(desktopConfig(configFactory()))
+  .then(() => {
+    if (customVersion) {
+      fs.writeFileSync(pkgPath, originalPkgContent)
+    }
+    const testDataPath = path.resolve(import.meta.dirname, '../build/test_data')
+    if (fs.existsSync(testDataPath)) {
+      fs.rmSync(testDataPath, { recursive: true })
+      console.log('Removed test_data from build')
+    }
+  })
+  .catch(err => {
+    if (customVersion) {
+      fs.writeFileSync(pkgPath, originalPkgContent)
+    }
+    throw err
+  })
--- products/jbrowse-desktop/scripts/packaging/config.ts
+++ products/jbrowse-desktop/scripts/packaging/config.ts
@@ -8,7 +8,7 @@ export const ASSETS = path.join(ROOT, 'assets')
 
 const pkg = JSON.parse(fs.readFileSync(path.join(ROOT, 'package.json'), 'utf8'))
 
-export const VERSION = pkg.version
+export const VERSION = process.env.JBROWSE_VERSION || pkg.version
 export const APP_NAME = 'jbrowse-desktop'
 export const PRODUCT_NAME = 'JBrowse 2'
 export const APP_ID = 'org.jbrowse2.app'
